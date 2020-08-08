555a0b30978b96a5175f84fba23c7c329990ae70
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-08 12:45:06 +0000

    Add Events for Mod

--- app/Http/Livewire/User/Moderator.php
+++ app/Http/Livewire/User/Moderator.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire\User;
 use App\User;
 use Auth;
 use Livewire\Component;
+use App\Notifications\Slack\Mod;
+use Notification;
 
 class Moderator extends Component
 {
@@ -20,6 +22,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $this->user->isBeta = ! $this->user->isBeta;
             $this->user->save();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('BETA', $this->user, Auth::user()));
         } else {
             return false;
         }
@@ -33,6 +37,8 @@ class Moderator extends Component
             }
             $this->user->isStaff = ! $this->user->isStaff;
             $this->user->save();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('STAFF', $this->user, Auth::user()));
         } else {
             return false;
         }
@@ -43,6 +49,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $this->user->isDeveloper = ! $this->user->isDeveloper;
             $this->user->save();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('CONTRIBUTOR', $this->user, Auth::user()));
         } else {
             return false;
         }
@@ -56,6 +64,8 @@ class Moderator extends Component
             }
             $this->user->isFlagged = ! $this->user->isFlagged;
             $this->user->save();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('FLAG', $this->user, Auth::user()));
         } else {
             return false;
         }
@@ -66,6 +76,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $this->user->isPatron = ! $this->user->isPatron;
             $this->user->save();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('PATRON', $this->user, Auth::user()));
         } else {
             return false;
         }
@@ -76,6 +88,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $this->user->darkMode = ! $this->user->darkMode;
             $this->user->save();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('DARKMODE', $this->user, Auth::user()));
         } else {
             return false;
         }
@@ -87,6 +101,8 @@ class Moderator extends Component
             if ($this->user->id === 1) {
                 return false;
             }
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('MASQUERADE', $this->user, Auth::user()));
             Auth::loginUsingId($this->user->id);
 
             return redirect()->route('home');
@@ -100,6 +116,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $user = User::find($this->user->id);
             $user->tasks()->delete();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('DELETE_TASKS', $this->user, Auth::user()));
 
             return redirect()->route('user.done', ['username' => $this->user->username]);
         } else {
@@ -112,6 +130,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $user = User::find($this->user->id);
             $user->task_comment()->delete();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('DELETE_COMMENTS', $this->user, Auth::user()));
 
             return redirect()->route('user.done', ['username' => $this->user->username]);
         } else {
@@ -124,6 +144,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $user = User::find($this->user->id);
             $user->questions()->delete();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('DELETE_QUESTIONS', $this->user, Auth::user()));
 
             return redirect()->route('user.done', ['username' => $this->user->username]);
         } else {
@@ -136,6 +158,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $user = User::find($this->user->id);
             $user->answers()->delete();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('DELETE_ANSWERS', $this->user, Auth::user()));
 
             return redirect()->route('user.done', ['username' => $this->user->username]);
         } else {
@@ -148,6 +172,8 @@ class Moderator extends Component
         if (Auth::check() && Auth::user()->isStaff) {
             $user = User::find($this->user->id);
             $user->products()->delete();
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('DELETE_PRODUCTS', $this->user, Auth::user()));
 
             return redirect()->route('user.done', ['username' => $this->user->username]);
         } else {
@@ -162,7 +188,10 @@ class Moderator extends Component
                 return false;
             }
             $user = User::find($this->user->id);
+            Notification::route('slack', env('SLACK_HOOK'))
+                    ->notify(new Mod('DELETE_USER', null, Auth::user()));
             $user->delete();
+            
 
             return redirect()->route('home');
         } else {
