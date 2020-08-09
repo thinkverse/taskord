3ee3a053db59aa76fa0270116cfdb530e4d8fb15
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-09 02:49:50 +0000

    Update Seeder with *@yogi.codes email

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -23,7 +23,7 @@ class UserSeeder extends Seeder
                 'username' => str_replace('.', '', $faker->unique()->userName),
                 'company' =>  $faker->firstName,
                 'bio' => $faker->sentence($nbWords = 6, $variableNbWords = true),
-                'email' => $faker->unique()->email,
+                'email' => str_replace('.', '', $faker->unique()->userName).'@yogi.codes',
                 'avatar' => 'https://avatar.tobi.sh/'.$faker->userName.'.svg?text='.strtoupper($faker->randomLetter).strtoupper($faker->randomLetter),
                 'password' => Hash::make('test'),
                 'reputation' => $faker->numberBetween($min = 50, $max = 150),
