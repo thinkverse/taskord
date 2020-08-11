64b779c444b75f7665cd367031fd2538d559d388
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-11 05:51:25 +0000

    Add User Suspension Middleware

---
+++ app/Http/Middleware/LogoutSuspendedUser.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class LogoutSuspendedUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check() && Auth::user()->isSuspended) {
+            Auth::logout();
+            
+            return redirect()->route('home');
+        }
+
+        return $next($request);
+    }
+}
