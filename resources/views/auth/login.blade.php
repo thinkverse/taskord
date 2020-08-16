32b304a2ecfd930d25df7e8131456ab3068bca6f
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-16 12:30:33 +0000

    Add title for auths

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('pageTitle', 'Login ·')
+@section('title', 'Login ·')
+@section('description', 'Get things done socially with Taskord.')
+@section('image', '')
+@section('url', url()->current())
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('pageTitle', 'Signup ·')
+@section('title', 'Signup ·')
+@section('description', 'Get things done socially with Taskord.')
+@section('image', '')
+@section('url', url()->current())
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
