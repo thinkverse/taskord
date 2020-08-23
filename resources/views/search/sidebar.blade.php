996900d61d7c89a42f40fa95035278ea0880c283
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-23 13:42:44 +0000

    Add Icons to search sidebar

--- resources/views/search/sidebar.blade.php
+++ resources/views/search/sidebar.blade.php
@@ -5,42 +5,42 @@
                 class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'search.tasks') active text-white @endif"
                 href="{{ route('search.tasks') }}"
             >
-                <i class="fa fa-user mr-1"></i>
+                <i class="fa fa-check mr-1"></i>
                 Tasks
             </a>
             <a
                 class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'search.tasks') active text-white @endif"
                 href="{{ route('search.tasks') }}"
             >
-                <i class="fa fa-user mr-1"></i>
+                <i class="fa fa-comments mr-1"></i>
                 Task Comments
             </a>
             <a
                 class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'search.tasks') active text-white @endif"
                 href="{{ route('search.tasks') }}"
             >
-                <i class="fa fa-user mr-1"></i>
+                <i class="fa fa-question mr-1"></i>
                 Questions
             </a>
             <a
                 class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'search.tasks') active text-white @endif"
                 href="{{ route('search.tasks') }}"
             >
-                <i class="fa fa-user mr-1"></i>
+                <i class="fa fa-comment mr-1"></i>
                 Answers
             </a>
             <a
                 class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'search.tasks') active text-white @endif"
                 href="{{ route('search.tasks') }}"
             >
-                <i class="fa fa-user mr-1"></i>
+                <i class="fa fa-box-open mr-1"></i>
                 Products
             </a>
             <a
                 class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'search.tasks') active text-white @endif"
                 href="{{ route('search.tasks') }}"
             >
-                <i class="fa fa-user mr-1"></i>
+                <i class="fa fa-users mr-1"></i>
                 Users
             </a>
         </ul>
