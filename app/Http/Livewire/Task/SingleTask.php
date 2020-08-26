e161a759819a2f1ab38b56ee454ab11e478a65cc
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-26 11:17:57 +0000

    Add Throttle Service in Create Task Home

--- app/Http/Livewire/Task/SingleTask.php
+++ app/Http/Livewire/Task/SingleTask.php
@@ -8,6 +8,8 @@ use App\Notifications\TaskPraised;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use Illuminate\Support\Facades\Request;
+use GrahamCampbell\Throttle\Facades\Throttle;
 
 class SingleTask extends Component
 {
@@ -21,6 +23,12 @@ class SingleTask extends Component
 
     public function checkTask()
     {
+        $throttler = Throttle::get(Request::instance(), 20, 5);
+        $throttler->hit();
+        if (! $throttler->check()) {
+            return session()->flash('error', 'Your are rate limited, try again later!');
+        }
+        
         if (Auth::check()) {
             if (Auth::id() === $this->task->user->id) {
                 if ($this->task->done) {
