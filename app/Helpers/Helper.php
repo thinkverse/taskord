c3db1ab800854b6df736804021bfcd92b1851056
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-26 04:31:37 +0000

    Update Helper function for task rendering

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -14,8 +14,7 @@ class Helper
     public static function renderTask($task)
     {
         $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
-        $products = preg_replace('/#(\w+)/', '<a class="product" href="product/$1">#$1</a>', $task);
-        $users = preg_replace('/@(\w+)/', '<a href="@$1">@$1</a>', $products);
+        $users = preg_replace('/@(\w+)/', '<a href="@$1">@$1</a>', $task);
         if (preg_match($reg_exUrl, $users, $url)) {
             $truncate = strlen($url[0]) > 30 ? substr($url[0], 0, 30).'...' : $url[0];
 
