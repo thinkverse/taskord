34848d959df01c28254da7ecffb2eb342e55df78
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-26 09:28:35 +0000

    Add user avatar for praise in answers

--- resources/views/livewire/answer/single-answer.blade.php
+++ resources/views/livewire/answer/single-answer.blade.php
@@ -37,6 +37,11 @@
                     <span class="small text-white font-weight-bold">
                         {{ number_format($answer->likes()->count('id')) }}
                     </span>
+                    <span class="avatar-stack ml-1">
+                    @foreach($answer->likers->take(5) as $user)
+                    <img class="praise-avatar rounded-circle {{ $loop->last ? 'mr-0' : '' }}" src="{{ $user->avatar }}" />
+                    @endforeach
+                    </span>
                 </button>
             @else
                 <button type="button" class="btn btn-task btn-outline-success mr-1" wire:click="togglePraise" wire:loading.attr="disabled" wire:offline.attr="disabled">
@@ -44,6 +49,11 @@
                     @if ($answer->likes()->count('id') !== 0)
                     <span class="small text-dark font-weight-bold">
                         {{ number_format($answer->likes()->count('id')) }}
+                    </span>
+                    <span class="avatar-stack ml-1">
+                    @foreach($answer->likers->take(5) as $user)
+                    <img class="praise-avatar rounded-circle {{ $loop->last ? 'mr-0' : '' }}" src="{{ $user->avatar }}" />
+                    @endforeach
                     </span>
                     @endif
                 </button>
@@ -67,6 +77,11 @@
                     @if ($answer->likes()->count('id') !== 0)
                     <span class="small text-dark font-weight-bold">
                         {{ number_format($answer->likes()->count('id')) }}
+                    </span>
+                    <span class="avatar-stack ml-1">
+                    @foreach($answer->likers->take(5) as $user)
+                    <img class="praise-avatar rounded-circle {{ $loop->last ? 'mr-0' : '' }}" src="{{ $user->avatar }}" />
+                    @endforeach
                     </span>
                     @endif
                 </a>
