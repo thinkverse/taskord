8c38213ae9ecec7421076ec655117014058c0047
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-28 13:30:34 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Livewire/Questions/Questions.php
+++ app/Http/Livewire/Questions/Questions.php
@@ -33,27 +33,27 @@ class Questions extends Component
     {
         if ($this->type === 'questions.newest') {
             $questions = Question::whereHas('user', function ($q) {
-                    $q->where([
-                        ['isFlagged', false],
-                    ]);
-                })
+                $q->where([
+                    ['isFlagged', false],
+                ]);
+            })
                 ->latest()
                 ->get();
         } elseif ($this->type === 'questions.unanswered') {
             $questions = Question::whereHas('user', function ($q) {
-                    $q->where([
-                        ['isFlagged', false],
-                    ]);
-                })
+                $q->where([
+                    ['isFlagged', false],
+                ]);
+            })
                 ->doesntHave('answer')
                 ->latest()
                 ->get();
         } elseif ($this->type === 'questions.popular') {
             $questions = Question::whereHas('user', function ($q) {
-                    $q->where([
-                        ['isFlagged', false],
-                    ]);
-                })
+                $q->where([
+                    ['isFlagged', false],
+                ]);
+            })
                 ->has('answer')
                 ->get()
                 ->sortByDesc(function ($question) {
