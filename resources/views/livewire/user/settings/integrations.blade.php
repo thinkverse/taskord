4213d9fb1e395e12a084f369b001bdda68838053
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-29 05:25:28 +0000

    Add Views

---
+++ resources/views/livewire/user/settings/integrations.blade.php
@@ -0,0 +1,12 @@
+<div class="col-md-8">
+    @include('components.alert')
+    <div class="card mb-4">
+        <div class="card-header pt-3 pb-3">
+            <span class="h5 text-danger">Integrations</span>
+            <div>TODO</div>
+        </div>
+        <div class="card-body">
+            
+        </div>
+    </div>
+</div>
