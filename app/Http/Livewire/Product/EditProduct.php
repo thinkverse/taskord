958ad5509940218fa0af0b4f456486ca5c910733
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-29 04:52:24 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Livewire/Product/EditProduct.php
+++ app/Http/Livewire/Product/EditProduct.php
@@ -92,7 +92,7 @@ class EditProduct extends Component
             session()->flash('error', 'Forbidden!');
         }
     }
-    
+
     public function confirmDelete()
     {
         $this->confirming = $this->product->id;
