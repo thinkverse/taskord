668ecb7849e2c71d6619530dc0e06ab3add25161
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-29 05:16:04 +0000

    Add some info lines near export and delete button

--- resources/views/livewire/user/settings/delete.blade.php
+++ resources/views/livewire/user/settings/delete.blade.php
@@ -6,12 +6,24 @@
             <div>Export and delete your account.</div>
         </div>
         <div class="card-body">
-            <div class="h5 mb-3">Export your account</div>
+            <div class="h5 mb-3">Download your data</div>
+            <div class="mb-3">
+                Most of the personal data that Taskord has about you is accessible through the Taskord app (e.g. tasks, comments, questions, answers, patron details, and user account). If you would like to get a consolidated copy of this data, you can download it by clicking the "Export Now" button.
+            </div>
+            <div class="mb-3">
+                As the downloadable file you will receive will contain your profile information, you should keep it secure and be careful when storing, sending, or uploading it to any other services.
+            </div>
+            <div class="mb-3">
+                If you have any questions or concerns about the personal data contained in your downloadable file, please <a href="https://taskord.freshdesk.com/support/tickets/new" target="_blank">contact us</a>.
+            </div>
             <a class="btn btn-success text-white" href="{{ route('user.settings.export') }}" target="_blank">
                 <i class="fa fa-question mr-1"></i>
                 Export now
             </a>
             <div class="h5 text-danger mt-3 mb-3">Delete your Account</div>
+            <div class="mb-3">
+                Deleting your account is permanent. All your data will be wiped out immediately and you won't be able to get it back.
+            </div>
             @if ($confirming === Auth::id())
             <button wire:click="deleteAccount" class="btn btn-danger">
                 <i class="fa fa-question mr-1"></i>
