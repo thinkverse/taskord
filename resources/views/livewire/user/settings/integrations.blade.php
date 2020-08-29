3858030c2c9a9f71fabbeab37496958fe7b3e2b1
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-29 06:09:26 +0000

    Add webhook table

--- resources/views/livewire/user/settings/integrations.blade.php
+++ resources/views/livewire/user/settings/integrations.blade.php
@@ -21,10 +21,60 @@
                 </button>
             </form>
             @if (session()->has('created'))
-                <div class="mt-2">
-                    {{ session('created')->name }}
+                <div class="mt-4">
+                    <span class="h5">
+                        Here's your webhook for Taskord. Keep it secret; we won't show it to you again.
+                    </span>
+                    <div class="font-weight-bold text-primary font-monospace mt-2">
+                        https://taskord.com/webhook/{{ session('created')->token }}
+                    </div>
                 </div>
             @endif
+        </div>
+    </div>
+    <div class="card mb-4">
+        <div class="card-header pt-3 pb-3">
+            <span class="h5">Active webhooks</span>
+            <div>TODO</div>
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Type</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Token</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($user->webhooks as $webhook)
+                    <tr>
+                        <td>
+                            @if ($webhook->type === 'web')
+                            <i class="fa fa-globe mr-1"></i>
+                            @endif
+                        </td>
+                        <td>
+                            {{ Str::limit($webhook->name, '20') }}
+                        </td>
+                        <td>
+                            {{ Str::limit($webhook->token, '4', '****************') }}
+                        </td>
+                        <td>
+                            {{ Carbon::parse($webhook->created_at)->format('M d, Y') }}
+                        </td>
+                        <td>
+                            <button class="btn btn-sm btn-danger">
+                                <i class="fa fa-trash mr-1"></i>
+                                Delete
+                            </button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
