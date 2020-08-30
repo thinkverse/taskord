eeca42885ab417d1d45a762fbd7e7dfe4c5b0088
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-30 05:46:26 +0000

    Add fillables to model

--- app/Models/Deals.php
+++ app/Models/Deals.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Deals extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'offer',
+        'coupon',
+        'description',
+        'website',
+        'logo',
+    ];
 }
