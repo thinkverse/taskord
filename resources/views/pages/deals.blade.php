7e79398491444dfeb36ec530148b67d799e13ad1
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-30 06:23:50 +0000

    Styled Deals Page

--- resources/views/pages/deals.blade.php
+++ resources/views/pages/deals.blade.php
@@ -15,7 +15,7 @@
             @auth
             @if (Auth::user()->staffShip)
             <button type="button" class="mt-2 btn btn-success text-white" data-toggle="modal" data-target="#newQuestionModal">
-                <i class="fa fa-plus"></i>
+                <i class="fa fa-plus mr-1"></i>
                 Add a Deal
             </button>
             @livewire('pages.create-deal')
@@ -23,7 +23,50 @@
             @endauth
         </div>
         <div class="card-body">
-            {{ $deals }}
+            @foreach ($deals as $deal)
+                <div class="d-flex align-items-center {{ $loop->last ? '' : 'mb-5' }}">
+                    <div>
+                        <img class="rounded avatar-100" src="{{ $deal->logo }}" />
+                    </div>
+                    <div class="ml-3">
+                        <span class="h4">
+                            {{ $deal->name }}
+                        </span>
+                        <span class="h5">
+                            <span class="align-text-top badge bg-success font-weight-normal p-1 ml-1">{{ $deal->offer }}% OFF</span>
+                        </span>
+                        <div class="h6 mt-3">{{ $deal->description }}</div>
+                        <div class="mt-2">
+                            <a href="{{ $deal->website }}" target="_blank">{{ $deal->website }}</a>
+                        </div>
+                        @auth
+                        @if (Auth::user()->isPatron)
+                        <div class="mt-2">
+                            Copuon Code
+                            <code class="ml-2 font-weight-bold">
+                                {{ $deal->coupon }}
+                            </code>
+                        </div>
+                        @else
+                        <div class="mt-2">
+                            <a class="text-black-50" href="{{ route('patron.home') }}">You must be a patron to see coupon code.</a>
+                        </div>
+                        @endif
+                        @endauth
+                        @guest
+                        <div class="mt-2">
+                            <a class="text-black-50" href="{{ route('login') }}">Login to view coupon code.</a>
+                        </div>
+                        @endguest
+                        @if (Auth::user()->staffShip)
+                        <button type="button" class="mt-3 btn btn-sm btn-danger text-white" wire:click="deleteDeal">
+                            <i class="fa fa-trash mr-1"></i>
+                            Delete
+                        </button>
+                        @endif
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
