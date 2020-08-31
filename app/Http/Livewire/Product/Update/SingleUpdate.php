91d7b2899bea88681d6fff987d1afa8d96fa595d
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-08-31 12:40:51 +0000

    Add SingleUpdate wire component

---
+++ app/Http/Livewire/Product/Update/SingleUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Product\Update;
+
+use Livewire\Component;
+
+class SingleUpdate extends Component
+{
+    public function render()
+    {
+        return view('livewire.product.update.single-update');
+    }
+}
