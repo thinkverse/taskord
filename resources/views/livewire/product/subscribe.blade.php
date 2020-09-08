f5c9d383e3ad61a2248b458cb4aed321e00a3472
Filip Troníček <filip.tronicek@seznam.cz>
2020-09-08 14:07:14 +0200

    Add subscriber plurals

--- resources/views/livewire/product/subscribe.blade.php
+++ resources/views/livewire/product/subscribe.blade.php
@@ -20,6 +20,6 @@
     @endif
     @endauth
     <div class="small">
-        <span class="font-weight-bold">{{ number_format($product->subscribersCount()) }}</span> Subscribers
+        <span class="font-weight-bold">{{ number_format($product->subscribersCount()) }}</span> Subscriber{{ $product->subscribersCount() != 1 ? 's' : '' }}
     </div>
 </div>
