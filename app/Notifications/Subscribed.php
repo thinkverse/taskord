2b6083608e035ab2d39fd0ba24c1fe266cdc8d46
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-09 07:21:17 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Notifications/Subscribed.php
+++ app/Notifications/Subscribed.php
@@ -2,11 +2,11 @@
 
 namespace App\Notifications;
 
+use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
-use App\Models\User;
 
 class Subscribed extends Notification implements ShouldQueue
 {
