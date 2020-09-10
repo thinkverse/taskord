3f805ab0d314bb8ab7ec043daf82d7bd9e66273c
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-10 08:32:16 +0000

    Email update need verification

--- app/Http/Livewire/User/Settings/Account.php
+++ app/Http/Livewire/User/Settings/Account.php
@@ -79,11 +79,16 @@ class Account extends Component
                 'username' => 'required|min:2|max:20|alpha_dash|unique:users,username,'.$this->user->id,
                 'email' => 'required|email:rfc,dns|max:255|unique:users,email,'.$this->user->id,
             ]);
-
+            
+            if ($this->email !== $this->user->email) {
+                $this->user->email_verified_at = null;
+            }
+            
             if (Auth::check() && Auth::id() === $this->user->id) {
                 $this->user->username = $this->username;
                 $this->user->email = $this->email;
                 $this->user->save();
+                $this->user->sendEmailVerificationNotification();
 
                 return session()->flash('success', 'Your account has been updated!');
             }
