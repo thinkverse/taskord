10a17884918a0b5b7b3bb6bf6854e836b7d6dc9a
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-10 03:40:46 +0000

    Move livewireScripts to top

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -216,8 +216,8 @@
         </main>
     </div>
 </body>
+@livewireScripts
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
 <script src="{{ asset('js/app.js') }}" defer></script>
 <script src="https://kit.fontawesome.com/4f46a7856f.js" crossorigin="anonymous"></script>
-@livewireScripts
 </html>
