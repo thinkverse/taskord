3f7e0ba343f8f594f8864a95220d384cc43a1a49
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-10 08:01:23 +0000

    Rename to Nav from Sidebar

---
+++ resources/views/admin/nav.blade.php
@@ -0,0 +1,8 @@
+<ul class="nav nav-pills justify-content-center mb-3">
+    <li class="nav-item">
+        <a class="nav-link active" href="{{ route('admin.users') }}">Users</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Products</a>
+    </li>
+</ul>
