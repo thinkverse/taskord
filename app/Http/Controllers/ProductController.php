21d47770332e7beafd5409de1561fa3ffddb4084
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-12 06:00:45 +0000

    Fix count

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -36,23 +36,30 @@ class ProductController extends Controller
                 $countArr[$i] = 0;
             }
         }
+        
+        $members = $product->members->pluck('id');
+        $members->push($product->owner->id);
+        
+        $done_count = Task::where([
+                ['product_id', $product->id],
+                ['done', true],
+            ])
+            ->whereIn('user_id', $members)
+            ->count('id');
+            
+        $pending_count = Task::where([
+                ['product_id', $product->id],
+                ['done', false],
+            ])
+            ->whereIn('user_id', $members)
+            ->count('id');
 
         $response = [
             'product' => $product,
             'type' => $type,
             'graph' => $countArr,
-            'done_count' => Task::where([
-                ['product_id', $product->id],
-                ['done', true],
-                ['user_id', $product->owner->id],
-            ])
-                ->count('id'),
-            'pending_count' => Task::where([
-                ['product_id', $product->id],
-                ['done', false],
-                ['user_id', $product->owner->id],
-            ])
-                ->count('id'),
+            'done_count' => $done_count,
+            'pending_count' => $pending_count,
             'updates_count' => ProductUpdate::where([
                 ['product_id', $product->id],
             ])
