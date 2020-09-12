337181ea6b763e964322065d1859dd6633096999
Bigint <6395788-yo@users.noreply.gitlab.com>
2020-09-12 04:18:36 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Livewire/Product/AddMember.php
+++ app/Http/Livewire/Product/AddMember.php
@@ -2,39 +2,39 @@
 
 namespace App\Http\Livewire\Product;
 
-use Livewire\Component;
-use App\Models\Product;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Component;
 
 class AddMember extends Component
 {
     public $product;
     public $username;
-    
+
     public function mount($product)
     {
         $this->product = $product;
     }
-    
+
     public function submit()
     {
         if (Auth::check()) {
             $user = User::where('username', $this->username)->first();
             if ($user === null) {
-               return session()->flash('team-error', 'User does not exists');
+                return session()->flash('team-error', 'User does not exists');
             }
             if (Auth::user()->username === $this->username) {
                 return session()->flash('team-error', 'You can\'t add yourself to the team!');
             }
-            
+
             $user->products()->sync($this->product);
-            
+
             session()->flash('global', 'User has been added to the team!');
+
             return redirect()->route('product.done', ['slug' => $this->product->slug]);
         }
     }
-    
+
     public function render()
     {
         return view('livewire.product.add-member');
