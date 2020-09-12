9c69da658e463cce08f5f64a9d615eb2b3d6ac24
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-12 03:44:15 +0000

    Show members in product

--- resources/views/product/sidebar.blade.php
+++ resources/views/product/sidebar.blade.php
@@ -79,6 +79,20 @@
                     @endif
                 </a>
             </li>
+            @foreach ($product->members()->get() as $user)
+            <li class="list-group-item pt-2 pb-2">
+                <a href="{{ route('user.done', ['username' => $user->username]) }}">
+                    <img class="rounded-circle avatar-30" src="{{ $user->avatar }}" />
+                </a>
+                <a href="{{ route('user.done', ['username' => $user->username]) }}" class="ml-2 align-middle font-weight-bold text-dark">
+                    @if ($user->firstname or $user->lastname)
+                        {{ $user->firstname }}{{ ' '.$user->lastname }}
+                    @else
+                        {{ $user->username }}
+                    @endif
+                </a>
+            </li>
+            @endforeach
         </ul>
     </div>
     @include('components.footer')
