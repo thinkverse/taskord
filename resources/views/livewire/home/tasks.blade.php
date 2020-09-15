8789a6ecb8ff8c8bfedd6e4132b8695299b4b372
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-15 04:39:15 +0000

    Move beta badge avay from label

--- resources/views/livewire/home/tasks.blade.php
+++ resources/views/livewire/home/tasks.blade.php
@@ -7,8 +7,9 @@
         <span class="float-right">
             <input wire:click="onlyFollowingsTasks" id="onlyFollowingsTasks" class="form-check-input" type="checkbox" {{ Auth::user()->onlyFollowingsTasks ? 'checked' : '' }}>
             <label for="onlyFollowingsTasks" class="ml-1">
-                Only following @include('components.beta', ['background' => 'light'])
+                Only following
             </label>
+            @include('components.beta', ['background' => 'light'])
             <span wire:loading wire:target="onlyFollowingsTasks" class="small ml-2 spinner-border spinner-border-sm text-primary"></span>
         </span>
         @endauth
