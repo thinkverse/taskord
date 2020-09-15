687d1f0dcfc70da7809baed05ee072f5f0aba5b0
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-15 15:24:32 +0000

    Log user IP on poll

--- app/Http/Livewire/Notification/Icon.php
+++ app/Http/Livewire/Notification/Icon.php
@@ -3,11 +3,16 @@
 namespace App\Http\Livewire\Notification;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Icon extends Component
 {
     public function render()
     {
+        if (Auth::check()) {
+            Auth::user()->lastIP = request()->ip();
+            Auth::user()->save();
+        }
         return view('livewire.notification.icon');
     }
 }
