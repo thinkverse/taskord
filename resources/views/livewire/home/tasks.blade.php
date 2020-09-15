0a4ded8811d64a4260d0b808bc91a52b6e700145
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-15 04:37:59 +0000

    Add Beta Badge

--- resources/views/livewire/home/tasks.blade.php
+++ resources/views/livewire/home/tasks.blade.php
@@ -6,7 +6,9 @@
         @auth
         <span class="float-right">
             <input wire:click="onlyFollowingsTasks" id="onlyFollowingsTasks" class="form-check-input" type="checkbox" {{ Auth::user()->onlyFollowingsTasks ? 'checked' : '' }}>
-            <label for="onlyFollowingsTasks" class="ml-1">Show only following</label>
+            <label for="onlyFollowingsTasks" class="ml-1">
+                Only following @include('components.beta', ['background' => 'light'])
+            </label>
             <span wire:loading wire:target="onlyFollowingsTasks" class="small ml-2 spinner-border spinner-border-sm text-primary"></span>
         </span>
         @endauth
