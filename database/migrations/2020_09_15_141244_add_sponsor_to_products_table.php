b7b6264655e78efc27cdf9ff894bc2d2afea2dfc
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-15 14:15:19 +0000

    Add sponsor column

--- database/migrations/2020_09_15_141244_add_sponsor_to_products_table.php
+++ database/migrations/2020_09_15_141244_add_sponsor_to_products_table.php
@@ -14,7 +14,7 @@ class AddSponsorToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('sponsor')->nullable();
         });
     }
 
