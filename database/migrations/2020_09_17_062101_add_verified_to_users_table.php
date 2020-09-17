f2da207a9434334669ba82784d59c515b98c4ead
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-17 06:22:38 +0000

    Add isVerified Column

--- database/migrations/2020_09_17_062101_add_verified_to_users_table.php
+++ database/migrations/2020_09_17_062101_add_verified_to_users_table.php
@@ -14,7 +14,7 @@ class AddVerifiedToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('isVerified')->default(false);
         });
     }
 
