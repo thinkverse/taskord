4a36121d832756de6cd5d8e6fc4daf04a44dcb90
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-17 07:26:16 +0000

    Add comment to jobs

--- app/Jobs/VerifyEmailQueue.php
+++ app/Jobs/VerifyEmailQueue.php
@@ -13,19 +13,24 @@ use Illuminate\Queue\SerializesModels;
 class VerifyEmailQueue implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    
+    protected $user;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    protected $user;
-
     public function __construct(User $user)
     {
         $this->user = $user;
     }
 
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
     public function handle()
     {
         // Here the email verification will be sent to the user
