5731968aa15bce86966ae42400a66ff90c6cc138
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-19 05:26:24 +0000

    Remove Purge CSS

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,9 @@
 const mix = require('laravel-mix');
-require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/darkmode.scss', 'public/css');
 
 if(mix.inProduction()) {
-    mix.purgeCss()
-        .version();
+    mix.version();
 }
