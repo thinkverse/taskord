c5397eddf1d3f365850852ee5c95f8bcfadbefe1
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-19 03:41:26 +0000

    Add ID to telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -63,8 +63,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return in_array($user->email, [
-                //
+            return in_array($user->id, [
+                1
             ]);
         });
     }
