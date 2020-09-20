fcba02d59719ee6bf363039b0f901df351ed6def
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-20 09:59:03 +0000

    404 MVP

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        404
+        <img class="w-25" src="/images/404.svg" />
     </div>
 </div>
 @endsection
