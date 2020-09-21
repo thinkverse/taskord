ec0c4f33d27345fcef5b67534420da9eb7f35ca8
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-21 17:43:17 +0000

    Rollback to normal component

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function users()
     {
-        return view('admin.users');
+        $users = User::latest('updated_at')->paginate(50);
+        $count = User::all()->count('id');
+
+        return view('admin.users', [
+            'users' => $users,
+            'count' => $count,
+        ]);
     }
 
     public static function toggle()
