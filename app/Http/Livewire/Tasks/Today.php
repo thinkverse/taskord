e87a8d638614a770b6327c7c70f10e44fe625585
Bigint <6395788-yo@users.noreply.gitlab.com>
2020-09-21 03:52:23 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Livewire/Tasks/Today.php
+++ app/Http/Livewire/Tasks/Today.php
@@ -20,7 +20,7 @@ class Today extends Component
         $tasks = Task::cacheFor(60 * 60)
             ->select('id', 'task', 'done', 'image', 'user_id', 'created_at', 'due_at', 'type', 'product_id')
             ->where('user_id', Auth::id())
-            ->whereDate('due_at', '<=' ,Carbon::today())
+            ->whereDate('due_at', '<=', Carbon::today())
             ->orWhereDate('created_at', Carbon::today())
             ->where('done', false)
             ->latest()
