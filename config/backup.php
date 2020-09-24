c4640dc0755c2eb5bc83441227285102a1a1ac2e
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-24 10:17:11 +0000

    Reduce Days

--- config/backup.php
+++ config/backup.php
@@ -203,12 +203,12 @@ return [
             /*
              * The number of days for which backups must be kept.
              */
-            'keep_all_backups_for_days' => 7,
+            'keep_all_backups_for_days' => 5,
 
             /*
              * The number of days for which daily backups must be kept.
              */
-            'keep_daily_backups_for_days' => 3,
+            'keep_daily_backups_for_days' => 16,
 
             /*
              * The number of weeks for which one weekly backup must be kept.
