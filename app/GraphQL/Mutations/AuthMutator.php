72d4e78a0fa49b54eb3310412283d8d11b5b9688
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-25 08:35:51 +0000

    More Check for Login

--- app/GraphQL/Mutations/AuthMutator.php
+++ app/GraphQL/Mutations/AuthMutator.php
@@ -14,12 +14,21 @@ class AuthMutator
     public function __invoke($_, array $args)
     {
         if (Auth::check()) {
+            if (Auth::user()->isSuspended) {
+                return 'Your account is suspended!';
+            }
+            
             return "Already logged in";
         } else {
             $credentials = Arr::only($args, ['email', 'password']);
 
             if (Auth::once($credentials)) {
+                if (Auth::user()->isSuspended) {
+                    return 'Your account is suspended!';
+                }
                 return Auth::user()->api_token;
+            } else {
+                return 'Invalid Credentials';
             }
         }
 
