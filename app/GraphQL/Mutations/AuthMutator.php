90337ab4275aa4a86c3e333b0f27ae1c760d8e8c
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-25 07:30:23 +0000

    Add API LW

---
+++ app/GraphQL/Mutations/AuthMutator.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class AuthMutator
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+    }
+    
+    public function login($root, array $args)
+    {
+        $credentials = Arr::only($args, ['email', 'password']);
+        
+        if (Auth::once($credentials)) {
+            $token = Str::random(60);
+            $user = auth()->user();
+            $user->api_token = $token;
+            $user->save();
+            
+            return $token;
+        }
+        
+        return null;
+    }
+}
---
+++ app/Http/Livewire/User/Settings/Api.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User\Settings;
+
+use Livewire\Component;
+
+class Api extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.settings.api');
+    }
+}
