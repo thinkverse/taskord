7db6e0704092576a4c1f21fb91ae23532821e31c
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-26 14:33:54 +0000

    Add daily_goal column

--- database/migrations/2020_09_26_143203_add_goal_to_users_table.php
+++ database/migrations/2020_09_26_143203_add_goal_to_users_table.php
@@ -14,7 +14,7 @@ class AddGoalToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->integer('daily_goal')->nullable()->default(0);
         });
     }
 
@@ -26,7 +26,7 @@ class AddGoalToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('daily_goal');
         });
     }
 }
