f6cf6002321755a90f31dfabb9624c2d116b46c8
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-27 05:56:06 +0000

    Update Collector

--- app/Http/Livewire/Notification/Icon.php
+++ app/Http/Livewire/Notification/Icon.php
@@ -5,16 +5,11 @@ namespace App\Http\Livewire\Notification;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
-use App\Jobs\AuthGetIP;
 
 class Icon extends Component
 {
     public function render()
     {
-        if (Auth::check()) {
-            AuthGetIP::dispatch(Auth::user(), request()->ip());
-        }
-
         return view('livewire.notification.icon');
     }
 }
