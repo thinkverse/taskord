c02b076060c9c7de3aa331d8dbed254833b64463
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-27 06:04:38 +0000

    Add timezone column

--- database/migrations/2020_09_27_060254_add_timezone_to_users_table.php
+++ database/migrations/2020_09_27_060254_add_timezone_to_users_table.php
@@ -14,7 +14,7 @@ class AddTimezoneToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->integer('timezone')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddTimezoneToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('timezone');
         });
     }
 }
