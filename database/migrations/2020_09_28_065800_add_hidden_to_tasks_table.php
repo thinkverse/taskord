5afe33e198cb987534f81b059d70538f087b9aa2
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-28 06:59:35 +0000

    Add hidden Column

--- database/migrations/2020_09_28_065800_add_hidden_to_tasks_table.php
+++ database/migrations/2020_09_28_065800_add_hidden_to_tasks_table.php
@@ -14,7 +14,7 @@ class AddHiddenToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $table->boolean('hidden')->default(false)->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddHiddenToTasksTable extends Migration
     public function down()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            //
+            $table->dropColumn('hidden');
         });
     }
 }
