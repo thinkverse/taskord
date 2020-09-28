183f9c4b5259cbdc942d32fae790d316af31bb2e
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-28 11:55:06 +0000

    Check user isFlagged

---
+++ app/GraphQL/Queries/TaskQuery.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Task;
+
+class TaskQuery
+{
+    public function find($root, array $args)
+    {
+        $task = Task::find($args['id']);
+        
+        if ($task->hidden) {
+            return null;
+        }
+        
+        if ($task->user->isPrivate) {
+            return null;
+        }
+        
+        if ($task->user->isFlagged) {
+            return null;
+        }
+        
+        return $task;
+    }
+}
