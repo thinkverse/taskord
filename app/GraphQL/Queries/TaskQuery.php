7663188d05cb0b3069f2805ae095e635ef262f25
Bigint <6395788-yo@users.noreply.gitlab.com>
2020-09-28 11:55:25 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/GraphQL/Queries/TaskQuery.php
+++ app/GraphQL/Queries/TaskQuery.php
@@ -9,19 +9,19 @@ class TaskQuery
     public function find($root, array $args)
     {
         $task = Task::find($args['id']);
-        
+
         if ($task->hidden) {
             return null;
         }
-        
+
         if ($task->user->isPrivate) {
             return null;
         }
-        
+
         if ($task->user->isFlagged) {
             return null;
         }
-        
+
         return $task;
     }
 }
