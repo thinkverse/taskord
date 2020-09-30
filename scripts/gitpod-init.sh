782c1c9fc5b4e33f6ab56b0bc1366266c4bb5dba
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-30 10:33:32 +0000

    Fix URL Issue

--- scripts/gitpod-init.sh
+++ scripts/gitpod-init.sh
@@ -5,4 +5,7 @@ sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
 composer install
 npm install
 php artisan key:generate
+php artisan horizon:install
+php artisan horizon:publish
+php artisan migrate --seed
 npm run dev
