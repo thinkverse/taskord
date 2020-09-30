4462254f28d4b0779441699174a156dfbeb98fe1
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-09-30 06:44:47 +0000

    Update Gitpod Config

---
+++ scripts/gitpod-init.sh
@@ -0,0 +1,7 @@
+mysql -u root -e "create database taskord"
+cp .env.gitpod .env
+sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
+sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
+composer install
+npm install
+php artisan key:generate
