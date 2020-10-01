6bef4e34559daf7dba7643bacb5b760c599e6b46
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-01 09:41:36 +0000

    ShouldQueue

--- app/Notifications/VersionReleased.php
+++ app/Notifications/VersionReleased.php
@@ -4,8 +4,9 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class VersionReleased extends Notification
+class VersionReleased extends Notification implements ShouldQueue
 {
     use Queueable;
 
