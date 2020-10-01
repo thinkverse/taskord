56521f1a70c2b17884d41b5ada46c8eece94c173
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-01 12:07:33 +0000

    Add LH

--- resources/views/pages/sponsors.blade.php
+++ resources/views/pages/sponsors.blade.php
@@ -14,163 +14,165 @@
             <div>for supporting our community</div>
         </div>
         <div class="card-body">
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/QSAzjno.jpg" />
-                </div>
-                <div>
-                    <span class="h5">
-                        Amazon Web Service
-                    </span>
-                    <div class="h6 mt-3">
-                        AWS (Amazon Web Services) is a comprehensive, evolving cloud computing platform provided by Amazon that includes a mixture of infrastructure as a service (IaaS), platform as a service (PaaS) and packaged software as a service (SaaS) offerings.
+            <div class="">
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/QSAzjno.jpg" />
                     </div>
-                    <div class="mt-2">
-                        <a href="https://aws.amazon.com" target="_blank">➜ Go to AWS</a>
-                    </div>
-                </div>
-            </div>
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/LnDFjfr.jpg" />
-                </div>
-                <div>
-                    <span class="h5">
-                        DigitalOcean
-                    </span>
-                    <div class="h6 mt-3">
-                        DigitalOcean is a cloud hosting provider that offers cloud computing services to business entities so that they can scale themselves by deploying DigitalOcean applications that run parallel across multiple cloud servers without compromising on performance!
-                    </div>
-                    <div class="mt-2">
-                        <a href="https://www.digitalocean.com" target="_blank">➜ Go to DigitalOcean</a>
+                    <div>
+                        <span class="h5">
+                            Amazon Web Service
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            AWS (Amazon Web Services) is a comprehensive, evolving cloud computing platform provided by Amazon that includes a mixture of infrastructure as a service (IaaS), platform as a service (PaaS) and packaged software as a service (SaaS) offerings.
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://aws.amazon.com" target="_blank">➜ Go to AWS</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/vkatjP6.png" />
-                </div>
-                <div>
-                    <span class="h5">
-                        ClickUp
-                    </span>
-                    <div class="h6 mt-3">
-                        ClickUp is a cloud-based collaboration and project management tool suitable for businesses of all sizes and industries. Features include communication and collaboration tools, task assignments and statuses, alerts and a task toolbar.
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/LnDFjfr.jpg" />
                     </div>
-                    <div class="mt-2">
-                        <a href="https://aws.amazon.com" target="_blank">➜ Go to ClickUp</a>
-                    </div>
-                </div>
-            </div>
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/uRbpWBH.jpg" />
-                </div>
-                <div>
-                    <span class="h5">
-                        GitLab
-                    </span>
-                    <div class="h6 mt-3">
-                        GitLab is a web-based DevOps lifecycle tool that provides a Git-repository manager providing wiki, issue-tracking and continuous integration and deployment pipeline features.
-                    </div>
-                    <div class="mt-2">
-                        <a href="https://gitlab.com" target="_blank">➜ Go to GitLab</a>
+                    <div>
+                        <span class="h5">
+                            DigitalOcean
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            DigitalOcean is a cloud hosting provider that offers cloud computing services to business entities so that they can scale themselves by deploying DigitalOcean applications that run parallel across multiple cloud servers without compromising on performance!
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://www.digitalocean.com" target="_blank">➜ Go to DigitalOcean</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/bHamHlT.png" />
-                </div>
-                <div>
-                    <span class="h5">
-                        Gitpod
-                    </span>
-                    <div class="h6 mt-3">
-                        Gitpod is an online IDE which can be launched from any GitHub/GitLab page within seconds, Gitpod provides you with a fully working development environment, including a VS Code-powered IDE and a cloud-based Linux container configured specifically for the project at hand.
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/vkatjP6.png" />
                     </div>
-                    <div class="mt-2">
-                        <a href="https://gitpod.io" target="_blank">➜ Go to GitPod</a>
-                    </div>
-                </div>
-            </div>
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/K1oZwr6.jpg" />
-                </div>
-                <div>
-                    <span class="h5">
-                        Sentry
-                    </span>
-                    <div class="h6 mt-3">
-                        Track errors & monitor performance in all major languages & frameworks with Sentry. Open-source error tracking with full stacktraces & asynchronous context.
-                    </div>
-                    <div class="mt-2">
-                        <a href="https://sentry.io" target="_blank">➜ Go to Sentry</a>
+                    <div>
+                        <span class="h5">
+                            ClickUp
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            ClickUp is a cloud-based collaboration and project management tool suitable for businesses of all sizes and industries. Features include communication and collaboration tools, task assignments and statuses, alerts and a task toolbar.
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://aws.amazon.com" target="_blank">➜ Go to ClickUp</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/a4hBf5f.png" />
-                </div>
-                <div>
-                    <span class="h5">
-                        BrowserStack
-                    </span>
-                    <div class="h6 mt-3">
-                        Speed up your release cycles and deploy bug-free websites and mobile apps with BrowserStack, the industry’s most reliable cloud web and mobile testing platform.
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/uRbpWBH.jpg" />
                     </div>
-                    <div class="mt-2">
-                        <a href="https://www.browserstack.com" target="_blank">➜ Go to BrowserStack</a>
-                    </div>
-                </div>
-            </div>
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/XSNz1FI.png" />
-                </div>
-                <div>
-                    <span class="h5">
-                        Todoist
-                    </span>
-                    <div class="h6 mt-3">
-                        Todoist is a cloud-based service, so all your tasks and notes sync automatically to any device on which you use the app. You can also use the app offline and have your changes sync later. It's available on every major platform, including Android, iOS, macOS, Windows, Android Wear, and Apple Watch.
-                    </div>
-                    <div class="mt-2">
-                        <a href="https://doist.grsm.io/yogi" target="_blank">➜ Go to Todoist</a>
+                    <div>
+                        <span class="h5">
+                            GitLab
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            GitLab is a web-based DevOps lifecycle tool that provides a Git-repository manager providing wiki, issue-tracking and continuous integration and deployment pipeline features.
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://gitlab.com" target="_blank">➜ Go to GitLab</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="text-center mb-5">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/VNRFI9v.jpg" />
-                </div>
-                <div>
-                    <span class="h5">
-                        Notion
-                    </span>
-                    <div class="h6 mt-3">
-                        Notion is an application that provides components such as databases, kanban boards, wikis, calendars and reminders. Users can connect these components to create their own systems for knowledge management, note taking, data management, project management, among others.
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/bHamHlT.png" />
                     </div>
-                    <div class="mt-2">
-                        <a href="https://notion.so" target="_blank">➜ Go to Notion</a>
+                    <div>
+                        <span class="h5">
+                            Gitpod
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            Gitpod is an online IDE which can be launched from any GitHub/GitLab page within seconds, Gitpod provides you with a fully working development environment, including a VS Code-powered IDE and a cloud-based Linux container configured specifically for the project at hand.
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://gitpod.io" target="_blank">➜ Go to GitPod</a>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="text-center">
-                <div>
-                    <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/AUd0N20.jpg" />
-                </div>
-                <div>
-                    <span class="h5">
-                        UptimeRobot
-                    </span>
-                    <div class="h6 mt-3">
-                        Uptime Robot is a free tool used to monitor websites . It monitors your websites every 5 minutes and alerts you if your sites are down. It has free uptime monitoring service that supports multiple monitoring types (HTTP, keyword, ping and port).
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/K1oZwr6.jpg" />
                     </div>
-                    <div class="mt-2">
-                        <a href="https://uptimerobot.com" target="_blank">➜ Go to UptimeRobot</a>
+                    <div>
+                        <span class="h5">
+                            Sentry
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            Track errors & monitor performance in all major languages & frameworks with Sentry. Open-source error tracking with full stacktraces & asynchronous context.
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://sentry.io" target="_blank">➜ Go to Sentry</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/a4hBf5f.png" />
+                    </div>
+                    <div>
+                        <span class="h5">
+                            BrowserStack
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            Speed up your release cycles and deploy bug-free websites and mobile apps with BrowserStack, the industry’s most reliable cloud web and mobile testing platform.
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://www.browserstack.com" target="_blank">➜ Go to BrowserStack</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/XSNz1FI.png" />
+                    </div>
+                    <div>
+                        <span class="h5">
+                            Todoist
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            Todoist is a cloud-based service, so all your tasks and notes sync automatically to any device on which you use the app. You can also use the app offline and have your changes sync later. It's available on every major platform, including Android, iOS, macOS, Windows, Android Wear, and Apple Watch.
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://doist.grsm.io/yogi" target="_blank">➜ Go to Todoist</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center mb-5">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/VNRFI9v.jpg" />
+                    </div>
+                    <div>
+                        <span class="h5">
+                            Notion
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            Notion is an application that provides components such as databases, kanban boards, wikis, calendars and reminders. Users can connect these components to create their own systems for knowledge management, note taking, data management, project management, among others.
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://notion.so" target="_blank">➜ Go to Notion</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-center">
+                    <div>
+                        <img class="rounded avatar-150 mb-3" src="https://i.imgur.com/AUd0N20.jpg" />
+                    </div>
+                    <div>
+                        <span class="h5">
+                            UptimeRobot
+                        </span>
+                        <div class="task-font h6 mt-3 lh-base">
+                            Uptime Robot is a free tool used to monitor websites . It monitors your websites every 5 minutes and alerts you if your sites are down. It has free uptime monitoring service that supports multiple monitoring types (HTTP, keyword, ping and port).
+                        </div>
+                        <div class="mt-2">
+                            <a href="https://uptimerobot.com" target="_blank">➜ Go to UptimeRobot</a>
+                        </div>
                     </div>
                 </div>
             </div>
