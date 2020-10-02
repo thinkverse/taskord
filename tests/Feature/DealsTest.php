bbd0e59c4ce4a0746ff45c8233ef84bda4cea13e
Bigint <6395788-yo@users.noreply.gitlab.com>
2020-10-02 08:42:03 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/Feature/DealsTest.php
+++ tests/Feature/DealsTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature;
 
 use App\Http\Livewire\Pages\CreateDeal;
-use App\Models\Task;
 use App\Models\User;
 use Livewire;
 use Tests\TestCase;
@@ -36,12 +35,12 @@ class DealsTest extends TestCase
     public function test_create_deal()
     {
         Livewire::test(CreateDeal::class)
-            ->set('name', "Test Deal")
-            ->set('description', "Test Deal Dexription")
-            ->set('offer', "20")
-            ->set('coupon', "TASKORDTEST")
-            ->set('website', "https://taskord.com")
-            ->set('logo', "https://taskord.com/images/logo.svg")
+            ->set('name', 'Test Deal')
+            ->set('description', 'Test Deal Dexription')
+            ->set('offer', '20')
+            ->set('coupon', 'TASKORDTEST')
+            ->set('website', 'https://taskord.com')
+            ->set('logo', 'https://taskord.com/images/logo.svg')
             ->call('submit')
             ->assertSeeHtml('Forbidden!');
     }
@@ -49,14 +48,14 @@ class DealsTest extends TestCase
     public function test_admin_create_deal()
     {
         $this->actingAs($this->user);
-        
+
         Livewire::test(CreateDeal::class)
-            ->set('name', "Test Deal")
-            ->set('description', "Test Deal Dexription")
-            ->set('offer', "20")
-            ->set('coupon', "TASKORDTEST")
-            ->set('website', "https://taskord.com")
-            ->set('logo', "https://taskord.com/images/logo.svg")
+            ->set('name', 'Test Deal')
+            ->set('description', 'Test Deal Dexription')
+            ->set('offer', '20')
+            ->set('coupon', 'TASKORDTEST')
+            ->set('website', 'https://taskord.com')
+            ->set('logo', 'https://taskord.com/images/logo.svg')
             ->call('submit')
             ->assertRedirect('/deals');
     }
@@ -64,7 +63,7 @@ class DealsTest extends TestCase
     public function test_admin_create_deal_required()
     {
         $this->actingAs($this->user);
-        
+
         Livewire::test(CreateDeal::class)
             ->call('submit')
             ->assertHasErrors([
