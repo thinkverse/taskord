db70b3a5d50c5e9c635bdc53bba1b1d17972208d
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-02 13:28:44 +0000

    Remove Email Validation

--- app/GraphQL/Mutations/TaskMutator.php
+++ app/GraphQL/Mutations/TaskMutator.php
@@ -66,4 +66,59 @@ class TaskMutator
             ];
         }
     }
+    
+    public function praise($_, array $args)
+    {
+        $throttler = Throttle::get(Request::instance(), 20, 5);
+        $throttler->hit();
+        if (! $throttler->check()) {
+            return [
+                'response' => 'Your are rate limited, try again later!',
+            ];
+        }
+
+        if (Auth::check()) {
+            if (! Auth::user()->hasVerifiedEmail()) {
+                return [
+                    'response' => 'Your email is not verified!',
+                ];
+            }
+
+            if (Auth::user()->isFlagged) {
+                return [
+                    'response' => 'Your account is flagged!',
+                ];
+            }
+
+            $task = Task::create([
+                'user_id' =>  Auth::id(),
+                'product_id' =>  null,
+                'task' => $args['task'],
+                'done' => $args['done'],
+                'done_at' => $args['done'] ? Carbon::now() : null,
+                'image' => null,
+                'due_at' => null,
+                'type' => 'user',
+                'source' => $args['source'],
+            ]);
+            givePoint(new TaskCreated($task));
+            Auth::user()->notify(
+                new TelegramLogger(
+                    '*✅ New Task* by @'
+                    .Auth::user()->username."\n\n"
+                    .$task->task."\n\nhttps://taskord.com/task/"
+                    .$task->id
+                )
+            );
+
+            return [
+                'task' => $task,
+                'response' => 'Task has been created!',
+            ];
+        } else {
+            return [
+                'response' => 'Login to create task!',
+            ];
+        }
+    }
 }
