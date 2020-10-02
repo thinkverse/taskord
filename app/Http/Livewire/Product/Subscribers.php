4ed186d5dca1b08e4d3c92656826fd9b83bb1fdd
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-02 07:26:33 +0000

    Add Mount

--- app/Http/Livewire/Product/Subscribers.php
+++ app/Http/Livewire/Product/Subscribers.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class Subscribers extends Component
 {
+    public $product;
+
+    public function mount($product)
+    {
+        $this->product = $product;
+    }
+    
     public function render()
     {
         return view('livewire.product.subscribers');
