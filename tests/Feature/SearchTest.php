3f556d1d990362dc987a201ecf3a3b7dffd31a6f
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-02 08:54:34 +0000

    Add Search test

--- tests/Feature/SearchTest.php
+++ tests/Feature/SearchTest.php
@@ -29,4 +29,41 @@ class SearchTest extends TestCase
             ->set('query', '')
             ->assertDontSee('Test Search');
     }
+    
+    public function test_search_url()
+    {
+        $response = $this->get(route('search.home'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_search_displays_the_search_page()
+    {
+        $response = $this->get(route('search.home'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('search.search');
+    }
+    
+    public function test_empty_search_tasks_url()
+    {
+        $response = $this->get(route('search.tasks'));
+
+        $response->assertStatus(302);
+    }
+
+    public function test_search_tasks_url()
+    {
+        $response = $this->get(route('search.tasks', ['q' => 'a']));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_search_tasks_displays_the_search_tasks_page()
+    {
+        $response = $this->get(route('search.tasks', ['q' => 'a']));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('search.result');
+    }
 }
