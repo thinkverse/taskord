41a7418d1c87f22d063e41d01158185354960b9c
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-02 08:48:03 +0000

    Update Pages Test

--- tests/Feature/PagesTest.php
+++ tests/Feature/PagesTest.php
@@ -24,48 +24,109 @@ class PagesTest extends TestCase
         $response->assertStatus(200);
         $response->assertViewIs('pages.deals');
     }
-
-    public function test_create_deal()
+    
+    public function test_about_url()
     {
-        Livewire::test(CreateDeal::class)
-            ->set('name', "Test Deal")
-            ->set('description', "Test Deal Dexription")
-            ->set('offer', "20")
-            ->set('coupon', "TASKORDTEST")
-            ->set('website', "https://taskord.com")
-            ->set('logo', "https://taskord.com/images/logo.svg")
-            ->call('submit')
-            ->assertSeeHtml('Forbidden!');
+        $response = $this->get(route('about'));
+
+        $response->assertStatus(200);
     }
 
-    public function test_admin_create_deal()
+    public function test_about_displays_the_about_page()
     {
-        $this->actingAs($this->user);
-        
-        Livewire::test(CreateDeal::class)
-            ->set('name', "Test Deal")
-            ->set('description', "Test Deal Dexription")
-            ->set('offer', "20")
-            ->set('coupon', "TASKORDTEST")
-            ->set('website', "https://taskord.com")
-            ->set('logo', "https://taskord.com/images/logo.svg")
-            ->call('submit')
-            ->assertRedirect('/deals');
+        $response = $this->get(route('about'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.about');
+    }
+    
+    public function test_reputation_url()
+    {
+        $response = $this->get(route('reputation'));
+
+        $response->assertStatus(200);
     }
 
-    public function test_admin_create_deal_required()
+    public function test_reputation_displays_the_reputation_page()
     {
-        $this->actingAs($this->user);
-        
-        Livewire::test(CreateDeal::class)
-            ->call('submit')
-            ->assertHasErrors([
-                'name' => 'required',
-                'description' => 'required',
-                'offer' => 'required',
-                'website' => 'required',
-                'logo' => 'required',
-            ])
-            ->assertSeeHtml('The name field is required.');
+        $response = $this->get(route('reputation'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.reputation');
+    }
+    
+    public function test_terms_url()
+    {
+        $response = $this->get(route('terms'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_terms_displays_the_terms_page()
+    {
+        $response = $this->get(route('terms'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.terms');
+    }
+    
+    public function test_privacy_url()
+    {
+        $response = $this->get(route('privacy'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_privacy_displays_the_privacy_page()
+    {
+        $response = $this->get(route('privacy'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.privacy');
+    }
+    
+    public function test_security_url()
+    {
+        $response = $this->get(route('security'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_security_displays_the_security_page()
+    {
+        $response = $this->get(route('security'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.security');
+    }
+    
+    public function test_sponsors_url()
+    {
+        $response = $this->get(route('sponsors'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_sponsors_displays_the_sponsors_page()
+    {
+        $response = $this->get(route('sponsors'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.sponsors');
+    }
+    
+    public function test_open_url()
+    {
+        $response = $this->get(route('open'));
+
+        $response->assertStatus(200);
+    }
+
+    public function test_open_displays_the_open_page()
+    {
+        $response = $this->get(route('open'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('pages.open');
     }
 }
