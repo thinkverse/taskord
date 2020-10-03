7b2c74b6b6f0dc97b6a44766930c267d76dd0b43
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-03 08:52:57 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- config/unleash.php
+++ config/unleash.php
@@ -1,32 +1,32 @@
 <?php
 
 return [
-  // URL of the Unleash server.
-  // This should be the base URL, do not include /api or anything else.
-  'url' => env('UNLEASH_URL', 'https://gitlab.com'),
-  'instance_id' => env('UNLEASH_INSTANCE_ID'),
-  'project_id' => env('UNLEASH_PROJECT_ID', '20359920'),
-  'app_name'  => env('APP_NAME'),
+    // URL of the Unleash server.
+    // This should be the base URL, do not include /api or anything else.
+    'url' => env('UNLEASH_URL', 'https://gitlab.com'),
+    'instance_id' => env('UNLEASH_INSTANCE_ID'),
+    'project_id' => env('UNLEASH_PROJECT_ID', '20359920'),
+    'app_name'  => env('APP_NAME'),
 
-  // Globally control whether Unleash is enabled or disabled.
-  // If not enabled, no API requests will be made and all "enabled" checks will return `false` and
-  // "disabled" checks will return `true`.
-  'isEnabled' => env('UNLEASH_ENABLED', true),
+    // Globally control whether Unleash is enabled or disabled.
+    // If not enabled, no API requests will be made and all "enabled" checks will return `false` and
+    // "disabled" checks will return `true`.
+    'isEnabled' => env('UNLEASH_ENABLED', true),
 
-  // Allow the Unleash API response to be cached.
-  'cache' => [
-    'isEnabled' => false,
-    'ttl' => 3600,
-  ],
+    // Allow the Unleash API response to be cached.
+    'cache' => [
+        'isEnabled' => false,
+        'ttl' => 3600,
+    ],
 
-  // Mapping of strategies used to guard features on Unleash. The default strategies are already
-  // mapped below, and more strategies can be added - they just need to implement the
-  // `\Taskord\LaravelUnleash\Strategies\Strategy` interface. If you would like to disable
-  // a built-in strategy, please comment it out or remove it below.
-  'strategies' => [
-    'applicationHostname' => \Taskord\LaravelUnleash\Strategies\ApplicationHostnameStrategy::class,
-    'default' => \Taskord\LaravelUnleash\Strategies\DefaultStrategy::class,
-    'remoteAddress' => \Taskord\LaravelUnleash\Strategies\RemoteAddressStrategy::class,
-    'userWithIds' => \Taskord\LaravelUnleash\Strategies\UserWithIdStrategy::class,
-  ],
+    // Mapping of strategies used to guard features on Unleash. The default strategies are already
+    // mapped below, and more strategies can be added - they just need to implement the
+    // `\Taskord\LaravelUnleash\Strategies\Strategy` interface. If you would like to disable
+    // a built-in strategy, please comment it out or remove it below.
+    'strategies' => [
+        'applicationHostname' => \Taskord\LaravelUnleash\Strategies\ApplicationHostnameStrategy::class,
+        'default' => \Taskord\LaravelUnleash\Strategies\DefaultStrategy::class,
+        'remoteAddress' => \Taskord\LaravelUnleash\Strategies\RemoteAddressStrategy::class,
+        'userWithIds' => \Taskord\LaravelUnleash\Strategies\UserWithIdStrategy::class,
+    ],
 ];
