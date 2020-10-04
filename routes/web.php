2c4ea497c69c21e7d4dc932f98b87a55c8be4061
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-04 18:26:32 +0000

    Update Route

--- routes/web.php
+++ routes/web.php
@@ -178,5 +178,5 @@ Route::group(['prefix' => 'status'], function () {
 });
 
 Route::domain('{username}.taskord.com')->group(function () {
-    Route::get('/', [UserController::class, 'profile']);
+    Route::get('/', [UserController::class, 'profile'])->name('subdomain');
 });
