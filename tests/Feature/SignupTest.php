f4d55c36e89d392001ccc8317ffc733a0f146e35
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-04 11:55:42 +0000

    Add Register Test

--- tests/Feature/SignupTest.php
+++ tests/Feature/SignupTest.php
@@ -39,13 +39,25 @@ class SignupTest extends TestCase
         $response->assertViewIs('auth.register');
     }
     
+    public function test_user_can_register()
+    {
+        $response = $this->from('/register')->post('/register', [
+            'username' => Str::random(10),
+            'email' => Str::random(5).'@taskord.com',
+            'password' => 'Taskord!test@Taskord',
+            'password_confirmation' => 'Taskord!test@Taskord',
+        ]);
+        
+        $response->assertRedirect('/');
+    }
+    
     public function test_user_can_register_with_disposable_email()
     {
         $response = $this->from('/register')->post('/register', [
             'username' => Str::random(10),
             'email' => Str::random(5).'@mailinator.com',
             'password' => 'Taskord!test@Taskord',
-            'password-confirm' => 'Taskord!test@Taskord',
+            'password_confirmation' => 'Taskord!test@Taskord',
         ]);
         
         $response->assertRedirect('/register');
@@ -61,7 +73,7 @@ class SignupTest extends TestCase
             'username' => Str::random(10),
             'email' => Str::random(5).'@taskord.com',
             'password' => 'qwerty',
-            'password-confirm' => 'qwerty',
+            'password_confirmation' => 'qwerty',
         ]);
         
         $response->assertRedirect('/register');
