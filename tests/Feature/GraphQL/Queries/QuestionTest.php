ee772be505b314161d6380df5a3cd9e588480544
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-04 09:10:43 +0000

    Add Single Question Test

---
+++ tests/Feature/GraphQL/Queries/QuestionTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Tests\Feature\GraphQL\Queries;
+
+use Tests\TestCase;
+
+class QuestionTest extends TestCase
+{
+    public function test_single_question_query()
+    {
+        $response = $this->graphQL('
+        query {
+          question(id: 1) {
+            id
+            title
+            body
+            patronOnly
+          }
+        }
+        ')
+        ->assertJson([
+            'data' => [
+                'comment' => [
+                    'id' => '1',
+                    'title' => 'Hello, World!',
+                    'body' => 'Hello, World!',
+                    'patronOnly' => false,
+                ],
+            ],
+        ]);
+    }
+
+    public function test_all_comments_query()
+    {
+        $response = $this->graphQL('
+        query {
+          comments(first: 1) {
+            edges {
+              node {
+                id
+                comment
+              }
+            }
+          }
+        }
+        ')
+        ->assertJson([
+            'data' => [
+                'comments' => [
+                    'edges' => [
+                        [
+                            'node' => [
+                                'id' => '1',
+                                'comment' => 'Hello, World!',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+}
