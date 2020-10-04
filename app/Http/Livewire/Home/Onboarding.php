f0b59e815087182a489ef367da52daf3720ea9e2
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-04 15:34:26 +0000

    Remove Discord Invite component

--- app/Http/Livewire/Home/Onboarding.php
+++ app/Http/Livewire/Home/Onboarding.php
@@ -4,7 +4,6 @@ namespace App\Http\Livewire\Home;
 
 use App\Models\Product;
 use App\Models\Task;
-use App\Notifications\DiscordInvite;
 use GrahamCampbell\Throttle\Facades\Throttle;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Request;
@@ -12,27 +11,6 @@ use Livewire\Component;
 
 class Onboarding extends Component
 {
-    public function discordInvite()
-    {
-        $throttler = Throttle::get(Request::instance(), 3, 10);
-        $throttler->hit();
-        if (! $throttler->check()) {
-            return session()->flash('error', 'Your are rate limited, try after 10 minutes!');
-        }
-
-        if (Auth::check()) {
-            if (Auth::user()->isFlagged) {
-                return session()->flash('error', 'Your account is flagged!');
-            } else {
-                Auth::user()->notify(new DiscordInvite(Auth::user()));
-
-                return session()->flash('success', 'Please check your email!');
-            }
-        } else {
-            return session()->flash('error', 'Forbidden!');
-        }
-    }
-
     public function calculateCompleteness($task_count, $praise_count, $product_count, $has_name)
     {
         $completed = [];
