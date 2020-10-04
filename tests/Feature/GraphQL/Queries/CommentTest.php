5f7e117400128d0fc03dd089cad5df82555f1719
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-04 09:05:39 +0000

    Add Single Comment Test

--- tests/Feature/GraphQL/Queries/CommentTest.php
+++ tests/Feature/GraphQL/Queries/CommentTest.php
@@ -4,20 +4,15 @@ namespace Tests\Feature\GraphQL\Queries;
 
 use Tests\TestCase;
 
-class TaskTest extends TestCase
+class CommentTest extends TestCase
 {
-    public function test_single_task_query()
+    public function test_single_comment_query()
     {
         $response = $this->graphQL('
         query {
-          task(id: 1) {
+          comment(id: 1) {
             id
-            task
-            image
-            done
-            type
-            source
-            hidden
+            comment
           }
         }
         ')
@@ -25,12 +20,7 @@ class TaskTest extends TestCase
             'data' => [
                 'task' => [
                     'id' => '1',
-                    'task' => 'Hello, World!',
-                    'image' => null,
-                    'done' => true,
-                    'type' => 'user',
-                    'source' => 'Taskord for Web',
-                    'hidden' => false,
+                    'comment' => 'Hello, World!',
                 ],
             ],
         ]);
