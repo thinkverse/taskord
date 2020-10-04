7342971b2d3b3e0e31e153bc1292e1619529f448
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-04 09:02:37 +0000

    Add All Tasks Test

--- tests/Feature/GraphQL/Queries/TaskTest.php
+++ tests/Feature/GraphQL/Queries/TaskTest.php
@@ -36,5 +36,43 @@ class TaskTest extends TestCase
         ]);
     }
 
-    
+    public function test_all_tasks_query()
+    {
+        $response = $this->graphQL('
+        query {
+          tasks(first: 1) {
+            edges {
+              node {
+                id
+                task
+                image
+                done
+                type
+                source
+                hidden
+              }
+            }
+          }
+        }
+        ')
+        ->assertJson([
+            'data' => [
+                'tasks' => [
+                    'edges' => [
+                        [
+                            'node' => [
+                                'id' => '1',
+                                'task' => 'Hello, World!',
+                                'image' => null,
+                                'done' => true,
+                                'type' => 'user',
+                                'source' => 'Taskord for Web',
+                                'hidden' => false,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
 }
