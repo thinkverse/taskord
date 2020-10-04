0a0f10df562d0bf2784c8bae8261ae11c644c089
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-04 15:02:58 +0000

    Standard Query

--- scripts/gitpod-init.sh
+++ scripts/gitpod-init.sh
@@ -1,4 +1,4 @@
-mysql -u root -e "create database taskord"
+mysql -u root -e "CREATE DATABASE taskord"
 cp .env.gitpod .env
 sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
 sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
