4cc28f45d29ffc8029088900dc1483d371730fe4
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-04 09:04:20 +0000

    Add Comment Test

--- database/seeders/CommentSeeder.php
+++ database/seeders/CommentSeeder.php
@@ -15,6 +15,12 @@ class CommentSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        DB::table('comments')->insert([
+            'user_id' => 1,
+            'task_id' => 1,
+            'comment' => "Hello, World!",
+            'created_at' => $faker->dateTimeBetween($startDate = '-5 days', $endDate = 'now'),
+        ]);
         foreach (range(1, 500) as $index) {
             DB::table('comments')->insert([
                 'user_id' => $faker->numberBetween($min = 1, $max = 50),
