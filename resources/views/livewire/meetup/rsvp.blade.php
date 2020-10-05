9b0fe3699114b65e2f6dd640d6e1d3b2e439b0a0
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 14:57:54 +0000

    Move to Spatie's emoji

--- resources/views/livewire/meetup/rsvp.blade.php
+++ resources/views/livewire/meetup/rsvp.blade.php
@@ -5,16 +5,16 @@
     @auth
     @if (Auth::user()->hasSubscribed($meetup))
     <button class="btn btn-task btn-success text-white float-right font-weight-bold" wire:click="toggleRSVP">
-        ✅ RSVPd
+        {{ Emoji::checkMarkButton() }} RSVPd
     </button>
     @else
     <button class="btn btn-task btn-outline-success text-dark float-right" wire:click="toggleRSVP">
-        ✅ RSVP
+        {{ Emoji::checkMarkButton() }} RSVP
     </button>
     @endif
     @else
     <a class="btn btn-task btn-outline-secondary text-dark float-right" href="{{ route('login') }}">
-        ✅ RSVP
+        {{ Emoji::checkMarkButton() }} RSVP
     </a>
     @endauth
 </div>
