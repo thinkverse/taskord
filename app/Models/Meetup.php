6eb6416e8f0316f6d28f80327c87078713f776c6
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 13:26:28 +0000

    Add Cache and CanBeSubscribed

--- app/Models/Meetup.php
+++ app/Models/Meetup.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Multicaret\Acquaintances\Traits\CanBeSubscribed;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Meetup extends Model
 {
+    use CanBeSubscribed, QueryCacheable;
 }
