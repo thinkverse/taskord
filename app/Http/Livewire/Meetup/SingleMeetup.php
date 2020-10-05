180b431c00b6a840ebada81e16222da38d9f4b8c
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 13:48:15 +0000

    Add SingleMeetup comp

---
+++ app/Http/Livewire/Meetup/SingleMeetup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Meetup;
+
+use Livewire\Component;
+
+class SingleMeetup extends Component
+{
+    public function render()
+    {
+        return view('livewire.meetup.single-meetup');
+    }
+}
