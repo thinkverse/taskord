6c962bd07120504d9c6e7ad399f7c228caa471e6
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 14:23:01 +0000

    Add RSVP to component

--- resources/views/livewire/meetup/rsvp.blade.php
+++ resources/views/livewire/meetup/rsvp.blade.php
@@ -1,3 +1,5 @@
-<div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+<div class="card-footer text-muted">
+    <button class="btn btn-sm btn-outline-success text-dark float-right">
+        ✅ RSVP
+    </button>
 </div>
--- resources/views/livewire/meetup/single-meetup.blade.php
+++ resources/views/livewire/meetup/single-meetup.blade.php
@@ -15,10 +15,6 @@
                 {{ $meetup->tagline }}
             </div>
         </div>
-        <div class="card-footer text-muted">
-            <button class="btn btn-sm btn-outline-success text-dark float-right">
-                ✅ RSVP
-            </button>
-        </div>
+        @livewire('meetup.rsvp')
     </div>
 </div>
