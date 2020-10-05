ff458e255b058c5219848b5a17b0690830e8e96c
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 14:02:05 +0000

    Add Card

--- resources/views/livewire/meetup/single-meetup.blade.php
+++ resources/views/livewire/meetup/single-meetup.blade.php
@@ -1,11 +1,16 @@
 <div class="col-6 col-md-4 col-lg-3 mb-4">
-    <div class="card mx-auto text-center">
+    <div class="card mx-auto">
         <a href="#url"><img class="card-img-top" src="{{ $meetup->cover }}" alt="Sample Title"></a>
         <div class="card-body">
-            <h2 class="card-title">
-                <span class="fa fa-external-link mr-1"></span>
+            <div>
+                Time
+            </div>
+            <h4 class="card-title">
                 <a href="#url">{{ $meetup->name }}</a>
-            </h2>
+            </h4>
+            <div>
+                {{ $meetup->description }}
+            </div>
         </div>
     </div>
 </div>
