f93e069108f127bea8cd9229481b63b016122938
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 13:40:44 +0000

    Update Icon

--- resources/views/meetups/meetups.blade.php
+++ resources/views/meetups/meetups.blade.php
@@ -15,7 +15,7 @@
         </div>
         <div class="card-body">
             @if (count($meetups) === 0)
-            <x-empty icon="gifts" text="No meetups found" />
+            <x-empty icon="handshake" text="No meetups found" />
             @endif
         </div>
     </div>
