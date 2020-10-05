fbdf1c72594c6d31a05b223313d9105bc4063274
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 13:33:38 +0000

    Add meetups() method

--- app/Http/Controllers/MeetupController.php
+++ app/Http/Controllers/MeetupController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Meetup;
 
 class MeetupController extends Controller
 {
-    //
+    public function meetups()
+    {
+        $meetups = Meetup::cacheFor(60 * 60)->get();
+
+        return view('meetups.meetups', [
+            'meetups' => $meetups,
+        ]);
+    }
 }
