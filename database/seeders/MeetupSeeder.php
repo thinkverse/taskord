44dc84a57cb6456f87b8640722ea80263463c2dd
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 14:11:24 +0000

    Update seed time

--- database/seeders/MeetupSeeder.php
+++ database/seeders/MeetupSeeder.php
@@ -22,7 +22,7 @@ class MeetupSeeder extends Seeder
                 'tagline' => $faker->sentence($nbWords = 6, $variableNbWords = true),
                 'description' => $faker->sentence($nbWords = 15, $variableNbWords = true),
                 'cover' => 'https://pbs.twimg.com/profile_banners/864525927438536707/1598315700/1500x500',
-                'starts_at' => $faker->dateTimeBetween($startDate = '-5 days', $endDate = 'now'),
+                'starts_at' => $faker->dateTimeBetween($startDate = '-5 days', $endDate = '+5 days'),
                 'ends_at' => $faker->dateTimeBetween($startDate = '-2 days', $endDate = '+10 days'),
                 'created_at' => $faker->dateTimeBetween($startDate = '-5 days', $endDate = 'now'),
                 'updated_at' => $faker->dateTimeBetween($startDate = '-5 days', $endDate = 'now'),
