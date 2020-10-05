0a4a587402a44ce3c14e7c35051cac407f83b28a
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 13:46:13 +0000

    Add Meetup Seeder

---
+++ database/seeders/MeetupSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MeetupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        foreach (range(1, 500) as $index) {
+            DB::table('answers')->insert([
+                'user_id' => $faker->numberBetween($min = 1, $max = 50),
+                'name' => $faker->sentence($nbWords = 2, $variableNbWords = true),
+                'tagline' => $faker->sentence($nbWords = 6, $variableNbWords = true),
+                'description' => $faker->sentence($nbWords = 15, $variableNbWords = true),
+                'cover' => 'https://pbs.twimg.com/profile_banners/864525927438536707/1598315700/1500x500',
+                'starts_at' => $faker->dateTimeBetween($startDate = '-5 days', $endDate = 'now'),
+                'ends_at' => $faker->dateTimeBetween($startDate = '-2 days', $endDate = '+10 days'),
+                'created_at' => $faker->dateTimeBetween($startDate = '-5 days', $endDate = 'now'),
+                'updated_at' => $faker->dateTimeBetween($startDate = '-5 days', $endDate = 'now'),
+            ]);
+        }
+    }
+}
