ecc9f6561027d28943d668ad96c3f0baac188c3e
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 14:13:15 +0000

    Update Fetch Date

--- app/Http/Controllers/MeetupController.php
+++ app/Http/Controllers/MeetupController.php
@@ -8,7 +8,10 @@ class MeetupController extends Controller
 {
     public function meetups()
     {
-        $meetups = Meetup::cacheFor(60 * 60)->get();
+        $meetups = Meetup::cacheFor(60 * 60)
+            ->where('starts_at', '>=', date('Y-m-d'))
+            ->orderBy('starts_at')
+            ->get();
 
         return view('meetups.meetups', [
             'meetups' => $meetups,
