28ae8bd3ec04e2f61eedf18edab63d8ac9b04d49
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-05 14:54:14 +0000

    Reduce button size

--- resources/views/livewire/meetup/rsvp.blade.php
+++ resources/views/livewire/meetup/rsvp.blade.php
@@ -4,16 +4,16 @@
     @endforeach
     @auth
     @if (Auth::user()->hasSubscribed($meetup))
-    <button class="btn btn-sm btn-success text-white float-right font-weight-bold" wire:click="toggleRSVP">
+    <button class="btn btn-task btn-success text-white float-right font-weight-bold" wire:click="toggleRSVP">
         ✅ RSVPd
     </button>
     @else
-    <button class="btn btn-sm btn-outline-secondary text-dark float-right" wire:click="toggleRSVP">
+    <button class="btn btn-task btn-outline-success text-dark float-right" wire:click="toggleRSVP">
         ✅ RSVP
     </button>
     @endif
     @else
-    <a class="btn btn-sm btn-outline-secondary text-dark float-right" href="{{ route('login') }}">
+    <a class="btn btn-task btn-outline-secondary text-dark float-right" href="{{ route('login') }}">
         ✅ RSVP
     </a>
     @endauth
