5fa53d3c8c522ed83573c718ad5de07caa03504b
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-06 06:48:26 +0000

    Add NewMeetup Component

---
+++ app/Http/Livewire/Meetup/NewMeetup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Meetup;
+
+use Livewire\Component;
+
+class NewMeetup extends Component
+{
+    public function render()
+    {
+        return view('livewire.meetup.new-meetup');
+    }
+}
