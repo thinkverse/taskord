efe332d99dd0240f8cef6bf8f3b86846b384d4f2
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-07 12:14:56 +0000

    Add Flagger for Subscribe

--- app/Http/Livewire/Product/Subscribe.php
+++ app/Http/Livewire/Product/Subscribe.php
@@ -19,8 +19,11 @@ class Subscribe extends Component
 
     public function subscribeProduct()
     {
-        $throttler = Throttle::get(Request::instance(), 20, 5);
+        $throttler = Throttle::get(Request::instance(), 10, 5);
         $throttler->hit();
+        if (count($throttler) > 20) {
+            Helper::flagAccount(Auth::user());
+        }
         if (! $throttler->check()) {
             return session()->flash('error', 'Please slow down!');
         }
