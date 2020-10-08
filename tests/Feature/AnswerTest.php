bcccf2439780a2d4479871fbf6c32fcd45820098
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-08 11:01:27 +0000

    Add View Answers and answer test

--- tests/Feature/AnswerTest.php
+++ tests/Feature/AnswerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Http\Livewire\Answer\CreateAnswer;
 use App\Http\Livewire\Answer\SingleAnswer;
+use App\Http\Livewire\Answer\Answers;
 use App\Models\Answer;
 use App\Models\Question;
 use App\Models\User;
@@ -164,5 +165,41 @@ class AnswerTest extends TestCase
         Livewire::test(SingleAnswer::class, ['answer' => $answer])
             ->call('deleteAnswer')
             ->assertEmitted('answerDeleted');
+    }
+    
+    public function test_view_answers()
+    {
+        $this->actingAs($this->user);
+        $question = Question::create([
+            'user_id' => 1,
+            'title' => "Test Question",
+            'body' => "Test Question Body",
+        ]);
+        $answer = Answer::create([
+            'user_id' =>  1,
+            'question_id' => $question->id,
+            'answer' => "Test Answer",
+        ]);
+
+        Livewire::test(Answers::class, ['question' => $question, 'page' => 1, 'perPage' => 10])
+            ->assertSeeHtml('Test Answer');
+    }
+    
+    public function test_view_single_answer()
+    {
+        $this->actingAs($this->user);
+        $question = Question::create([
+            'user_id' => 1,
+            'title' => "Test Question",
+            'body' => "Test Question Body",
+        ]);
+        $answer = Answer::create([
+            'user_id' =>  1,
+            'question_id' => $question->id,
+            'answer' => "Test Answer",
+        ]);
+
+        Livewire::test(SingleAnswer::class, ['answer' => $answer])
+            ->assertSeeHtml('Test Answer');
     }
 }
