01c9e06b67e7b435648ac0327b029d3b38af337f
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-08 11:25:53 +0000

    Add Onboarding Test

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,10 +2,29 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\Home\Onboarding;
+use App\Http\Livewire\Answer\CreateAnswer;
+use App\Http\Livewire\Answer\LoadMore;
+use App\Http\Livewire\Answer\SingleAnswer;
+use App\Models\Answer;
+use App\Models\Question;
+use App\Models\User;
+use Livewire;
 use Tests\TestCase;
 
 class HomeTest extends TestCase
 {
+    public $user;
+    public $unverified;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = User::find(10);
+        $this->onboarded_user = User::find(1);
+        $this->unverified = User::find(2);
+    }
+    
     public function test_home_url()
     {
         $response = $this->get(route('home'));
@@ -19,5 +38,21 @@ class HomeTest extends TestCase
 
         $response->assertStatus(200);
         $response->assertViewIs('home.home');
+    }
+    
+    public function test_see_onboarding()
+    {
+        $this->actingAs($this->user);
+        
+        Livewire::test(Onboarding::class)
+            ->assertSeeHtml('Getting Started');
+    }
+    
+    public function test_done_see_onboarding()
+    {
+        $this->actingAs($this->onboarded_user);
+        
+        Livewire::test(Onboarding::class)
+            ->assertDontSeeHtml('Getting Started');
     }
 }
