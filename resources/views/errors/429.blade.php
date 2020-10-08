da5b7844c20114c4c9c6751a1c9201ee0614c375
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-08 04:14:46 +0000

    Update Design

--- resources/views/errors/429.blade.php
+++ resources/views/errors/429.blade.php
@@ -1,57 +1,65 @@
 <!DOCTYPE html>
-<html>
+<html lang="en" >
     <head>
         <meta http-equiv="Content-type" content="text/html; charset=utf-8">
         <meta http-equiv="Content-Security-Policy" content="default-src 'none'; base-uri 'self'; connect-src 'self'; form-action 'self'; img-src 'self' data:; script-src 'self'; style-src 'unsafe-inline'">
         <meta content="origin" name="referrer">
         <title>Rate limit · Taskord</title>
         <meta name="viewport" content="width=device-width">
-        <style type="text/css" media="screen">
-            body {
-                background-color: #f6f8fa;
-                color: #24292e;
-                font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
-                font-size: 14px;
-                line-height: 1.5;
+        <style>
+            * {
                 margin: 0;
+                padding: 0;
+                font-family: "Fira Code", monospace;
             }
-            .container { margin: 50px auto; max-width: 600px; text-align: center; padding: 0 24px; }
-            a { color: #0366d6; text-decoration: none; }
-            h1 { line-height: 60px; font-size: 48px; font-weight: 300; margin: 0px; text-shadow: 0 1px 0 #fff; }
-            p { color: rgba(0, 0, 0, 0.5); margin: 20px 0 40px; }
-            .logo { display: inline-block; margin-top: 35px; }
-            #suggestions {
-                margin-top: 35px;
-                color: #ccc;
+            body {
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                background-color: #ecf0f1;
             }
-            #suggestions a {
-                color: #666666;
-                font-weight: 200;
-                font-size: 14px;
-                margin: 0 10px;
+            .container {
+                text-align: center;
+                margin: auto;
+                padding: 20px;
+            }
+            .container img {
+                width: 100px;
+                height: 100px;
+            }
+            .container h1 {
+                margin-top: 30px;
+                margin-bottom: 30px;
+                font-size: 35px;
+                text-align: center;
+            }
+            .container p {
+                margin-top: 10px;
+            }
+            .container p.info {
+                margin-top: 4em;
+                font-size: 12px;
+            }
+            .container p.info a {
+                text-decoration: none;
+                color: #5454ce;
             }
         </style>
     </head>
     <body>
         <div class="container">
+            <img src="/images/pride.svg" />
             <h1>Whoa there!</h1>
-            <p>
-                You have triggered an abuse detection mechanism.
-                <br>
-                <br>
-                Please wait a few minutes before you try again.
-                <br>
-                Make sure that you are not abusing our system!
-                <br>
+            <p>You have triggered an abuse detection mechanism.</p>
+            <p>Please wait a few minutes before you try again.</p>
+            <p class="info">
+                Illustration taken from
+                <a href="https://www.kapwing.com/404-illustrations" target="_blank"
+                    >kapwing.com</a
+                    >
             </p>
-            <div id="suggestions">
-                <a href="https://taskord.com/contact">Taskord Support</a> —
-                <a href="https://status.taskord.com">Taskord Status</a> —
-                <a href="https://twitter.com/taskord">@taskord</a>
-            </div>
-            <a href="/" class="logo">
-            <img width="32" height="32" title="" alt="" src="/images/error.svg">
-            </a>
         </div>
     </body>
 </html>
