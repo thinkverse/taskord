9a38aee3613705de64223945a9292f85e786824c
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-09 07:29:26 +0000

    Remove Jquery in scroll event

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,8 +28,9 @@ document.addEventListener("DOMContentLoaded", () => {
   })();
 }, false);
 
-$(window).scroll(() => {
-  if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+window.addEventListener('scroll', () => {
+  var scrollTop =  window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  if (scrollTop + window.innerHeight > document.documentElement.scrollHeight - 100) {
     document.getElementById("load-more").click();
     document.getElementById("load-more").innerHTML = "Loading";
     document.getElementById("load-more").disabled = true;
