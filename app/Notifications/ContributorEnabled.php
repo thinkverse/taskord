cef38d5ce5421d34fddd7fef564dcccb095ac242
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-10 14:05:37 +0000

    Add ShouldQueue to ContributorEnabled

--- app/Notifications/ContributorEnabled.php
+++ app/Notifications/ContributorEnabled.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class ContributorEnabled extends Notification
+class ContributorEnabled extends Notification implements ShouldQueue
 {
     use Queueable;
 
