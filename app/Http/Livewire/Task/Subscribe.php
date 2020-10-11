67ee6cb0cfba60bdb1e05e2e0416f706749e4012
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-11 10:15:19 +0000

    Add Subscribe Component

---
+++ app/Http/Livewire/Task/Subscribe.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Task;
+
+use Livewire\Component;
+
+class Subscribe extends Component
+{
+    public function render()
+    {
+        return view('livewire.task.subscribe');
+    }
+}
