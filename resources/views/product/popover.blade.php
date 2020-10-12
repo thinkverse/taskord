6ce5748cfdc44bca4532f6a4c6bdbe5bce73cf5b
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-12 15:45:34 +0000

    Color Tweaks

--- resources/views/product/popover.blade.php
+++ resources/views/product/popover.blade.php
@@ -1,11 +1,11 @@
 <div class="d-flex p-3">
     <div>
-        <div class="font-weight-bold text-white">
+        <div class="font-weight-bold text-dark">
             {{ $product->name }}
         </div>
-        <div class="small text-white">{{ '#'.$product->slug }}</div>
+        <div class="small text-dark">{{ '#'.$product->slug }}</div>
         @if ($product->description)
-        <div class="mt-2 text-white">{{ $product->description }}</div>
+        <div class="mt-2 text-dark">{{ $product->description }}</div>
         @endif
     </div>
 </div>
