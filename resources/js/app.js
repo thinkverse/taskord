80c2a18d0b9dc8df58c364fc57057e5441cdae3f
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-12 08:35:27 +0000

    Lint

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,122 +9,122 @@ Turbolinks.start();
 
 window.lightbox = lightbox;
 window.lightbox.option({
-    disableScrolling: true,
-    fadeDuration: 100,
-    imageFadeDuration: 100,
-    resizeDuration: 300,
-    fitImagesInViewport: true,
-    maxWidth: 1000,
+  disableScrolling: true,
+  fadeDuration: 100,
+  imageFadeDuration: 100,
+  resizeDuration: 300,
+  fitImagesInViewport: true,
+  maxWidth: 1000,
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-    (async () => {
-        const target = document.querySelector("#load-more");
-        if (await isInViewport(target)) {
-            document.getElementById("load-more").click();
-            document.getElementById("load-more").innerHTML = "Loading";
-            document.getElementById("load-more").disabled = true;
-        }
-    })();
+  (async () => {
+    const target = document.querySelector("#load-more");
+    if (await isInViewport(target)) {
+      document.getElementById("load-more").click();
+      document.getElementById("load-more").innerHTML = "Loading";
+      document.getElementById("load-more").disabled = true;
+    }
+  })();
 }, false);
 
 window.addEventListener('scroll', () => {
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (scrollTop + window.innerHeight > document.documentElement.scrollHeight - 100) {
-        document.getElementById("load-more").click();
-        document.getElementById("load-more").innerHTML = "Loading";
-        document.getElementById("load-more").disabled = true;
-    }
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  if (scrollTop + window.innerHeight > document.documentElement.scrollHeight - 100) {
+    document.getElementById("load-more").click();
+    document.getElementById("load-more").innerHTML = "Loading";
+    document.getElementById("load-more").disabled = true;
+  }
 });
 
 // Admin Bar toggle in dropdown
 document.addEventListener("turbolinks:load", () => {
-    document.getElementById("admin-bar-click").addEventListener("click", () => {
-        window.fetch("/admin/adminbar")
-            .then(async r => {
-                const data = await r.text();
-                if (data === "enabled" || data === "disabled") {
-                    if (r.status === 200) {
-                        window.location.reload();
-                    }
-                }
-            });
-    });
+  document.getElementById("admin-bar-click").addEventListener("click", () => {
+    window.fetch("/admin/adminbar")
+      .then(async r => {
+        const data = await r.text();
+        if (data === "enabled" || data === "disabled") {
+          if (r.status === 200) {
+            window.location.reload();
+          }
+        }
+      });
+  });
 });
 
 // Dark mode toggle in dropdown
 document.addEventListener("turbolinks:load", () => {
-    document.getElementById("dark-mode").addEventListener("click", () => {
-        window.fetch("/darkmode")
-            .then(async r => {
-                const data = await r.text();
-                if (data === "enabled" || data === "disabled") {
-                    if (r.status === 200) {
-                        window.location.reload();
-                    }
-                }
-            });
-    });
+  document.getElementById("dark-mode").addEventListener("click", () => {
+    window.fetch("/darkmode")
+      .then(async r => {
+        const data = await r.text();
+        if (data === "enabled" || data === "disabled") {
+          if (r.status === 200) {
+            window.location.reload();
+          }
+        }
+      });
+  });
 });
 
 // Hide Alert
 document.addEventListener("livewire:load", () => {
-    window.Livewire.hook("element.updated", () => {
-        setTimeout(() => {
-            $(".fade").fadeOut("fast");
-        }, 2000);
-    });
+  window.Livewire.hook("element.updated", () => {
+    setTimeout(() => {
+      $(".fade").fadeOut("fast");
+    }, 2000);
+  });
 });
 
 // Hide search dropdown on clicking the body
 document.body.addEventListener("click", () => {
-    $("ul").remove(".search-dropdown");
+  $("ul").remove(".search-dropdown");
 });
 
 document.addEventListener("turbolinks:load", () => {
-    $("[data-toggle='tooltip']").tooltip();
+  $("[data-toggle='tooltip']").tooltip();
 });
 
 document.addEventListener("turbolinks:load", () => {
-    $(".user-hover").hover(
-        onUserHover,
-        function () {
-            $(this).popover("toggle")
-            $(this).popover("dispose")
-        }
-    );
-
-    function onUserHover() {
-        var $el = $(this);
-        var content = $el.attr("data-content");
-        var id = $el.attr("data-id");
-        if (content === "") {
-            $.get(`/hovercard/user/${id}`, function (data) {
-                setTimeout(function () {
-                    $el.attr("data-content", getUserData(data));
-                    $el.popover("show");
-                }, 350);
-            }).fail(function () {
-                console.log("error");
-            });
-        } else {
-            setTimeout(function () {
-                $el.popover("show");
-            }, 350);
-        }
+  $(".user-hover").hover(
+    onUserHover,
+    function () {
+      $(this).popover("toggle")
+      $(this).popover("dispose")
     }
+  );
 
-    function getUserData(data) {
-        return `
-    <div class="d-flex">
-      <img class="avatar-40 rounded-circle mr-2" src="${data.avatar}" />
-      <div>
-        <div class="font-weight-bold text-dark">${data.firstname ? data.firstname : ''} ${data.lastname ? data.lastname : ''}</div>
-        <div class="small text-dark">@${data.username}</div>
-        ${data.bio ? `<div class="mt-2 text-dark">${data.bio}</div>` : ``}
-        ${data.location ? `<div class="mt-1 text-dark"><i class="fa fa-compass mr-1 text-dark"></i>${data.location}</div>` : ``}
+  function onUserHover() {
+    var $el = $(this);
+    var content = $el.attr("data-content");
+    var id = $el.attr("data-id");
+    if (content === "") {
+      $.get(`/hovercard/user/${id}`, function (data) {
+        setTimeout(function () {
+          $el.attr("data-content", getUserData(data));
+          $el.popover("show");
+        }, 350);
+      }).fail(function () {
+        console.log("error");
+      });
+    } else {
+      setTimeout(function () {
+        $el.popover("show");
+      }, 350);
+    }
+  }
+
+  function getUserData(data) {
+    return `
+      <div class="d-flex">
+        <img class="avatar-40 rounded-circle mr-2" src="${data.avatar}" />
+        <div>
+          <div class="font-weight-bold text-dark">${data.firstname ? data.firstname : ''} ${data.lastname ? data.lastname : ''}</div>
+          <div class="small text-dark">@${data.username}</div>
+          ${data.bio ? `<div class="mt-2 text-dark">${data.bio}</div>` : ``}
+          ${data.location ? `<div class="mt-1 text-dark"><i class="fa fa-compass mr-1 text-dark"></i>${data.location}</div>` : ``}
+        </div>
       </div>
-    </div>
     `;
-    }
+  }
 });
