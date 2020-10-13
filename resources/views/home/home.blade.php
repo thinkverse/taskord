80df4132131f7a082deba1189d60b254e741ad13
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-13 13:01:28 +0000

    Add Popover to sidebar

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -215,7 +215,12 @@
                         <a href="{{ route('product.done', ['slug' => $product->slug]) }}">
                             <img class="rounded avatar-30 mt-1 ml-2" src="{{ $product->avatar }}" alt="{{ $product->slug }}" height="50" width="50" />
                         </a>
-                        <a href="{{ route('product.done', ['slug' => $product->slug]) }}" class="ml-2 mr-2 align-text-top font-weight-bold text-dark">
+                        <a
+                            href="{{ route('product.done', ['slug' => $product->slug]) }}"
+                            class="ml-2 mr-2 align-text-top font-weight-bold text-dark"
+                            id="product-hover"
+                            data-id="{{ $product->id }}"
+                        >
                             {{ $product->name }}
                             @if ($product->launched)
                                 <a href="{{ route('products.launched') }}" class="small" data-toggle="tooltip" data-placement="right" title="Launched">
@@ -229,7 +234,11 @@
                                 <img class="rounded-circle avatar-30 mt-1 mr-1" src="{{ $user->avatar }}" />
                             </a>
                             @endforeach
-                            <a href="{{ route('user.done', ['username' => $product->owner->username]) }}">
+                            <a
+                                href="{{ route('user.done', ['username' => $product->owner->username]) }}"
+                                id="user-hover"
+                                data-id="{{ $product->owner->id }}"
+                            >
                                 <img class="rounded-circle avatar-30 mt-1 mr-0" src="{{ $product->owner->avatar }}" />
                             </a>
                         </span>
