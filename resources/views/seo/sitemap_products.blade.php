d93fc5fb72711944fc7e07da2a019850282b58c6
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-19 03:24:33 +0000

    Update Product Sitemap

--- resources/views/seo/sitemap_products.blade.php
+++ resources/views/seo/sitemap_products.blade.php
@@ -1,3 +1,6 @@
 @foreach ($products as $product)
 {{ 'https://taskord.com/product/'.$product->slug }}
+{{ 'https://taskord.com/product/'.$product->slug.'/pending' }}
+{{ 'https://taskord.com/product/'.$product->slug.'/updates' }}
+{{ 'https://taskord.com/product/'.$product->slug.'/subscribers' }}
 @endforeach
