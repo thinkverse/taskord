37251801f338e7486e3d9f32bc881546723699a4
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-19 03:23:34 +0000

    Update User Sitemap

--- resources/views/seo/sitemap_users.blade.php
+++ resources/views/seo/sitemap_users.blade.php
@@ -1,3 +1,9 @@
 @foreach ($users as $user)
 {{ 'https://taskord.com/@'.$user->username }}
+{{ 'https://taskord.com/@'.$user->username.'/pending' }}
+{{ 'https://taskord.com/@'.$user->username.'/products' }}
+{{ 'https://taskord.com/@'.$user->username.'/questions' }}
+{{ 'https://taskord.com/@'.$user->username.'/answers' }}
+{{ 'https://taskord.com/@'.$user->username.'/following' }}
+{{ 'https://taskord.com/@'.$user->username.'/followers' }}
 @endforeach
