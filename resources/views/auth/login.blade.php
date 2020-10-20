74a1ab53865abc29f9aa6ecf7f08b6eb0498afba
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-10-20 11:54:29 +0000

    Update Auth Page Style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,114 +9,116 @@
 @section('content')
 <div class="container-md">
     <div class="row justify-content-center">
-        <div class="col-lg-8">
+        <div class="col-lg-5">
             <form class="form-signin" method="POST" action="{{ route('login') }}">
                 @csrf
-                <div class="text-center mb-4">
+                <div class="text-center mb-3">
                     <img
                         class="mb-4"
                         src="/images/logo.svg"
                         alt=""
                         height="60"
                     >
-                    <h1 class="h3 mb-3 font-weight-bold">
+                    <h1 class="h3 mb-2 font-weight-bold">
                         Sign in to your account
                     </h1>
                     <p>
-                        Or <a class="font-weight-bold" href="/">signup now!</a>
+                        Or <a class="font-weight-bold" href="/register">signup now!</a>
                     </p>
                 </div>
-                <div class="form-label-group">
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value="{{ old('username') }}"
-                        class="form-control {{ session('error') ? 'is-invalid' : '' }}"
-                        placeholder="Username or Email"
-                        autocomplete="username"
-                        required
-                        autofocus
-                    >
-                    <label for="username">Username or Email</label>
-                </div>
-                <div class="form-label-group">
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        class="form-control {{ session('error') ? 'is-invalid' : '' }}"
-                        placeholder="Password"
-                        autocomplete="current-password"
-                    >
-                    <label for="password">Password</label>
-                    @if (session()->has('error'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ session('error') }}</strong>
-                        </span>
-                    @endif
-                </div>
-                <div class="checkbox mb-3">
-                    <label>
+                <div class="card p-4">
+                    <div class="form-label-group">
                         <input
-                            type="checkbox"
-                            name="remember"
-                            id="remember" {{ old('remember') ? 'checked' : '' }}
+                            type="text"
+                            id="username"
+                            name="username"
+                            value="{{ old('username') }}"
+                            class="form-control {{ session('error') ? 'is-invalid' : '' }}"
+                            placeholder="Username or Email"
+                            autocomplete="username"
+                            required
+                            autofocus
                         >
-                        Remember me
-                    </label>
-                    @if (Route::has('password.request'))
-                        <a class="float-right font-weight-bold" href="{{ route('password.request') }}">
-                            Forgot Password?
-                        </a>
-                    @endif
-                </div>
-                <div class="btn-group btn-block" role="group">
-                    <button class="btn btn-lg btn-primary" name="submit" value="login" type="submit">
-                        <span class="small">
-                            <i class="fa fa-lock mr-1"></i>
-                            Login
-                        </span>
-                    </button>
-                    <button class="btn btn-lg btn-dark" name="submit" value="magic-link" type="submit">
-                        <span class="small">
-                            <i class="fa fa-magic mr-1"></i>
-                            Magic link
-                        </span>
-                    </button>
-                </div>
-                <div class="mt-3 row">
-                    <div class="col-6">
-                        <a href="/login/google" class="btn btn-social btn-google btn-block" type="submit">
-                            <span class="small">
-                                <i class="fab fa-google mr-1"></i>
-                                Google
-                            </span>
-                        </a>
+                        <label for="username">Username or Email</label>
                     </div>
-                    <div class="col-6">
-                        <a href="/login/twitter" class="btn btn-social btn-twitter btn-block" type="submit">
-                            <span class="small">
-                                <i class="fab fa-twitter mr-1"></i>
-                                Twitter
+                    <div class="form-label-group">
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            class="form-control {{ session('error') ? 'is-invalid' : '' }}"
+                            placeholder="Password"
+                            autocomplete="current-password"
+                        >
+                        <label for="password">Password</label>
+                        @if (session()->has('error'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ session('error') }}</strong>
                             </span>
-                        </a>
+                        @endif
                     </div>
-                    <div class="col-6 mt-2">
-                        <a href="/login/github" class="btn btn-social btn-github btn-block" type="submit">
-                            <span class="small">
-                                <i class="fab fa-github mr-1"></i>
-                                GitHub
-                            </span>
-                        </a>
+                    <div class="checkbox mb-3">
+                        <label>
+                            <input
+                                type="checkbox"
+                                name="remember"
+                                id="remember" {{ old('remember') ? 'checked' : '' }}
+                            >
+                            Remember me
+                        </label>
+                        @if (Route::has('password.request'))
+                            <a class="float-right font-weight-bold" href="{{ route('password.request') }}">
+                                Forgot Password?
+                            </a>
+                        @endif
                     </div>
-                    <div class="col-6 mt-2">
-                        <a href="/login/gitlab" class="btn btn-social btn-gitlab btn-block" type="submit">
+                    <div class="btn-group btn-block" role="group">
+                        <button class="btn btn-lg btn-primary" name="submit" value="login" type="submit">
                             <span class="small">
-                                <i class="fab fa-gitlab mr-1"></i>
-                                GitLab
+                                <i class="fa fa-lock mr-1"></i>
+                                Login
                             </span>
-                        </a>
+                        </button>
+                        <button class="btn btn-lg btn-dark" name="submit" value="magic-link" type="submit">
+                            <span class="small">
+                                <i class="fa fa-magic mr-1"></i>
+                                Magic link
+                            </span>
+                        </button>
+                    </div>
+                    <div class="mt-3 row">
+                        <div class="col-6">
+                            <a href="/login/google" class="btn btn-social btn-google btn-block" type="submit">
+                                <span class="small">
+                                    <i class="fab fa-google mr-1"></i>
+                                    Google
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <a href="/login/twitter" class="btn btn-social btn-twitter btn-block" type="submit">
+                                <span class="small">
+                                    <i class="fab fa-twitter mr-1"></i>
+                                    Twitter
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-6 mt-2">
+                            <a href="/login/github" class="btn btn-social btn-github btn-block" type="submit">
+                                <span class="small">
+                                    <i class="fab fa-github mr-1"></i>
+                                    GitHub
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-6 mt-2">
+                            <a href="/login/gitlab" class="btn btn-social btn-gitlab btn-block" type="submit">
+                                <span class="small">
+                                    <i class="fab fa-gitlab mr-1"></i>
+                                    GitLab
+                                </span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,124 +12,129 @@
         <div class="col-lg-8">
             <form class="form-signin" method="POST" action="{{ route('register') }}">
                 @csrf
-                <div class="text-center mb-4">
+                <div class="text-center mb-3">
                     <img
                         class="mb-4"
                         src="/images/logo.svg"
                         alt=""
                         height="60"
                     >
-                    <h1 class="h3 mb-3 font-weight-bold">
+                    <h1 class="h3 mb-2 font-weight-bold">
                         Sign up to Taskord
                     </h1>
+                    <p>
+                        Or <a class="font-weight-bold" href="/login">login now!</a>
+                    </p>
                 </div>
-                <div class="form-label-group">
-                    <input
-                        type="text"
-                        id="username"
-                        name="username"
-                        value="{{ old('username') }}"
-                        class="form-control @error('username') is-invalid @enderror"
-                        placeholder="Username"
-                        autocomplete="username"
-                        required
-                        autofocus
-                    >
-                    <label for="username">Username</label>
-                    @error('username')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-
-                <div class="form-label-group">
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value="{{ old('email') }}"
-                        class="form-control @error('email') is-invalid @enderror"
-                        placeholder="E-Mail Address"
-                        required
-                        autocomplete="email"
-                    >
-                    <label for="email">E-Mail Address</label>
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-
-                <div class="form-label-group">
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        class="form-control @error('password') is-invalid @enderror"
-                        placeholder="Password"
-                        required
-                        autocomplete="new-password"
-                    >
-                    <label for="password">Password</label>
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-
-                <div class="form-label-group">
-                    <input
-                        type="password"
-                        id="password-confirm"
-                        name="password_confirmation"
-                        class="form-control"
-                        placeholder="Confirm Password"
-                        required
-                        autocomplete="new-password"
-                    >
-                    <label for="password-confirm">Confirm Password</label>
-                </div>
-                <button class="btn btn-lg btn-primary btn-block" type="submit">
-                    <span class="small">
-                        <i class="fa fa-user-plus mr-1"></i>
-                        Sign up
-                    </span>
-                </button>
-                <div class="mt-3 row">
-                    <div class="col-6">
-                        <a href="/login/google" class="btn btn-social btn-google btn-block" type="submit">
-                            <span class="small">
-                                <i class="fab fa-google mr-1"></i>
-                                Google
+                <div class="card p-4">
+                    <div class="form-label-group">
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            value="{{ old('username') }}"
+                            class="form-control @error('username') is-invalid @enderror"
+                            placeholder="Username"
+                            autocomplete="username"
+                            required
+                            autofocus
+                        >
+                        <label for="username">Username</label>
+                        @error('username')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
                             </span>
-                        </a>
+                        @enderror
                     </div>
-                    <div class="col-6">
-                        <a href="/login/twitter" class="btn btn-social btn-twitter btn-block" type="submit">
-                            <span class="small">
-                                <i class="fab fa-twitter mr-1"></i>
-                                Twitter
+    
+                    <div class="form-label-group">
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value="{{ old('email') }}"
+                            class="form-control @error('email') is-invalid @enderror"
+                            placeholder="E-Mail Address"
+                            required
+                            autocomplete="email"
+                        >
+                        <label for="email">E-Mail Address</label>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
                             </span>
-                        </a>
+                        @enderror
                     </div>
-                    <div class="col-6 mt-2">
-                        <a href="/login/github" class="btn btn-social btn-github btn-block" type="submit">
-                            <span class="small">
-                                <i class="fab fa-github mr-1"></i>
-                                GitHub
+    
+                    <div class="form-label-group">
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            class="form-control @error('password') is-invalid @enderror"
+                            placeholder="Password"
+                            required
+                            autocomplete="new-password"
+                        >
+                        <label for="password">Password</label>
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
                             </span>
-                        </a>
+                        @enderror
                     </div>
-                    <div class="col-6 mt-2">
-                        <a href="/login/gitlab" class="btn btn-social btn-gitlab btn-block" type="submit">
-                            <span class="small">
-                                <i class="fab fa-gitlab mr-1"></i>
-                                GitLab
-                            </span>
-                        </a>
+    
+                    <div class="form-label-group">
+                        <input
+                            type="password"
+                            id="password-confirm"
+                            name="password_confirmation"
+                            class="form-control"
+                            placeholder="Confirm Password"
+                            required
+                            autocomplete="new-password"
+                        >
+                        <label for="password-confirm">Confirm Password</label>
+                    </div>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">
+                        <span class="small">
+                            <i class="fa fa-user-plus mr-1"></i>
+                            Sign up
+                        </span>
+                    </button>
+                    <div class="mt-3 row">
+                        <div class="col-6">
+                            <a href="/login/google" class="btn btn-social btn-google btn-block" type="submit">
+                                <span class="small">
+                                    <i class="fab fa-google mr-1"></i>
+                                    Google
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-6">
+                            <a href="/login/twitter" class="btn btn-social btn-twitter btn-block" type="submit">
+                                <span class="small">
+                                    <i class="fab fa-twitter mr-1"></i>
+                                    Twitter
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-6 mt-2">
+                            <a href="/login/github" class="btn btn-social btn-github btn-block" type="submit">
+                                <span class="small">
+                                    <i class="fab fa-github mr-1"></i>
+                                    GitHub
+                                </span>
+                            </a>
+                        </div>
+                        <div class="col-6 mt-2">
+                            <a href="/login/gitlab" class="btn btn-social btn-gitlab btn-block" type="submit">
+                                <span class="small">
+                                    <i class="fab fa-gitlab mr-1"></i>
+                                    GitLab
+                                </span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </form>
