6aa9a0cb84e2a37d86e8d5eba17d32ec5688333f
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-11-29 12:31:55 +0530

    Dynamic active for admins

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -1,8 +1,8 @@
 <ul class="nav nav-pills justify-content-center mb-3">
     <li class="nav-item">
-        <a class="nav-link active" href="{{ route('admin.users') }}">Users</a>
+        <a class="nav-link @if (Route::currentRouteName() === 'admin.users') active @endif" href="{{ route('admin.users') }}">Users</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="{{ route('admin.tasks') }}">Tasks</a>
+        <a class="nav-link @if (Route::currentRouteName() === 'admin.tasks') active @endif" href="{{ route('admin.tasks') }}">Tasks</a>
     </li>
 </ul>
