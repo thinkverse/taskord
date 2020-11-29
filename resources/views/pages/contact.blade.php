7ee6077aa7844c07e67d0ce0c6fdd5ad40bb10c1
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-11-29 18:40:32 +0530

    Add Tally

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -9,11 +9,11 @@
 @section('content')
 <div class="container-md">
     <div class="row justify-content-center">
-        <div class="col-lg-7">
+        <div class="col-lg-7 mt-5">
             <iframe
                 src="https://tally.so/embed/63lL53?transparentBackground=1"
                 width="100%"
-                height="1000"
+                height="650"
                 frameborder="0"
                 marginheight="0"
                 marginwidth="0"
