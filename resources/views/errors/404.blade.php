0528a7b8af91540bb711e7d46d3489aadba1e26c
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-11-29 18:22:21 +0530

    Update 404 Page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -2,8 +2,25 @@
 
 @section('content')
 <div class="container-md">
-    <div class="row justify-content-center">
-        <img class="w-25" src="/images/404.svg" />
-    </div>
+    <main>
+        <h1>4<span><i class="fas fa-check-circle text-primary"></i></span>4</h1>
+        <h2>Page not found</h2>
+        <p>This is not the web page you are looking for.</p>
+    </main>
 </div>
+<style>
+main {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 90vh;
+  justify-content: center;
+  text-align: center;
+}
+
+h1 {
+  font-size: 10rem;
+  letter-spacing: .10em;
+}
+</style>
 @endsection
