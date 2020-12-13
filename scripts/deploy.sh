f52f810b62c2f2924cbd7de961b25a3b2f7dcfe2
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-13 18:51:56 +0530

    Simplify deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,8 +4,4 @@ git pull origin main
 composer install
 npm install
 npm run prod
-php artisan config:cache
-php artisan config:clear
-php artisan view:clear
-php artisan cache:clear
-php artisan lighthouse:clear-cache
+php artisan app:clean
