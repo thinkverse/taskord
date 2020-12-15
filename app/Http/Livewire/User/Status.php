eb00c5b49e0022b6796eef900dbd780e03ce9366
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-15 18:28:14 +0530

    Add Controller

--- app/Http/Livewire/User/Status.php
+++ app/Http/Livewire/User/Status.php
@@ -3,14 +3,38 @@
 namespace App\Http\Livewire\User;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Status extends Component
 {
     public $user;
+    public $status;
 
     public function mount($user)
     {
         $this->user = $user;
+        $this->status = $user->status;
+    }
+
+    public function resetStatus()
+    {
+        if (Auth::check()) {
+            Auth::user()->status = null;
+            Auth::user()->status_emoji = null;
+            Auth::user()->save();
+        } else {
+            return session()->flash('error', 'Forbidden!');
+        }
+    }
+
+    public function setStatus()
+    {
+        if (Auth::check()) {
+            Auth::user()->status = $this->status;
+            Auth::user()->save();
+        } else {
+            return session()->flash('error', 'Forbidden!');
+        }
     }
 
     public function render()
--- resources/views/livewire/user/status.blade.php
+++ resources/views/livewire/user/status.blade.php
@@ -1,14 +1,14 @@
 <div class="card mb-4">
     <div class="card-body">
         <div class="input-group">
-            <button class="btn btn-outline-secondary trigger" type="button">🍑</button>
-            <input type="text" class="form-control" placeholder="What's happening?">
+            <button class="btn btn-outline-secondary trigger" wire:model="status_emoji" type="button">🍑</button>
+            <input type="text" class="form-control" value="{{ $status }}" wire:model="status" placeholder="What's happening?">
         </div>
         <div class="d-flex justify-content-around pt-3">
-            <button class="btn btn-sm btn-danger text-white me-1 w-100">
+            <button wire:click="resetStatus" class="btn btn-sm btn-danger text-white me-1 w-100">
                 Clear Status
             </button>
-            <button class="btn btn-sm btn-success text-white ms-1 w-100">
+            <button wire:click="setStatus" class="btn btn-sm btn-success text-white ms-1 w-100">
                 Set Status
             </button>
         </div>
