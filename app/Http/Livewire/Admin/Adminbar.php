9829204b52132b759ce5df1111e63f2172e95c91
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-20 14:19:01 +0530

    Test Deploy

--- app/Http/Livewire/Admin/Adminbar.php
+++ app/Http/Livewire/Admin/Adminbar.php
@@ -43,7 +43,7 @@ class Adminbar extends Component
 
     public function deploy()
     {
-        shell_exec('cd /var/www/taskord && ./scripts/deploy.sh  > /dev/null 2>/dev/null &');
+        shell_exec('cd /var/www/taskord && ./scripts/deploy.sh');
         activity()
             ->withProperties(['type' => 'Admin'])
             ->log('Deployed the Taskord Application 🐿');
