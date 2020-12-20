b6197ffc5e600e5fc8e10154241fb81fb90987d7
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-20 08:49:07 +0530

    Add emoji picker #44

---
+++ resources/js/emoji-picker.js
@@ -0,0 +1,11 @@
+import { EmojiButton } from '@joeattardi/emoji-button';
+
+const trigger = document.querySelector(".trigger");
+const picker = new EmojiButton({
+  showPreview: false
+});
+picker.on("emoji", selection => {
+  trigger.innerHTML = selection.emoji;
+  document.getElementById("emoji_input").value = selection.emoji;
+});
+trigger.addEventListener("click", () => picker.togglePicker(trigger));
