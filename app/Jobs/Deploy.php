d6b6ddcd0ca0842528f9b67c8d0a429bf01c6fbf
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-20 08:32:39 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Jobs/Deploy.php
+++ app/Jobs/Deploy.php
@@ -7,8 +7,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Process\Process;
 
 class Deploy implements ShouldQueue
 {
