fcbb47d56b692cbf0f221492b2680cfac5da8d6a
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-21 07:52:51 +0530

    Add Cloudflare Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,4 +83,5 @@
 @livewireScripts
 <script src="{{ asset('js/app.js', config('app.env') === 'production' ? true : false) }}" defer data-turbolinks-track="true" data-turbolinks-eval=false></script>
 @yield('emoji-picker')
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js" data-cf-beacon='{"token": "0ebade4d5737444ea409cab9471ddb4d"}'></script>
 </html>
