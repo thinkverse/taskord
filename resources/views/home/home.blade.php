a156985547c011ba778c6dd2b2f9495a9039e505
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-22 09:33:52 +0530

    Redesign Recent questions section and Closes #60

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -26,7 +26,7 @@
             <div class="card mb-4">
                 <div class="card-body">
                     @foreach ($recent_questions as $question)
-                        <div class="{{ $loop->index === count($recent_questions) - 1 ? '' : 'mb-2' }} {{ $question->patronOnly ? 'bg-patron recent-questions' : '' }}">
+                        <div class="{{ $loop->index === count($recent_questions) - 1 ? '' : 'mb-2' }} {{ $question->patronOnly ? 'bg-patron recent-questions' : '' }} d-flex align-items-center">
                             <a
                                 href="{{ route('user.done', ['username' => $question->user->username]) }}"
                                 class="user-popover"
@@ -34,28 +34,37 @@
                             >
                                 <img loading=lazy class="rounded-circle avatar-30" src="{{ Helper::getCDNImage($question->user->avatar, 80) }}" alt="{{ $question->user->username }}'s avatar" />
                             </a>
-                            <a href="{{ route('question.question', ['id' => $question->id]) }}">
-                                <span class="align-middle ms-1 text-dark fw-bold">{{ Str::words($question->title, '10') }}</span>
-                                @if ($question->answer->count('id') >= 1)
-                                <span class="ms-1 align-middle text-secondary">
-                                    {{ $question->answer->count('id') }} {{ $question->answer->count('id') >= 1 ? 'answers' : 'answer' }}
-                                </span>
+                            <div class="ms-3">
+                                <a href="{{ route('question.question', ['id' => $question->id]) }}" class="align-middle text-dark fw-bold">{{ Str::words($question->title, '10') }}</a>
+                                @if ($question->created_at->isToday())
+                                <span class="badge bg-success ms-2 align-middle">New</span>
                                 @endif
-                            </a>
-                            @if ($question->created_at->isToday())
-                            <span class="badge bg-success ms-2 align-middle">New</span>
-                            @endif
-                            <span class="avatar-stack ms-1">
-                                @foreach ($question->answer->groupBy('user_id')->take(5) as $answer)
-                                <img
-                                    loading=lazy
-                                    class="user-popover replies-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}"
-                                    data-id="{{ $answer[0]->user->id }}"
-                                    src="{{ Helper::getCDNImage($answer[0]->user->avatar, 80) }}"
-                                    alt="{{ $answer[0]->user->username }}'s avatar"
-                                />
-                                @endforeach
-                            </span>
+                                <div>
+                                    <a
+                                        href="{{ route('user.done', ['username' => $question->user->username]) }}"
+                                        data-id="{{ $question->user->id }}"
+                                        class="user-popover text-secondary"
+                                    >
+                                        by {{ '@'.$question->user->username }}
+                                    </a>
+                                    @if ($question->answer->count('id') >= 1)
+                                    <span class="ms-1 text-secondary">
+                                        · {{ $question->answer->count('id') }} {{ $question->answer->count('id') >= 1 ? 'answers' : 'answer' }}
+                                    </span>
+                                    @endif
+                                    <span class="avatar-stack ms-1">
+                                        @foreach ($question->answer->groupBy('user_id')->take(5) as $answer)
+                                        <img
+                                            loading=lazy
+                                            class="user-popover replies-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}"
+                                            data-id="{{ $answer[0]->user->id }}"
+                                            src="{{ Helper::getCDNImage($answer[0]->user->avatar, 80) }}"
+                                            alt="{{ $answer[0]->user->username }}'s avatar"
+                                        />
+                                        @endforeach
+                                    </span>
+                                </div>
+                            </div>
                         </div>
                     @endforeach
                 </div>
