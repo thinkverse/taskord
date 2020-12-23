751a7bd77838a8fafcfb539ec789907a52374b10
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-23 19:10:05 +0530

    :heavy_plus_sign: Add Reset Section

--- resources/views/livewire/user/settings/delete.blade.php
+++ resources/views/livewire/user/settings/delete.blade.php
@@ -7,7 +7,22 @@
             </div>
         </div>
         <div class="card-body">
-            <div class="h5 text-danger mb-3">Delete your Account</div>
+            <div class="h5 text-danger mb-3">Reset your Account</div>
+            <div class="mb-3">
+                Resetting your account is will be wiped out all your data immediately and you won't be able to get it back.
+            </div>
+            @if ($confirming === Auth::id())
+            <button wire:click="deleteAccount" class="btn btn-danger">
+                <x-heroicon-o-question-mark-circle class="heroicon" />
+                Are you sure?
+            </button>
+            @else
+            <button wire:click="confirmDelete" class="btn btn-danger">
+                <x-heroicon-o-trash class="heroicon" />
+                Reset now
+            </button>
+            @endif
+            <div class="h5 text-danger mb-3 mt-4">Delete your Account</div>
             <div class="mb-3">
                 Deleting your account is permanent. All your data will be wiped out immediately and you won't be able to get it back.
             </div>
