304882e7c4ae00a9dd80806fb9be1c18e68e7136
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-23 04:45:09 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\FeedController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MeetupController;
 use App\Http\Controllers\PagesController;
@@ -14,7 +15,6 @@ use App\Http\Controllers\StatusController;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WebhookController;
-use App\Http\Controllers\FeedController;
 use Illuminate\Support\Facades\Route;
 
 /*
