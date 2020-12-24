609a5b317661c2a5b9d84fb04f6dde1440ae0d23
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-24 16:55:17 +0530

    :heavy_plus_sign: Add Auth settings Test

--- tests/Feature/SettingsTest.php
+++ tests/Feature/SettingsTest.php
@@ -1,5 +1,9 @@
 <?php
 
+use function Tests\actingAs;
+
+// Unauth
+
 it('has settings page - profile - unauth - response test', function () {
     $response = $this->get('/settings');
 
@@ -58,4 +62,46 @@ it('has settings page - delete - unauth - response test', function () {
     $response = $this->get('/settings/delete');
 
     $response->assertStatus(302);
+});
+
+// Auth
+
+it('has settings page - profile - auth - response test', function () {
+    actingAs(1)->get('/settings')->assertStatus(200);
+});
+
+it('has settings page - account - auth - response test', function () {
+    actingAs(1)->get('/settings/account')->assertStatus(200);
+});
+
+it('has settings page - patron - auth - response test', function () {
+    actingAs(1)->get('/settings/patron')->assertStatus(200);
+});
+
+it('has settings page - password - auth - response test', function () {
+    actingAs(1)->get('/settings/password')->assertStatus(200);
+});
+
+it('has settings page - notifications - auth - response test', function () {
+    actingAs(1)->get('/settings/notifications')->assertStatus(200);
+});
+
+it('has settings page - integrations - auth - response test', function () {
+    actingAs(1)->get('/settings/integrations')->assertStatus(200);
+});
+
+it('has settings page - api - auth - response test', function () {
+    actingAs(1)->get('/settings/api')->assertStatus(200);
+});
+
+it('has settings page - logs - auth - response test', function () {
+    actingAs(1)->get('/settings/logs')->assertStatus(200);
+});
+
+it('has settings page - data - auth - response test', function () {
+    actingAs(1)->get('/settings/data')->assertStatus(200);
+});
+
+it('has settings page - delete - auth - response test', function () {
+    actingAs(1)->get('/settings/delete')->assertStatus(200);
 });
