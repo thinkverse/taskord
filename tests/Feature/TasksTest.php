64dc51d153464ffef11d76170b7b1143da4806e3
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-24 16:46:47 +0530

    Add auth test

--- tests/Feature/TasksTest.php
+++ tests/Feature/TasksTest.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Models\User;
+use function Tests\actingAs;
+
 it('has tasks page - unauth - response test', function () {
     $response = $this->get('/tasks');
 
     $response->assertStatus(302);
+});
+
+it('has tasks page - auth - response test', function () {
+    define('LARAVEL_START', microtime(true));
+    $user = User::find(1)->first();
+
+    actingAs($user)->get('/tasks')->assertStatus(200);
 });
