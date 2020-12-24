59f9dbd7e5ea5040bf27a45e23dde81f65a9bdc1
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-24 09:06:23 +0530

    Press `NI` shortcut to create new issue in GitLab with browser params and Closes #72

--- resources/js/shortcuts.js
+++ resources/js/shortcuts.js
@@ -30,3 +30,66 @@ hotkeys("g+p", () => {
 hotkeys("g+s", () => {
   window.location.href = "/settings";
 });
+
+// Create new issue in GitLab
+hotkeys("n+i", () => {
+  var nAgt = navigator.userAgent;
+  var browserName = navigator.appName;
+  var fullVersion = '' + parseFloat(navigator.appVersion);
+  var nameOffset, verOffset, ix;
+
+  // In Opera 15+, the true version is after "OPR/"
+  if ((verOffset = nAgt.indexOf("OPR/")) != -1) {
+    browserName = "Opera";
+    fullVersion = nAgt.substring(verOffset + 4);
+  }
+  // In older Opera, the true version is after "Opera" or after "Version"
+  else if ((verOffset = nAgt.indexOf("Opera")) != -1) {
+    browserName = "Opera";
+    fullVersion = nAgt.substring(verOffset + 6);
+    if ((verOffset = nAgt.indexOf("Version")) != -1)
+      fullVersion = nAgt.substring(verOffset + 8);
+  }
+  // In MSIE, the true version is after "MSIE" in userAgent
+  else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
+    browserName = "Microsoft Internet Explorer";
+    fullVersion = nAgt.substring(verOffset + 5);
+  }
+  // In Chrome, the true version is after "Chrome"
+  else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
+    browserName = "Chrome";
+    fullVersion = nAgt.substring(verOffset + 7);
+  }
+  // In Safari, the true version is after "Safari" or after "Version"
+  else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
+    browserName = "Safari";
+    fullVersion = nAgt.substring(verOffset + 7);
+    if ((verOffset = nAgt.indexOf("Version")) != -1)
+      fullVersion = nAgt.substring(verOffset + 8);
+  }
+  // In Firefox, the true version is after "Firefox"
+  else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
+    browserName = "Firefox";
+    fullVersion = nAgt.substring(verOffset + 8);
+  }
+  // In most other browsers, "name/version" is at the end of userAgent
+  else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
+    (verOffset = nAgt.lastIndexOf('/'))) {
+    browserName = nAgt.substring(nameOffset, verOffset);
+    fullVersion = nAgt.substring(verOffset + 1);
+    if (browserName.toLowerCase() == browserName.toUpperCase()) {
+      browserName = navigator.appName;
+    }
+  }
+  // trim the fullVersion string at semicolon/space if present
+  if ((ix = fullVersion.indexOf(";")) != -1)
+    fullVersion = fullVersion.substring(0, ix);
+  if ((ix = fullVersion.indexOf(" ")) != -1)
+    fullVersion = fullVersion.substring(0, ix);
+
+  const description = "\n\n```\n" +
+    "Browser name: " + browserName + "\n" +
+    "Full version: " + fullVersion
+    + "\n```";
+  window.open("https://gitlab.com/taskord/taskord/-/issues/new?issue[description]=" + encodeURI(description));
+});
