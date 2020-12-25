8d45929fd4d0750f52d556564d1b71c64092b8c4
thinkverse <hallberg.kim@gmail.com>
2020-12-25 17:54:55 +0100

    Add empty state to questions component

--- resources/views/livewire/question/questions.blade.php
+++ resources/views/livewire/question/questions.blade.php
@@ -1,4 +1,12 @@
 <div>
+    @if (count($questions) === 0)
+    <div class="card-body text-center mt-3 mb-3">
+        <x-heroicon-o-question-mark-circle class="heroicon-4x text-primary mb-2" />
+        <div class="h4">
+            No questions asked
+        </div>
+    </div>
+    @endif
     @foreach ($questions as $question)
         @livewire('question.single-question', [
             'type' => $type,
