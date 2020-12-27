565b3d06fbbdac3e2a411a41c568383733fdb5e5
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-27 13:43:32 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Livewire/User/Settings/Data.php
+++ app/Http/Livewire/User/Settings/Data.php
@@ -3,8 +3,6 @@
 namespace App\Http\Livewire\User\Settings;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class Data extends Component
