ba986e085eb19505e6ec076c8f8564ee0f3bcd70
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-27 12:32:58 +0530

    Add aria-label for Notifications icon #123

--- resources/views/livewire/notification/icon.blade.php
+++ resources/views/livewire/notification/icon.blade.php
@@ -1,5 +1,5 @@
 <li wire:poll.5s class="nav-item me-2">
-    <a class="nav-link text-white" href="{{ route('notifications.unread') }}">
+    <a class="nav-link text-white" href="{{ route('notifications.unread') }}" aria-label="Notifications">
         <x-heroicon-o-bell class="heroicon-notification me-0" />
         @auth
         @if (Auth::user()->unreadNotifications->count('id') !== 0)
