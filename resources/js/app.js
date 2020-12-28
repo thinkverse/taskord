9b6d0c703696e1a9f4f644cb4643c6c0cd808842
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-28 08:18:44 +0530

    Add spinner for loading button

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,53 +9,43 @@ var Turbolinks = require("turbolinks");
 Turbolinks.start();
 TurbolinksPrefetch.start();
 
-document.addEventListener("DOMContentLoaded", () => {
-  (async () => {
-    const target = document.querySelector("#load-more");
-    if (await isInViewport(target)) {
-      document.getElementById("load-more").click();
-      document.getElementById("load-more").innerHTML = "Loading";
-      document.getElementById("load-more").disabled = true;
-    }
-  })();
-}, false);
-
-window.addEventListener('scroll', () => {
-  var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  if (scrollTop + window.innerHeight > document.documentElement.scrollHeight - 100) {
+// Initial Pagination
+document.addEventListener("DOMContentLoaded", async () => {
+  const target = document.querySelector("#load-more");
+  if (await isInViewport(target)) {
     document.getElementById("load-more").click();
-    document.getElementById("load-more").innerHTML = "Loading";
+    document.getElementById("load-more").innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
     document.getElementById("load-more").disabled = true;
   }
-});
+}, false);
 
-// Admin Bar toggle in dropdown
 document.addEventListener("turbolinks:load", () => {
+  // Pagination
+  window.addEventListener('scroll', () => {
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (scrollTop + window.innerHeight > document.documentElement.scrollHeight - 100) {
+      document.getElementById("load-more").click();
+      document.getElementById("load-more").innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
+      document.getElementById("load-more").disabled = true;
+    }
+  });
+
+  // Admin Bar toggle in dropdown
   document.getElementById("admin-bar-click").addEventListener("click", async () => {
     const res = await window.fetch(`/admin/adminbar`);
     if (res.status === 200) {
       location.reload();
     }
   });
-});
 
-// Dark mode toggle in dropdown
-document.addEventListener("turbolinks:load", () => {
+  // Dark mode toggle in dropdown
   document.getElementById("dark-mode").addEventListener("click", async () => {
     const res = await window.fetch(`/darkmode`);
     if (res.status === 200) {
       location.reload();
     }
   });
-});
 
-// Hide search dropdown on clicking the body
-document.body.addEventListener("click", () => {
-  // var search = document.getElementsByClassName("search-dropdown")[0];
-  // search.remove();
-});
-
-document.addEventListener("turbolinks:load", () => {
   var shortcutsModal = document.getElementById('shortcutsModal')
   shortcutsModal.addEventListener('shown.bs.modal', async () => {
     var shortcutsModalBody = document.getElementById('shortcutsModalBody');
