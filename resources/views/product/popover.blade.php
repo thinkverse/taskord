65112f1e070a2b895c1387f41e959e520b72a4b2
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-28 16:13:28 +0530

    Add links in product popover

--- resources/views/product/popover.blade.php
+++ resources/views/product/popover.blade.php
@@ -1,10 +1,14 @@
 <div class="d-flex p-3">
-    <img loading=lazy class="avatar-50 rounded me-3" src="{{ Helper::getCDNImage($product->avatar, 80) }}" height="50" width="50" alt="{{ $product->slug }}'s avatar" />
+    <a href="{{ route('product.done', ['slug' => $product->slug]) }}">
+        <img loading=lazy class="avatar-50 rounded me-3" src="{{ Helper::getCDNImage($product->avatar, 80) }}" height="50" width="50" alt="{{ $product->slug }}'s avatar" />
+    </a>
     <div>
-        <div class="fw-bold text-dark">
+        <a class="fw-bold text-dark" href="{{ route('product.done', ['slug' => $product->slug]) }}">
             {{ $product->name }}
+        </a>
+        <div>
+            <a class="small text-dark" href="{{ route('product.done', ['slug' => $product->slug]) }}">{{ '#'.$product->slug }}</a>
         </div>
-        <div class="small text-dark">{{ '#'.$product->slug }}</div>
         @if ($product->description)
         <div class="mt-2 text-dark">{{ $product->description }}</div>
         @endif
