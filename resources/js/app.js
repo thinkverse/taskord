75f193495ec60fb27d830488e6bc5d8e4ef97c33
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-29 19:03:37 +0530

    Remove TL

--- resources/js/app.js
+++ resources/js/app.js
@@ -23,38 +23,34 @@ document.addEventListener("DOMContentLoaded", () => {
   })
 });
 
-document.addEventListener("turbolinks:load", async () => {
-  // Initial Pagination
-  const target = document.querySelector("#load-more");
-  if (await isInViewport(target)) {
-    document.getElementById("load-more").click();
-    document.getElementById("load-more").innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
-    document.getElementById("load-more").disabled = true;
+// Initial Pagination
+const target = document.querySelector("#load-more");
+if (await isInViewport(target)) {
+  document.getElementById("load-more").click();
+  document.getElementById("load-more").innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`;
+  document.getElementById("load-more").disabled = true;
+}
+
+// Admin Bar toggle in dropdown
+document.getElementById("admin-bar-click").addEventListener("click", async () => {
+  const res = await window.fetch(`/admin/adminbar`);
+  if (res.status === 200) {
+    location.reload();
   }
+});
 
-  // Admin Bar toggle in dropdown
-  document.getElementById("admin-bar-click").addEventListener("click", async () => {
-    const res = await window.fetch(`/admin/adminbar`);
-    if (res.status === 200) {
-      location.reload();
-    }
-  });
-
-  // Dark mode toggle in dropdown
-  document.getElementById("dark-mode").addEventListener("click", async () => {
-    const res = await window.fetch(`/darkmode`);
-    if (res.status === 200) {
-      location.reload();
-    }
-  });
+// Dark mode toggle in dropdown
+document.getElementById("dark-mode").addEventListener("click", async () => {
+  const res = await window.fetch(`/darkmode`);
+  if (res.status === 200) {
+    location.reload();
+  }
 });
 
 // Load shortcuts
-document.addEventListener("turbolinks:load", async () => {
-  var shortcutsModal = document.getElementById("shortcutsModal")
-  shortcutsModal.addEventListener("shown.bs.modal", async () => {
-    var shortcutsModalBody = document.getElementById("shortcutsModalBody");
-    const res = await window.fetch(`/site/shortcuts`);
-    shortcutsModalBody.innerHTML = await res.text();
-  });
+var shortcutsModal = document.getElementById("shortcutsModal")
+shortcutsModal.addEventListener("shown.bs.modal", async () => {
+  var shortcutsModalBody = document.getElementById("shortcutsModalBody");
+  const res = await window.fetch(`/site/shortcuts`);
+  shortcutsModalBody.innerHTML = await res.text();
 });
