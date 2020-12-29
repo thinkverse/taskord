f1174163f41b82a28be219fa41b7c48a15843d7a
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-29 20:16:21 +0530

    Update to h6

--- resources/views/livewire/home/tasks.blade.php
+++ resources/views/livewire/home/tasks.blade.php
@@ -2,7 +2,7 @@
     @if (!$readyToLoad)
     <div class="card-body text-center mt-3 mb-3">
         <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-        <div class="h5">
+        <div class="h6">
             Loading Tasks...
         </div>
     </div>
