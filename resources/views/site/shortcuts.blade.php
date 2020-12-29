6c2f99a78c83bec85da19856d3b2bef2d26f1f6b
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-29 09:41:31 +0530

    Bring ? down

--- resources/views/site/shortcuts.blade.php
+++ resources/views/site/shortcuts.blade.php
@@ -41,15 +41,15 @@
     </span>
 </div>
 <div class="d-flex justify-content-between mt-2">
-    <span>Bring up this help dialog</span>
-    <span>
-        <kbd>?</kbd>
-    </span>
-</div>
-<div class="d-flex justify-content-between mt-2">
     <span>Create new GitLab Issue</span>
     <span>
         <kbd class="me-1">n</kbd><kbd>i</kbd>
+    </span>
+</div>
+<div class="d-flex justify-content-between mt-2">
+    <span>Bring up this help dialog</span>
+    <span>
+        <kbd>?</kbd>
     </span>
 </div>
 @if (Auth::user()->isStaff)
