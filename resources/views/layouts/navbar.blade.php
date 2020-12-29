428378cca3c9bc5a7481f9becbc872d0ffd8db6f
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-29 17:55:04 +0530

    Align shortcults loader to center

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -204,7 +204,9 @@
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body" id="shortcutsModalBody">
-                <div class="spinner-border text-primary" role="status"></div>
+                <div class="d-flex justify-content-center">
+                    <div class="spinner-border text-primary" role="status"></div>
+                </div>
             </div>
             <div class="d-flex justify-content-between modal-footer">
                 <x-beta background="light" />
