bf300177f4bb22cbe49d0362ecd2b756b67a4ef4
thinkverse <hallberg.kim@gmail.com>
2020-12-29 21:09:32 +0100

    Fix tasks not rendering mentions with dashes and Closes #150

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -170,8 +170,8 @@ class Helper
     public static function renderTask($task)
     {
         $reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,100}(\/\S*)?/";
-        $products = preg_replace('/#(\w+)/', '<a href="/product/$1">#$1</a>', $task);
-        $users = preg_replace('/@(\w+)/', '<a href="/@$1">@$1</a>', $products);
+        $products = preg_replace('/#([\w-]+)/', '<a href="/product/$1">#$1</a>', $task);
+        $users = preg_replace('/@([\w-]+)/', '<a href="/@$1">@$1</a>', $products);
         if (preg_match($reg_exUrl, $users, $url)) {
             $truncate = strlen($url[0]) > 30 ? substr($url[0], 0, 30).'...' : $url[0];
 
