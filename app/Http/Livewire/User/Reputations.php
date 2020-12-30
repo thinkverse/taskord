b6b4da48d1495c73ef7caf549716a12359e62efd
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-30 11:24:52 +0530

    Add Reps LW

---
+++ app/Http/Livewire/User/Reputations.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class Reputations extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.reputations');
+    }
+}
