9e997982f3028ebafb2e3f1325dbba9c2c60ddc3
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-30 13:49:31 +0530

    Remove notification polling

--- resources/views/livewire/notification/icon.blade.php
+++ resources/views/livewire/notification/icon.blade.php
@@ -1,4 +1,4 @@
-<li wire:poll.5s class="nav-item me-2">
+<li class="nav-item me-2">
     <a class="nav-link text-white" href="{{ route('notifications.unread') }}" aria-label="Notifications">
         <x-heroicon-o-bell class="heroicon-notification me-0" />
         @auth
