404bcf528cfe54ec3be28d9fe76f0d4b872604f5
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-30 15:47:15 +0530

    Rollback

--- app/Http/Livewire/User/Followers.php
+++ app/Http/Livewire/User/Followers.php
@@ -4,9 +4,12 @@ namespace App\Http\Livewire\User;
 
 use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Followers extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
     public User $user;
     public $readyToLoad = false;
 
