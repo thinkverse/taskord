fab6e50644c4f2e9363c68dc59179431f858a9b4
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-30 16:03:50 +0530

    Fix borders on product tasks while paginated

--- resources/views/livewire/product/tasks.blade.php
+++ resources/views/livewire/product/tasks.blade.php
@@ -18,7 +18,7 @@
     <div class="card-body text-center mt-3 mb-3">
         <x-heroicon-o-check-circle class="heroicon-4x text-primary mb-2" />
         <div class="h4">
-            No tasks made!
+            {{ $message }}
         </div>
     </div>
     @endif
@@ -26,7 +26,7 @@
     <ul class="list-group">
     @endif
     @foreach ($tasks as $task)
-    <li class="list-group-item p-3">
+    <li class="list-group-item p-3 {{($loop->index == 0 and $page > 1) ? "border-top-0 rounded-0" : ""}}">
         @livewire('task.single-task', [
             'task' => $task
         ], key($task->id))
