608f2a4fc1e8efa95c33b9abf31609e0c35542f3
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-30 15:27:33 +0530

    Add subscribers to LW

--- app/Http/Livewire/Product/Subscribers.php
+++ app/Http/Livewire/Product/Subscribers.php
@@ -8,9 +8,24 @@ use Livewire\Component;
 class Subscribers extends Component
 {
     public Product $product;
+    public $readyToLoad = false;
+
+    public function loadSubscribers()
+    {
+        $this->readyToLoad = true;
+    }
 
     public function mount($product)
     {
         $this->product = $product;
+    }
+
+    public function render()
+    {
+        $subscribers = $this->product->subscribers;
+
+        return view('livewire.product.subscribers', [
+            'subscribers' => $this->readyToLoad ? $subscribers : [],
+        ]);
     }
 }
