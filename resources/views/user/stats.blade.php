067105a1cb9a358d6178249a408bb12052bd2a63
Yogi <6395788-yo@users.noreply.gitlab.com>
2020-12-31 15:43:41 +0530

    Add Stats Route and file

---
+++ resources/views/user/stats.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@php
+if ($user->lastname and $user->lastname) {
+    $name = '('.$user->firstname.' '.$user->lastname.')';
+} else if ($user->firstname) {
+    $name = '('.$user->firstname.')';
+} else {
+    $name = '';
+}
+@endphp
+
+@section('pageTitle', $user->username.' '.$name.' / Stats ·')
+@section('title', $user->username.' '.$name.' / Stats ·')
+@section('title', $user->username.' ('.$name.') ·')
+@section('description', $user->bio)
+@section('image', $user->avatar)
+@section('url', url()->current())
+
+@section('content')
+<div class="container-md">
+    @include('user.profile')
+    <div class="row justify-content-center mt-4">
+        <div class="col-lg-8">
+
+        </div>
+        @include('user.sidebar')
+    </div>
+</div>
+@endsection
