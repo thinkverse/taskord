eab6b5b0b7f368d57da694192456bb506a14db56
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-01 09:45:20 +0530

    Add Staff Ship Badge

--- resources/views/components/beta.blade.php
+++ resources/views/components/beta.blade.php
@@ -1,9 +1,9 @@
 @if ($background === 'dark')
-<span class="badge border border-success ms-1 rounded-pill text-white">
+<span class="badge border border-success ms-1 rounded-pill text-white" title="Feature Release Label: Beta">
     Beta
 </span>
 @else
-<span class="badge border border-success ms-1 rounded-pill text-dark">
+<span class="badge border border-success ms-1 rounded-pill text-dark" title="Feature Release Label: Beta">
     Beta
 </span>
 @endif
---
+++ resources/views/components/staffship.blade.php
@@ -0,0 +1,9 @@
+@if ($background === 'dark')
+<span class="badge border border-danger ms-1 rounded-pill text-white" title="Feature Release Label: Staff Ship">
+    Staff Ship
+</span>
+@else
+<span class="badge border border-danger ms-1 rounded-pill text-dark" title="Feature Release Label: Staff Ship">
+    Staff Ship
+</span>
+@endif
