97d2bf19efac809449cd8662361f56c41a94f36d
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-01 20:03:53 +0530

    Update footer

--- resources/views/components/bottom-footer.blade.php
+++ resources/views/components/bottom-footer.blade.php
@@ -1,6 +1,6 @@
-<div class="d-flex lh-lg mt-5 text-secondary justify-content-center">
-    <span>
-        <span class="pe-2 fw-bold">
+<div class="d-flex lh-lg mt-5 text-secondary justify-content-center align-items-center">
+    <div class="d-flex justify-content-evenly flex-grow-1">
+        <span class="fw-bold">
             © Taskord
         </span>
         <a href="{{ route('about') }}">
@@ -12,28 +12,34 @@
         <a href="https://dev.to/taskord" target="_blank" rel="noreferrer">
             Blog
         </a>
+        <a href="/graphiql" target="_blank" rel="noreferrer">
+            API
+        </a>
+        <a href="{{ route('contact') }}">
+            Contact
+        </a>
+    </div>
+    <div class="mx-3">
+        <img loading=lazy src="/images/pride.svg" height="30" alt="Taskord Beta">
+    </div>
+    <div class="d-flex justify-content-evenly flex-grow-1">
+        <a href="{{ route('terms') }}">
+            Terms
+        </a>
+        <a href="{{ route('privacy') }}">
+            Privacy
+        </a>
+        <a href="{{ route('security') }}">
+            Security
+        </a>
+        <a href="{{ route('sponsors') }}">
+            Sponsors
+        </a>
+        <a href="/graphiql" target="_blank" rel="noreferrer">
+            API
+        </a>
         <a href="https://gitlab.com/taskord/taskord" target="_blank" rel="noreferrer">
             GitLab
         </a>
-    </span>
-    <span class="mx-5">
-        <img loading=lazy src="/images/pride.svg" height="30" alt="Taskord Beta">
-    </span>
-    <span>
-        <a class="pe-2" href="{{ route('terms') }}">
-            Terms
-        </a>
-        <a class="pe-2" href="{{ route('sponsors') }}">
-            Sponsors
-        </a>
-        <a class="pe-2" href="{{ route('contact') }}">
-            Contact
-        </a>
-        <a class="pe-2" href="{{ route('contact') }}">
-            Contact
-        </a>
-        <a class="pe-2" href="{{ route('contact') }}">
-            Contact
-        </a>
-    </span>
+    </div>
 </div>
