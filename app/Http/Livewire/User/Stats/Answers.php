36e195db10b51212545ac4b596466413b8617788
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-01 08:53:54 +0530

    Add more stats LW

---
+++ app/Http/Livewire/User/Stats/Answers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User\Stats;
+
+use Livewire\Component;
+
+class Answers extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.stats.answers');
+    }
+}
---
+++ app/Http/Livewire/User/Stats/Comments.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User\Stats;
+
+use Livewire\Component;
+
+class Comments extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.stats.comments');
+    }
+}
