28e45a4b5baa04d2fa7b0fef55553d3c9d2b9159
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-01 10:31:42 +0530

    Fix CreateNewTask import issue in /tasks page

--- app/Http/Livewire/Tasks/CreateTask.php
+++ app/Http/Livewire/Tasks/CreateTask.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Str;
 use Intervention\Image\Facades\Image;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use App\Actions\CreateNewTask;
 
 class CreateTask extends Component
 {
