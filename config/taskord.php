89900257582068eb9f5b0265177f90cf3ab27d0a
thinkverse <hallberg.kim@gmail.com>
2021-01-02 00:08:44 +0100

    Move task templates into taskord config

--- config/taskord.php
+++ config/taskord.php
@@ -16,4 +16,12 @@ return [
         'version_key' => env('APP_VERSION_KEY'),
     ],
 
+    'tasks' => [
+        'templates' => [
+            'I just launched a new product! Check out #%s',
+            'Check  out #%s, I just lauched it today! 🚀',
+            'Welcome into the world #%s!',
+        ]
+    ]
+
 ];
