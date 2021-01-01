0e42a03a716d7b7cdec718fb737827658186bcbe
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-01 17:32:32 +0530

    Refactor Test

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,7 +2,13 @@
 
 use function Tests\actingAs;
 
-it('has home page - response test', function () {
-    $this->get('/')->assertStatus(200);
-    actingAs(1)->get('/')->assertStatus(200);
-});
+it('has home page', function ($url, $expected, $auth) {
+    if ($auth) {
+        $this->get($url)->assertStatus($expected);
+    } else {
+        actingAs(1)->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/', 200, false],
+    ['/', 200, true],
+]);
