3f5cb564b8c63d2adfbafcb0ab606ed157f97b70
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-01 09:50:10 +0530

    Update badge style

--- resources/views/components/staffship.blade.php
+++ resources/views/components/staffship.blade.php
@@ -1,9 +1,9 @@
 @if ($background === 'dark')
-<span class="badge border border-danger ms-1 rounded-pill text-white" title="Feature Release Label: Staff Ship">
+<span class="pe-2 px-2 align-text-top badge border border-danger ms-1 rounded-pill text-white" title="Feature Release Label: Staff Ship">
     Staff Ship
 </span>
 @else
-<span class="badge border border-danger ms-1 rounded-pill text-dark" title="Feature Release Label: Staff Ship">
+<span class="pe-2 px-2 align-text-top badge border border-danger ms-1 rounded-pill text-dark" title="Feature Release Label: Staff Ship">
     Staff Ship
 </span>
 @endif
