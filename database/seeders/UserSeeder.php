3b22af2cebe1c78c8d9b56d53d5d9693a1e071aa
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-02 17:48:47 +0530

    Add user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,10 @@
 
 namespace Database\Seeders;
 
-use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class UserSeeder extends Seeder
 {
@@ -14,32 +14,8 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     {
-        foreach (range(1, 50) as $index) {
-            DB::table('users')->insert([
-                'firstname' => $faker->firstName,
-                'lastname' => $faker->lastName,
-                'username' => str_replace('.', '', $faker->unique()->userName),
-                'company' =>  $faker->firstName,
-                'bio' => $faker->sentence($nbWords = 6, $variableNbWords = true),
-                'email' => str_replace('.', '', $faker->unique()->userName).'@yogi.codes',
-                'avatar' => 'https://avatar.tobi.sh/'.$faker->userName.'.svg?text='.strtoupper($faker->randomLetter).strtoupper($faker->randomLetter),
-                'password' => Hash::make('test'),
-                'reputation' => $faker->numberBetween($min = 50, $max = 150),
-                'website' => 'https://example.com',
-                'twitter' => str_replace('.', '', $faker->userName),
-                'twitch' => str_replace('.', '', $faker->userName),
-                'github' => str_replace('.', '', $faker->userName),
-                'telegram' => str_replace('.', '', $faker->userName),
-                'youtube' => str_replace('.', '', $faker->userName),
-                'isStaff' => false,
-                'isDeveloper' => false,
-                'isBeta' => false,
-                'created_at' => $faker->dateTimeBetween($startDate = '-10 days', $endDate = 'now'),
-                'updated_at' => $faker->dateTimeBetween($startDate = '-10 days', $endDate = 'now'),
-                'email_verified_at' => date('Y-m-d H:i:s'),
-            ]);
-        }
+        User::factory()->count(50)->create();
     }
 }
