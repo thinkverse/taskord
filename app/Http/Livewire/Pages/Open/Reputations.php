7036312e01f2213b777379b25979b9063bc5b0d9
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-02 13:51:47 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Livewire/Pages/Open/Reputations.php
+++ app/Http/Livewire/Pages/Open/Reputations.php
@@ -4,8 +4,8 @@ namespace App\Http\Livewire\Pages\Open;
 
 use App\Models\Task;
 use Carbon\CarbonPeriod;
-use Livewire\Component;
 use Illuminate\Support\Facades\DB;
+use Livewire\Component;
 
 class Reputations extends Component
 {
