51fc8fd131ceea385761583ed964041186e71f03
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-02 12:48:21 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/seeders/MeetupSeeder.php
+++ database/seeders/MeetupSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 use App\Models\Meetup;
+use Illuminate\Database\Seeder;
 
 class MeetupSeeder extends Seeder
 {
