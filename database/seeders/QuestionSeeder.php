aa35c66a8bb79befb27944c0ae6d832fbd4d8b02
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-03 11:37:40 +0530

    Update QuestionSeeder.php

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -9,13 +9,22 @@ class QuestionSeeder extends Seeder
 {
     public function run()
     {
+        // Public Question
         Question::factory()->create([
             'user_id' => 1,
-            'title' => "Hello, World!",
+            'title' => "Hello, World this is public question!",
             'body' => "Hello, World!",
             'patronOnly' => false,
         ]);
 
-        Question::factory()->count(99)->create();
+        // Patron Only Question
+        Question::factory()->create([
+            'user_id' => 1,
+            'title' => "Hello, World this is patron only question!",
+            'body' => "Hello, World!",
+            'patronOnly' => true,
+        ]);
+
+        Question::factory()->count(98)->create();
     }
 }
