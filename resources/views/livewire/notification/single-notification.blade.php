feeff868567ba0a99dc8ac4e51a43c6982d5e1e9
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-03 10:07:50 +0530

    Able to mark single notification as read and Closes #172

--- resources/views/livewire/notification/single-notification.blade.php
+++ resources/views/livewire/notification/single-notification.blade.php
@@ -228,7 +228,10 @@
             @if ($page_type === 'unread')
             <div>
                 <button wire:click="markSingleNotificationAsRead" class="btn btn-sm btn-task ps-5">
-                    <x-heroicon-o-check class="heroicon-2x text-secondary me-0" />
+                    <span wire:loading.remove>
+                        <x-heroicon-o-check class="heroicon-2x text-secondary me-0" />
+                    </span>
+                    <span wire:loading class="spinner-border spinner-border-sm"></span>
                 </button>
             </div>
             @endif
