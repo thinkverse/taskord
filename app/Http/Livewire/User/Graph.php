d3f51cffb96c60dac34e60072ff49835232bb8f7
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-03 11:40:50 +0530

    Add user graph

---
+++ app/Http/Livewire/User/Graph.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class Graph extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.graph');
+    }
+}
