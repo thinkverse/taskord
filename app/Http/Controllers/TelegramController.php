9199a25de622ea9435e5e7d594e05306f6f896e3
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-05 13:18:02 +0000

    Add start message

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -181,7 +181,22 @@ class TelegramController extends Controller
 
     public function start($chat_id)
     {
-        return $this->send($chat_id, '🚪 *Logout successful*');
+        $res = "*Hi 👋, I'm Taskord Bot, I can help you stay productive without leaving your chat application.*\n\n"
+               ."You can use these commands\n\n"
+               ."*New Task*\n\n"
+               ."/todo `<task>` - Create a new pending task\n"
+               ."/done `<task>`- Create a new completed task\n\n"
+               ."*Task Status*\n\n"
+               ."/complete `<task id>` - Complete a pending task\n"
+               ."/uncomplete `<task id>`- Uncomplete a completed task\n\n"
+               ."*Profile*\n\n"
+               ."/pending - See all pending tasks\n\n"
+               ."*Account*\n\n"
+               ."/auth `<API token>`- Connect Taskord account with Telegram\n"
+               ."/logout - Disconnect Taskord account from Telegram\n\n"
+               ."*Others*\n\n"
+               ."/start - See this message again anytime\n";
+        return $this->send($chat_id, $res);
     }
 
     public function logout($chat_id)
