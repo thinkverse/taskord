19256490cf05e9dd46bd2fa250803154f48e2952
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-05 07:39:48 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\CreateNewTask;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Telegram;
-use App\Actions\CreateNewTask;
 
 class TelegramController extends Controller
 {
@@ -18,7 +18,7 @@ class TelegramController extends Controller
         if (Str::of($message)->startsWith('/pair')) {
             $token = substr($message, strpos($message, '/pair') + 6);
             $this->authUser($token, $chat_id);
-        } else if (Str::of($message)->startsWith('/task')) {
+        } elseif (Str::of($message)->startsWith('/task')) {
             $task = substr($message, strpos($message, '/todo') + 6);
             $this->createTask($task, $chat_id);
         } else {
@@ -54,13 +54,14 @@ class TelegramController extends Controller
         } else {
             $user->telegram_chat_id = $chat_id;
             $user->save();
+
             return Telegram::sendMessage([
                 'chat_id' => $user->telegram_chat_id,
                 'text' => 'Authentication successful!',
             ]);
         }
     }
-    
+
     public function createTask($todo, $chat_id)
     {
         if (strlen($todo) < 6) {
@@ -69,25 +70,26 @@ class TelegramController extends Controller
                 'text' => 'Task should have at least 5 characters!',
             ]);
         }
-        
+
         if ($this->authCheck($chat_id)) {
             $user = User::where('telegram_chat_id', $chat_id)->first();
-            
+
             $task = (new CreateNewTask($user, [
                 'task' => $todo,
                 'done' => false,
                 'type' => 'user',
                 'source' => 'Telegram',
             ]))();
-            
+
             return Telegram::sendMessage([
                 'chat_id' => $user->telegram_chat_id,
                 'text' => 'Task has been Created - #'.$task->id,
             ]);
         }
     }
-    
-    public function authCheck($chat_id) {
+
+    public function authCheck($chat_id)
+    {
         $user = User::where('telegram_chat_id', $chat_id)->first();
         if (! $user->telegram_chat_id) {
             return Telegram::sendMessage([
