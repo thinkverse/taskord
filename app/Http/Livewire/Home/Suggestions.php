67942fa08a67fd2f453ceefd67ebd4899028b174
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-08 11:45:54 +0000

    Add who to follow card

--- app/Http/Livewire/Home/Suggestions.php
+++ app/Http/Livewire/Home/Suggestions.php
@@ -3,11 +3,37 @@
 namespace App\Http\Livewire\Home;
 
 use Livewire\Component;
+use App\Models\User;
 
 class Suggestions extends Component
 {
+    public $readyToLoad = false;
+    public User $user;
+    
+    public function mount($user)
+    {
+        $this->user = $user;
+    }
+
+    public function loadSuggestions()
+    {
+        $this->readyToLoad = true;
+    }
+
     public function render()
     {
-        return view('livewire.home.suggestions');
+        $users  = User::cacheFor(60 * 60)
+            ->select('id', 'username', 'firstname', 'lastname', 'avatar', 'reputation', 'isVerified')
+            ->where([
+                ['isFlagged', false],
+                ['id', '!=', 1],
+            ])
+            ->orderBy('reputation', 'DESC')
+            ->take(10)
+            ->get();
+            
+        return view('livewire.home.suggestions', [
+            'users' => $this->readyToLoad ? $users : [],
+        ]);
     }
 }
