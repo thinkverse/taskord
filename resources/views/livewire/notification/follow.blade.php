6e9764aa974731384aaf7169d61db884759dbe9b
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-08 12:36:34 +0000

    Add Spinner while follow

--- resources/views/livewire/notification/follow.blade.php
+++ resources/views/livewire/notification/follow.blade.php
@@ -1,12 +1,14 @@
 <div>
     @if (auth()->user()->isFollowing($user))
     <button wire:click="followUser" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
-        <x-heroicon-o-user-remove class="heroicon" />
+        <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
+        <x-heroicon-o-user-remove wire:loading.remove class="heroicon" />
         Unfollow <span class="fw-bold">{{ '@'.$user->username }}</span>
     </button>
     @else
     <button wire:click="followUser" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
-        <x-heroicon-o-user-add class="heroicon" />
+        <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
+        <x-heroicon-o-user-add wire:loading.remove class="heroicon" />
         Follow back <span class="fw-bold">{{ '@'.$user->username }}</span>
     </button>
     @endif
