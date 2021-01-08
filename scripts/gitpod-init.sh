5b963a9c6c753f8c06f0c81a004e856b355822d2
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-08 14:35:32 +0000

    Update gitpod config

--- scripts/gitpod-init.sh
+++ scripts/gitpod-init.sh
@@ -5,9 +5,7 @@ cp .env.gitpod .env
 sed -i "s|APP_URL=|APP_URL=${GITPOD_WORKSPACE_URL}|g" .env
 sed -i "s|APP_URL=https://|APP_URL=https://8000-|g" .env
 composer install
-yarn install
 php artisan key:generate
 php artisan horizon:install
 php artisan horizon:publish
 php artisan migrate --seed --force
-yarn development
