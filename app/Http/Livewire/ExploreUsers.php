d5ed6e31ec027f71302b0e796d5c0643ea1c1f5c
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-08 11:25:32 +0000

    Add Explore Users

---
+++ app/Http/Livewire/ExploreUsers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class ExploreUsers extends Component
+{
+    public function render()
+    {
+        return view('livewire.explore-users');
+    }
+}
