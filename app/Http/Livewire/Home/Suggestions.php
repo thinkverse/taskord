05f53fa313b343691984d967821aec3620da3d83
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-08 11:40:48 +0000

    Add suggestions

---
+++ app/Http/Livewire/Home/Suggestions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Home;
+
+use Livewire\Component;
+
+class Suggestions extends Component
+{
+    public function render()
+    {
+        return view('livewire.home.suggestions');
+    }
+}
