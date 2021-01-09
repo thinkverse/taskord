b4f2dba33f3e45babcc8585c6403cdfef7d851e0
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-09 15:48:01 +0000

    Remove ul and add div

--- resources/views/site/cache.blade.php
+++ resources/views/site/cache.blade.php
@@ -1,7 +1,7 @@
-<ul class="d-inline mb-0 text-white font-monospace">
+<div class="mb-0 text-white font-monospace">
     @foreach($cache as $key)
         <div class="mb-2 bg-dark px-3 py-2">
             {{ $key }}
         </div>
     @endforeach
-</ul>
+</div>
