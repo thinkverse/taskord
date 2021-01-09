ddd7fe0a1941856c601b6623fbd6fca7167f76e0
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-09 06:56:21 +0000

    Separate nav file

--- resources/views/explore/explore.blade.php
+++ resources/views/explore/explore.blade.php
@@ -7,20 +7,7 @@
 @section('url', url()->current())
 
 @section('content')
-<ul class="nav nav-pills justify-content-center explore-nav bg-white py-3">
-    <li class="nav-item">
-        <a class="nav-link active" href="{{ route('explore') }}">Explore</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Makers</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Products</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="#">Tasks</a>
-    </li>
-</ul>
+@include('explore.nav')
 <div class="container-md">
     <div class="row justify-content-center">
         <div class="col-sm explore-user-card">
---
+++ resources/views/explore/nav.blade.php
@@ -0,0 +1,14 @@
+<ul class="nav nav-pills justify-content-center explore-nav bg-white py-3">
+    <li class="nav-item">
+        <a class="nav-link active" href="{{ route('explore') }}">Popular Tasks</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Makers</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Products</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">Tasks</a>
+    </li>
+</ul>
