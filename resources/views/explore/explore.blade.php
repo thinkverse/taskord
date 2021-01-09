1d717074ae606f536c957f5b205d058e1cedb8cf
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-09 03:19:54 +0000

    Add page

--- resources/views/explore/explore.blade.php
+++ resources/views/explore/explore.blade.php
@@ -1,1 +1,17 @@
-hi
+@extends('layouts.app')
+
+@section('pageTitle', 'Explore ·')
+@section('title', 'Explore ·')
+@section('description', 'Get things done socially with Taskord.')
+@section('image', 'https://ik.imagekit.io/taskordimg/cover_i8r6XmiSW.png')
+@section('url', url()->current())
+
+@section('content')
+<div class="container-md">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <x-bottom-footer />
+        </div>
+    </div>
+</div>
+@endsection
