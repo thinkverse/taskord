c9b1b7f2d6366f841bb9bed61e9f060576ee320a
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-09 06:58:40 +0000

    Add popular tasks livewire

---
+++ app/Http/Livewire/Explore/PopularTasks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Explore;
+
+use Livewire\Component;
+
+class PopularTasks extends Component
+{
+    public function render()
+    {
+        return view('livewire.explore.popular-tasks');
+    }
+}
