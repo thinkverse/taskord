ae4fccd3187f455cdb8536a5edcfe7e56ee44343
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-09 07:05:43 +0000

    Add loader

--- resources/views/livewire/explore/popular-tasks.blade.php
+++ resources/views/livewire/explore/popular-tasks.blade.php
@@ -2,9 +2,12 @@
     <div class="pb-2 h5 text-secondary">
         Recent popular tasks
     </div>
-    <div class="card">
-        <div class="card-body">
-            
+    @if (!$readyToLoad)
+    <div class="card-body text-center mt-3 mb-3">
+        <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+        <div class="h6">
+            Loading popular tasks...
         </div>
     </div>
+    @endif
 </div>
