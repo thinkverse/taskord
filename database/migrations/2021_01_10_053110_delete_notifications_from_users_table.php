c67388efbbdfbc876527c8f4b5c3a277cf5364f6
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-10 05:32:54 +0000

    Add delete notification migration

---
+++ database/migrations/2021_01_10_053110_delete_notifications_from_users_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DeleteNotificationsFromUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('taskMentionedEmail');
+            $table->dropColumn('taskMentionedWeb');
+            $table->dropColumn('taskPraisedEmail');
+            $table->dropColumn('taskPraisedWeb');
+            $table->dropColumn('commentPraisedEmail');
+            $table->dropColumn('commentPraisedWeb');
+            $table->dropColumn('questionPraisedEmail');
+            $table->dropColumn('questionPraisedWeb');
+            $table->dropColumn('answerPraisedEmail');
+            $table->dropColumn('answerPraisedWeb');
+            $table->dropColumn('commentAddedEmail');
+            $table->dropColumn('commentAddedWeb');
+            $table->dropColumn('answerAddedEmail');
+            $table->dropColumn('answerAddedWeb');
+            $table->dropColumn('userFollowedEmail');
+            $table->dropColumn('userFollowedWeb');
+            $table->dropColumn('productSubscribedWeb');
+            $table->dropColumn('productSubscribedEmail');
+            $table->dropColumn('productUpdatesWeb');
+            $table->dropColumn('productUpdatesEmail');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
