e9cfdb25ea79dbfdad959bf3c61cf677c38ea4bb
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-01-10 06:46:11 +0000

    Add Trending makers LW

---
+++ app/Http/Livewire/Explore/TrendingMakers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Explore;
+
+use Livewire\Component;
+
+class TrendingMakers extends Component
+{
+    public function render()
+    {
+        return view('livewire.explore.trending-makers');
+    }
+}
