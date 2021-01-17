c2fe1bbe175959c9f3663e3b005e3d0bb03ba814
thinkverse <hallberg.kim@gmail.com>
2021-01-17 08:11:04 +0100

    Create and add settings/products livewire component

---
+++ resources/views/livewire/user/settings/products.blade.php
@@ -0,0 +1,11 @@
+<div class="col-lg-8">
+    <div class="card mb-4">
+        <div class="card-header pt-3 pb-3">
+            <span class="h5">Products</span>
+            <div>Update your associated products.</div>
+        </div>
+        <div class="card-body">
+            Find out more about your associated products.
+        </div>
+    </div>
+</div>
--- resources/views/user/settings/products.blade.php
+++ resources/views/user/settings/products.blade.php
@@ -6,17 +6,9 @@
 <div class="container-md">
     <div class="row justify-content-center mt-4">
         @include('user.settings.sidebar')
-        <div class="col-lg-8">
-            <div class="card mb-4">
-                <div class="card-header pt-3 pb-3">
-                    <span class="h5">Products</span>
-                    <div>Update your associated products.</div>
-                </div>
-                <div class="card-body">
-                    Find out more about your associated products.
-                </div>
-            </div>
-        </div>
+        @livewire('user.settings.products', [
+            'user' => $user
+        ])
     </div>
 </div>
 @endsection
