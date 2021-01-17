c66eb8262315824355ffe78f66daf6a870e59739
thinkverse <hallberg.kim@gmail.com>
2021-01-17 08:03:42 +0100

    Update settings sidebar and Add link to settings/products

--- resources/views/user/settings/sidebar.blade.php
+++ resources/views/user/settings/sidebar.blade.php
@@ -16,6 +16,13 @@
                 Account
             </a>
             <a
+                class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'user.settings.products') active text-white @endif"
+                href="{{ route('user.settings.products') }}"
+            >
+                <x-heroicon-o-office-building class="heroicon" />
+                Products
+            </a>
+            <a
                 class="list-group-item text-dark pt-2 pb-2 @if (Route::currentRouteName() === 'user.settings.patron') active text-white @endif"
                 href="{{ route('user.settings.patron') }}"
             >
