12623b885d1784e56b4e3ab14d2c4a8548c9c6ce
Yogi <yoginth@hey.com>
2021-03-23 13:02:39 +0000

    Add laravel-acquaintances migration

---
+++ database/migrations/2021_03_23_122213_update_timestamps_in_interactions.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateTimestampsInInteractions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('interactions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('interactions', function (Blueprint $table) {
+            //
+        });
+    }
+}
