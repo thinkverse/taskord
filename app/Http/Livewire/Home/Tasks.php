baaa7fc4ef1d87cddc936c42104c8f09827479cb
Yogi <yoginth@hey.com>
2021-03-24 16:28:49 +0000

    Fix styleci

--- app/Http/Livewire/Home/Tasks.php
+++ app/Http/Livewire/Home/Tasks.php
@@ -33,6 +33,7 @@ class Tasks extends Component
         if (Auth::check() && auth()->user()->onlyFollowingsTasks) {
             $userIds = auth()->user()->followings->pluck('id');
             $userIds->push(auth()->user()->id);
+
             return Task::select('id', 'task', 'done', 'type', 'done_at', 'user_id', 'product_id', 'source', 'images', 'hidden')
                 ->whereIn('user_id', $userIds)
                 ->whereHas('user', function ($q) {
