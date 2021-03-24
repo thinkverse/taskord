4ccd54c3259082dd134c57a1d2f58757c4737744
Yogi <yoginth@hey.com>
2021-03-24 12:23:21 +0000

    Add getTodayTasks function

--- app/Http/Livewire/Tasks/AllTime.php
+++ app/Http/Livewire/Tasks/AllTime.php
@@ -19,16 +19,19 @@ class AllTime extends Component
         $this->readyToLoad = true;
     }
 
-    public function render()
+    public function getAllTimeTasks()
     {
-        $tasks = Task::select('id', 'task', 'done', 'images', 'user_id', 'created_at', 'due_at', 'type', 'product_id')
+        return Task::select('id', 'task', 'done', 'images', 'user_id', 'created_at', 'due_at', 'type', 'product_id')
             ->where('user_id', auth()->user()->id)
             ->where('done', false)
             ->latest('due_at')
             ->get();
+    }
 
+    public function render()
+    {
         return view('livewire.tasks.all-time', [
-            'tasks' => $this->readyToLoad ? $tasks : [],
+            'tasks' => $this->readyToLoad ? $this->getAllTimeTasks() : [],
         ]);
     }
 }
--- app/Http/Livewire/Tasks/Today.php
+++ app/Http/Livewire/Tasks/Today.php
@@ -19,17 +19,20 @@ class Today extends Component
         $this->readyToLoad = true;
     }
 
-    public function render()
+    public function getTodayTasks()
     {
-        $tasks = Task::select('id', 'task', 'done', 'images', 'user_id', 'created_at', 'due_at', 'type', 'product_id')
+        return Task::select('id', 'task', 'done', 'images', 'user_id', 'created_at', 'due_at', 'type', 'product_id')
             ->where('user_id', auth()->user()->id)
             ->whereDate('created_at', carbon('today'))
             ->where('done', false)
             ->latest('due_at')
             ->get();
+    }
 
+    public function render()
+    {
         return view('livewire.tasks.today', [
-            'tasks' => $this->readyToLoad ? $tasks : [],
+            'tasks' => $this->readyToLoad ? $this->getTodayTasks() : [],
         ]);
     }
 }
