06fa72ad4e201211e576fe5f9d76d88d18c46c77
Yogi <yoginth@hey.com>
2021-03-25 12:21:54 +0000

    More space

--- resources/views/explore/nav.blade.php
+++ resources/views/explore/nav.blade.php
@@ -1,16 +1,16 @@
 <ul class="nav nav-pills justify-content-center align-items-center explore-nav bg-white py-3">
-    <li class="nav-item">
+    <li class="nav-item px-1">
         <a class="nav-link px-2 py-1 @if (Route::currentRouteName() === 'explore.explore') active @endif" href="{{ route('explore.explore') }}">Popular Tasks</a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item px-1">
         <a class="nav-link px-2 py-1 @if (Route::currentRouteName() === 'explore.makers') active @endif" href="{{ route('explore.makers') }}">Makers <x-beta background="light" /></a>
     </li>
     @auth
     @if (auth()->user()->staffShip)
-    <li class="nav-item">
+    <li class="nav-item px-1">
         <a class="nav-link px-2 py-1" href="#">Products <x-staffship background="light" /></a>
     </li>
-    <li class="nav-item">
+    <li class="nav-item px-1">
         <a class="nav-link px-2 py-1" href="#">Tasks <x-staffship background="light" /></a>
     </li>
     @endif
