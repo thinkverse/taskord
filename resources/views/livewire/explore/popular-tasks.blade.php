96b2e2d8bcb0c3b37974a51f9eadf5f19e36f2dd
Yogi <yoginth@hey.com>
2021-03-25 11:55:58 +0000

    Remove unused tasks warning

--- resources/views/livewire/explore/popular-tasks.blade.php
+++ resources/views/livewire/explore/popular-tasks.blade.php
@@ -7,17 +7,6 @@
         </div>
     </div>
     @endif
-    @if ($readyToLoad and count($tasks) === 0)
-    <div class="card-body text-center mt-3 mb-3">
-        <x-heroicon-o-check-circle class="heroicon-4x text-primary mb-2" />
-        <div class="h4">
-            Follow some users to see tasks!
-        </div>
-        <div class="text-secondary">
-            You can also follow some users displayed in the <b>"Who to follow"</b> in sidebar
-        </div>
-    </div>
-    @endif
     @foreach ($tasks as $task)
     <div class="card mb-3">
         <span class="p-3">
