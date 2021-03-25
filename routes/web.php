9daac80c1a9120befbefff234b817ab519838825
Yogi <yoginth@hey.com>
2021-03-25 07:57:46 +0000

    Add Deployment Data

--- routes/web.php
+++ routes/web.php
@@ -227,6 +227,7 @@ Route::group(['prefix' => 'site'], function () {
     Route::view('shortcuts', 'site.shortcuts')->name('shortcuts')->middleware('auth');
     Route::get('commit-data', [AdminController::class, 'commitData'])->name('commit-data')->middleware('staffship');
     Route::get('ci-data', [AdminController::class, 'ciData'])->name('ci-data')->middleware('staffship');
+    Route::get('deployment-data', [AdminController::class, 'deploymentData'])->name('deployment-data')->middleware('staffship');
     Route::get('cache-hits', [AdminController::class, 'cacheHits'])->name('cache-hits')->middleware('staffship');
 });
 
