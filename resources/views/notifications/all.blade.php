012539799e373efc2904de9e82b3b409c186b6e7
Yogi <yoginth@hey.com>
2021-03-25 11:31:07 +0000

    Fix spacing in notification buttons

--- resources/views/notifications/all.blade.php
+++ resources/views/notifications/all.blade.php
@@ -6,13 +6,13 @@
 <div class="container-md">
     <div class="row justify-content-center">
         <div class="col-lg-8">
-            <a role="button" class="btn btn-primary mb-3" href="{{ route('notifications.unread') }}">
+            <a role="button" class="btn btn-primary mb-3 me-2" href="{{ route('notifications.unread') }}">
                 Unread
                 <span class="ms-1 badge bg-white text-secondary">
                     {{ auth()->user()->unreadNotifications->count('id') }}
                 </span>
             </a>
-            <a role="button" class="btn btn-primary mb-3 me-2" href="{{ route('notifications.all') }}">
+            <a role="button" class="btn btn-primary mb-3" href="{{ route('notifications.all') }}">
                 All
             </a>
             @if (auth()->user()->notifications->count('id') !== 0)
