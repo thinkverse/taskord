3ded48d4830c7be975648b9b8aee3e0bcf63c4cc
Yogi <yoginth@hey.com>
2021-03-25 08:34:37 +0000

    Add more test to SiteTest

--- tests/Feature/SiteTest.php
+++ tests/Feature/SiteTest.php
@@ -12,3 +12,47 @@ it('has keyboard shortcuts skeleton', function ($url, $expected, $auth) {
     ['/site/shortcuts', 302, false],
     ['/site/shortcuts', 200, true],
 ]);
+
+it('has ci data details skeleton', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/site/ci-data', 302, false],
+    ['/site/ci-data', 200, true],
+]);
+
+it('has deployment data details skeleton', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/site/deployment-data', 302, false],
+    ['/site/deployment-data', 200, true],
+]);
+
+it('has commit data details skeleton', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/site/commit-data', 302, false],
+    ['/site/commit-data', 200, true],
+]);
+
+it('has cache hits skeleton', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/site/cache-hits', 302, false],
+    ['/site/cache-hits', 200, true],
+]);
