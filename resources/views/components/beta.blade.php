98024c6645e7da7a8a22d9ddf538c2811f8a6cf7
Yogi <yoginth@hey.com>
2021-03-25 12:07:16 +0000

    Simplify beta and staff badge component

--- resources/views/components/beta.blade.php
+++ resources/views/components/beta.blade.php
@@ -1,13 +1,6 @@
-@php
-    $badge_class = "badge-font border border-success fw-bold ms-1 px-2 rounded-pill small text-white text-capitalize"
-@endphp
-
-@if ($background === 'dark')
-<span class="{{ $badge_class }}" title="Feature Release Label: Beta">
+<span
+    class="badge-font border border-success fw-bold ms-1 px-2 rounded-pill small text-{{ $background === 'dark' ? 'white' : 'dark' }} text-capitalize"
+    title="Feature Release Label: Beta"
+>
     Beta
 </span>
-@else
-<span class="{{ $badge_class }}" title="Feature Release Label: Beta">
-    Beta
-</span>
-@endif
--- resources/views/components/staffship.blade.php
+++ resources/views/components/staffship.blade.php
@@ -1,9 +1,7 @@
-@if ($background === 'dark')
-<span class="badge-font border border-danger fw-bold ms-1 px-2 rounded-pill small text-white" title="Feature Release Label: Staff Ship">
+<span
+    class="badge-font border border-danger fw-bold ms-1 px-2 rounded-pill small text-{{ $background === 'dark' ? 'white' : 'dark' }} text-capitalize"
+    title="Feature Release Label: Staff Ship"
+>
     Staff Ship
 </span>
-@else
-<span class="badge-font border border-danger fw-bold ms-1 px-2 rounded-pill small text-dark" title="Feature Release Label: Staff Ship">
-    Staff Ship
-</span>
-@endif
+
