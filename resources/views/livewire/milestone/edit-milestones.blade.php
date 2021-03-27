8fa09118ade39de1883b51b1824ed19b29328407
Yogi <yoginth@hey.com>
2021-03-27 13:10:57 +0000

    Update edit-milestone

--- resources/views/livewire/milestone/edit-milestones.blade.php
+++ resources/views/livewire/milestone/edit-milestones.blade.php
@@ -1,3 +1,38 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div wire:ignore.self class="modal fade" id="editMilestoneModal" tabindex="-1" role="dialog" aria-labelledby="editMilestoneModalTitle" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Edit Milestone</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form wire:target="submit" wire:submit.prevent="submit">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label class="form-label fw-bold">Name</label>
+                        <input type="text" value="{{ $name }}" class="form-control @error('name') is-invalid @enderror" placeholder="Ask and discuss!" wire:model.defer="name">
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label fw-bold">Description</label>
+                        <textarea class="form-control @error('description') is-invalid @enderror" rows="6" placeholder="What's on your mind?" wire:model.lazy="description">{{ $description }}</textarea>
+                        @error('description')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">
+                        Update
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
