97f130b62f07b8b8d4b9a584997c6748f853f1e0
Yogi <yoginth@hey.com>
2021-03-27 03:59:41 +0000

    Add cache in milestone model

--- app/Models/Milestone.php
+++ app/Models/Milestone.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Milestone extends Model
 {
     use HasFactory;
+    use QueryCacheable;
+
+    public $cacheFor = 3600;
+    public $cacheTags = ['products'];
+    public $cachePrefix = 'products_';
+    protected static $flushCacheOnUpdate = true;
 }
