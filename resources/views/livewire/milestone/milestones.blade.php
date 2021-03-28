3c77ea5bf5660f918f85d29593aa211f5be3f740
Yogi <yoginth@hey.com>
2021-03-28 08:34:34 +0000

    MVP milestones

--- resources/views/livewire/milestone/milestones.blade.php
+++ resources/views/livewire/milestone/milestones.blade.php
@@ -1,3 +1,31 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div wire:init="loadMilestones">
+    @if (!$readyToLoad)
+    <div class="card-body text-center mt-3 mb-3">
+        <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+        <div class="h6">
+            Loading Milestones...
+        </div>
+    </div>
+    @endif
+    @if ($readyToLoad and count($milestones) === 0)
+    <div class="card-body text-center mt-3 mb-3">
+        <x-heroicon-o-question-mark-circle class="heroicon-4x text-primary mb-2" />
+        <div class="h4">
+            No milestones asked
+        </div>
+    </div>
+    @endif
+    @foreach ($milestones as $milestone)
+        @livewire('milestone.single-milestone', [
+            'type' => $type,
+            'milestone' => $milestone,
+        ], key($milestone->id))
+    @endforeach
+    @if ($readyToLoad and $milestones->hasMorePages())
+        @livewire('milestone.load-more', [
+            'type' => $type,
+            'page' => $page,
+            'perPage' => $perPage
+        ])
+    @endif
 </div>
