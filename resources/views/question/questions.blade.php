df0a6095540f1a36daaabb5176d8cb1af81df16b
Yogi <yoginth@hey.com>
2021-04-02 12:34:08 +0000

    Simplify questions page

--- resources/views/question/questions.blade.php
+++ resources/views/question/questions.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 
+@if ($type === 'questions.newest')
 @section('pageTitle', 'Questions / Newest ·')
+@elseif ($type === 'questions.unanswered')
+@section('pageTitle', 'Questions / Unanswered ·')
+@else
+@section('pageTitle', 'Questions / Popular ·')
+@endif
 @section('title', 'Questions / Newest ·')
 @section('description', 'Browse questions and discuss, answer, give feedbacks, etc.')
 @section('image', '')
