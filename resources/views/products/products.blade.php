e13b1f9511c9f19cfe2a0c8734dee0f70e9abda6
Yogi <yoginth@hey.com>
2021-04-02 12:42:49 +0000

    Fix conflicts

---
+++ resources/views/products/products.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@if ($type === 'products.newest')
+@section('pageTitle', 'Products / Newest ·')
+@section('title', 'Products / Newest ·')
+@else
+@section('pageTitle', 'Products / Launched ·')
+@section('title', 'Products / Launched ·')
+@endif
+@section('description', 'Public products available in Taskord.')
+@section('image', '')
+@section('url', url()->current())
+
+@section('content')
+<div class="container-md">
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            @include('products.nav')
+            @livewire('products.products', [
+                'type' => 'products.newest',
+                'page' => 1,
+                'perPage' => 1
+            ])
+        </div>
+        <div class="col-sm">
+            @livewire('products.active-products')
+            <x-footer />
+        </div>
+    </div>
+</div>
+@endsection
