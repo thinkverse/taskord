681ef4c4f8cbce16bdb172cabe8108f89308ef47
Yogi <yoginth@hey.com>
2021-04-15 17:34:00 +0000

    Add system tab

--- resources/views/admin/nav.blade.php
+++ resources/views/admin/nav.blade.php
@@ -11,4 +11,7 @@
     <li class="nav-item">
         <a class="nav-link @if (Route::currentRouteName() === 'admin.products') active @endif" href="{{ route('admin.products') }}">Products</a>
     </li>
+    <li class="nav-item">
+        <a class="nav-link" href="{{ route('admin.products') }}">System</a>
+    </li>
 </ul>
