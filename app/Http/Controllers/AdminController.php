1ba969d0b69579b94786a16aaab48cb5d3a69c4f
Yogi <6395788-yo@users.noreply.gitlab.com>
2021-04-16 11:55:45 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -106,6 +106,7 @@ class AdminController extends Controller
         $contents = file_get_contents('/proc/meminfo');
         preg_match_all('/(\w+):\s+(\d+)\s/', $contents, $matches);
         $meminfo = array_combine($matches[1], $matches[2]);
+        $meminfo = array_combine($matches[1], $matches[2]);
 
         return view('admin.system', [
             'meminfo' => $meminfo,
