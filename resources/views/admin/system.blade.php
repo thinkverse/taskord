0e57b0abaca29fc8f5e9187be48683cb11c4be6f
Yogi <yoginth@hey.com>
2021-04-16 10:11:43 +0000

    Add System Stats page

---
+++ resources/views/admin/system.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Admin - Tasks ·')
+@section('title', 'Admin - Tasks ·')
+@section('description', 'Get things done socially with Taskord.')
+@section('image', '')
+@section('url', url()->current())
+
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="container-md">
+            @include('admin.nav')
+            @livewire('admin.tasks')
+        </div>
+    </div>
+</div>
+@endsection
