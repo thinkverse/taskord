14548e2588f3c4ebdd95463e5fdf569e57596767
Yogi <yoginth@hey.com>
2021-04-17 12:53:24 +0000

    Add stats to livewire

---
+++ resources/views/livewire/admin/stats.blade.php
@@ -0,0 +1,23 @@
+<div class="card" wire:init="loadStats">
+    <div class="card-header h6 pt-3 pb-3">
+        <div class="h5">Stats</div>
+        Taskord Stats
+    </div>
+    <div class="card-body">
+        <div class="row">
+            <div class="col-md-3 mb-4">
+                <div class="card">
+                    <div class="card-header fw-bold d-flex align-items-center">
+                        <x-heroicon-o-database class="heroicon me-1" />
+                        <span>Disk usage</span>
+                    </div>
+                    <div class="card-body">
+                        <div class="h5 mb-0">
+                            WIP
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
