1c8f836eefa230c24cd851098f13b6d5953d276e
Yogi <yoginth@hey.com>
2021-04-23 12:28:13 +0000

    Add click logic

--- app/Http/Livewire/Misc/Report.php
+++ app/Http/Livewire/Misc/Report.php
@@ -3,9 +3,23 @@
 namespace App\Http\Livewire\Misc;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class Report extends Component
 {
+    public $title;
+    public $description;
+    
+    public function createIssue()
+    {
+        if (Auth::check()) {
+            loggy(request()->ip(), 'Issue', auth()->user(), 'Reported a new issue');
+            return $this->alert('success', 'Issue has been reported successfully!');
+        } else {
+            return $this->alert('error', 'Forbidden!');
+        }
+    }
+
     public function render()
     {
         return view('livewire.misc.report');
