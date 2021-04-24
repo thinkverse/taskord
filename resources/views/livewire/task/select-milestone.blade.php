614b58f0ee350f1a200734e2c15229e4e1e66a62
Yogi <yoginth@hey.com>
2021-04-24 18:10:32 +0000

    Add none

--- resources/views/livewire/task/select-milestone.blade.php
+++ resources/views/livewire/task/select-milestone.blade.php
@@ -3,6 +3,11 @@
         <x-heroicon-o-truck class="heroicon-small text-secondary" />
     </a>
     <ul class="dropdown-menu" aria-labelledby="milestoneMenuItem">
+        <li>
+            <a class="dropdown-item cursor-pointer">
+                None
+            </a>
+        </li>
         @foreach ($milestones as $milestone)
             <li wire:key="{{ $task->id }}_{{ $milestone->id }}">
                 <a class="dropdown-item cursor-pointer" wire:click="selectMilestone({{ $milestone }})">
