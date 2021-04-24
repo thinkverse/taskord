36fcea5192c7d9ac08684cbad3feb91d18e632c9
Yogi <yoginth@hey.com>
2021-04-24 11:44:03 +0000

    Add more admin tests

--- tests/Feature/AdminTest.php
+++ tests/Feature/AdminTest.php
@@ -1,5 +1,11 @@
 <?php
 
+it('has stafftools page', function ($url, $expected) {
+    $this->get($url)->assertStatus($expected);
+})->with([
+    ['/stafftools', 302, false],
+]);
+
 it('has stafftools/users page', function ($url, $expected) {
     $this->get($url)->assertStatus($expected);
 })->with([
@@ -22,4 +28,10 @@ it('has stafftools/products page', function ($url, $expected) {
     $this->get($url)->assertStatus($expected);
 })->with([
     ['/stafftools/products', 302, false],
+]);
+
+it('has stafftools/system page', function ($url, $expected) {
+    $this->get($url)->assertStatus($expected);
+})->with([
+    ['/stafftools/system', 302, false],
 ]);
