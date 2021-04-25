c4f4b469e428b5392b35cc212292a278b176536f
Yogi <yoginth@hey.com>
2021-04-25 05:31:45 +0000

    Add milestone/tasks LW

---
+++ app/Http/Livewire/Milestone/Tasks.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Milestone;
+
+use Livewire\Component;
+
+class Tasks extends Component
+{
+    public function render()
+    {
+        return view('livewire.milestone.tasks');
+    }
+}
