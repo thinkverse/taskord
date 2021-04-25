5b1ef0c3b3d9276bbed690ee20fa469c4f7260dd
Yogi <yoginth@hey.com>
2021-04-25 06:44:22 +0000

    Add milestone progress

---
+++ app/Http/Livewire/Milestone/Progress.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Milestone;
+
+use Livewire\Component;
+
+class Progress extends Component
+{
+    public function render()
+    {
+        return view('livewire.milestone.progress');
+    }
+}
