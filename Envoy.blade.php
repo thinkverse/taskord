fa8b60643320923c321052bd4aa6efbc9cf9f336
Yogi <yoginth@hey.com>
2021-04-28 07:43:45 +0000

    Add stories to envoy script

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,16 +1,38 @@
 @servers(['web' => '127.0.0.1'])
 
-@task('deploy', ['on' => 'web'])
+@story('deploy', ['on' => 'web'])
+    pull-from-gitlab
+    install-laravel-dependencies
+    install-yarn-dependencies
+    build-assets
+    clean-application
+@endstory
+
+@task('pull-from-gitlab')
     git pull origin main
+@endtask
+
+@task('install-laravel-dependencies')
     composer install --no-progress
     composer dump-autoload
+@endtask
+
+@task('install-yarn-dependencies')
     yarn install
+@endtask
+
+@task('build-assets')
     yarn production
+@endtask
+
+@task('clean-application')
     php artisan app:clean
 @endtask
+
 @success
-    Taskord has been successfully deployed!
+    echo "Taskord has been successfully deployed!";
 @endsuccess
+
 @error
-    Something went wrong while deploying Taskord!
+    echo "Something went wrong while deploying Taskord!";
 @enderror
