5d4e77e96209aa54e45ef3d4954a226ffb59e473
Yogi <yoginth@hey.com>
2021-04-29 17:06:48 +0530

    Add features page

---
+++ resources/views/admin/features.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Admin - Features ·')
+@section('title', 'Admin - Features ·')
+@section('description', 'Get things done socially with Taskord.')
+@section('image', '')
+@section('url', url()->current())
+
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="container-md">
+            @include('admin.nav')
+        </div>
+    </div>
+</div>
+@endsection
