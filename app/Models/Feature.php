2562445fe372ee6e49407c0d63f2ec80dd3c8273
Yogi <yoginth@hey.com>
2021-04-29 17:08:36 +0530

    Add QueryCacheable

--- app/Models/Feature.php
+++ app/Models/Feature.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Feature extends Model
 {
     use HasFactory;
+    use QueryCacheable;
 }
