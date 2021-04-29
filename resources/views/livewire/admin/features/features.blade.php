48906b3ea940ad9d0886fd286e298a9dd2e09dae
Yogi <yoginth@hey.com>
2021-04-29 17:45:31 +0530

    Add create button

--- resources/views/livewire/admin/features/features.blade.php
+++ resources/views/livewire/admin/features/features.blade.php
@@ -1,7 +1,12 @@
 <div class="card">
-    <div class="card-header h6 pt-3 pb-3">
-        <div class="h5">Features</div>
-        Taskord Feature flags
+    <div class="card-header h6 pt-3 pb-3 d-flex align-items-center justify-content-between">
+        <div>
+            <div class="h5">Features</div>
+            Taskord Feature flags
+        </div>
+        <div>
+            <button class="btn btn-primary">Create Feature flag</button>
+        </div>
     </div>
     <div class="card-body">
         @livewire('admin.features.feature')
