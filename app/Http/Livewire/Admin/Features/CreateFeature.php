b1c4656aa5c4779ba91cd480c868fa6d9469b59d
Yogi <yoginth@hey.com>
2021-04-29 17:11:20 +0530

    Add features livewire

---
+++ app/Http/Livewire/Admin/Features/CreateFeature.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Features;
+
+use Livewire\Component;
+
+class CreateFeature extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.features.create-feature');
+    }
+}
