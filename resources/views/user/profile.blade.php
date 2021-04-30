4f873e5ca65a5515c99400e050ed04632b3c2104
Yogi <yoginth@hey.com>
2021-04-30 10:32:58 +0530

    Remove staffship badge

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -167,7 +167,6 @@
         <a class="text-dark fw-bold me-4" href="{{ route('user.answers', ['username' => $user->username]) }}">
             <span class="@if (Route::currentRouteName() === 'user.answers') text-primary @endif me-1">Milestones</span>
             <span class="small fw-normal text-secondary">{{ number_format($milestone_count) }}</span>
-            <x-staffship background="light" />
         </a>
         @endif
         <a class="text-dark fw-bold me-4" href="{{ route('user.stats', ['username' => $user->username]) }}">
