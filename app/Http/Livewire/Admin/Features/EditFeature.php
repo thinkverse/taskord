6f42dbab8aca43b9821cb469708097468dd87fe3
Yogi <yoginth@hey.com>
2021-04-30 09:43:53 +0530

    Add edit feature livewire

---
+++ app/Http/Livewire/Admin/Features/EditFeature.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Features;
+
+use Livewire\Component;
+
+class EditFeature extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.features.edit-feature');
+    }
+}
