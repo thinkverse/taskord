b5efcc29c8b09cbc4f3650cef20e412124b7eaf5
Yogi <yoginth@hey.com>
2021-05-02 18:40:45 +0530

    Update form-check

--- resources/views/livewire/user/settings/notifications.blade.php
+++ resources/views/livewire/user/settings/notifications.blade.php
@@ -7,13 +7,17 @@
         <div class="card-body">
             <div>
                 <div class="mb-2">Get all notifications via email</div>
-                <input wire:click="notificationsEmail" id="notificationsEmail" class="form-check-input" type="checkbox" {{ $user->notifications_email ? 'checked' : '' }}>
-                <label for="notificationsEmail" class="ms-1">Email</label>
+                <div class="form-check">
+                    <input wire:click="notificationsEmail" id="notificationsEmail" class="form-check-input" type="checkbox" {{ $user->notifications_email ? 'checked' : '' }}>
+                    <label for="notificationsEmail" class="form-check-label">Email</label>
+                </div>
             </div>
             <div>
                 <div class="mb-2 mt-3">Get all notifications via web</div>
-                <input wire:click="notificationsWeb" id="notificationsWeb" class="form-check-input" type="checkbox" {{ $user->notifications_web ? 'checked' : '' }}>
-                <label for="notificationsWeb" class="ms-1">Web</label>
+                <div class="form-check">
+                    <input wire:click="notificationsWeb" id="notificationsWeb" class="form-check-input" type="checkbox" {{ $user->notifications_web ? 'checked' : '' }}>
+                    <label for="notificationsWeb" class="form-check-label">Web</label>
+                </div>
             </div>
         </div>
     </div>
