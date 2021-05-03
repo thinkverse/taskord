10abad8400efc60246a86abbf14d63bb46898285
Yogi <yoginth@hey.com>
2021-05-03 13:33:53 +0530

    Remove Task model in ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -17,7 +17,7 @@ class ProductController extends Controller
         $product = Product::where('slug', $slug)
             ->firstOrFail();
         $type = Route::current()->getName();
-        $tasks = Task::where('product_id', $product->id)
+        $tasks = $product->tasks()
             ->select('created_at')
             ->get()
             ->groupBy(function ($date) {
@@ -42,17 +42,13 @@ class ProductController extends Controller
         $members = $product->members->pluck('id');
         $members->push($product->owner->id);
 
-        $done_count = Task::where([
-            ['product_id', $product->id],
-            ['done', true],
-        ])
+        $done_count = $product->tasks()
+            ->where('done', true)
             ->whereIn('user_id', $members)
             ->count('id');
 
-        $pending_count = Task::where([
-            ['product_id', $product->id],
-            ['done', false],
-        ])
+        $pending_count = $product->tasks()
+            ->where('done', false)
             ->whereIn('user_id', $members)
             ->count('id');
 
