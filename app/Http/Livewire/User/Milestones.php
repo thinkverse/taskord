4eb77155a8816b19b4588f479333f7a8bbf24f4f
Yogi <yoginth@hey.com>
2021-05-03 12:10:15 +0530

    Add milestone lw controller

--- app/Http/Livewire/User/Milestones.php
+++ app/Http/Livewire/User/Milestones.php
@@ -2,12 +2,39 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Milestone;
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Milestones extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+    public User $user;
+    public $readyToLoad = false;
+
+    public function mount($user)
+    {
+        $this->user = $user;
+    }
+
+    public function loadMilestones()
+    {
+        $this->readyToLoad = true;
+    }
+
+    public function getMilestones()
+    {
+        return Milestone::where('user_id', $this->user->id)
+            ->latest()
+            ->paginate(10);
+    }
+
     public function render()
     {
-        return view('livewire.user.milestones');
+        return view('livewire.user.milestones', [
+            'milestones' => $this->readyToLoad ? $this->getMilestones() : [],
+        ]);
     }
 }
