8fb1275f3796791806de6fc80470139197c2b228
Yogi <yoginth@hey.com>
2021-05-03 12:03:05 +0530

    Add user.milestones lw

---
+++ app/Http/Livewire/User/Milestones.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class Milestones extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.milestones');
+    }
+}
---
+++ resources/views/user/milestones.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@php
+if ($user->lastname and $user->lastname) {
+    $name = '('.$user->firstname.' '.$user->lastname.')';
+} else if ($user->firstname) {
+    $name = '('.$user->firstname.')';
+} else {
+    $name = '';
+}
+@endphp
+
+@section('pageTitle', $user->username.' '.$name.' / Milestones ·')
+@section('title', $user->username.' '.$name.' / Milestones ·')
+@section('title', $user->username.' ('.$name.') ·')
+@section('description', $user->bio)
+@section('image', $user->avatar)
+@section('url', url()->current())
+
+@section('content')
+<div class="container-md">
+    @include('user.profile')
+    <div class="row justify-content-center mt-4">
+        <div class="col-lg-8">
+            @livewire('user.questions', [
+                'user' => $user
+            ])
+        </div>
+        @include('user.sidebar')
+    </div>
+</div>
+@endsection
