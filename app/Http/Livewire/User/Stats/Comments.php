52746a736b6dc92d546df5bb5e9e5ef7c9f26143
Yogi <yoginth@hey.com>
2021-05-03 13:44:51 +0530

    Remove Comment model in stats

--- app/Http/Livewire/User/Stats/Comments.php
+++ app/Http/Livewire/User/Stats/Comments.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Livewire\User\Stats;
 
-use App\Models\Comment;
 use Carbon\CarbonPeriod;
 use Livewire\Component;
 
@@ -26,8 +25,8 @@ class Comments extends Component
         $created_at = $this->user->created_at->format('Y-m-d');
         $current_date = carbon()->format('Y-m-d');
         $period = CarbonPeriod::create($created_at, '7 days', $current_date);
-        $comments_count = Comment::select('id')
-            ->where('user_id', $this->user->id)
+        $comments_count = $this->user->comments()
+            ->select('id')
             ->count();
 
         $week_dates = [];
@@ -35,8 +34,8 @@ class Comments extends Component
         $tasks = [];
         foreach ($period->toArray() as $date) {
             array_push($week_dates, carbon($date)->format('d M Y'));
-            $count = Comment::select('id')
-                ->where('user_id', $this->user->id)
+            $count = $this->user->comments()
+                ->select('id')
                 ->whereBetween('created_at', [carbon($date), carbon($date)->addDays(7)])
                 ->count();
             array_push($comments, $count);
