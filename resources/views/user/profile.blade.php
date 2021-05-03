3b631815159f72edc60c40acaec0925d354495fa
Yogi <yoginth@hey.com>
2021-05-03 12:17:48 +0530

    Link to milestones

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -176,8 +176,8 @@
             <span class="small fw-normal text-secondary">{{ number_format($answer_count) }}</span>
         </a>
         @if (feature('milestone_tab'))
-        <a class="text-dark fw-bold me-4" href="{{ route('user.answers', ['username' => $user->username]) }}">
-            <span class="@if (Route::currentRouteName() === 'user.answers') text-primary @endif me-1">Milestones</span>
+        <a class="text-dark fw-bold me-4" href="{{ route('user.milestones', ['username' => $user->username]) }}">
+            <span class="@if (Route::currentRouteName() === 'user.milestones') text-primary @endif me-1">Milestones</span>
             <span class="small fw-normal text-secondary">{{ number_format($milestone_count) }}</span>
         </a>
         @endif
