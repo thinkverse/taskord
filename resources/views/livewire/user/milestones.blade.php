f397d77049b8ec469c5ec98b3dedf81a0ae0aac9
Yogi <yoginth@hey.com>
2021-05-03 12:16:55 +0530

    Add milestone to user profile

--- resources/views/livewire/user/milestones.blade.php
+++ resources/views/livewire/user/milestones.blade.php
@@ -1,3 +1,26 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div wire:init="loadMilestones">
+    @if (!$readyToLoad)
+    <div class="card-body text-center mt-3 mb-3">
+        <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+        <div class="h6">
+            Loading milestones...
+        </div>
+    </div>
+    @endif
+    @if ($readyToLoad and count($milestones) === 0)
+    <div class="card-body text-center mt-3 mb-3">
+        <x-heroicon-o-chat-alt-2 class="heroicon-4x text-primary mb-2" />
+        <div class="h4">
+            No milestones made
+        </div>
+    </div>
+    @endif
+    @foreach ($milestones as $milestone)
+        @livewire('milestone.single-milestone', [
+            'type' => 'milestones.opened',
+            'milestone' => $milestone,
+        ], key($milestone->id))
+    @endforeach
+
+    {{ $readyToLoad ? $milestones->links() : '' }}
 </div>
