071fe507152efa4d6ef45e81466c8e59e0fcb3c0
Yogi <yoginth@hey.com>
2021-05-04 18:28:36 +0530

    Add create product lw

---
+++ resources/views/livewire/products/create-product.blade.php
@@ -0,0 +1,12 @@
+<div>
+    <div class="card mb-4">
+        <div class="card-header h6 pt-3 pb-3">
+            <div class="h5 mb-0">
+                Create new product
+            </div>
+        </div>
+        <div class="card-body">
+            Hi
+        </div>
+    </div>
+</div>
--- resources/views/products/new.blade.php
+++ resources/views/products/new.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.app')
 
-@section('pageTitle', 'Tasks ·')
+@section('pageTitle', 'Products · New')
 
 @section('content')
 <div class="container-md">
     <div class="row justify-content-center">
         <div class="col-lg-8">
             @auth
-                @if (!auth()->user()->isFlagged)
-                Hi
+                @if (auth()->user()->isFlagged)
+                @livewire('products.create-product')
+                @else
+                <div class="text-center">
+                    <div class="alert alert-danger" role="alert">
+                        You can't create new product, because your account has been flagged 😢
+                    </div>
+                    <a class="btn btn-primary" href="{{ route('home') }}">Go to home</a>
+                </div>
                 @endif
             @endauth
         </div>
