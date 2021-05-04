2cb564b6b280d68a749fcd32f729f9d272592448
Yogi <yoginth@hey.com>
2021-05-04 19:40:13 +0530

    Add edit product lw

---
+++ app/Http/Livewire/Product/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Product;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.product.edit');
+    }
+}
