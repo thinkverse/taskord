78156c35b8e7223d8c55e71bd6a7bc12002c86f6
Yogi <yoginth@hey.com>
2021-05-05 12:52:17 +0530

    Remove downtime

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,27 +1,21 @@
 @servers(['web' => '127.0.0.1'])
 
 @story('deploy', ['on' => 'web'])
-    pull-from-gitlab
-    database-migration
+    pull-from-gitlab-and-migrate
     install-laravel-dependencies
     install-yarn-dependencies
     clean-application
     build-assets
 @endstory
 
-@task('pull-from-gitlab')
+@task('pull-from-gitlab-and-migrate')
     echo "Pulling latest changes from GitLab";
     php artisan down
     git pull origin main
-    php artisan up
-@endtask
-
-@task('database-migration')
     echo "Database Migration Started ✅";
-    php artisan down
     php artisan migrate --force
-    php artisan up
     echo "Database Migration Ended ✅";
+    php artisan up
 @endtask
 
 @task('install-laravel-dependencies')
