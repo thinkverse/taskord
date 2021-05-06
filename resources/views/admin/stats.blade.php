1ece3bd1a896588766fe4e8dc858478ca2b34c8f
Yogi <yoginth@hey.com>
2021-05-06 18:29:02 +0530

    Update stats.blade.php

--- resources/views/admin/stats.blade.php
+++ resources/views/admin/stats.blade.php
@@ -11,7 +11,7 @@
     <div class="row justify-content-center">
         <div class="container-md">
             @include('admin.nav')
-            @livewire('admin.stats')
+            <livewire:admin.stats />
         </div>
     </div>
 </div>
