94ddaef63fe62037b8baba02ad14ca5d2a19ec87
Yogi <yoginth@hey.com>
2021-05-07 18:27:47 +0530

    Update nav.blade.php

--- resources/views/explore/nav.blade.php
+++ resources/views/explore/nav.blade.php
@@ -4,7 +4,7 @@
     </li>
     @if (feature('explore_makers'))
     <li class="nav-item px-1">
-        <a class="nav-link @if (Route::currentRouteName() === 'explore.makers') active @endif" href="{{ route('explore.makers') }}">Makers <x-beta /></a>
+        <a class="nav-link @if (Route::currentRouteName() === 'explore.makers') active @endif" href="{{ route('explore.makers') }}">Makers <x-staffship /></a>
     </li>
     @endif
     @if (feature('explore_products'))
