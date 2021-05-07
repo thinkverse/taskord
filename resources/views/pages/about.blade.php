aec678e78300ffae367cf4d641b45839979ee0d7
Yogi <yoginth@hey.com>
2021-05-07 12:09:32 +0530

    :heavy_plus_sign: Add headings

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -20,17 +20,17 @@
             <p class="lead mb-0">When you use a theme created by Start Bootstrap, you know that the theme will look great on any device, whether it's a phone, tablet, or desktop the page will behave responsively!</p>
         </div>
     </div>
-    <div class="row mt-3">
+    <div class="row mt-4">
         <div class="col-lg-6 text-white"></div>
         <div class="col-lg-6 my-auto">
-            <h2>Updated For Bootstrap 4</h2>
+            <h2>💬 Get feedback</h2>
             <p class="lead mb-0">Newly improved, and full of great utility classes, Bootstrap 4 is leading the way in mobile responsive web development! All of the themes on Start Bootstrap are now using Bootstrap 4!</p>
         </div>
     </div>
-    <div class="row mt-3">
+    <div class="row mt-4">
         <div class="col-lg-6 order-lg-2 text-white"></div>
         <div class="col-lg-6 order-lg-1 my-auto">
-            <h2>Easy to Use & Customize</h2>
+            <h2>🔥 Earn reputations and streaks</h2>
             <p class="lead mb-0">
                 Landing Page is just HTML and CSS with a splash of SCSS for users who demand some deeper customization options. Out of the box, just add your content and images, and your new landing page will be ready to go!
             </p>
