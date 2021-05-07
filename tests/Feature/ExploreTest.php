51986010d8f42f03ee3ee83469ae63ce026094ae
Yogi <yoginth@hey.com>
2021-05-07 20:03:46 +0530

    Update ExploreTest.php

--- tests/Feature/ExploreTest.php
+++ tests/Feature/ExploreTest.php
@@ -12,14 +12,3 @@ it('has explore page', function ($url, $expected, $auth) {
     ['/explore', 200, false],
     ['/explore', 200, true],
 ]);
-
-it('has explore/makers page', function ($url, $expected, $auth) {
-    if ($auth) {
-        actingAs(1)->get($url)->assertStatus($expected);
-    } else {
-        $this->get($url)->assertStatus($expected);
-    }
-})->with([
-    ['/explore/makers', 200, false],
-    ['/explore/makers', 200, true],
-]);
