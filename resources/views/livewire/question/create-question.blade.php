02b3be13ecdfba6025ad8bd9039476eb0468093b
Yogi <yoginth@hey.com>
2021-05-08 10:18:18 +0530

    :truck: Move ask question from modal to card

--- resources/views/livewire/question/create-question.blade.php
+++ resources/views/livewire/question/create-question.blade.php
@@ -1,58 +1,53 @@
-<div wire:ignore.self class="modal fade" id="newQuestionModal" tabindex="-1" role="dialog" aria-labelledby="newQuestionModalTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">New Question</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<div>
+    <div class="h5 mb-3 text-secondary d-flex align-content-center">
+        <x-heroicon-o-cube class="heroicon-2x" />
+        <span class="ms-1">Create new question</span>
+    </div>
+    <div class="card">
+        <form wire:target="submit" wire:submit.prevent="submit">
+            <div class="card-body">
+                <div class="mb-3">
+                    <label class="form-label fw-bold">Title</label>
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" placeholder="Ask and discuss!" wire:model.defer="title">
+                    @error('title')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label class="form-label fw-bold">Body</label>
+                    <textarea class="form-control @error('body') is-invalid @enderror mentionInput" rows="6" placeholder="What's on your mind?" wire:model.defer="body"></textarea>
+                    @error('body')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <div class="small fw-bold text-secondary mt-3">
+                        <x-heroicon-o-pencil-alt class="heroicon" />
+                        Markdown is supported
+                        <x-beta />
+                    </div>
+                </div>
+                @auth
+                @if (auth()->user()->isPatron)
+                <div class="mb-3">
+                    <div class="fw-bold mb-2">Patron only</div>
+                    <input id="patronOnly" class="form-check-input" type="checkbox" wire:model.defer="patronOnly">
+                    <label for="patronOnly" class="ms-1">This question will visible only for patrons</label>
+                </div>
+                @else
+                <div class="mb-2">
+                    <a class="text-primary" href="{{ route('patron.home') }}">
+                        ❤ Support Taskord to post patron only questions!
+                    </a>
+                </div>
+                @endif
+                @endauth
+                <button type="submit" class="btn btn-primary mt-2">
+                    Post
+                </button>
             </div>
-            <form wire:target="submit" wire:submit.prevent="submit">
-                <div class="modal-body">
-                    <div class="mb-3">
-                        <label class="form-label fw-bold">Title</label>
-                        <input type="text" class="form-control @error('title') is-invalid @enderror" placeholder="Ask and discuss!" wire:model.defer="title">
-                        @error('title')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label fw-bold">Body</label>
-                        <textarea class="form-control @error('body') is-invalid @enderror mentionInput" rows="6" placeholder="What's on your mind?" wire:model.defer="body"></textarea>
-                        @error('body')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                        <div class="small fw-bold text-secondary mt-3">
-                            <x-heroicon-o-pencil-alt class="heroicon" />
-                            Markdown is supported
-                            <x-beta />
-                        </div>
-                    </div>
-                    @auth
-                    @if (auth()->user()->isPatron)
-                    <div class="mb-3">
-                        <div class="fw-bold mb-2">Patron only</div>
-                        <input id="patronOnly" class="form-check-input" type="checkbox" wire:model.defer="patronOnly">
-                        <label for="patronOnly" class="ms-1">This question will visible only for patrons</label>
-                    </div>
-                    @else
-                    <div class="mb-2">
-                        <a class="text-primary" href="{{ route('patron.home') }}">
-                            ❤ Support Taskord to post patron only questions!
-                        </a>
-                    </div>
-                    @endif
-                    @endauth
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">
-                        Ask
-                    </button>
-                </div>
-            </form>
-        </div>
+        </form>
     </div>
 </div>
