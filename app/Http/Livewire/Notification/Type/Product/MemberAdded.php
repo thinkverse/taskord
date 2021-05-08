f30a188b9acdb96f36bea34a6ae1a832f4fe102b
Yogi <yoginth@hey.com>
2021-05-08 14:09:50 +0530

    :heavy_plus_sign: Add MemberAdded type

---
+++ app/Http/Livewire/Notification/Type/Product/MemberAdded.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Livewire\Notification\Type\Product;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class MemberAdded extends Component
+{
+    public $data;
+
+    public function mount($data)
+    {
+        $this->data = $data;
+    }
+
+    public function render()
+    {
+        $product = Product::find($this->data['product_id']);
+
+        return view('livewire.notification.type.product.member-added', [
+            'product' => $product,
+        ]);
+    }
+}
--- resources/views/livewire/notification/single-notification.blade.php
+++ resources/views/livewire/notification/single-notification.blade.php
@@ -79,13 +79,7 @@
                 @elseif ($type === "App\Notifications\Subscribed")
                     <livewire:notification.type.product.subscribed :data="$data" />
                 @elseif ($type === "App\Notifications\Product\MemberAdded")
-                    <span class="align-middle">
-                        added you to the product
-                        <a class="fw-bold" href="{{ route('product.done', ['slug' => \App\Models\Product::find($data['product_id'])->slug]) }}">
-                            <img loading=lazy class="rounded avatar-20 ms-1 me-1" src="{{ Helper::getCDNImage(\App\Models\Product::find($data['product_id'])->avatar, 80) }}" height="20" width="20" />
-                            {{ \App\Models\Product::find($data['product_id'])->name }}
-                        </a>
-                    </span>
+                    <livewire:notification.type.product.member-added :data="$data" />
                 @elseif ($type === "App\Notifications\Product\MemberRemoved")
                     <span class="align-middle">
                         removed you from the product
