98ba79a6d40cff1abd64b2bae2c38faa2003e5d5
Yogi <yoginth@hey.com>
2021-05-08 19:11:18 +0530

    :arrow_up: Change feature creation order

--- database/seeders/FeatureSeeder.php
+++ database/seeders/FeatureSeeder.php
@@ -15,27 +15,9 @@ class FeatureSeeder extends Seeder
     public function run()
     {
         Feature::factory()->create([
-            'name' => 'Meetups',
-            'slug' => 'meetups',
-            'description' => 'User can host meetups in Taskord',
-        ]);
-
-        Feature::factory()->create([
-            'name' => 'Help menu',
-            'slug' => 'help_menu',
-            'description' => 'Help menu in navbar dropdown',
-        ]);
-
-        Feature::factory()->create([
-            'name' => 'About page',
-            'slug' => 'about_page',
-            'description' => "Taskord's about page",
-        ]);
-
-        Feature::factory()->create([
-            'name' => 'Explore - Makers',
-            'slug' => 'explore_makers',
-            'description' => 'Makers explore page that show famous makers',
+            'name' => 'Year in review',
+            'slug' => 'year_in_review',
+            'description' => 'Year in review page for all Taskord users',
         ]);
 
         Feature::factory()->create([
@@ -45,9 +27,27 @@ class FeatureSeeder extends Seeder
         ]);
 
         Feature::factory()->create([
-            'name' => 'Year in review',
-            'slug' => 'year_in_review',
-            'description' => 'Year in review page for all Taskord users',
+            'name' => 'Explore - Makers',
+            'slug' => 'explore_makers',
+            'description' => 'Makers explore page that show famous makers',
+        ]);
+
+        Feature::factory()->create([
+            'name' => 'About page',
+            'slug' => 'about_page',
+            'description' => "Taskord's about page",
+        ]);
+
+        Feature::factory()->create([
+            'name' => 'Help menu',
+            'slug' => 'help_menu',
+            'description' => 'Help menu in navbar dropdown',
+        ]);
+
+        Feature::factory()->create([
+            'name' => 'Meetups',
+            'slug' => 'meetups',
+            'description' => 'User can host meetups in Taskord',
         ]);
     }
 }
