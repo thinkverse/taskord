a5bd076cebe315c5cbca3e5748614785c24754fe
Yogi <yoginth@hey.com>
2021-05-09 11:28:58 +0530

    :dash: Add middleware('auth') for edit routes

--- routes/web.php
+++ routes/web.php
@@ -108,7 +108,7 @@ Route::group(['middleware' => ['throttle:60,1']], function () {
         Route::get('pending', [ProductController::class, 'profile'])->name('pending');
         Route::get('updates', [ProductController::class, 'profile'])->name('updates');
         Route::get('subscribers', [ProductController::class, 'profile'])->name('subscribers');
-        Route::get('edit', [ProductController::class, 'edit'])->name('edit');
+        Route::get('edit', [ProductController::class, 'edit'])->middleware('auth')->name('edit');
     });
 
     // Products
@@ -121,7 +121,7 @@ Route::group(['middleware' => ['throttle:60,1']], function () {
     // Question
     Route::group(['prefix' => 'question', 'as' => 'question.'], function () {
         Route::get('{id}', [QuestionController::class, 'question'])->name('question');
-        Route::get('{question}/edit', [QuestionController::class, 'edit'])->name('edit');
+        Route::get('{question}/edit', [QuestionController::class, 'edit'])->middleware('auth')->name('edit');
     });
 
     // Questions
@@ -137,7 +137,7 @@ Route::group(['middleware' => ['throttle:60,1']], function () {
         Route::get('', [MilestoneController::class, 'opened'])->name('opened');
         Route::get('closed', [MilestoneController::class, 'closed'])->name('closed');
         Route::view('new', 'milestone.new')->middleware('auth')->name('new');
-        Route::get('{milestone}/edit', [MilestoneController::class, 'edit'])->name('edit');
+        Route::get('{milestone}/edit', [MilestoneController::class, 'edit'])->middleware('auth')->name('edit');
         Route::get('{milestone}', [MilestoneController::class, 'milestone'])->name('milestone');
     });
 
