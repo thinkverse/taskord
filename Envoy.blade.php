a7cee665379e39f8ab29be31004455e3686dd42b
Yogi <yoginth@hey.com>
2021-05-10 16:13:41 +0530

    Update Envoy.blade.php

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -26,37 +26,32 @@
     php artisan migrate --force
     echo "Database Migration Ended ✅";
     php artisan up
-    @discord(env('DISCORD_WEBHOOK_URL'), '🎉 Taskord has been successfully deployed!')
 @endtask
 
 @task('install-laravel-dependencies')
-    @discord(env('DISCORD_WEBHOOK_URL'), '🐘 Installing Laravel dependencies')
     echo "Installing PHP dependencies";
     composer install --no-progress
     composer dump-autoload
 @endtask
 
 @task('install-yarn-dependencies')
-    @discord(env('DISCORD_WEBHOOK_URL'), '🧶 Installing Yarn dependencies')
     echo "Install Yarn dependencies";
     yarn install
 @endtask
 
 @task('clean-application')
-    @discord(env('DISCORD_WEBHOOK_URL'), '🧽 Cleaning the application')
     echo "Cleaning the Taskord application";
     php artisan app:clean
 @endtask
 
 @task('build-assets')
-    @discord(env('DISCORD_WEBHOOK_URL'), '🏗 Building CSS and JS assets')
     echo "Building CSS and JS assets";
     yarn production
 @endtask
 
 @success
     echo "Taskord has been successfully deployed!\n";
-    @discord(env('DISCORD_WEBHOOK_URL'), '🎉 Taskord has been successfully deployed!')
+    @discord(env('DISCORD_WEBHOOK_URL'), '✅ Taskord has been successfully deployed!')
 @endsuccess
 
 @error
