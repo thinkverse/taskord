4c3e0b9f2f022ec57154a85cde4b41ce0cd55051
Yogi <yoginth@hey.com>
2021-05-10 19:48:33 +0530

    :heavy_plus_sign: Add sentry middleware

---
+++ app/Http/Middleware/Sentry.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Sentry\State\Scope;
+use function Sentry\configureScope;
+
+class Sentry
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (app()->bound('sentry')) {
+            if (auth()->check()) {
+                configureScope(function (Scope $scope): void {
+                    $scope->setUser(['id' => auth()->user()->id]);
+                });
+            }
+        }
+
+        return $next($request);
+    }
+}
