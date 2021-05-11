35d710a55d391a7bc4d3eefd02553e3f18986850
Yogi <yoginth@hey.com>
2021-05-11 09:23:46 +0530

    Update backup.php

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'laravel-backup'),
+        'name' => env('APP_NAME', 'taskord-backup'),
 
         'source' => [
 
@@ -181,7 +181,7 @@ return [
      */
     'monitor_backups' => [
         [
-            'name' => env('APP_NAME', 'laravel-backup'),
+            'name' => env('APP_NAME', 'taskord-backup'),
             'disks' => ['local'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class => 1,
