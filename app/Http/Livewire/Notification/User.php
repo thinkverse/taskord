fabdf3114e2133d11106833e3340e264dd9b5ce8
Yogi <yoginth@hey.com>
2021-05-11 15:30:16 +0530

    Add user lw

---
+++ app/Http/Livewire/Notification/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Notification;
+
+use Livewire\Component;
+
+class User extends Component
+{
+    public function render()
+    {
+        return view('livewire.notification.user');
+    }
+}
