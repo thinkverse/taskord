dc62dcd01925eaf14a57fdb118b91fd6e3216215
Yogi <yoginth@hey.com>
2021-05-11 17:36:25 +0530

    Align product image in notification

--- resources/views/livewire/notification/type/product/subscribed.blade.php
+++ resources/views/livewire/notification/type/product/subscribed.blade.php
@@ -3,7 +3,7 @@
     <div class="mt-2 text-black-50">
         subscribed to your product
         <a class="fw-bold" href="{{ route('product.done', ['slug' => $product->slug]) }}">
-            <img loading=lazy class="rounded avatar-20 ms-1 me-1" src="{{ Helper::getCDNImage($product->avatar, 80) }}" height="20" width="20" />
+            <img loading=lazy class="rounded avatar-20 ms-1 me-1 mb-1" src="{{ Helper::getCDNImage($product->avatar, 80) }}" height="20" width="20" />
             {{ $product->name }}
         </a>
     </div>
