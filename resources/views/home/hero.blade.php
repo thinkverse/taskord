aa4180656a97c4feafc1a3638542065b79dfe480
Yogi <yoginth@hey.com>
2021-05-12 12:35:31 +0530

    Update hero.blade.php

--- resources/views/home/hero.blade.php
+++ resources/views/home/hero.blade.php
@@ -11,21 +11,21 @@
                 <h4><span class="badge bg-success rounded-end">1</span></h4>
                 <div class="ms-3">
                     <div class="h5">Build in public</div>
-                    <div class="lead">Share your completed tasks publicly to stay accountable.</div>
+                    <div class="lead">Get support from makers by sharing your ideas or product in public.</div>
                 </div>
             </div>
             <div class="col-lg mt-4 d-flex align-items-center">
                 <h4><span class="badge bg-success rounded-end">2</span></h4>
                 <div class="ms-3">
-                    <div class="h5">Get valuable feedback</div>
-                    <div class="lead">A streak is the count of consecutive days of completed tasks.</div>
+                    <div class="h5">Increase your productivity</div>
+                    <div class="lead">You can build better productivity with other awesome makers.</div>
                 </div>
             </div>
             <div class="col-lg mt-4 d-flex align-items-center">
                 <h4><span class="badge bg-success rounded-end">3</span></h4>
                 <div class="ms-3">
                     <div class="h5">Earn reputations</div>
-                    <div class="lead">Share your achievements, encourage others, and grow with us.</div>
+                    <div class="lead">Earn streaks and reputations based on your activity in the platform.</div>
                 </div>
             </div>
         </div>
