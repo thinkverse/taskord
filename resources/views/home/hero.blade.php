5c4a32d1ef2e4b090db020e053dc44a45c512c3d
Yogi <yoginth@hey.com>
2021-05-12 11:25:04 +0530

    Update hero.blade.php

--- resources/views/home/hero.blade.php
+++ resources/views/home/hero.blade.php
@@ -7,17 +7,26 @@
             </div>
         </div>
         <div class="row align-items-center">
-            <div class="col">
-                <div class="h5">Log your daily tasks.</div>
-                <div class="lead">Share your completed tasks publicly to stay accountable.</div>
+            <div class="col-lg mt-4 d-flex align-items-center">
+                <h4><span class="badge bg-primary rounded-end">1</span></h4>
+                <div class="ms-3">
+                    <div class="h5">Log your daily tasks.</div>
+                    <div class="lead">Share your completed tasks publicly to stay accountable.</div>
+                </div>
             </div>
-            <div class="col">
-                <div class="h5">Earn a streak.</div>
-                <div class="lead">A streak is the count of consecutive days of completed tasks.</div>
+            <div class="col-lg mt-4 d-flex align-items-center">
+                <h4><span class="badge bg-primary rounded-end">2</span></h4>
+                <div class="ms-3">
+                    <div class="h5">Earn a streak.</div>
+                    <div class="lead">A streak is the count of consecutive days of completed tasks.</div>
+                </div>
             </div>
-            <div class="col">
-                <div class="h5">Learn & grow.</div>
-                <div class="lead">Share your achievements, encourage others, and grow with us.</div>
+            <div class="col-lg mt-4 d-flex align-items-center">
+                <h4><span class="badge bg-primary rounded-end">3</span></h4>
+                <div class="ms-3">
+                    <div class="h5">Learn & grow.</div>
+                    <div class="lead">Share your achievements, encourage others, and grow with us.</div>
+                </div>
             </div>
         </div>
     </div>
