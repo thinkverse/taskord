185dcfb30c49fcb834acbb7357177372058e820b
Yogi <yoginth@hey.com>
2021-05-15 20:16:51 +0530

    Simplify CheckUser middleware

--- app/Http/Middleware/CheckUser.php
+++ app/Http/Middleware/CheckUser.php
@@ -2,40 +2,11 @@
 
 namespace App\Http\Middleware;
 
-use Carbon\CarbonImmutable;
 use Closure;
-use Illuminate\Contracts\Auth\Factory as Auth;
 use Illuminate\Http\Request;
 
 class CheckUser
 {
-    /**
-     * The authentication factory instance.
-     *
-     * @var \Illuminate\Contracts\Auth\Factory
-     */
-    protected $auth;
-
-    /**
-     * The immutable carbon instance.
-     *
-     * @var \Carbon\CarbonImmutable
-     */
-    protected $carbon;
-
-    /**
-     * Create a new middleware instance.
-     *
-     * @param  \Illuminate\Contracts\Auth\Factory  $auth
-     * @param  \Carbon\CarbonImmutable             $carbon
-     * @return void
-     */
-    public function __construct(Auth $auth, CarbonImmutable $carbon)
-    {
-        $this->carbon = $carbon;
-        $this->auth = $auth;
-    }
-
     /**
      * Handle an incoming request.
      *
@@ -45,12 +16,12 @@ class CheckUser
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($this->auth->check()) {
-            $request->user()->last_active = $this->carbon->now();
+        if (auth()->check()) {
+            $request->user()->last_active = carbon();
             $request->user()->save();
 
             if ($request->user()->isSuspended) {
-                $this->auth->logout();
+                auth()->logout();
 
                 return redirect()->route('suspended');
             }
