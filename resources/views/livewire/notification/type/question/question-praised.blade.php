6ec096b0e797660ecc98b820c0e7fbd44251ed05
Yogi <yoginth@hey.com>
2021-05-15 21:39:36 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/notification/type/question/question-praised.blade.php
+++ resources/views/livewire/notification/type/question/question-praised.blade.php
@@ -1,18 +1,18 @@
 <div>
     @if ($question)
-    <div class="mt-2 text-secondary">
-        praised your
-        <a class="fw-bold" href="{{ route('question.question', ['id' => $question->id]) }}">
-            question
-        </a>
-    </div>
-    <div class="mt-3">
-        @livewire('question.single-question', [
-            'type' => 'question.newest',
-            'question' => $question,
-        ], key($question->id))
-        @else
-    </div>
-    <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
+        <div class="mt-2 text-secondary">
+            praised your
+            <a class="fw-bold" href="{{ route('question.question', ['id' => $question->id]) }}">
+                question
+            </a>
+        </div>
+        <div class="mt-3">
+            @livewire('question.single-question', [
+                'type' => 'question.newest',
+                'question' => $question,
+            ], key($question->id))
+        </div>
+    @else
+        <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
     @endif
 </div>
