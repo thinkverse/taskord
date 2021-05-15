767a76b223f8afe0bf52ba555f179453cdc89b48
Yogi <yoginth@hey.com>
2021-05-15 12:33:29 +0530

    Add single comment to notification

--- resources/views/livewire/notification/type/comment/comment-praised.blade.php
+++ resources/views/livewire/notification/type/comment/comment-praised.blade.php
@@ -6,11 +6,9 @@
             comment
         </a>
     </div>
-    @if ($comment->hidden)
-        <div class="body-font fst-italic text-secondary mt-2">Comment was hidden by moderator</div>
-    @else
-        <div class="mt-2 body-font">{!! markdown($comment->comment) !!}</div>
-    @endif
+    <div class="mt-3">
+        <livewire:comment.single-comment :comment="$comment" :wire:key="$comment->id" />
+    </div>
     @else
     <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
     @endif
