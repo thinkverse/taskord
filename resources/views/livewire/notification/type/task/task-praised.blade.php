a7807d06b7b934394e90b03f595e75c50747b6bb
Yogi <yoginth@hey.com>
2021-05-15 21:39:50 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/notification/type/task/task-praised.blade.php
+++ resources/views/livewire/notification/type/task/task-praised.blade.php
@@ -1,17 +1,17 @@
 <div>
     @if ($task)
-    <div class="mt-2 text-secondary">
-        praised your
-        <a class="fw-bold" href="{{ route('task', ['id' => $task->id]) }}">
-            task
-        </a>
-    </div>
-    <div class="card mt-3">
-        <span class="p-3">
-            <livewire:task.single-task :task="$task" :showComments="false" :wire:key="$task->id" />
-        </span>
-    </div>
+        <div class="mt-2 text-secondary">
+            praised your
+            <a class="fw-bold" href="{{ route('task', ['id' => $task->id]) }}">
+                task
+            </a>
+        </div>
+        <div class="card mt-3">
+            <span class="p-3">
+                <livewire:task.single-task :task="$task" :showComments="false" :wire:key="$task->id" />
+            </span>
+        </div>
     @else
-    <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
+        <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
     @endif
 </div>
