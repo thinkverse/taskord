fc9843f4772733683b9909fcc0162b761d5a0622
Yogi <yoginth@hey.com>
2021-05-15 21:25:33 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/home/follow.blade.php
+++ resources/views/livewire/home/follow.blade.php
@@ -1,15 +1,15 @@
 <div>
     @if (auth()->user()->isFollowing($user))
-    <button wire:click="followUser" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
-        <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
-        <x-heroicon-o-user-remove wire:loading.remove class="heroicon" />
-        {{ $showText ? 'Unfollow' : '' }}
-    </button>
+        <button wire:click="followUser" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
+            <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
+            <x-heroicon-o-user-remove wire:loading.remove class="heroicon" />
+            {{ $showText ? 'Unfollow' : '' }}
+        </button>
     @else
-    <button wire:click="followUser" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
-        <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
-        <x-heroicon-o-user-add wire:loading.remove class="heroicon" />
-        {{ $showText ? 'Follow' : '' }}
-    </button>
+        <button wire:click="followUser" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
+            <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
+            <x-heroicon-o-user-add wire:loading.remove class="heroicon" />
+            {{ $showText ? 'Follow' : '' }}
+        </button>
     @endif
 </div>
