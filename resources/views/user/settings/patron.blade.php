ae9b6a98826f4300f8a62eea581129091eebbcec
Yogi <yoginth@hey.com>
2021-05-15 20:48:42 +0530

    refactor: Fix blade template alignment

--- resources/views/user/settings/patron.blade.php
+++ resources/views/user/settings/patron.blade.php
@@ -19,50 +19,50 @@
                         </a>
                     @else
                         @if ($user->patron)
-                        <div>
                             <div>
-                                <span class="fw-bold">Checkout ID:</span>
-                                <span class="text-danger fw-bold font-monospace">{{ $user->patron->checkout_id }}</span>
+                                <div>
+                                    <span class="fw-bold">Checkout ID:</span>
+                                    <span class="text-danger fw-bold font-monospace">{{ $user->patron->checkout_id }}</span>
+                                </div>
+                                <div>
+                                    <span class="fw-bold">Subscribed to:</span>
+                                    @if ($user->patron->subscription_plan_id === 619848)
+                                        <span>Tier 1</span>
+                                    @elseif ($user->patron->subscription_plan_id === 621377)
+                                        <span>Tier 2</span>
+                                    @elseif ($user->patron->subscription_plan_id === 621379)
+                                        <span>Tier 3</span>
+                                    @elseif ($user->patron->subscription_plan_id === 621380)
+                                        <span>Tier 4</span>
+                                    @endif
+                                </div>
+                                <div>
+                                    <span class="fw-bold">Subscribed at:</span>
+                                    <span>{{ carbon($user->patron->created_at)->format('d M Y') }}</span>
+                                </div>
+                                <div>
+                                    <span class="fw-bold">Last transaction:</span>
+                                    <span>{{ carbon($user->patron->event_time)->format('d M Y') }}</span>
+                                </div>
+                                <div>
+                                    <span class="fw-bold">Next bill date:</span>
+                                    <span>{{ carbon($user->patron->next_bill_date)->format('d M Y') }}</span>
+                                </div>
+                                <div class="mt-2">
+                                    @if ($user->patron->update_url)
+                                        <a class="btn btn-success text-white" href="{{ $user->patron->update_url }}" target="_blank" rel="noreferrer">
+                                            <x-heroicon-o-pencil class="heroicon" />
+                                            Update Payment
+                                        </a>
+                                    @endif
+                                    @if ($user->patron->cancel_url)
+                                        <a class="btn btn-danger" href="{{ $user->patron->cancel_url }}" target="_blank" rel="noreferrer">
+                                            <x-heroicon-o-x class="heroicon" />
+                                            Cancel Patron
+                                        </a>
+                                    @endif
+                                </div>
                             </div>
-                            <div>
-                                <span class="fw-bold">Subscribed to:</span>
-                                @if ($user->patron->subscription_plan_id === 619848)
-                                <span>Tier 1</span>
-                                @elseif ($user->patron->subscription_plan_id === 621377)
-                                <span>Tier 2</span>
-                                @elseif ($user->patron->subscription_plan_id === 621379)
-                                <span>Tier 3</span>
-                                @elseif ($user->patron->subscription_plan_id === 621380)
-                                <span>Tier 4</span>
-                                @endif
-                            </div>
-                            <div>
-                                <span class="fw-bold">Subscribed at:</span>
-                                <span>{{ carbon($user->patron->created_at)->format('d M Y') }}</span>
-                            </div>
-                            <div>
-                                <span class="fw-bold">Last transaction:</span>
-                                <span>{{ carbon($user->patron->event_time)->format('d M Y') }}</span>
-                            </div>
-                            <div>
-                                <span class="fw-bold">Next bill date:</span>
-                                <span>{{ carbon($user->patron->next_bill_date)->format('d M Y') }}</span>
-                            </div>
-                            <div class="mt-2">
-                                @if ($user->patron->update_url)
-                                <a class="btn btn-success text-white" href="{{ $user->patron->update_url }}" target="_blank" rel="noreferrer">
-                                    <x-heroicon-o-pencil class="heroicon" />
-                                    Update Payment
-                                </a>
-                                @endif
-                                @if ($user->patron->cancel_url)
-                                <a class="btn btn-danger" href="{{ $user->patron->cancel_url }}" target="_blank" rel="noreferrer">
-                                    <x-heroicon-o-x class="heroicon" />
-                                    Cancel Patron
-                                </a>
-                                @endif
-                            </div>
-                        </div>
                         @else
                             <div>You are using gifted account!</div>
                             <a class="btn btn-success text-white mt-2" href="{{ route('patron.home') }}">
