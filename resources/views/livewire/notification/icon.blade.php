c5a6509f90cf6a9dbbd6ca70ebe8b6784c78a36f
Yogi <yoginth@hey.com>
2021-05-15 21:40:40 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/notification/icon.blade.php
+++ resources/views/livewire/notification/icon.blade.php
@@ -2,11 +2,11 @@
     <a class="nav-link text-white" href="{{ route('notifications.unread') }}" aria-label="Notifications">
         <x-heroicon-o-bell class="heroicon-notification me-0" />
         @auth
-        @if (auth()->user()->unreadNotifications->count('id') !== 0)
-        <span class="badge badge-pill bg-danger fw-bold small ms-1 p-1">
-            {{ auth()->user()->unreadNotifications->count('id') }}
-        </span>
-        @endif
+            @if (auth()->user()->unreadNotifications->count('id') !== 0)
+                <span class="badge badge-pill bg-danger fw-bold small ms-1 p-1">
+                    {{ auth()->user()->unreadNotifications->count('id') }}
+                </span>
+            @endif
         @endauth
     </a>
 </li>
