15d79b6baf33c942f5f2b7dc5ae1515b60b3041c
Yogi <yoginth@hey.com>
2021-05-15 20:33:53 +0530

    refactor: Fix blade template alignment

--- resources/views/product/edit.blade.php
+++ resources/views/product/edit.blade.php
@@ -8,14 +8,14 @@
         <div class="col-lg-7">
             @auth
                 @if (!auth()->user()->isFlagged)
-                <livewire:product.edit-product :product="$product" />
+                    <livewire:product.edit-product :product="$product" />
                 @else
-                <div class="text-center">
-                    <div class="alert alert-danger" role="alert">
-                        You can't edit this product, because your account has been flagged 😢
+                    <div class="text-center">
+                        <div class="alert alert-danger" role="alert">
+                            You can't edit this product, because your account has been flagged 😢
+                        </div>
+                        <a class="btn btn-primary" href="{{ route('home') }}">Go to home</a>
                     </div>
-                    <a class="btn btn-primary" href="{{ route('home') }}">Go to home</a>
-                </div>
                 @endif
             @endauth
         </div>
