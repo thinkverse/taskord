b3595c3dc39f7f935deb22bed341637675044d4b
Yogi <yoginth@hey.com>
2021-05-15 20:30:54 +0530

    refactor: Fix blade template alignment

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -11,28 +11,28 @@
 <div class="row justify-content-center" style="height:70vh">
     <div class="col-lg-7 mt-5">
         @auth
-        @php
-        $username = "@".auth()->user()->username;
-        $email = urlencode(auth()->user()->email);
-        $url = "https://tally.so/embed/5mVVam?username=".$username."&email=".$email."&transparentBackground=1";
-        @endphp
-        <iframe
-            src="{{$url}}"
-            width="100%"
-            height="100%"
-            frameborder="0"
-            marginheight="0"
-            marginwidth="0"
-        ></iframe>
+            @php
+                $username = "@".auth()->user()->username;
+                $email = urlencode(auth()->user()->email);
+                $url = "https://tally.so/embed/5mVVam?username=".$username."&email=".$email."&transparentBackground=1";
+            @endphp
+            <iframe
+                src="{{$url}}"
+                width="100%"
+                height="100%"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+            ></iframe>
         @else
-        <iframe
-            src="https://tally.so/embed/63lL53?transparentBackground=1"
-            width="100%"
-            height="100%"
-            frameborder="0"
-            marginheight="0"
-            marginwidth="0"
-        ></iframe>
+            <iframe
+                src="https://tally.so/embed/63lL53?transparentBackground=1"
+                width="100%"
+                height="100%"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+            ></iframe>
         @endauth
     </div>
 </div>
