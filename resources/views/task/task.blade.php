2b545f7ca3c1fbd2b8967fb350a566b95d946be9
Yogi <yoginth@hey.com>
2021-05-15 20:47:55 +0530

    refactor: Fix blade template alignment

--- resources/views/task/task.blade.php
+++ resources/views/task/task.blade.php
@@ -25,11 +25,11 @@
                 'perPage' => 10
             ])
             @auth
-            @if (!auth()->user()->isFlagged)
-                @livewire('comment.create-comment', [
-                    'task' => $task
-                ])
-            @endif
+                @if (!auth()->user()->isFlagged)
+                    @livewire('comment.create-comment', [
+                        'task' => $task
+                    ])
+                @endif
             @endauth
             @guest
                 <a href="/login" class="btn w-100 btn-success mt-4 text-white fw-bold">
@@ -47,78 +47,78 @@
                 </div>
             </div>
             @if ($task->product_id)
-            <div class="fw-bold text-secondary pb-2">
-                Product
-            </div>
-            <div class="card mb-4">
-                <div class="card-body d-flex align-items-center">
-                    <a
-                        href="{{ route('product.done', ['slug' => \App\Models\Product::find($task->product_id)->slug]) }}"
-                        class="product-popover"
-                        data-id="{{ \App\Models\Product::find($task->product_id)->id }}"
-                    >
-                        <img loading=lazy class="rounded avatar-40 mt-1" src="{{ Helper::getCDNImage(\App\Models\Product::find($task->product_id)->avatar, 80) }}" height="40" width="40" />
-                    </a>
-                    <span class="ms-3">
+                <div class="fw-bold text-secondary pb-2">
+                    Product
+                </div>
+                <div class="card mb-4">
+                    <div class="card-body d-flex align-items-center">
                         <a
                             href="{{ route('product.done', ['slug' => \App\Models\Product::find($task->product_id)->slug]) }}"
-                            class="align-text-top text-dark product-popover"
+                            class="product-popover"
                             data-id="{{ \App\Models\Product::find($task->product_id)->id }}"
                         >
-                            <span class="fw-bold">
-                                {{ \App\Models\Product::find($task->product_id)->name }}
-                            </span>
-                            <div>{{ \App\Models\Product::find($task->product_id)->description }}</div>
+                            <img loading=lazy class="rounded avatar-40 mt-1" src="{{ Helper::getCDNImage(\App\Models\Product::find($task->product_id)->avatar, 80) }}" height="40" width="40" />
                         </a>
-                    </span>
+                        <span class="ms-3">
+                            <a
+                                href="{{ route('product.done', ['slug' => \App\Models\Product::find($task->product_id)->slug]) }}"
+                                class="align-text-top text-dark product-popover"
+                                data-id="{{ \App\Models\Product::find($task->product_id)->id }}"
+                            >
+                                <span class="fw-bold">
+                                    {{ \App\Models\Product::find($task->product_id)->name }}
+                                </span>
+                                <div>{{ \App\Models\Product::find($task->product_id)->description }}</div>
+                            </a>
+                        </span>
+                    </div>
                 </div>
-            </div>
             @endif
             @auth
-            <div class="fw-bold text-secondary pb-2">
-                Subscribe to this task
-            </div>
-            <div class="card mb-4">
-                <div class="card-body d-flex align-items-center">
-                    @livewire('task.subscribe', ['task' => $task])
+                <div class="fw-bold text-secondary pb-2">
+                    Subscribe to this task
                 </div>
-            </div>
+                <div class="card mb-4">
+                    <div class="card-body d-flex align-items-center">
+                        @livewire('task.subscribe', ['task' => $task])
+                    </div>
+                </div>
             @endauth
             @if ($task->comments->count('id') > 0)
-            <div class="fw-bold text-secondary pb-2">
-                Users Involved
-            </div>
-            <div class="card mb-4">
-                <div class="card-body align-items-center pb-2">
-                    @foreach ($task->comments->groupBy('user_id') as $comment)
-                        <a
-                            href="{{ route('user.done', ['username' => $comment[0]->user->username]) }}"
-                            class="me-1 user-popover"
-                            data-id="{{ $comment[0]->user->id }}"
-                        >
-                            <img loading=lazy class="rounded-circle avatar-30 mb-2" src="{{ Helper::getCDNImage($comment[0]->user->avatar, 80) }}" height="30" width="30" alt="{{ $comment[0]->user->username }}'s avatar" />
-                        </a>
-                    @endforeach
+                <div class="fw-bold text-secondary pb-2">
+                    Users Involved
                 </div>
-            </div>
+                <div class="card mb-4">
+                    <div class="card-body align-items-center pb-2">
+                        @foreach ($task->comments->groupBy('user_id') as $comment)
+                            <a
+                                href="{{ route('user.done', ['username' => $comment[0]->user->username]) }}"
+                                class="me-1 user-popover"
+                                data-id="{{ $comment[0]->user->id }}"
+                            >
+                                <img loading=lazy class="rounded-circle avatar-30 mb-2" src="{{ Helper::getCDNImage($comment[0]->user->avatar, 80) }}" height="30" width="30" alt="{{ $comment[0]->user->username }}'s avatar" />
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
             @endif
             @if ($task->likerscount() > 0)
-            <div class="fw-bold text-secondary pb-2">
-                Liked by
-            </div>
-            <div class="card mb-4">
-                <div class="card-body align-items-center pb-2">
-                    @foreach ($task->likers as $user)
-                        <a
-                            title="{{ $user->firstname ? $user->firstname . ' ' . $user->lastname : $user->username }}"
-                            href="{{ route('user.done', ['username' => $user->username]) }}"
-                            class="me-1"
-                        >
-                            <img loading=lazy class="rounded-circle avatar-30 mb-2" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="30" width="30" alt="{{ $user->username }}'s avatar" />
-                        </a>
-                    @endforeach
+                <div class="fw-bold text-secondary pb-2">
+                    Liked by
                 </div>
-            </div>
+                <div class="card mb-4">
+                    <div class="card-body align-items-center pb-2">
+                        @foreach ($task->likers as $user)
+                            <a
+                                title="{{ $user->firstname ? $user->firstname . ' ' . $user->lastname : $user->username }}"
+                                href="{{ route('user.done', ['username' => $user->username]) }}"
+                                class="me-1"
+                            >
+                                <img loading=lazy class="rounded-circle avatar-30 mb-2" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="30" width="30" alt="{{ $user->username }}'s avatar" />
+                            </a>
+                        @endforeach
+                    </div>
+                </div>
             @endif
             <x-footer />
         </div>
