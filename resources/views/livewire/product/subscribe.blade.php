a1b9457254574876af9d39906b90fd29590f153d
Yogi <yoginth@hey.com>
2021-05-15 21:44:44 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/product/subscribe.blade.php
+++ resources/views/livewire/product/subscribe.blade.php
@@ -1,20 +1,20 @@
 <div>
     @auth
-    @if (auth()->user()->id !== $product->owner->id)
-    @if (auth()->user()->hasSubscribed($product))
-    <button wire:click="subscribeProduct" wire:loading.attr="disabled" class="btn btn-sm btn-danger mb-2">
-        <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
-        <x-heroicon-o-minus-circle wire:loading.remove class="heroicon" />
-        Unsubscribe
-    </button>
-    @else
-    <button wire:click="subscribeProduct" wire:loading.attr="disabled" class="btn btn-sm btn-primary mb-2">
-        <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
-        <x-heroicon-o-plus wire:loading.remove class="heroicon" />
-        Subscribe
-    </button>
-    @endif
-    @endif
+        @if (auth()->user()->id !== $product->owner->id)
+            @if (auth()->user()->hasSubscribed($product))
+                <button wire:click="subscribeProduct" wire:loading.attr="disabled" class="btn btn-sm btn-danger mb-2">
+                    <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
+                    <x-heroicon-o-minus-circle wire:loading.remove class="heroicon" />
+                    Unsubscribe
+                </button>
+            @else
+                <button wire:click="subscribeProduct" wire:loading.attr="disabled" class="btn btn-sm btn-primary mb-2">
+                    <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
+                    <x-heroicon-o-plus wire:loading.remove class="heroicon" />
+                    Subscribe
+                </button>
+            @endif
+        @endif
     @endauth
     <div class="small">
         <a class="text-dark" href="{{ route('product.subscribers', ['slug' => $product->slug]) }}">
