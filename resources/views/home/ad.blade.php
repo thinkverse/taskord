9768f5348fa20aa12aea23e1e215e36e4f85764b
Yogi <yoginth@hey.com>
2021-05-15 20:23:32 +0530

    refactor: Fix blade template alignment

--- resources/views/home/ad.blade.php
+++ resources/views/home/ad.blade.php
@@ -1,15 +1,15 @@
 @if (true)
-<div class="card border-success mb-4">
-    <div class="card-body">
-        <h5>
-            <a class="text-dark" href="{{ route('milestones.opened') }}">
-                <x-heroicon-o-truck class="heroicon-2x" />
-                Milestone public beta
-            </a>
-        </h5>
-        <p class="mb-0">
-            <a href="{{ route('milestones.opened') }}">Milestones</a> are now available public beta 🎉
-        </p>
+    <div class="card border-success mb-4">
+        <div class="card-body">
+            <h5>
+                <a class="text-dark" href="{{ route('milestones.opened') }}">
+                    <x-heroicon-o-truck class="heroicon-2x" />
+                    Milestone public beta
+                </a>
+            </h5>
+            <p class="mb-0">
+                <a href="{{ route('milestones.opened') }}">Milestones</a> are now available public beta 🎉
+            </p>
+        </div>
     </div>
-</div>
 @endif
