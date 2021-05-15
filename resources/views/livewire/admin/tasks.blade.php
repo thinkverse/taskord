6ae74da37a07e05fa9d7a4566de6deccc975a7df
Yogi <yoginth@hey.com>
2021-05-15 21:02:48 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/admin/tasks.blade.php
+++ resources/views/livewire/admin/tasks.blade.php
@@ -6,56 +6,52 @@
     </div>
     <div class="table-responsive">
         @if (!$readyToLoad)
-        <div class="card-body text-center mt-3">
-            <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-            <div class="h6">
-                Loading tasks...
+            <div class="card-body text-center mt-3">
+                <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+                <div class="h6">
+                    Loading tasks...
+                </div>
             </div>
-        </div>
         @else
-        <table class="table text-dark">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">Status</th>
-                    <th scope="col">Task</th>
-                    <th scope="col">Updated</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <th>{{ $task->id }}</th>
-                    <td>
-                        <a href="{{ route('user.done', ['username' => $task->user->username]) }}" target="_blank">
-                            {{ '@'.$task->user->username }}
-                        </a>
-                    </td>
-                    <td>
-                        @if ($task->done)
-                        ✅
-                        @else
-                        ⌛
-                        @endif
-                    </td>
-                    <td>
-                        <a href="{{ route('task', ['id' => $task->id]) }}">
-                            {{ Str::limit($task->task, '100') }}
-                        </a>
-                        @if ($task->hidden)
-                        <span title="Flagged">🤢</span>
-                        @endif
-                    </td>
-                    <td>
-                        <span title="{{ $task->updated_at->format('M d, Y g:i A') }}">
-                            {{ $task->updated_at->format('M d, Y') }}
-                        </span>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+            <table class="table text-dark">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Task</th>
+                        <th scope="col">Updated</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <th>{{ $task->id }}</th>
+                            <td>
+                                <a href="{{ route('user.done', ['username' => $task->user->username]) }}" target="_blank">
+                                    {{ '@'.$task->user->username }}
+                                </a>
+                            </td>
+                            <td>
+                                @if ($task->done)✅@else⌛@endif
+                            </td>
+                            <td>
+                                <a href="{{ route('task', ['id' => $task->id]) }}">
+                                    {{ Str::limit($task->task, '100') }}
+                                </a>
+                                @if ($task->hidden)
+                                    <span title="Flagged">🤢</span>
+                                @endif
+                            </td>
+                            <td>
+                                <span title="{{ $task->updated_at->format('M d, Y g:i A') }}">
+                                    {{ $task->updated_at->format('M d, Y') }}
+                                </span>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
         @endif
     </div>
     {{ $readyToLoad ? $tasks->links() : '' }}
