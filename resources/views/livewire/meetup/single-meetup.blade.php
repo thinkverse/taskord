3d84628c3c314fede1319fb3b727c9444288b459
Yogi <yoginth@hey.com>
2021-05-15 21:30:55 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/meetup/single-meetup.blade.php
+++ resources/views/livewire/meetup/single-meetup.blade.php
@@ -6,12 +6,12 @@
         <div class="card-body">
             <div class="fw-bold text-uppercase small">
                 @auth
-                {{ carbon($meetup->date)->setTimezone(auth()->user()->timezone)->format('D, M d, H:i') }}
+                    {{ carbon($meetup->date)->setTimezone(auth()->user()->timezone)->format('D, M d, H:i') }}
                 @else
-                {{ carbon($meetup->date)->format('D, M d, H:i') }}
+                    {{ carbon($meetup->date)->format('D, M d, H:i') }}
                 @endauth
                 @if (carbon($meetup->date)->isToday())
-                <span class="text-secondary">(Happening)</span>
+                    <span class="text-secondary">(Happening)</span>
                 @endif
             </div>
             <h5 class="card-title mt-2">
