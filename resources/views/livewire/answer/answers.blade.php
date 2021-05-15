49e74d29c0f24d17ab93ce700b177729fe00551d
Yogi <yoginth@hey.com>
2021-05-15 21:06:37 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/answer/answers.blade.php
+++ resources/views/livewire/answer/answers.blade.php
@@ -1,19 +1,19 @@
 <div wire:init="loadAnswers">
     @if (!$readyToLoad)
-    <div class="card-body text-center mt-3">
-        <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-        <div class="h6">
-            Loading answers...
+        <div class="card-body text-center mt-3">
+            <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+            <div class="h6">
+                Loading answers...
+            </div>
         </div>
-    </div>
     @endif
     @if ($readyToLoad and count($answers) === 0)
-    <div class="card-body text-center mt-3 mb-3">
-        <x-heroicon-o-chat-alt-2 class="heroicon-4x text-primary mb-2" />
-        <div class="h4">
-            No answers found!
+        <div class="card-body text-center mt-3 mb-3">
+            <x-heroicon-o-chat-alt-2 class="heroicon-4x text-primary mb-2" />
+            <div class="h4">
+                No answers found!
+            </div>
         </div>
-    </div>
     @endif
     @foreach ($answers as $answer)
         <div class="card mt-3">
@@ -21,8 +21,8 @@
         </div>
     @endforeach
     <div class="mt-3">
-    @if ($readyToLoad and $answers->hasMorePages())
-        <livewire:answer.load-more :question="$answer->question" :page="$page" :perPage="$perPage" />
-    @endif
+        @if ($readyToLoad and $answers->hasMorePages())
+            <livewire:answer.load-more :question="$answer->question" :page="$page" :perPage="$perPage" />
+        @endif
     </div>
 </div>
