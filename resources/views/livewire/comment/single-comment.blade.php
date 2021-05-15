69e2655584462f37419fad7a3ff44aa2c7bed039
Yogi <yoginth@hey.com>
2021-05-15 21:07:41 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/comment/single-comment.blade.php
+++ resources/views/livewire/comment/single-comment.blade.php
@@ -9,59 +9,59 @@
         </a>
     </div>
     @if ($comment->hidden)
-    <span class="body-font fst-italic text-secondary">Comment was hidden by moderator</span>
+        <span class="body-font fst-italic text-secondary">Comment was hidden by moderator</span>
     @else
-    <span class="body-font">
-        {!! markdown($comment->comment) !!}
-    </span>
+        <span class="body-font">
+            {!! markdown($comment->comment) !!}
+        </span>
     @endif
     <div class="mt-2">
         @auth
-        @if (auth()->user()->hasLiked($comment))
-            <button type="button" class="btn btn-task btn-success text-white me-1" wire:click="togglePraise" wire:loading.attr="disabled" wire:offline.attr="disabled" aria-label="Praise">
-                <x-heroicon-s-thumb-up class="heroicon-small me-0" />
-                <span class="small text-white fw-bold">
-                    {{ number_format($comment->likerscount()) }}
-                </span>
-                <span class="avatar-stack ms-1">
-                @foreach($comment->likers->take(5) as $user)
-                <img loading=lazy class="praise-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="15" width="15" alt="{{ $user->username }}'s avatar" />
-                @endforeach
-                </span>
-            </button>
-        @else
-            <button type="button" class="btn btn-task btn-outline-success me-1" wire:click="togglePraise" wire:loading.attr="disabled" wire:offline.attr="disabled" aria-label="Praises">
-                <x-heroicon-o-thumb-up class="heroicon-small me-0 text-secondary" />
-                @if ($comment->likerscount() !== 0)
-                <span class="small text-dark fw-bold">
-                    {{ number_format($comment->likerscount()) }}
-                </span>
-                <span class="avatar-stack ms-1">
-                @foreach($comment->likers->take(5) as $user)
-                <img loading=lazy class="praise-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="15" width="15" alt="{{ $user->username }}'s avatar" />
-                @endforeach
-                </span>
-                @endif
-            </button>
-        @endif
-        @if (auth()->user()->staffShip or auth()->user()->id === $comment->user->id)
-        <button
-            type="button"
-            class="btn btn-task btn-outline-danger"
-            onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
-            wire:click="deleteComment"
-            wire:loading.attr="disabled"
-            wire:offline.attr="disabled"
-            aria-label="Delete"
-        >
-            <x-heroicon-o-trash class="heroicon-small me-0 text-secondary" />
-        </button>
-        @endif
-        @if (auth()->user()->staffShip)
-        <button type="button" class="btn btn-task {{ $comment->hidden ? 'btn-info' : 'btn-outline-info' }} ms-1" wire:click="hide" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $comment->id }}" title="Flag to admins" aria-label="Hide">
-            <x-heroicon-o-eye-off class="heroicon-small me-0" />
-        </button>
-        @endif
+            @if (auth()->user()->hasLiked($comment))
+                <button type="button" class="btn btn-task btn-success text-white me-1" wire:click="togglePraise" wire:loading.attr="disabled" wire:offline.attr="disabled" aria-label="Praise">
+                    <x-heroicon-s-thumb-up class="heroicon-small me-0" />
+                    <span class="small text-white fw-bold">
+                        {{ number_format($comment->likerscount()) }}
+                    </span>
+                    <span class="avatar-stack ms-1">
+                        @foreach($comment->likers->take(5) as $user)
+                            <img loading=lazy class="praise-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="15" width="15" alt="{{ $user->username }}'s avatar" />
+                        @endforeach
+                    </span>
+                </button>
+            @else
+                <button type="button" class="btn btn-task btn-outline-success me-1" wire:click="togglePraise" wire:loading.attr="disabled" wire:offline.attr="disabled" aria-label="Praises">
+                    <x-heroicon-o-thumb-up class="heroicon-small me-0 text-secondary" />
+                    @if ($comment->likerscount() !== 0)
+                        <span class="small text-dark fw-bold">
+                            {{ number_format($comment->likerscount()) }}
+                        </span>
+                        <span class="avatar-stack ms-1">
+                            @foreach($comment->likers->take(5) as $user)
+                                <img loading=lazy class="praise-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="15" width="15" alt="{{ $user->username }}'s avatar" />
+                            @endforeach
+                        </span>
+                    @endif
+                </button>
+            @endif
+            @if (auth()->user()->staffShip or auth()->user()->id === $comment->user->id)
+                <button
+                    type="button"
+                    class="btn btn-task btn-outline-danger"
+                    onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
+                    wire:click="deleteComment"
+                    wire:loading.attr="disabled"
+                    wire:offline.attr="disabled"
+                    aria-label="Delete"
+                >
+                    <x-heroicon-o-trash class="heroicon-small me-0 text-secondary" />
+                </button>
+            @endif
+            @if (auth()->user()->staffShip)
+                <button type="button" class="btn btn-task {{ $comment->hidden ? 'btn-info' : 'btn-outline-info' }} ms-1" wire:click="hide" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $comment->id }}" title="Flag to admins" aria-label="Hide">
+                    <x-heroicon-o-eye-off class="heroicon-small me-0" />
+                </button>
+            @endif
         @endauth
         @guest
             <a href="/login" class="btn btn-task btn-outline-success me-1" aria-label="Praises">
