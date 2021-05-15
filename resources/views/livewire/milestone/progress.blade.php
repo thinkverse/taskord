8bbd378480191ca458e49420a77b3e16de85038e
Yogi <yoginth@hey.com>
2021-05-15 21:31:29 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/milestone/progress.blade.php
+++ resources/views/livewire/milestone/progress.blade.php
@@ -1,17 +1,17 @@
 <div wire:init="loadProgress">
     @if (!$readyToLoad)
-    <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
+        <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
     @else
-    <div class="progress w-50" style="height:6px" title="{{ $percent }}% completed">
-        <div class="progress-bar bg-success" role="progressbar" style="width: {{ $percent }}%;" aria-valuenow="{{ $percent }}" aria-valuemin="0" aria-valuemax="100"></div>
-    </div>
-    <div class="mt-1">
-        <span class="me-2">
-            <span class="fw-bold">{{ $completed }}</span> completed
-        </span>
-        <span class="me-2">
-            <span class="fw-bold">{{ $pending }}</span> pending
-        </span>
-    </div>
+        <div class="progress w-50" style="height:6px" title="{{ $percent }}% completed">
+            <div class="progress-bar bg-success" role="progressbar" style="width: {{ $percent }}%;" aria-valuenow="{{ $percent }}" aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <div class="mt-1">
+            <span class="me-2">
+                <span class="fw-bold">{{ $completed }}</span> completed
+            </span>
+            <span class="me-2">
+                <span class="fw-bold">{{ $pending }}</span> pending
+            </span>
+        </div>
     @endif
 </div>
