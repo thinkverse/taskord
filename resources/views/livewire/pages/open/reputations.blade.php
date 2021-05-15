25222e094c4131d1f47d60e4bb4df24f6d949525
Yogi <yoginth@hey.com>
2021-05-15 21:42:56 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/pages/open/reputations.blade.php
+++ resources/views/livewire/pages/open/reputations.blade.php
@@ -3,28 +3,28 @@
     <div class="card mt-3">
         <div class="card-body">
             @if (!$readyToLoad)
-            <div class="card-body text-center mt-3">
-                <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-                <div class="h6">
-                    Loading reputations stats...
+                <div class="card-body text-center mt-3">
+                    <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+                    <div class="h6">
+                        Loading reputations stats...
+                    </div>
                 </div>
-            </div>
             @endif
             <div id="reputations"></div>
         </div>
     </div>
 
     @if ($readyToLoad)
-    <script>
-    var options = {
-        chart: { type: 'line', height: 300, animations: { enabled: false }},
-        dataLabels: { enabled: false },
-        series: [{name: 'Reputations', data: <?php echo $reputations ?> }],
-        xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
-    }
+        <script>
+        var options = {
+            chart: { type: 'line', height: 300, animations: { enabled: false }},
+            dataLabels: { enabled: false },
+            series: [{name: 'Reputations', data: <?php echo $reputations ?> }],
+            xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
+        }
 
-    var reputations = new ApexCharts(document.querySelector("#reputations"), options);
-    reputations.render();
-    </script>
+        var reputations = new ApexCharts(document.querySelector("#reputations"), options);
+        reputations.render();
+        </script>
     @endif
 </div>
