e4c9741d4a5f2ffa7d9062f52bea7f706e5d8f65
Yogi <yoginth@hey.com>
2021-05-15 17:15:02 +0530

    Remove comment-md class

--- resources/views/livewire/task/comments.blade.php
+++ resources/views/livewire/task/comments.blade.php
@@ -9,7 +9,7 @@
                 @foreach ($comments as $comment)
                 <x:shared.user-label-small :user="$comment->user" />
                 <div class="mt-2 card d-inline-block bg-light {{ $loop->last ? '' : 'mb-3' }}">
-                    <div class="card-body px-2 py-1 comment-md">
+                    <div class="card-body px-2 py-1 body-font">
                         {!! markdown($comment->comment) !!}
                     </div>
                 </div>
