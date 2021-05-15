72dd9c12eb783f16cb20cbe2897701cf8863ab36
Yogi <yoginth@hey.com>
2021-05-15 20:47:21 +0530

    refactor: Fix blade template alignment

--- resources/views/site/shortcuts.blade.php
+++ resources/views/site/shortcuts.blade.php
@@ -23,48 +23,63 @@
     </span>
 </div>
 @auth
-<div class="d-flex justify-content-between mt-2">
-    <span>Go to notifications</span>
-    <span>
-        <kbd class="me-1">g</kbd><kbd>n</kbd>
-    </span>
-</div>
-<div class="d-flex justify-content-between mt-2">
-    <span>Go to settings</span>
-    <span>
-        <kbd class="me-1">g</kbd><kbd>s</kbd>
-    </span>
-</div>
-<div class="d-flex justify-content-between mt-2">
-    <span>Compose new task</span>
-    <span>
-        <kbd>n</kbd>
-    </span>
-</div>
-<div class="d-flex justify-content-between mt-2">
-    <span>Create new product</span>
-    <span>
-        <kbd class="me-1">c</kbd><kbd>p</kbd>
-    </span>
-</div>
-<div class="d-flex justify-content-between mt-2">
-    <span>Create new question</span>
-    <span>
-        <kbd class="me-1">c</kbd><kbd>q</kbd>
-    </span>
-</div>
-<div class="d-flex justify-content-between mt-2">
-    <span>Create new milestone</span>
-    <span>
-        <kbd class="me-1">c</kbd><kbd>m</kbd>
-    </span>
-</div>
-<div class="d-flex justify-content-between mt-2">
-    <span>Enable dark mode</span>
-    <span>
-        <kbd class="me-1">d</kbd><kbd>m</kbd>
-    </span>
-</div>
+    <div class="d-flex justify-content-between mt-2">
+        <span>Go to notifications</span>
+        <span>
+            <kbd class="me-1">g</kbd><kbd>n</kbd>
+        </span>
+    </div>
+    <div class="d-flex justify-content-between mt-2">
+        <span>Go to settings</span>
+        <span>
+            <kbd class="me-1">g</kbd><kbd>s</kbd>
+        </span>
+    </div>
+    <div class="d-flex justify-content-between mt-2">
+        <span>Compose new task</span>
+        <span>
+            <kbd>n</kbd>
+        </span>
+    </div>
+    <div class="d-flex justify-content-between mt-2">
+        <span>Create new product</span>
+        <span>
+            <kbd class="me-1">c</kbd><kbd>p</kbd>
+        </span>
+    </div>
+    <div class="d-flex justify-content-between mt-2">
+        <span>Create new question</span>
+        <span>
+            <kbd class="me-1">c</kbd><kbd>q</kbd>
+        </span>
+    </div>
+    <div class="d-flex justify-content-between mt-2">
+        <span>Create new milestone</span>
+        <span>
+            <kbd class="me-1">c</kbd><kbd>m</kbd>
+        </span>
+    </div>
+    <div class="d-flex justify-content-between mt-2">
+        <span>Enable dark mode</span>
+        <span>
+            <kbd class="me-1">d</kbd><kbd>m</kbd>
+        </span>
+    </div>
+    @if (auth()->user()->isStaff)
+        <div class="d-flex justify-content-between mt-2">
+            <span>Enable staffship / performance bar</span>
+            <span>
+                <kbd class="me-1">p</kbd><kbd>b</kbd> or
+                <kbd>`</kbd>
+            </span>
+        </div>
+        <div class="d-flex justify-content-between mt-2">
+            <span>Bring up deploy dialog</span>
+            <span>
+                <kbd class="me-1">shift</kbd><kbd>d</kbd>
+            </span>
+        </div>
+    @endif
 @endauth
 <div class="d-flex justify-content-between mt-2">
     <span>Bring up this help dialog</span>
@@ -72,18 +87,3 @@
         <kbd>?</kbd>
     </span>
 </div>
-@if (auth()->check() and auth()->user()->isStaff)
-<div class="d-flex justify-content-between mt-2">
-    <span>Enable staffship / performance bar</span>
-    <span>
-        <kbd class="me-1">p</kbd><kbd>b</kbd> or
-        <kbd>`</kbd>
-    </span>
-</div>
-<div class="d-flex justify-content-between mt-2">
-    <span>Bring up deploy dialog</span>
-    <span>
-        <kbd class="me-1">shift</kbd><kbd>d</kbd>
-    </span>
-</div>
-@endif
