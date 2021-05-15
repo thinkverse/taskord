538d249dbe122e442ad7bf13513e3c6b34e56d65
Yogi <yoginth@hey.com>
2021-05-15 21:42:40 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/pages/open/all-tasks.blade.php
+++ resources/views/livewire/pages/open/all-tasks.blade.php
@@ -3,28 +3,28 @@
     <div class="card mt-3">
         <div class="card-body">
             @if (!$readyToLoad)
-            <div class="card-body text-center mt-3">
-                <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-                <div class="h6">
-                    Loading all tasks stats...
+                <div class="card-body text-center mt-3">
+                    <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+                    <div class="h6">
+                        Loading all tasks stats...
+                    </div>
                 </div>
-            </div>
             @endif
             <div id="allTasks"></div>
         </div>
     </div>
 
     @if ($readyToLoad)
-    <script>
-    var options = {
-        chart: { type: 'bar', height: 300, animations: { enabled: false }},
-        dataLabels: { enabled: false },
-        series: [{name: 'Tasks', data: <?php echo $all_tasks ?> }],
-        xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
-    }
+        <script>
+        var options = {
+            chart: { type: 'bar', height: 300, animations: { enabled: false }},
+            dataLabels: { enabled: false },
+            series: [{name: 'Tasks', data: <?php echo $all_tasks ?> }],
+            xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
+        }
 
-    var allTasks = new ApexCharts(document.querySelector("#allTasks"), options);
-    allTasks.render();
-    </script>
+        var allTasks = new ApexCharts(document.querySelector("#allTasks"), options);
+        allTasks.render();
+        </script>
     @endif
 </div>
