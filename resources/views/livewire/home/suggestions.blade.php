bdbbab939caf838903b31ad93f5ce8e7a573a3ec
Yogi <yoginth@hey.com>
2021-05-15 13:03:57 +0530

    Fix spacings

--- resources/views/livewire/home/suggestions.blade.php
+++ resources/views/livewire/home/suggestions.blade.php
@@ -3,28 +3,26 @@
         Who to follow <x-new />
     </div>
     <div class="card mb-4">
-        <div class="pt-2 pb-2">
-            @if (!$readyToLoad)
-            <div class="card-body text-center">
-                <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
-            </div>
-            @endif
-            @if ($readyToLoad and count($users) === 0)
-            <div class="card-body text-center fw-bold text-secondary">
-                <x-heroicon-o-user class="heroicon-2x text-primary" />
-                Nothing to suggest!
-            </div>
-            @endif
-            <ul class="list-group list-group-flush">
-                @foreach ($users as $user)
-                <li class="list-group-item d-flex align-items-center justify-content-between" wire:key="{{ $user->id }}">
-                    <x:shared.user :user="$user" />
-                    <span>
-                        <livewire:home.follow :user="$user" :showText="$showText" :wire:key="$user->id" />
-                    </span>
-                </li>
-                @endforeach
-            </ul>
+        @if (!$readyToLoad)
+        <div class="card-body text-center">
+            <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
         </div>
+        @endif
+        @if ($readyToLoad and count($users) === 0)
+        <div class="card-body text-center fw-bold text-secondary">
+            <x-heroicon-o-user class="heroicon-2x text-primary" />
+            Nothing to suggest!
+        </div>
+        @endif
+        <ul class="list-group list-group-flush">
+            @foreach ($users as $user)
+            <li class="list-group-item d-flex align-items-center justify-content-between py-2" wire:key="{{ $user->id }}">
+                <x:shared.user :user="$user" />
+                <span>
+                    <livewire:home.follow :user="$user" :showText="$showText" :wire:key="$user->id" />
+                </span>
+            </li>
+            @endforeach
+        </ul>
     </div>
 </div>
