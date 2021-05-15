1056483093a57702f10a2276dfd09faf47aca814
Yogi <yoginth@hey.com>
2021-05-15 21:38:36 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/notification/type/product/member-left.blade.php
+++ resources/views/livewire/notification/type/product/member-left.blade.php
@@ -1,13 +1,13 @@
 <div>
     @if ($product)
-    <div class="mt-2 text-secondary">
-        left from the product
-        <a class="fw-bold" href="{{ route('product.done', ['slug' => $product->slug]) }}">
-            <img loading=lazy class="rounded avatar-20 ms-1 me-1 mb-1" src="{{ Helper::getCDNImage($product->avatar, 80) }}" height="20" width="20" />
-            {{ $product->name }}
-        </a>
-    </div>
+        <div class="mt-2 text-secondary">
+            left from the product
+            <a class="fw-bold" href="{{ route('product.done', ['slug' => $product->slug]) }}">
+                <img loading=lazy class="rounded avatar-20 ms-1 me-1 mb-1" src="{{ Helper::getCDNImage($product->avatar, 80) }}" height="20" width="20" />
+                {{ $product->name }}
+            </a>
+        </div>
     @else
-    <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
+        <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
     @endif
 </div>
