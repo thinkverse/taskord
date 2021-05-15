f71bbe9812ea4e8adcc2aaa462e424470d5410e6
Yogi <yoginth@hey.com>
2021-05-15 20:08:02 +0530

    Add staff badge

--- resources/views/components/shared/user-label-small.blade.php
+++ resources/views/components/shared/user-label-small.blade.php
@@ -28,6 +28,11 @@
                     <x-heroicon-s-star class="heroicon text-gold" />
                 </a>
             @endif
+            @if ($user->isStaff)
+                <span title="Staff">
+                    <x-heroicon-o-shield-check class="heroicon text-primary" />
+                </span>
+            @endif
             <span class="small text-secondary fw-normal">{{ "@" . $user->username }}</span>
         </a>
     </div>
