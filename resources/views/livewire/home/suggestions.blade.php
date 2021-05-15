1f0ab754cdec3e04650b63457d21f9cf77c25d63
Yogi <yoginth@hey.com>
2021-05-15 21:27:01 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/home/suggestions.blade.php
+++ resources/views/livewire/home/suggestions.blade.php
@@ -4,24 +4,24 @@
     </div>
     <div class="card mb-4">
         @if (!$readyToLoad)
-        <div class="card-body text-center">
-            <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
-        </div>
+            <div class="card-body text-center">
+                <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
+            </div>
         @endif
         @if ($readyToLoad and count($users) === 0)
-        <div class="card-body text-center fw-bold text-secondary">
-            <x-heroicon-o-user class="heroicon-2x text-primary" />
-            Nothing to suggest!
-        </div>
+            <div class="card-body text-center fw-bold text-secondary">
+                <x-heroicon-o-user class="heroicon-2x text-primary" />
+                Nothing to suggest!
+            </div>
         @endif
         <ul class="list-group list-group-flush">
             @foreach ($users as $user)
-            <li class="list-group-item d-flex align-items-center justify-content-between py-2" wire:key="{{ $user->id }}">
-                <x:shared.user-label-big :user="$user" />
-                <span>
-                    <livewire:home.follow :user="$user" :showText="$showText" :wire:key="$user->id" />
-                </span>
-            </li>
+                <li class="list-group-item d-flex align-items-center justify-content-between py-2" wire:key="{{ $user->id }}">
+                    <x:shared.user-label-big :user="$user" />
+                    <span>
+                        <livewire:home.follow :user="$user" :showText="$showText" :wire:key="$user->id" />
+                    </span>
+                </li>
             @endforeach
         </ul>
     </div>
