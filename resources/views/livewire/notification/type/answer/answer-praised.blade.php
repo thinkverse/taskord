85ff53fbe669fce7408b8ca833d1c93e7733b79d
Yogi <yoginth@hey.com>
2021-05-15 21:38:07 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/notification/type/answer/answer-praised.blade.php
+++ resources/views/livewire/notification/type/answer/answer-praised.blade.php
@@ -1,15 +1,15 @@
 <div>
     @if ($answer)
-    <div class="mt-2 text-secondary">
-        praised your
-        <a class="fw-bold" href="{{ route('question.question', ['id' => $answer->question->id]) }}">
-            answer
-        </a>
-    </div>
-    <div class="card mt-3">
-        <livewire:answer.single-answer :answer="$answer" :wire:key="$answer->id" />
-    </div>
+        <div class="mt-2 text-secondary">
+            praised your
+            <a class="fw-bold" href="{{ route('question.question', ['id' => $answer->question->id]) }}">
+                answer
+            </a>
+        </div>
+        <div class="card mt-3">
+            <livewire:answer.single-answer :answer="$answer" :wire:key="$answer->id" />
+        </div>
     @else
-    <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
+        <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
     @endif
 </div>
--- resources/views/livewire/notification/type/answer/answered.blade.php
+++ resources/views/livewire/notification/type/answer/answered.blade.php
@@ -1,15 +1,15 @@
 <div>
     @if ($answer)
-    <div class="mt-2 text-secondary">
-        answered to your
-        <a class="fw-bold" href="{{ route('question.question', ['id' => $answer->id]) }}">
-            question
-        </a>
-    </div>
-    <div class="card mt-3">
-        <livewire:answer.single-answer :answer="$answer" :wire:key="$answer->id" />
-    </div>
+        <div class="mt-2 text-secondary">
+            answered to your
+            <a class="fw-bold" href="{{ route('question.question', ['id' => $answer->id]) }}">
+                question
+            </a>
+        </div>
+        <div class="card mt-3">
+            <livewire:answer.single-answer :answer="$answer" :wire:key="$answer->id" />
+        </div>
     @else
-    <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
+        <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
     @endif
 </div>
