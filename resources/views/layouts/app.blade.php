c219859bd807047ea2aa039d59dd5c9ba01a16bf
Yogi <yoginth@hey.com>
2021-05-15 20:26:35 +0530

    refactor: Fix blade template alignment

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,36 +18,34 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <title>@yield('pageTitle') Taskord</title>
     @auth
-    @if (auth()->user()->isBeta)
-    <link rel="icon" href="https://ik.imagekit.io/taskordimg/beta_J6zazpyIw.svg" sizes="any" type="image/svg+xml">
-    @else
-    <link rel="icon" href="https://ik.imagekit.io/taskordimg/logo_8lLu9EPFa.svg" sizes="any" type="image/svg+xml">
-    @endif
+        @if (auth()->user()->isBeta)
+            <link rel="icon" href="https://ik.imagekit.io/taskordimg/beta_J6zazpyIw.svg" sizes="any" type="image/svg+xml">
+        @else
+            <link rel="icon" href="https://ik.imagekit.io/taskordimg/logo_8lLu9EPFa.svg" sizes="any" type="image/svg+xml">
+        @endif
     @endauth
     @guest
-    <link rel="icon" href="https://ik.imagekit.io/taskordimg/logo_8lLu9EPFa.svg" sizes="any" type="image/svg+xml">
+        <link rel="icon" href="https://ik.imagekit.io/taskordimg/logo_8lLu9EPFa.svg" sizes="any" type="image/svg+xml">
     @endguest
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     @auth
-    @if (auth()->user()->isPatron or auth()->user()->isStaff)
-    @if (auth()->user()->darkMode)
-    <link href="{{ mix('css/darkmode.css') }}" rel="stylesheet">
-    @endif
-    @endif
+        @if (auth()->user()->isPatron or auth()->user()->isStaff)
+            @if (auth()->user()->darkMode)
+                <link href="{{ mix('css/darkmode.css') }}" rel="stylesheet">
+            @endif
+        @endif
     @endauth
     <livewire:styles />
 </head>
 <body>
     <div id="app">
         @auth
-        @if (auth()->user()->isStaff)
-            @if (auth()->user()->staffShip)
-            <div class="admin-bar">
-                <livewire:admin.adminbar />
-            </div>
+            @if (auth()->user()->isStaff and auth()->user()->staffShip)
+                <div class="admin-bar">
+                    <livewire:admin.adminbar />
+                </div>
             @endif
-        @endif
         @endauth
         @include('layouts.navbar')
         @if (session()->has('global'))
@@ -58,30 +56,30 @@
             </div>
         @endif
         @auth
-        @if (auth()->user()->isFlagged)
-        <div class="alert alert-danger rounded-0" role="alert">
-            <div class="fw-bold">
-                <x-heroicon-o-flag class="heroicon" />
-                Your account has been flagged.
-            </div>
-            <div class="mt-1">
-                Because of that, your profile will be hidden from the public. If you believe this is a mistake, <a href="https://forms.clickup.com/f/357rd-767/8PPJM0SN435CWUX4X2" target="_blank" rel="noreferrer">contact support</a> to have your account status reviewed.
-            </div>
-        </div>
-        @endif
-        @if (!auth()->user()->hasVerifiedEmail())
-        <div class="alert alert-warning rounded-0" role="alert">
-            <div class="fw-bold">
-                <x-heroicon-o-mail class="heroicon" />
-                Verify Your Email Address
-            </div>
-            <form class="mt-1" method="POST" action="{{ route('verification.resend') }}">
-                @csrf
-                Before proceeding, please check your email for a verification link. If you did not receive the email,
-                <button type="submit" class="align-baseline btn m-0 p-0 rm-shadow text-primary">click here to request another</button>.
-            </form>
-        </div>
-        @endif
+            @if (auth()->user()->isFlagged)
+                <div class="alert alert-danger rounded-0" role="alert">
+                    <div class="fw-bold">
+                        <x-heroicon-o-flag class="heroicon" />
+                        Your account has been flagged.
+                    </div>
+                    <div class="mt-1">
+                        Because of that, your profile will be hidden from the public. If you believe this is a mistake, <a href="https://forms.clickup.com/f/357rd-767/8PPJM0SN435CWUX4X2" target="_blank" rel="noreferrer">contact support</a> to have your account status reviewed.
+                    </div>
+                </div>
+            @endif
+            @if (!auth()->user()->hasVerifiedEmail())
+                <div class="alert alert-warning rounded-0" role="alert">
+                    <div class="fw-bold">
+                        <x-heroicon-o-mail class="heroicon" />
+                        Verify Your Email Address
+                    </div>
+                    <form class="mt-1" method="POST" action="{{ route('verification.resend') }}">
+                        @csrf
+                        Before proceeding, please check your email for a verification link. If you did not receive the email,
+                        <button type="submit" class="align-baseline btn m-0 p-0 rm-shadow text-primary">click here to request another</button>.
+                    </form>
+                </div>
+            @endif
         @endauth
         @guest
             @if (Route::current()->getName() === 'home')
@@ -99,15 +97,15 @@
 <script src="{{ mix('js/bootstrap.js') }}" defer></script>
 <script src="{{ mix('js/app.js') }}" defer></script>
 @if (App::environment() === 'local')
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-98MP737L0B"></script>
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-98MP737L0B', {
-        'user_id': "{{ auth()->check() ? auth()->user()->username.'_'.auth()->user()->id : '' }}"
-    });
-</script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-98MP737L0B"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-98MP737L0B', {
+            'user_id': "{{ auth()->check() ? auth()->user()->username.'_'.auth()->user()->id : '' }}"
+        });
+    </script>
 @endif
 @yield('scripts')
 </html>
