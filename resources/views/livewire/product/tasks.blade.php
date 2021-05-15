63aa76b10986e3e0b8dc8fe0a3b00fa14d9ab970
Yogi <yoginth@hey.com>
2021-05-15 21:45:22 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/product/tasks.blade.php
+++ resources/views/livewire/product/tasks.blade.php
@@ -1,34 +1,34 @@
 <div wire:init="loadTasks">
     @if (!$readyToLoad)
-    <div class="card-body text-center mt-3 mb-3">
-        <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-        <div class="h6">
-            Loading tasks...
+        <div class="card-body text-center mt-3 mb-3">
+            <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+            <div class="h6">
+                Loading tasks...
+            </div>
         </div>
-    </div>
     @endif
     @if ($readyToLoad and count($tasks) === 0)
-    @php
-    if ($type === 'product.done') {
-        $message = 'No completed todos found';
-    } else {
-        $message = 'All Done';
-    }
-    @endphp
-    <div class="card-body text-center mt-3 mb-3">
-        <x-heroicon-o-check-circle class="heroicon-4x text-primary mb-2" />
-        <div class="h4">
-            {{ $message }}
+        @php
+            if ($type === 'product.done') {
+                $message = 'No completed todos found';
+            } else {
+                $message = 'All Done';
+            }
+        @endphp
+        <div class="card-body text-center mt-3 mb-3">
+            <x-heroicon-o-check-circle class="heroicon-4x text-primary mb-2" />
+            <div class="h4">
+                {{ $message }}
+            </div>
         </div>
-    </div>
     @endif
     @if ($page === 1)
-    <ul class="list-group">
+        <ul class="list-group">
     @endif
-    @foreach ($tasks as $task)
-    <li class="list-group-item p-3 {{($loop->index == 0 and $page > 1) ? "border-top-0 rounded-0" : ""}}">
-        <livewire:task.single-task :task="$task" :wire:key="$task->id" />
-    </li>
+        @foreach ($tasks as $task)
+        <li class="list-group-item p-3 {{($loop->index == 0 and $page > 1) ? "border-top-0 rounded-0" : ""}}">
+            <livewire:task.single-task :task="$task" :wire:key="$task->id" />
+        </li>
     @endforeach
     @if ($readyToLoad and $tasks->hasMorePages())
         @livewire('product.load-more', [
@@ -38,6 +38,6 @@
         ])
     @endif
     @if ($page === 1)
-    </ul>
+        </ul>
     @endif
 </div>
