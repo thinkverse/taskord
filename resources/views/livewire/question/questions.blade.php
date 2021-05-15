7d0beb2ffc83da9fac41a7dae568fc1f0ae32f91
Yogi <yoginth@hey.com>
2021-05-15 21:47:37 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/question/questions.blade.php
+++ resources/views/livewire/question/questions.blade.php
@@ -1,19 +1,19 @@
 <div wire:init="loadQuestions">
     @if (!$readyToLoad)
-    <div class="card-body text-center mt-3 mb-3">
-        <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-        <div class="h6">
-            Loading questions...
+        <div class="card-body text-center mt-3 mb-3">
+            <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+            <div class="h6">
+                Loading questions...
+            </div>
         </div>
-    </div>
     @endif
     @if ($readyToLoad and count($questions) === 0)
-    <div class="card-body text-center mt-3 mb-3">
-        <x-heroicon-o-question-mark-circle class="heroicon-4x text-primary mb-2" />
-        <div class="h4">
-            No questions asked
+        <div class="card-body text-center mt-3 mb-3">
+            <x-heroicon-o-question-mark-circle class="heroicon-4x text-primary mb-2" />
+            <div class="h4">
+                No questions asked
+            </div>
         </div>
-    </div>
     @endif
     @foreach ($questions as $question)
         @livewire('question.single-question', [
