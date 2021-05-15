d6565418b9a0920e5e4dbd39265e4a46b6780d96
Yogi <yoginth@hey.com>
2021-05-15 20:23:10 +0530

    refactor: Fix blade template alignment

--- resources/views/explore/nav.blade.php
+++ resources/views/explore/nav.blade.php
@@ -3,13 +3,13 @@
         <a class="nav-link @if (Route::currentRouteName() === 'explore.explore') active @endif" href="{{ route('explore.explore') }}">Popular Tasks</a>
     </li>
     @if (feature('explore_makers'))
-    <li class="nav-item px-1">
-        <a class="nav-link @if (Route::currentRouteName() === 'explore.makers') active @endif" href="{{ route('explore.makers') }}">Makers <x-staffship /></a>
-    </li>
+        <li class="nav-item px-1">
+            <a class="nav-link @if (Route::currentRouteName() === 'explore.makers') active @endif" href="{{ route('explore.makers') }}">Makers <x-staffship /></a>
+        </li>
     @endif
     @if (feature('explore_products'))
-    <li class="nav-item px-1">
-        <a class="nav-link" href="#">Products <x-staffship /></a>
-    </li>
+        <li class="nav-item px-1">
+            <a class="nav-link" href="#">Products <x-staffship /></a>
+        </li>
     @endif
 </ul>
