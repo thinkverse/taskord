9364f55f54e8b6fedc6aba9c0679e7b4d0316959
Yogi <yoginth@hey.com>
2021-05-15 20:36:33 +0530

    refactor: Fix blade template alignment

--- resources/views/question/edit.blade.php
+++ resources/views/question/edit.blade.php
@@ -8,14 +8,14 @@
         <div class="col-lg-7">
             @auth
                 @if (!auth()->user()->isFlagged)
-                <livewire:question.edit-question :question="$question" />
+                    <livewire:question.edit-question :question="$question" />
                 @else
-                <div class="text-center">
-                    <div class="alert alert-danger" role="alert">
-                        You can't edit this question, because your account has been flagged 😢
+                    <div class="text-center">
+                        <div class="alert alert-danger" role="alert">
+                            You can't edit this question, because your account has been flagged 😢
+                        </div>
+                        <a class="btn btn-primary" href="{{ route('home') }}">Go to home</a>
                     </div>
-                    <a class="btn btn-primary" href="{{ route('home') }}">Go to home</a>
-                </div>
                 @endif
             @endauth
         </div>
