25ef3e617edee59e738eda1b20600636a59d4d4c
Yogi <yoginth@hey.com>
2021-05-15 21:38:56 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/notification/type/question/notify-subscribers.blade.php
+++ resources/views/livewire/notification/type/question/notify-subscribers.blade.php
@@ -1,16 +1,16 @@
 <div>
     @if ($answer and $answer->question)
-    <div class="mt-2 text-secondary">
-        answered to the
-        <a class="fw-bold" href="{{ route('question.question', ['id' => $answer->question->id]) }}">
-            question
-        </a>
-        you subscribed
-        <div class="card mt-3">
-            <livewire:answer.single-answer :answer="$answer" :wire:key="$answer->id" />
+        <div class="mt-2 text-secondary">
+            answered to the
+            <a class="fw-bold" href="{{ route('question.question', ['id' => $answer->question->id]) }}">
+                question
+            </a>
+            you subscribed
+            <div class="card mt-3">
+                <livewire:answer.single-answer :answer="$answer" :wire:key="$answer->id" />
+            </div>
         </div>
-    </div>
     @else
-    <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
+        <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
     @endif
 </div>
