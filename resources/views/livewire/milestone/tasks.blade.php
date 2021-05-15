4a2735a6452c5881c1c0fc4c719b47a0b61c7b5c
Yogi <yoginth@hey.com>
2021-05-15 21:37:48 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/milestone/tasks.blade.php
+++ resources/views/livewire/milestone/tasks.blade.php
@@ -1,32 +1,32 @@
 <div wire:init="loadTasks">
     @if (!$readyToLoad)
-    <div class="card-body text-center mt-3 mb-3">
-        <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-        <div class="h6">
-            Loading tasks...
+        <div class="card-body text-center mt-3 mb-3">
+            <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+            <div class="h6">
+                Loading tasks...
+            </div>
         </div>
-    </div>
     @endif
     @if ($readyToLoad and count($tasks) === 0)
-    <div class="card-body text-center mt-3 mb-3">
-        <x-heroicon-o-check-circle class="heroicon-4x text-primary mb-2" />
-        <div class="h4">
-            No tasks found for this milestone
+        <div class="card-body text-center mt-3 mb-3">
+            <x-heroicon-o-check-circle class="heroicon-4x text-primary mb-2" />
+            <div class="h4">
+                No tasks found for this milestone
+            </div>
         </div>
-    </div>
     @endif
     @if ($page === 1)
-    <ul class="list-group">
+        <ul class="list-group">
     @endif
     @foreach ($tasks as $task)
-    <li class="list-group-item p-3 {{($loop->index == 0 and $page > 1) ? "border-top-0 rounded-0" : ""}}">
-        <livewire:task.single-task :task="$task" :wire:key="$task->id" />
-    </li>
+        <li class="list-group-item p-3 {{($loop->index == 0 and $page > 1) ? "border-top-0 rounded-0" : ""}}">
+            <livewire:task.single-task :task="$task" :wire:key="$task->id" />
+        </li>
     @endforeach
     @if ($readyToLoad and $tasks->hasMorePages())
         <livewire:milestone.load-more-tasks :milestone="$task->milestone" :page="$page" />
     @endif
     @if ($page === 1)
-    </ul>
+        </ul>
     @endif
 </div>
