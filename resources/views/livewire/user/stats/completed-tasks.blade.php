b6785722c27b6dc51872cb5ef69e2b89b43d7491
Yogi <yoginth@hey.com>
2021-05-16 08:25:54 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/user/stats/completed-tasks.blade.php
+++ resources/views/livewire/user/stats/completed-tasks.blade.php
@@ -3,28 +3,28 @@
     <div class="card mt-3">
         <div class="card-body">
             @if (!$readyToLoad)
-            <div class="card-body text-center mt-3">
-                <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-                <div class="h6">
-                    Loading completed tasks stats...
+                <div class="card-body text-center mt-3">
+                    <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+                    <div class="h6">
+                        Loading completed tasks stats...
+                    </div>
                 </div>
-            </div>
             @endif
             <div id="completedTasks"></div>
         </div>
     </div>
 
     @if ($readyToLoad)
-    <script>
-    var options = {
-        chart: { type: 'bar', height: 300, animations: { enabled: false }},
-        dataLabels: { enabled: false },
-        series: [{name: 'Tasks', data: <?php echo $completed_tasks ?> }],
-        xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
-    }
+        <script>
+        var options = {
+            chart: { type: 'bar', height: 300, animations: { enabled: false }},
+            dataLabels: { enabled: false },
+            series: [{name: 'Tasks', data: <?php echo $completed_tasks ?> }],
+            xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
+        }
 
-    var completedTasks = new ApexCharts(document.querySelector("#completedTasks"), options);
-    completedTasks.render();
-    </script>
+        var completedTasks = new ApexCharts(document.querySelector("#completedTasks"), options);
+        completedTasks.render();
+        </script>
     @endif
 </div>
