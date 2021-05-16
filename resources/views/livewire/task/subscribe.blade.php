cdcc47637b038a64d7a0a09d7bb6494997483039
Yogi <yoginth@hey.com>
2021-05-16 08:19:58 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/task/subscribe.blade.php
+++ resources/views/livewire/task/subscribe.blade.php
@@ -1,16 +1,16 @@
 <div>
     @auth
-    @if (auth()->user()->hasSubscribed($task))
-    <button wire:click="subscribeTask" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
-        <x-heroicon-o-status-offline class="heroicon" />
-        Unsubscribe
-    </button>
-    @else
-    <button wire:click="subscribeTask" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
-        <x-heroicon-o-status-online class="heroicon" />
-        Subscribe
-    </button>
-    @endif
+        @if (auth()->user()->hasSubscribed($task))
+            <button wire:click="subscribeTask" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
+                <x-heroicon-o-status-offline class="heroicon" />
+                Unsubscribe
+            </button>
+        @else
+            <button wire:click="subscribeTask" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
+                <x-heroicon-o-status-online class="heroicon" />
+                Subscribe
+            </button>
+        @endif
     @endauth
     <span class="small ms-2">
         <span class="fw-bold">{{ number_format($task->subscribersCount()) }}</span>
