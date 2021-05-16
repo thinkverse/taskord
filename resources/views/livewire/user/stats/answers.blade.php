972737622e7bbd346fcc5647866f780b9a13bd36
Yogi <yoginth@hey.com>
2021-05-16 08:25:34 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/user/stats/answers.blade.php
+++ resources/views/livewire/user/stats/answers.blade.php
@@ -3,28 +3,28 @@
     <div class="card mt-3">
         <div class="card-body">
             @if (!$readyToLoad)
-            <div class="card-body text-center mt-3">
-                <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-                <div class="h6">
-                    Loading answers stats...
+                <div class="card-body text-center mt-3">
+                    <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+                    <div class="h6">
+                        Loading answers stats...
+                    </div>
                 </div>
-            </div>
             @endif
             <div id="answers"></div>
         </div>
     </div>
 
     @if ($readyToLoad)
-    <script>
-    var options = {
-        chart: { type: 'bar', height: 300, animations: { enabled: false }},
-        dataLabels: { enabled: false },
-        series: [{name: 'Answers', data: <?php echo $answers ?> }],
-        xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
-    }
+        <script>
+        var options = {
+            chart: { type: 'bar', height: 300, animations: { enabled: false }},
+            dataLabels: { enabled: false },
+            series: [{name: 'Answers', data: <?php echo $answers ?> }],
+            xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
+        }
 
-    var answers = new ApexCharts(document.querySelector("#answers"), options);
-    answers.render();
-    </script>
+        var answers = new ApexCharts(document.querySelector("#answers"), options);
+        answers.render();
+        </script>
     @endif
 </div>
