38a151ed3e6cd615902d8ac6363c684d34529691
Yogi <yoginth@hey.com>
2021-05-16 18:26:28 +0530

    Add product card

--- app/Http/Livewire/Product/SingleProduct.php
+++ app/Http/Livewire/Product/SingleProduct.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire\Product;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class SingleProduct extends Component
 {
+    public Product $product;
+
+    public function mount($product)
+    {
+        $this->product = $product;
+    }
+
     public function render()
     {
         return view('livewire.product.single-product');
