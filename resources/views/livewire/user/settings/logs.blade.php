dd8775c191ca180fbe5a7404b3cc6df07f427a5b
Yogi <yoginth@hey.com>
2021-05-16 08:23:07 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/user/settings/logs.blade.php
+++ resources/views/livewire/user/settings/logs.blade.php
@@ -5,17 +5,17 @@
             <div>Recent events that happend on your account</div>
         </div>
         @if (count($activities) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-collection class="heroicon-4x text-primary mb-2" />
-            <div class="h4">
-                Nothing has been logged!
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-collection class="heroicon-4x text-primary mb-2" />
+                <div class="h4">
+                    Nothing has been logged!
+                </div>
             </div>
-        </div>
         @endif
         <ul class="list-group list-group-flush">
-        @foreach ($activities as $activity)
-            <x-user-activity :activity="$activity" />
-        @endforeach
+            @foreach ($activities as $activity)
+                <x-user-activity :activity="$activity" />
+            @endforeach
         </ul>
         <div class="mt-3">
             {{ $activities->links() }}
