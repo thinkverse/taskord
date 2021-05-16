b45d7180d2c7d0ee11c01deb5a3668594ce683cc
Yogi <yoginth@hey.com>
2021-05-16 08:27:32 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/user/graph.blade.php
+++ resources/views/livewire/user/graph.blade.php
@@ -1,48 +1,48 @@
 <div wire:init="loadGraph">
     @if (!$readyToLoad)
-    <div class="card-body">
-        <div class="spinner-border spinner-border-sm taskord-spinner text-secondary me-2" role="status"></div>
-        Loading activity graph...
-    </div>
+        <div class="card-body">
+            <div class="spinner-border spinner-border-sm taskord-spinner text-secondary me-2" role="status"></div>
+            Loading activity graph...
+        </div>
     @else
         @if ($count === 0)
-        <div class="card-body">
-            This user has no activity
-        </div>
+            <div class="card-body">
+                This user has no activity
+            </div>
         @else
-        <div id="activityGraph"></div>
+            <div id="activityGraph"></div>
         @endif
     @endif
     @if ($readyToLoad)
-    <script>
-    var options = {
-        chart: {
-            type: 'area',
-            height: 100,
-            animations: { enabled: false },
-            toolbar: { show: false }
-        },
-        colors: ["#198754"],
-        stroke: { width: [2, 1], curve: 'smooth' },
-        tooltip: { enabled: false },
-        grid: { show: false },
-        dataLabels: { enabled: false },
-        series: [{name: 'Tasks', data: <?php echo $tasks ?> }],
-        xaxis: {
-            categories: <?php echo $week_dates ?>,
-            labels: { show: false },
-            axisTicks: { show: false },
-            axisBorder: { show: false }
-        },
-        yaxis: {
-            labels: { show: false },
-            axisTicks: { show: false },
-            axisBorder: { show: false }
+        <script>
+        var options = {
+            chart: {
+                type: 'area',
+                height: 100,
+                animations: { enabled: false },
+                toolbar: { show: false }
+            },
+            colors: ["#198754"],
+            stroke: { width: [2, 1], curve: 'smooth' },
+            tooltip: { enabled: false },
+            grid: { show: false },
+            dataLabels: { enabled: false },
+            series: [{name: 'Tasks', data: <?php echo $tasks ?> }],
+            xaxis: {
+                categories: <?php echo $week_dates ?>,
+                labels: { show: false },
+                axisTicks: { show: false },
+                axisBorder: { show: false }
+            },
+            yaxis: {
+                labels: { show: false },
+                axisTicks: { show: false },
+                axisBorder: { show: false }
+            }
         }
-    }
 
-    var activityGraph = new ApexCharts(document.querySelector("#activityGraph"), options);
-    activityGraph.render();
-    </script>
+        var activityGraph = new ApexCharts(document.querySelector("#activityGraph"), options);
+        activityGraph.render();
+        </script>
     @endif
 </div>
