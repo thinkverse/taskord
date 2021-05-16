d2c74a21f937d4500c3e5d04902cff43f439926a
Yogi <yoginth@hey.com>
2021-05-16 20:26:01 +0530

    refactor: Move to product folder in notification

--- app/Http/Livewire/Product/Update/NewUpdate.php
+++ app/Http/Livewire/Product/Update/NewUpdate.php
@@ -4,7 +4,7 @@ namespace App\Http\Livewire\Product\Update;
 
 use App\Models\Product;
 use App\Models\ProductUpdate;
-use App\Notifications\NewProductUpdate;
+use App\Notifications\Product\NewProductUpdate;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
