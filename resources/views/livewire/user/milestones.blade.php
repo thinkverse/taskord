4a19a02bf07b14453130f1ea22d770b7836cc3d6
Yogi <yoginth@hey.com>
2021-05-16 08:27:44 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/user/milestones.blade.php
+++ resources/views/livewire/user/milestones.blade.php
@@ -1,19 +1,19 @@
 <div wire:init="loadMilestones">
     @if (!$readyToLoad)
-    <div class="card-body text-center mt-3 mb-3">
-        <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-        <div class="h6">
-            Loading milestones...
+        <div class="card-body text-center mt-3 mb-3">
+            <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+            <div class="h6">
+                Loading milestones...
+            </div>
         </div>
-    </div>
     @endif
     @if ($readyToLoad and count($milestones) === 0)
-    <div class="card-body text-center mt-3 mb-3">
-        <x-heroicon-o-chat-alt-2 class="heroicon-4x text-primary mb-2" />
-        <div class="h4">
-            No milestones made
+        <div class="card-body text-center mt-3 mb-3">
+            <x-heroicon-o-chat-alt-2 class="heroicon-4x text-primary mb-2" />
+            <div class="h4">
+                No milestones made
+            </div>
         </div>
-    </div>
     @endif
     @foreach ($milestones as $milestone)
         @livewire('milestone.single-milestone', [
