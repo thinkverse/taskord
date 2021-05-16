b17032b74fb8d962c4df6f8d4aed2e98c9993c7e
Yogi <yoginth@hey.com>
2021-05-16 08:25:44 +0530

    refactor: Fix blade template alignment

--- resources/views/livewire/user/stats/comments.blade.php
+++ resources/views/livewire/user/stats/comments.blade.php
@@ -3,28 +3,28 @@
     <div class="card mt-3">
         <div class="card-body">
             @if (!$readyToLoad)
-            <div class="card-body text-center mt-3">
-                <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-                <div class="h6">
-                    Loading comments stats...
+                <div class="card-body text-center mt-3">
+                    <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
+                    <div class="h6">
+                        Loading comments stats...
+                    </div>
                 </div>
-            </div>
             @endif
             <div id="comments"></div>
         </div>
     </div>
 
     @if ($readyToLoad)
-    <script>
-    var options = {
-        chart: { type: 'bar', height: 300, animations: { enabled: false }},
-        dataLabels: { enabled: false },
-        series: [{name: 'Comments', data: <?php echo $comments ?> }],
-        xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
-    }
+        <script>
+        var options = {
+            chart: { type: 'bar', height: 300, animations: { enabled: false }},
+            dataLabels: { enabled: false },
+            series: [{name: 'Comments', data: <?php echo $comments ?> }],
+            xaxis: { categories: <?php echo $week_dates ?>, labels: { show: false } }
+        }
 
-    var comments = new ApexCharts(document.querySelector("#comments"), options);
-    comments.render();
-    </script>
+        var comments = new ApexCharts(document.querySelector("#comments"), options);
+        comments.render();
+        </script>
     @endif
 </div>
