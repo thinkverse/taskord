ed6bf6e3dcae5fe200a2c7a53f27f3e15c8d7c7e
Yogi <yoginth@hey.com>
2021-05-18 17:46:59 +0530

    Remove padding

--- resources/views/components/shared/toast.blade.php
+++ resources/views/components/shared/toast.blade.php
@@ -1,5 +1,5 @@
-<div class="position-fixed top-0 end-0 p-3" style="z-index: 5">
-    <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+<div class="position-fixed top-0 end-0" style="z-index: 5">
+    <div id="liveToast" class="toast hide m-3" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
             <strong class="me-auto" id="toast-title"></strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
