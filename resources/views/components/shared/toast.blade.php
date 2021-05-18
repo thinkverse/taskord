5ced42e05a538bdbdf37fbcb1193fb15857230af
Yogi <yoginth@hey.com>
2021-05-18 17:37:41 +0530

    Add component for toast

---
+++ resources/views/components/shared/toast.blade.php
@@ -0,0 +1,9 @@
+<div class="position-fixed bottom-0 end-0 p-3" style="z-index: 5">
+    <div id="liveToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header">
+            <strong class="me-auto" id="toast-title"></strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body" id="toast-body"></div>
+    </div>
+</div>
