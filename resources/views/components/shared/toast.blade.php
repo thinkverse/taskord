27f73e42c08b2b281b15d237e690eadf7b58abab
Yogi <yoginth@hey.com>
2021-05-19 11:16:27 +0530

    Update icon

--- resources/views/components/shared/toast.blade.php
+++ resources/views/components/shared/toast.blade.php
@@ -1,17 +1,10 @@
-<div class="toast show d-flex align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
-    <div>
-        ✅
-    </div>
-    <div class="toast-body d-flex justify-content-between align-items-center py-3">
-        <div>Hello, world! This is a toast message.</div>
-        <button type="button" class="btn-close btn-sm" data-bs-dismiss="toast" aria-label="Close"></button>
-    </div>
-</div>
-
 <div class="position-fixed top-0 end-0" style="z-index: 5">
     <div id="liveToast" class="toast hide m-3" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-body d-flex justify-content-between align-items-center py-3">
-            <div id="toast-body"></div>
+            <div class="d-flex align-items-center">
+                <div id="toast-icon" class="me-2"></div>
+                <div id="toast-body"></div>
+            </div>
             <button type="button" class="btn-close btn-sm" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
     </div>
