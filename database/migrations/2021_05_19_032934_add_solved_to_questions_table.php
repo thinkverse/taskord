803cae5ec3771cc74aea8bdb430fca1536f9b340
Yogi <yoginth@hey.com>
2021-05-19 09:01:51 +0530

    Add is_solvable and solved column

--- database/migrations/2021_05_19_032934_add_solved_to_questions_table.php
+++ database/migrations/2021_05_19_032934_add_solved_to_questions_table.php
@@ -14,7 +14,8 @@ class AddSolvedToQuestionsTable extends Migration
     public function up()
     {
         Schema::table('questions', function (Blueprint $table) {
-            //
+            $table->boolean('is_solvable')->default(true);
+            $table->boolean('solved')->default(false);
         });
     }
 
@@ -26,7 +27,8 @@ class AddSolvedToQuestionsTable extends Migration
     public function down()
     {
         Schema::table('questions', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_solvable');
+            $table->dropColumn('solved');
         });
     }
 }
