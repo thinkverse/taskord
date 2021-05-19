8609dc4340b3c98c47d21e751a54628e2a5d9f96
Yogi <yoginth@hey.com>
2021-05-19 10:46:30 +0530

    Toast on dark mode

--- resources/views/components/shared/toast.blade.php
+++ resources/views/components/shared/toast.blade.php
@@ -1,10 +1,13 @@
-<div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
-    <div class="toast-body d-flex justify-content-between align-items-center py-3">
-      <div>Hello, world! This is a toast message.</div>
-      <button type="button" class="btn-close btn-sm" data-bs-dismiss="toast" aria-label="Close"></button>
+<div class="toast show d-flex align-items-center" role="alert" aria-live="assertive" aria-atomic="true">
+    <div>
+        ✅
     </div>
-  </div>
-  
+    <div class="toast-body d-flex justify-content-between align-items-center py-3">
+        <div>Hello, world! This is a toast message.</div>
+        <button type="button" class="btn-close btn-sm" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+</div>
+
 <div class="position-fixed top-0 end-0" style="z-index: 5">
     <div id="liveToast" class="toast hide m-3" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-body d-flex justify-content-between align-items-center py-3">
