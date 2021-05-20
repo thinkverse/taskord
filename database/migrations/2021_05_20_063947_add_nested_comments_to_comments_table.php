a62ea1eb04a58543b1ba92bab4e4dc81ad7a8378
Yogi <yoginth@hey.com>
2021-05-20 10:35:33 +0000

    Set nullable to parent_id

--- database/migrations/2021_05_20_063947_add_nested_comments_to_comments_table.php
+++ database/migrations/2021_05_20_063947_add_nested_comments_to_comments_table.php
@@ -14,7 +14,7 @@ class AddNestedCommentsToCommentsTable extends Migration
     public function up()
     {
         Schema::table('comments', function (Blueprint $table) {
-            $table->integer('parent_id')->unsigned()->default(0);
+            $table->integer('parent_id')->unsigned()->nullable();
         });
     }
 
