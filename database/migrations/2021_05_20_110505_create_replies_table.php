0c67bbbb3feb10db8f8f405e42c03113190422f9
Yogi <yoginth@hey.com>
2021-05-20 11:06:51 +0000

    Add replies table

--- database/migrations/2021_05_20_110505_create_replies_table.php
+++ database/migrations/2021_05_20_110505_create_replies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddNestedCommentsToCommentsTable extends Migration
+class CreateRepliesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class AddNestedCommentsToCommentsTable extends Migration
      */
     public function up()
     {
-        Schema::table('comments', function (Blueprint $table) {
-            $table->integer('parent_id')->unsigned()->nullable();
+        Schema::create('replies', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('comment_id')->constrained()->onDelete('cascade');
+            $table->text('comment');
+            $table->timestamps();
         });
     }
 
@@ -25,8 +28,6 @@ class AddNestedCommentsToCommentsTable extends Migration
      */
     public function down()
     {
-        Schema::table('comments', function (Blueprint $table) {
-            $table->dropColumn('parent_id');
-        });
+        Schema::dropIfExists('replies');
     }
 }
