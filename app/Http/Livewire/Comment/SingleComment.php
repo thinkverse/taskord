1e783b3e3af7f2423c8e8d0dfa4f7d934d1333ac
Yogi <yoginth@hey.com>
2021-05-22 20:12:14 +0530

    refactor: Update auth check

--- app/Http/Livewire/Comment/SingleComment.php
+++ app/Http/Livewire/Comment/SingleComment.php
@@ -31,22 +31,22 @@ class SingleComment extends Component
             return toast($this, 'error', 'Your are rate limited, try again later!');
         }
 
-        if (auth()->check()) {
-            if (! auth()->user()->hasVerifiedEmail()) {
-                return toast($this, 'error', 'Your email is not verified!');
-            }
-
-            if (auth()->user()->isFlagged) {
-                return toast($this, 'error', 'Your account is flagged!');
-            }
-            if (auth()->user()->id === $this->comment->user->id) {
-                return toast($this, 'error', 'You can\'t praise your own comment!');
-            }
-            Helper::togglePraise($this->comment, 'COMMENT');
-            loggy(request(), 'Comment', auth()->user(), 'Toggled comment praise | Comment ID: '.$this->comment->id);
-        } else {
+        if (! auth()->check()) {
             return toast($this, 'error', 'Forbidden!');
         }
+
+        if (! auth()->user()->hasVerifiedEmail()) {
+            return toast($this, 'error', 'Your email is not verified!');
+        }
+
+        if (auth()->user()->isFlagged) {
+            return toast($this, 'error', 'Your account is flagged!');
+        }
+        if (auth()->user()->id === $this->comment->user->id) {
+            return toast($this, 'error', 'You can\'t praise your own comment!');
+        }
+        Helper::togglePraise($this->comment, 'COMMENT');
+        loggy(request(), 'Comment', auth()->user(), 'Toggled comment praise | Comment ID: '.$this->comment->id);
     }
 
     public function toggleCommentBox()
@@ -56,15 +56,15 @@ class SingleComment extends Component
 
     public function hide()
     {
-        if (auth()->check()) {
-            if (auth()->user()->isStaff and auth()->user()->staffShip) {
-                Helper::hide($this->comment);
-                loggy(request(), 'Admin', auth()->user(), 'Toggled hide comment | Comment ID: '.$this->comment->id);
+        if (! auth()->check()) {
+            return toast($this, 'error', 'Forbidden!');
+        }
 
-                return toast($this, 'success', 'Comment is hidden from public!');
-            } else {
-                return toast($this, 'error', 'Forbidden!');
-            }
+        if (auth()->user()->isStaff and auth()->user()->staffShip) {
+            Helper::hide($this->comment);
+            loggy(request(), 'Admin', auth()->user(), 'Toggled hide comment | Comment ID: '.$this->comment->id);
+
+            return toast($this, 'success', 'Comment is hidden from public!');
         } else {
             return toast($this, 'error', 'Forbidden!');
         }
@@ -72,20 +72,21 @@ class SingleComment extends Component
 
     public function deleteComment()
     {
-        if (auth()->check()) {
-            if (auth()->user()->isFlagged) {
-                return toast($this, 'error', 'Your account is flagged!');
-            }
-            if (auth()->user()->staffShip or auth()->user()->id === $this->comment->user->id) {
-                loggy(request(), 'Comment', auth()->user(), 'Deleted a comment | Comment ID: '.$this->comment->id);
-                $this->comment->delete();
-                $this->emit('refreshComments');
-                auth()->user()->touch();
+        if (! auth()->check()) {
+            return toast($this, 'error', 'Forbidden!');
+        }
 
-                return toast($this, 'success', 'Comment has been deleted successfully!');
-            } else {
-                return toast($this, 'error', 'Forbidden!');
-            }
+        if (auth()->user()->isFlagged) {
+            return toast($this, 'error', 'Your account is flagged!');
+        }
+
+        if (auth()->user()->staffShip or auth()->user()->id === $this->comment->user->id) {
+            loggy(request(), 'Comment', auth()->user(), 'Deleted a comment | Comment ID: '.$this->comment->id);
+            $this->comment->delete();
+            $this->emit('refreshComments');
+            auth()->user()->touch();
+
+            return toast($this, 'success', 'Comment has been deleted successfully!');
         } else {
             return toast($this, 'error', 'Forbidden!');
         }
