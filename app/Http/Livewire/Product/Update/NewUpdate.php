89289da336b25c39e8495237d8d2e20f4577689a
Yogi <yoginth@hey.com>
2021-05-22 10:08:37 +0530

    refactor: Add toast helper

--- app/Http/Livewire/Product/Update/NewUpdate.php
+++ app/Http/Livewire/Product/Update/NewUpdate.php
@@ -26,13 +26,11 @@ class NewUpdate extends Component
             ]);
 
             if (! auth()->user()->hasVerifiedEmail()) {
-                return  toast($this, 'error', 'Your email is not verified!',
-                ]);
+                return  toast($this, 'error', 'Your email is not verified!');
             }
 
             if (auth()->user()->isFlagged) {
-                return  toast($this, 'error', 'Your account is flagged!',
-                ]);
+                return  toast($this, 'error', 'Your account is flagged!');
             }
 
             $update = auth()->user()->product_updates()->create([
