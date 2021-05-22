b73ad0d9abeb728621a60c649b50931165b05eb0
Yogi <yoginth@hey.com>
2021-05-22 20:38:25 +0530

    refactor: Update auth check

--- app/Http/Livewire/Pages/CreateDeal.php
+++ app/Http/Livewire/Pages/CreateDeal.php
@@ -17,33 +17,34 @@ class CreateDeal extends Component
 
     public function submit()
     {
-        if (auth()->check()) {
-            $this->validate([
-                'name' => ['required', 'min:2'],
-                'description' => ['required', 'min:5'],
-                'offer' => ['required', 'integer', 'max:100'],
-                'coupon' => ['nullable'],
-                'referral' => ['nullable', 'active_url'],
-                'website' => ['required', 'active_url'],
-                'logo' => ['required', 'active_url'],
-            ]);
-            if (auth()->user()->isStaff) {
-                $deal = Deal::create([
-                    'name' =>  $this->name,
-                    'description' => $this->description,
-                    'offer' => $this->offer,
-                    'coupon' => $this->coupon,
-                    'referral' => $this->referral,
-                    'website' => $this->website,
-                    'logo' => $this->logo,
-                ]);
-                auth()->user()->touch();
-                loggy(request(), 'Admin', auth()->user(), 'Created a new deal | Deal ID: '.$deal->id);
+        if (! auth()->check()) {
+            return toast($this, 'error', 'Forbidden!');
+        }
 
-                return redirect()->route('deals');
-            } else {
-                toast($this, 'error', 'Forbidden!');
-            }
+        $this->validate([
+            'name' => ['required', 'min:2'],
+            'description' => ['required', 'min:5'],
+            'offer' => ['required', 'integer', 'max:100'],
+            'coupon' => ['nullable'],
+            'referral' => ['nullable', 'active_url'],
+            'website' => ['required', 'active_url'],
+            'logo' => ['required', 'active_url'],
+        ]);
+
+        if (auth()->user()->isStaff) {
+            $deal = Deal::create([
+                'name' =>  $this->name,
+                'description' => $this->description,
+                'offer' => $this->offer,
+                'coupon' => $this->coupon,
+                'referral' => $this->referral,
+                'website' => $this->website,
+                'logo' => $this->logo,
+            ]);
+            auth()->user()->touch();
+            loggy(request(), 'Admin', auth()->user(), 'Created a new deal | Deal ID: '.$deal->id);
+
+            return redirect()->route('deals');
         } else {
             toast($this, 'error', 'Forbidden!');
         }
