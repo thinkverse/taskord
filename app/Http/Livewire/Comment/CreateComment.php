51bf2c6d5808737153ed6fe91c1a5bdda009e727
Yogi <yoginth@hey.com>
2021-05-22 20:11:21 +0530

    refactor: Update auth check

--- app/Http/Livewire/Comment/CreateComment.php
+++ app/Http/Livewire/Comment/CreateComment.php
@@ -33,47 +33,47 @@ class CreateComment extends Component
 
     public function submit()
     {
-        if (auth()->check()) {
-            $this->validate();
-
-            if (! auth()->user()->hasVerifiedEmail()) {
-                return toast($this, 'error', 'Your email is not verified!');
-            }
-
-            if (auth()->user()->isFlagged) {
-                return toast($this, 'error', 'Your account is flagged!');
-            }
-
-            $comment = auth()->user()->comments()->create([
-                'task_id' =>  $this->task->id,
-                'comment' => $this->comment,
-            ]);
-            auth()->user()->touch();
-            $this->emit('refreshComments');
-
-            $users = Helper::getUsernamesFromMentions($this->comment);
-
-            if ($users) {
-                $this->comment = Helper::parseUserMentionsToMarkdownLinks($this->comment, $users);
-            }
-
-            $this->reset('comment');
-            Helper::mentionUsers($users, $comment, auth()->user(), 'comment');
-            Helper::notifySubscribers($comment->task->subscribers, $comment, 'comment');
-            if (auth()->user()->id !== $this->task->user->id) {
-                if (! auth()->user()->hasSubscribed($comment->task)) {
-                    auth()->user()->subscribe($comment->task);
-                    $this->emit('refreshTaskSubscribed');
-                }
-                $this->task->user->notify(new Commented($comment));
-                givePoint(new CommentCreated($comment));
-            }
-            loggy(request(), 'Comment', auth()->user(), 'Created a new comment | Comment ID: '.$comment->id);
-
-            return toast($this, 'success', 'Comment has been added!');
-        } else {
+        if (! auth()->check()) {
             return toast($this, 'error', 'Forbidden!');
         }
+
+        $this->validate();
+
+        if (! auth()->user()->hasVerifiedEmail()) {
+            return toast($this, 'error', 'Your email is not verified!');
+        }
+
+        if (auth()->user()->isFlagged) {
+            return toast($this, 'error', 'Your account is flagged!');
+        }
+
+        $comment = auth()->user()->comments()->create([
+            'task_id' =>  $this->task->id,
+            'comment' => $this->comment,
+        ]);
+        auth()->user()->touch();
+        $this->emit('refreshComments');
+
+        $users = Helper::getUsernamesFromMentions($this->comment);
+
+        if ($users) {
+            $this->comment = Helper::parseUserMentionsToMarkdownLinks($this->comment, $users);
+        }
+
+        $this->reset('comment');
+        Helper::mentionUsers($users, $comment, auth()->user(), 'comment');
+        Helper::notifySubscribers($comment->task->subscribers, $comment, 'comment');
+        if (auth()->user()->id !== $this->task->user->id) {
+            if (! auth()->user()->hasSubscribed($comment->task)) {
+                auth()->user()->subscribe($comment->task);
+                $this->emit('refreshTaskSubscribed');
+            }
+            $this->task->user->notify(new Commented($comment));
+            givePoint(new CommentCreated($comment));
+        }
+        loggy(request(), 'Comment', auth()->user(), 'Created a new comment | Comment ID: '.$comment->id);
+
+        return toast($this, 'success', 'Comment has been added!');
     }
 
     public function render()
