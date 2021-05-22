2ed931f71017f7359a3c75b169630c9e7ac5dd64
Yogi <yoginth@hey.com>
2021-05-22 20:08:23 +0530

    refactor: Update auth check

--- app/Http/Livewire/Admin/Features/CreateFeature.php
+++ app/Http/Livewire/Admin/Features/CreateFeature.php
@@ -13,24 +13,24 @@ class CreateFeature extends Component
 
     public function submit()
     {
-        if (auth()->check()) {
-            $this->validate([
-                'name' => ['required', 'min:5', 'max:100'],
-                'description' => ['required', 'min:3', 'max:10000'],
-                'slug' => ['required', 'unique:features', 'min:5', 'max:100'],
-            ]);
-
-            $feature = Feature::create([
-                'name' => $this->name,
-                'description' => $this->description,
-                'slug' => $this->slug,
-            ]);
-
-            loggy(request(), 'Admin', auth()->user(), 'Created a new feature flag | Feature ID: '.$feature->id);
-
-            return redirect()->route('admin.features');
-        } else {
-            toast($this, 'error', 'Forbidden!');
+        if (! auth()->check()) {
+            return toast($this, 'error', 'Forbidden!');
         }
+
+        $this->validate([
+            'name' => ['required', 'min:5', 'max:100'],
+            'description' => ['required', 'min:3', 'max:10000'],
+            'slug' => ['required', 'unique:features', 'min:5', 'max:100'],
+        ]);
+
+        $feature = Feature::create([
+            'name' => $this->name,
+            'description' => $this->description,
+            'slug' => $this->slug,
+        ]);
+
+        loggy(request(), 'Admin', auth()->user(), 'Created a new feature flag | Feature ID: '.$feature->id);
+
+        return redirect()->route('admin.features');
     }
 }
