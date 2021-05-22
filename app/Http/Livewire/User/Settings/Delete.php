7125a93ec04542912a19841d883c20cf8bc68655
Yogi <yoginth@hey.com>
2021-05-22 20:29:09 +0530

    refactor: Update auth check

--- app/Http/Livewire/User/Settings/Delete.php
+++ app/Http/Livewire/User/Settings/Delete.php
@@ -17,42 +17,38 @@ class Delete extends Component
 
     public function resetAccount()
     {
-        if (auth()->check()) {
-            if (auth()->user()->id === $this->user->id) {
-                loggy(request(), 'User', auth()->user(), 'Resetted the user account');
-                $user = User::find($this->user->id);
-                // Delete Task Images
-                foreach ($user->tasks as $task) {
-                    foreach ($task->images ?? [] as $image) {
-                        Storage::delete($image);
-                    }
+        if (auth()->user()->id === $this->user->id) {
+            loggy(request(), 'User', auth()->user(), 'Resetted the user account');
+            $user = User::find($this->user->id);
+            // Delete Task Images
+            foreach ($user->tasks as $task) {
+                foreach ($task->images ?? [] as $image) {
+                    Storage::delete($image);
                 }
-                // Delete Product Logos
-                foreach ($user->ownedProducts as $product) {
-                    $product->tasks()->delete();
-                    $product->webhooks()->delete();
-                    $avatar = explode('storage/', $product->avatar);
-                    if (array_key_exists(1, $avatar)) {
-                        Storage::delete($avatar[1]);
-                    }
-                }
-                // Delete User Avatar
-                $avatar = explode('storage/', $user->avatar);
+            }
+            // Delete Product Logos
+            foreach ($user->ownedProducts as $product) {
+                $product->tasks()->delete();
+                $product->webhooks()->delete();
+                $avatar = explode('storage/', $product->avatar);
                 if (array_key_exists(1, $avatar)) {
                     Storage::delete($avatar[1]);
                 }
-                $user->tasks()->delete();
-                $user->comments()->delete();
-                $user->questions()->delete();
-                $user->answers()->delete();
-                $user->ownedProducts()->delete();
-                $user->notifications()->delete();
-                $user->likes()->delete();
-
-                return redirect()->route('home');
-            } else {
-                return toast($this, 'error', 'Forbidden!');
             }
+            // Delete User Avatar
+            $avatar = explode('storage/', $user->avatar);
+            if (array_key_exists(1, $avatar)) {
+                Storage::delete($avatar[1]);
+            }
+            $user->tasks()->delete();
+            $user->comments()->delete();
+            $user->questions()->delete();
+            $user->answers()->delete();
+            $user->ownedProducts()->delete();
+            $user->notifications()->delete();
+            $user->likes()->delete();
+
+            return redirect()->route('home');
         } else {
             return toast($this, 'error', 'Forbidden!');
         }
@@ -60,43 +56,39 @@ class Delete extends Component
 
     public function deleteAccount()
     {
-        if (auth()->check()) {
-            if (auth()->user()->id === $this->user->id) {
-                loggy(request(), 'User', auth()->user(), 'Deleted the user account');
-                $user = User::find($this->user->id);
-                // Delete Task Images
-                foreach ($user->tasks as $task) {
-                    foreach ($task->images ?? [] as $image) {
-                        Storage::delete($image);
-                    }
+        if (auth()->user()->id === $this->user->id) {
+            loggy(request(), 'User', auth()->user(), 'Deleted the user account');
+            $user = User::find($this->user->id);
+            // Delete Task Images
+            foreach ($user->tasks as $task) {
+                foreach ($task->images ?? [] as $image) {
+                    Storage::delete($image);
                 }
-                // Delete Product Logos
-                foreach ($user->ownedProducts as $product) {
-                    $product->tasks()->delete();
-                    $product->webhooks()->delete();
-                    $avatar = explode('storage/', $product->avatar);
-                    if (array_key_exists(1, $avatar)) {
-                        Storage::delete($avatar[1]);
-                    }
-                }
-                // Delete User Avatar
-                $avatar = explode('storage/', $user->avatar);
+            }
+            // Delete Product Logos
+            foreach ($user->ownedProducts as $product) {
+                $product->tasks()->delete();
+                $product->webhooks()->delete();
+                $avatar = explode('storage/', $product->avatar);
                 if (array_key_exists(1, $avatar)) {
                     Storage::delete($avatar[1]);
                 }
-                $user->tasks()->delete();
-                $user->comments()->delete();
-                $user->questions()->delete();
-                $user->answers()->delete();
-                $user->ownedProducts()->delete();
-                $user->notifications()->delete();
-                $user->likes()->delete();
-                $user->delete();
-
-                return redirect()->route('home');
-            } else {
-                return toast($this, 'error', 'Forbidden!');
             }
+            // Delete User Avatar
+            $avatar = explode('storage/', $user->avatar);
+            if (array_key_exists(1, $avatar)) {
+                Storage::delete($avatar[1]);
+            }
+            $user->tasks()->delete();
+            $user->comments()->delete();
+            $user->questions()->delete();
+            $user->answers()->delete();
+            $user->ownedProducts()->delete();
+            $user->notifications()->delete();
+            $user->likes()->delete();
+            $user->delete();
+
+            return redirect()->route('home');
         } else {
             return toast($this, 'error', 'Forbidden!');
         }
