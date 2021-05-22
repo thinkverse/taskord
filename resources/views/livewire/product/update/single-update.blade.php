0dfcc11665e3f1a772d7bf36979f40bcd09440b3
Yogi <yoginth@hey.com>
2021-05-22 15:21:05 +0530

    Add spinner while loading in single update

--- resources/views/livewire/product/update/single-update.blade.php
+++ resources/views/livewire/product/update/single-update.blade.php
@@ -10,29 +10,35 @@
                     @if (auth()->user()->hasLiked($update))
                         <span>
                             <button type="button" class="btn btn-task btn-success text-white me-1" wire:click="togglePraise" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $update->id }}" aria-label="Praise">
-                                <x-heroicon-s-thumb-up class="heroicon heroicon-15px me-0" />
+                                <span wire:loading wire:target="togglePraise">
+                                    <span class="spinner-border spinner-border-task" role="status"></span>
+                                </span>
+                                <x-heroicon-s-thumb-up wire:loading.remove class="heroicon heroicon-15px me-0" />
                                 <span class="small text-white fw-bold">
                                     {{ number_format($update->likerscount()) }}
                                 </span>
                                 <span class="avatar-stack ms-1">
-                                @foreach($update->likers->take(5) as $user)
-                                <img loading=lazy class="praise-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="15" width="15" alt="{{ $user->username }}'s avatar" />
-                                @endforeach
+                                    @foreach($update->likers->take(5) as $user)
+                                        <img loading=lazy class="praise-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="15" width="15" alt="{{ $user->username }}'s avatar" />
+                                    @endforeach
                                 </span>
                             </button>
                         </span>
                     @else
                         <span>
                             <button type="button" class="btn btn-task btn-outline-success me-1" wire:click="togglePraise" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $update->id }}" aria-label="Praises">
-                                <x-heroicon-o-thumb-up class="heroicon heroicon-15px me-0" />
+                                <span wire:loading wire:target="togglePraise">
+                                    <span class="spinner-border spinner-border-task" role="status"></span>
+                                </span>
+                                <x-heroicon-o-thumb-up wire:loading.remove class="heroicon heroicon-15px me-0" />
                                 @if ($update->likerscount() !== 0)
                                 <span class="small text-dark fw-bold">
                                     {{ number_format($update->likerscount()) }}
                                 </span>
                                 <span class="avatar-stack ms-1">
-                                @foreach($update->likers->take(5) as $user)
-                                <img loading=lazy class="praise-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="15" width="15" alt="{{ $user->username }}'s avatar" />
-                                @endforeach
+                                    @foreach($update->likers->take(5) as $user)
+                                        <img loading=lazy class="praise-avatar rounded-circle {{ $loop->last ? 'me-0' : '' }}" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="15" width="15" alt="{{ $user->username }}'s avatar" />
+                                    @endforeach
                                 </span>
                                 @endif
                             </button>
