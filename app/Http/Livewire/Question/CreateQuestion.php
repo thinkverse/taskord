b6b1595e3e55364f7469d2b5fbc77af36e652e69
Yogi <yoginth@hey.com>
2021-05-22 20:41:33 +0530

    refactor: Update auth check

--- app/Http/Livewire/Question/CreateQuestion.php
+++ app/Http/Livewire/Question/CreateQuestion.php
@@ -14,35 +14,35 @@ class CreateQuestion extends Component
 
     public function submit()
     {
-        if (auth()->check()) {
-            $this->validate([
-                'title' => ['required', 'min:5', 'max:100'], ['required', 'min:3', 'max:10000'],
-            ]);
-
-            if (! auth()->user()->hasVerifiedEmail()) {
-                return toast($this, 'error', 'Your email is not verified!');
-            }
-
-            if (auth()->user()->isFlagged) {
-                return toast($this, 'error', 'Your account is flagged!');
-            }
-
-            $solvable = ! $this->solvable ? false : true;
-            $patronOnly = ! $this->patronOnly ? false : true;
-
-            $question = auth()->user()->questions()->create([
-                'title' => $this->title, $this->body,
-                'is_solvable' => $solvable,
-                'patronOnly' => $patronOnly,
-            ]);
-            auth()->user()->touch();
-
-            givePoint(new QuestionCreated($question));
-            loggy(request(), 'Question', auth()->user(), 'Created a new question | Question ID: '.$question->id);
-
-            return redirect()->route('question.question', ['id' => $question->id]);
-        } else {
-            toast($this, 'error', 'Forbidden!');
+        if (! auth()->check()) {
+            return toast($this, 'error', 'Forbidden!');
         }
+
+        $this->validate([
+            'title' => ['required', 'min:5', 'max:100'], ['required', 'min:3', 'max:10000'],
+        ]);
+
+        if (! auth()->user()->hasVerifiedEmail()) {
+            return toast($this, 'error', 'Your email is not verified!');
+        }
+
+        if (auth()->user()->isFlagged) {
+            return toast($this, 'error', 'Your account is flagged!');
+        }
+
+        $solvable = ! $this->solvable ? false : true;
+        $patronOnly = ! $this->patronOnly ? false : true;
+
+        $question = auth()->user()->questions()->create([
+            'title' => $this->title, $this->body,
+            'is_solvable' => $solvable,
+            'patronOnly' => $patronOnly,
+        ]);
+        auth()->user()->touch();
+
+        givePoint(new QuestionCreated($question));
+        loggy(request(), 'Question', auth()->user(), 'Created a new question | Question ID: '.$question->id);
+
+        return redirect()->route('question.question', ['id' => $question->id]);
     }
 }
