4722135c178b72fb9c1770b0c5e67d7f6dd707f9
Yogi <yoginth@hey.com>
2021-05-22 10:07:05 +0530

    refactor: Add toast helper

--- app/Http/Livewire/Task/Subscribe.php
+++ app/Http/Livewire/Task/Subscribe.php
@@ -36,16 +36,13 @@ class Subscribe extends Component
 
         if (auth()->check()) {
             if (! auth()->user()->hasVerifiedEmail()) {
-                return  toast($this, 'error', 'Your email is not verified!',
-                ]);
+                return  toast($this, 'error', 'Your email is not verified!');
             }
             if (auth()->user()->isFlagged) {
-                return  toast($this, 'error', 'Your account is flagged!',
-                ]);
+                return  toast($this, 'error', 'Your account is flagged!');
             }
             if (auth()->user()->id === $this->task->user->id) {
-                return  toast($this, 'error', 'You can\'t subscribe your own task!',
-                ]);
+                return  toast($this, 'error', 'You can\'t subscribe your own task!');
             } else {
                 auth()->user()->toggleSubscribe($this->task);
                 $this->task->refresh();
