39a37a92aa07736d4cd23a4f388a8aad712031d7
Yogi <yoginth@hey.com>
2021-05-22 20:32:25 +0530

    refactor: Update auth check

--- app/Http/Livewire/Milestone/CreateMilestone.php
+++ app/Http/Livewire/Milestone/CreateMilestone.php
@@ -14,34 +14,34 @@ class CreateMilestone extends Component
 
     public function submit()
     {
-        if (auth()->check()) {
-            $this->validate([
-                'name' => ['required', 'min:5', 'max:100'],
-                'description' => ['required', 'min:3', 'max:10000'],
-            ]);
-
-            if (! auth()->user()->hasVerifiedEmail()) {
-                return toast($this, 'error', 'Your email is not verified!');
-            }
-
-            if (auth()->user()->isFlagged) {
-                return toast($this, 'error', 'Your account is flagged!');
-            }
-
-            $milestone = Milestone::create([
-                'user_id' =>  auth()->user()->id,
-                'name' => $this->name,
-                'description' => $this->description,
-                'start_date' => $this->start_date,
-                'end_date' => $this->end_date,
-            ]);
-            auth()->user()->touch();
-
-            loggy(request(), 'Milestone', auth()->user(), 'Created a new milestone | Milestone ID: '.$milestone->id);
-
-            return redirect()->route('milestones.milestone', ['milestone' => $milestone]);
-        } else {
-            toast($this, 'error', 'Forbidden!');
+        if (! auth()->check()) {
+            return toast($this, 'error', 'Forbidden!');
         }
+
+        $this->validate([
+            'name' => ['required', 'min:5', 'max:100'],
+            'description' => ['required', 'min:3', 'max:10000'],
+        ]);
+
+        if (! auth()->user()->hasVerifiedEmail()) {
+            return toast($this, 'error', 'Your email is not verified!');
+        }
+
+        if (auth()->user()->isFlagged) {
+            return toast($this, 'error', 'Your account is flagged!');
+        }
+
+        $milestone = Milestone::create([
+            'user_id' =>  auth()->user()->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+        ]);
+        auth()->user()->touch();
+
+        loggy(request(), 'Milestone', auth()->user(), 'Created a new milestone | Milestone ID: '.$milestone->id);
+
+        return redirect()->route('milestones.milestone', ['milestone' => $milestone]);
     }
 }
