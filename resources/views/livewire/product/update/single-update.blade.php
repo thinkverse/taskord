d2584c6fe1ac86b41cbe2a25a7d7dd79a62541b6
Yogi <yoginth@hey.com>
2021-05-22 16:41:55 +0530

    Add wire:target=togglePraise

--- resources/views/livewire/product/update/single-update.blade.php
+++ resources/views/livewire/product/update/single-update.blade.php
@@ -13,7 +13,7 @@
                                 <span wire:loading wire:target="togglePraise">
                                     <span class="spinner-border spinner-border-task" role="status"></span>
                                 </span>
-                                <x-heroicon-s-thumb-up wire:loading.remove class="heroicon heroicon-15px me-0" />
+                                <x-heroicon-s-thumb-up wire:loading.remove wire:target="togglePraise" class="heroicon heroicon-15px me-0" />
                                 <span class="small text-white fw-bold">
                                     {{ number_format($update->likerscount()) }}
                                 </span>
@@ -30,7 +30,7 @@
                                 <span wire:loading wire:target="togglePraise">
                                     <span class="spinner-border spinner-border-task" role="status"></span>
                                 </span>
-                                <x-heroicon-o-thumb-up wire:loading.remove class="heroicon heroicon-15px me-0" />
+                                <x-heroicon-o-thumb-up wire:loading.remove wire:target="togglePraise" class="heroicon heroicon-15px me-0" />
                                 @if ($update->likerscount() !== 0)
                                     <span class="small text-dark fw-bold">
                                         {{ number_format($update->likerscount()) }}
