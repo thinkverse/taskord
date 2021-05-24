0ea07e34a6ff07389479da9d49d7db64c0ffecda
Yogi <yoginth@hey.com>
2021-05-24 14:30:01 +0530

    Add card for appearance

--- resources/views/livewire/user/settings/appearance.blade.php
+++ resources/views/livewire/user/settings/appearance.blade.php
@@ -1,3 +1,24 @@
-<div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+<div class="col-lg-8">
+    <div class="card">
+        <div class="card-header py-3">
+            <span class="h5">Theme preferences</span>
+            <div>Choose how Taskord looks to you.</div>
+        </div>
+        <div class="card-body">
+            <div>
+                <div class="mb-2">Get all notifications via email</div>
+                <div class="form-check">
+                    <input wire:click="notificationsEmail" id="notificationsEmail" class="form-check-input" type="checkbox" {{ $user->notifications_email ? 'checked' : '' }}>
+                    <label for="notificationsEmail" class="form-check-label">Email</label>
+                </div>
+            </div>
+            <div>
+                <div class="mb-2 mt-3">Get all notifications via web</div>
+                <div class="form-check">
+                    <input wire:click="notificationsWeb" id="notificationsWeb" class="form-check-input" type="checkbox" {{ $user->notifications_web ? 'checked' : '' }}>
+                    <label for="notificationsWeb" class="form-check-label">Web</label>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
