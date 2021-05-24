edde6342594bb99394c0092917ce7940d79b7c98
Yogi <yoginth@hey.com>
2021-05-24 07:41:34 +0530

    Remove getComment

--- app/GraphQL/Queries/CommentQuery.php
+++ app/GraphQL/Queries/CommentQuery.php
@@ -4,18 +4,6 @@ namespace App\GraphQL\Queries;
 
 class CommentQuery
 {
-    public function getComment($comment, array $args)
-    {
-        if (
-            $comment->hidden or
-            $comment->user->isFlagged
-        ) {
-            return null;
-        }
-
-        return $comment->comment;
-    }
-
     public function hasPraised($comment, array $args)
     {
         if (auth()->check()) {
