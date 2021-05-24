b20c18c7af0541bdc8b7b7300d6742a7a1a0dbe2
Yogi <yoginth@hey.com>
2021-05-24 14:24:51 +0530

    Add Appearance file

---
+++ resources/views/user/settings/appearance.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('pageTitle', 'Settings / Notifications ·')
+
+@section('content')
+<div class="container-md">
+    <div class="row justify-content-center mt-4">
+        @include('user.settings.sidebar')
+        @livewire('user.settings.notifications', [
+            'user' => $user
+        ])
+    </div>
+</div>
+@endsection
