9a4e7adadcd2434d015d139f5aeed11e5d457f52
Yogi <yoginth@hey.com>
2021-05-24 07:42:32 +0530

    Remove get title and body

--- app/GraphQL/Queries/QuestionQuery.php
+++ app/GraphQL/Queries/QuestionQuery.php
@@ -4,32 +4,6 @@ namespace App\GraphQL\Queries;
 
 class QuestionQuery
 {
-    public function getTitle($question, array $args)
-    {
-        if (
-            $question->hidden or
-            $question->patronOnly or
-            $question->user->isFlagged
-        ) {
-            return null;
-        }
-
-        return $question->title;
-    }
-
-    public function getBody($question, array $args)
-    {
-        if (
-            $question->hidden or
-            $question->patronOnly or
-            $question->user->isFlagged
-        ) {
-            return null;
-        }
-
-        return $question->body;
-    }
-
     public function hasPraised($question, array $args)
     {
         if (auth()->check()) {
