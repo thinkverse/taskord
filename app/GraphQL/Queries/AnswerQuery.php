6d7a71273b265ea3ef0656bf44801c7b84718e9b
Yogi <yoginth@hey.com>
2021-05-24 07:41:17 +0530

    Remove getAnswer

--- app/GraphQL/Queries/AnswerQuery.php
+++ app/GraphQL/Queries/AnswerQuery.php
@@ -4,19 +4,6 @@ namespace App\GraphQL\Queries;
 
 class AnswerQuery
 {
-    public function getAnswer($answer, array $args)
-    {
-        if (
-            $answer->hidden or
-            $answer->question->patronOnly or
-            $answer->user->isFlagged
-        ) {
-            return null;
-        }
-
-        return $answer->answer;
-    }
-
     public function hasPraised($answer, array $args)
     {
         if (auth()->check()) {
