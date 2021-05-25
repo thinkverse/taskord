4557ce24c7ef1e262e5d51febd9bbdf42d1590b4
Yogi <yoginth@hey.com>
2021-05-25 11:11:25 +0530

    Rename admin to staff

--- resources/views/admin/stats.blade.php
+++ resources/views/admin/stats.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
-@section('pageTitle', 'Admin - Stats ·')
-@section('title', 'Admin - Stats ·')
+@section('pageTitle', 'Stafftool - Stats ·')
+@section('title', 'Stafftool - Stats ·')
 @section('description', 'Get things done socially with Taskord.')
 @section('image', '')
 @section('url', url()->current())
@@ -10,8 +10,8 @@
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="container-md">
-            @include('admin.nav')
-            <livewire:admin.stats />
+            @include('staff.nav')
+            <livewire:staff.stats />
         </div>
     </div>
 </div>
