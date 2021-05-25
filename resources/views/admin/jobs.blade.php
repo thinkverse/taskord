17bf750f2948de387cb1059effe63f9031379c00
Yogi <yoginth@hey.com>
2021-05-25 11:11:12 +0530

    Rename admin to staff

--- resources/views/admin/jobs.blade.php
+++ resources/views/admin/jobs.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
-@section('pageTitle', 'Admin - Jobs ·')
-@section('title', 'Admin - Jobs ·')
+@section('pageTitle', 'Stafftool - Jobs ·')
+@section('title', 'Stafftool - Jobs ·')
 @section('description', 'Get things done socially with Taskord.')
 @section('image', '')
 @section('url', url()->current())
@@ -10,7 +10,7 @@
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="container-md">
-            @include('admin.nav')
+            @include('staff.nav')
             <div class="card">
                 <iframe src="horizon" style="height:75vh">
             </div>
