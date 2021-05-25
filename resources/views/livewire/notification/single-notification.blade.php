fa1eb4fa88a0bef0879abf8ec87b1e78cac93fc8
Yogi <yoginth@hey.com>
2021-05-25 11:12:28 +0530

    Rename admin to staff

--- resources/views/livewire/notification/single-notification.blade.php
+++ resources/views/livewire/notification/single-notification.blade.php
@@ -8,7 +8,7 @@
     <div class="card mb-3">
         <div class="card-body">
             <div class="d-flex justify-content-between">
-                @if ($type !== "App\Notifications\Welcome" and $type !== "App\Notifications\Admin\VersionReleased")
+                @if ($type !== "App\Notifications\Welcome" and $type !== "App\Notifications\Staff\VersionReleased")
                     <x:shared.user-label-small :user="$user" />
                 @endif
                 @if ($page_type === 'unread')
@@ -67,7 +67,7 @@
                 <div class="mt-2">
                     Have a nice day 💜
                 </div>
-            @elseif ($type === "App\Notifications\Admin\VersionReleased")
+            @elseif ($type === "App\Notifications\Staff\VersionReleased")
                 <div class="mt-2 fw-bold">
                     Version {{ $data['tagName'] }} has been released!
                 </div>
