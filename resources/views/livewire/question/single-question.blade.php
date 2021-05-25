8f0189630d3007b001739a60e04d551f0077fd3e
Yogi <yoginth@hey.com>
2021-05-25 11:24:36 +0530

    Rename admin to staff

--- resources/views/livewire/question/single-question.blade.php
+++ resources/views/livewire/question/single-question.blade.php
@@ -127,7 +127,7 @@
                     @endif
                 @endif
                 @if (auth()->user()->staffShip)
-                    <button type="button" class="btn btn-task {{ $question->hidden ? 'btn-info' : 'btn-outline-info' }}" wire:click="hide" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $question->id }}" title="Flag to admins" aria-label="Hide">
+                    <button type="button" class="btn btn-task {{ $question->hidden ? 'btn-info' : 'btn-outline-info' }}" wire:click="hide" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $question->id }}" aria-label="Hide">
                         <x-heroicon-o-eye-off class="heroicon heroicon-15px me-0" />
                     </button>
                 @endif
