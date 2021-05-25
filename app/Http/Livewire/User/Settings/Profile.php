e67a6bf426026c6b6d0378283247b73609fd390e
Yogi <yoginth@hey.com>
2021-05-25 13:26:58 +0530

    Rename to has_goal

--- app/Http/Livewire/User/Settings/Profile.php
+++ app/Http/Livewire/User/Settings/Profile.php
@@ -23,7 +23,7 @@ class Profile extends Component
     public $avatar;
     // Goal
     public $daily_goal;
-    public $hasGoal;
+    public $has_goal;
     // Sponsor
     public $sponsor;
     // Social
@@ -44,7 +44,7 @@ class Profile extends Component
         $this->location = $user->location;
         $this->company = $user->company;
         // Goal
-        $this->hasGoal = $user->hasGoal;
+        $this->has_goal = $user->has_goal;
         $this->daily_goal = $user->daily_goal;
         // Sponsor
         $this->sponsor = $user->sponsor;
@@ -141,7 +141,7 @@ class Profile extends Component
     public function enableGoal()
     {
         if (auth()->user()->id === $this->user->id) {
-            $this->user->hasGoal = ! $this->user->hasGoal;
+            $this->user->has_goal = ! $this->user->has_goal;
             $this->user->save();
             loggy(request(), 'User', auth()->user(), 'Toggled goals settings');
         } else {
