6590c4f836b0e4142bbf7b97ff55f665bd8c2879
Yogi <yoginth@hey.com>
2021-05-25 12:32:23 +0530

    Rename to only_followings_tasks

--- resources/views/livewire/home/only-following.blade.php
+++ resources/views/livewire/home/only-following.blade.php
@@ -1,6 +1,6 @@
 <span class="float-end">
     <div class="form-check">
-        <input wire:click="onlyFollowingsTasks" class="form-check-input" type="checkbox" id="onlyFollowingsTasks" {{ auth()->user()->onlyFollowingsTasks ? 'checked' : '' }}>
+        <input wire:click="onlyFollowingsTasks" class="form-check-input" type="checkbox" id="onlyFollowingsTasks" {{ auth()->user()->only_followings_tasks ? 'checked' : '' }}>
         <label class="form-check-label" for="onlyFollowingsTasks">
             Only following
         </label>
