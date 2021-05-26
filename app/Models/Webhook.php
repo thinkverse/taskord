568aecaeed2deca5cc20457ae8e9dd2d2f32e713
Yogi <yoginth@hey.com>
2021-05-26 18:58:55 +0530

    Lint app folder #358

--- app/Models/Webhook.php
+++ app/Models/Webhook.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Webhook extends Model
@@ -16,6 +17,6 @@ class Webhook extends Model
 
     public function user()
     {
-        return $this->belongsTo(\App\Models\User::class);
+        return $this->belongsTo(User::class);
     }
 }
