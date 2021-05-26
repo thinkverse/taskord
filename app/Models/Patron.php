a46a11ddfd4bac7bdf733144250fab07605de60b
Yogi <yoginth@hey.com>
2021-05-26 18:47:35 +0530

    Lint app folder #358

--- app/Models/Patron.php
+++ app/Models/Patron.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Patron extends Model
@@ -18,6 +19,6 @@ class Patron extends Model
 
     public function user()
     {
-        return $this->belongsTo(\App\Models\User::class);
+        return $this->belongsTo(User::class);
     }
 }
