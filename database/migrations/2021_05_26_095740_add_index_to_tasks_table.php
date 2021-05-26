4c447cc4ccd84c8df8c7c903c4f9d0ede88cce91
Yogi <yoginth@hey.com>
2021-05-26 15:29:35 +0530

    Add tasks index

---
+++ database/migrations/2021_05_26_095740_add_index_to_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIndexToTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->index('id');
+            $table->index('user_id');
+            $table->index('product_id');
+            $table->index('source');
+            $table->index('hidden');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropIndex('id');
+            $table->dropIndex('user_id');
+            $table->dropIndex('product_id');
+            $table->dropIndex('source');
+            $table->dropIndex('hidden');
+        });
+    }
+}
