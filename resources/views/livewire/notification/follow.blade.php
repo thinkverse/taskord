4b63115ec6983326535b21fecbdc7f54e5f43d94
Yogi <yoginth@hey.com>
2021-05-26 18:02:54 +0530

    Add wire:target #316

--- resources/views/livewire/notification/follow.blade.php
+++ resources/views/livewire/notification/follow.blade.php
@@ -1,14 +1,14 @@
 <div>
     @if (auth()->user()->isFollowing($user))
         <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
-            <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
-            <x-heroicon-o-user-remove wire:loading.remove class="heroicon" />
+            <div wire:loading wire:target="toggleFollow" class="spinner-border spinner-border-sm me-1"></div>
+            <x-heroicon-o-user-remove wire:loading.remove wire:target="toggleFollow" class="heroicon" />
             Unfollow <span class="fw-bold">{{ '@'.$user->username }}</span>
         </button>
     @else
         <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
-            <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
-            <x-heroicon-o-user-add wire:loading.remove class="heroicon" />
+            <div wire:loading wire:target="toggleFollow" class="spinner-border spinner-border-sm me-1"></div>
+            <x-heroicon-o-user-add wire:loading.remove wire:target="toggleFollow" class="heroicon" />
             Follow back <span class="fw-bold">{{ '@'.$user->username }}</span>
         </button>
     @endif
