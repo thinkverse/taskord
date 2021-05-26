a4906ae95ed5ada1730d5f3d67ac4ba10a8590a7
Yogi <yoginth@hey.com>
2021-05-26 18:00:07 +0530

    Add wire:target #316

--- resources/views/livewire/home/follow.blade.php
+++ resources/views/livewire/home/follow.blade.php
@@ -1,14 +1,14 @@
 <div>
     @if (auth()->user()->isFollowing($user))
         <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
-            <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
-            <x-heroicon-o-user-remove wire:loading.remove class="heroicon" />
+            <div wire:loading wire:target="toggleFollow" class="spinner-border spinner-border-sm me-1"></div>
+            <x-heroicon-o-user-remove wire:loading.remove wire:target="toggleFollow" class="heroicon" />
             {{ $showText ? 'Unfollow' : '' }}
         </button>
     @else
         <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
-            <div wire:loading class="spinner-border spinner-border-sm me-1"></div>
-            <x-heroicon-o-user-add wire:loading.remove class="heroicon" />
+            <div wire:loading wire:target="toggleFollow" class="spinner-border spinner-border-sm me-1"></div>
+            <x-heroicon-o-user-add wire:loading.remove wire:target="toggleFollow" class="heroicon" />
             {{ $showText ? 'Follow' : '' }}
         </button>
     @endif
