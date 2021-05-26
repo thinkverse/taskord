bf22014967a63d4b8c16ce595e145e3648dc6613
Yogi <yoginth@hey.com>
2021-05-26 16:39:04 +0530

    Add index to webhooks table

--- database/migrations/2021_05_26_110745_add_index_to_webhooks_table.php
+++ database/migrations/2021_05_26_110745_add_index_to_webhooks_table.php
@@ -14,7 +14,12 @@ class AddIndexToWebhooksTable extends Migration
     public function up()
     {
         Schema::table('webhooks', function (Blueprint $table) {
-            //
+            $table->index('id');
+            $table->index('user_id');
+            $table->index('product_id');
+            $table->index('token');
+            $table->index('name');
+            $table->index('type');
         });
     }
 
@@ -26,7 +31,12 @@ class AddIndexToWebhooksTable extends Migration
     public function down()
     {
         Schema::table('webhooks', function (Blueprint $table) {
-            //
+            $table->dropIndex('id');
+            $table->dropIndex('user_id');
+            $table->dropIndex('product_id');
+            $table->dropIndex('token');
+            $table->dropIndex('name');
+            $table->dropIndex('type');
         });
     }
 }
