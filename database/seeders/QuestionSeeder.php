8bf09c7301e98efd2056360251e8499a004bfb0e
Yogi <yoginth@hey.com>
2021-05-26 12:07:51 +0530

    Seed tags

--- database/seeders/QuestionSeeder.php
+++ database/seeders/QuestionSeeder.php
@@ -10,12 +10,19 @@ class QuestionSeeder extends Seeder
     public function run()
     {
         // Public Question
-        Question::factory()->create([
+        $question = Question::factory()->create([
             'user_id' => 1,
             'title' => 'Hello, World this is public question!',
             'body' => 'Hello, World!',
             'patron_only' => false,
         ]);
+
+        $question->tag('Facebook');
+        $question->tag('Twitter');
+        $question->tag('Google');
+        $question->tag('Taskord');
+        $question->tag('GitLab');
+        $question->tag('GitHub');
 
         // Patron Only Question
         Question::factory()->create([
