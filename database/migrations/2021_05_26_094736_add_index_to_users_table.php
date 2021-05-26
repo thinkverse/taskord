2490ca2f40b4a59cf3e37ea29286052a36d99983
Yogi <yoginth@hey.com>
2021-05-26 15:27:11 +0530

    Add user index

--- database/migrations/2021_05_26_094736_add_index_to_users_table.php
+++ database/migrations/2021_05_26_094736_add_index_to_users_table.php
@@ -14,7 +14,20 @@ class AddIndexToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->index('id');
+            $table->index('username');
+            $table->index('firstname');
+            $table->index('lastname');
+            $table->index('email');
+            $table->index('provider');
+            $table->index('provider_id');
+            $table->index('email_verified_at');
+            $table->index('is_staff');
+            $table->index('is_contributor');
+            $table->index('is_beta');
+            $table->index('is_patron');
+            $table->index('is_suspended');
+            $table->index('spammy');
         });
     }
 
@@ -26,7 +39,20 @@ class AddIndexToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropIndex('id');
+            $table->dropIndex('username');
+            $table->dropIndex('firstname');
+            $table->dropIndex('lastname');
+            $table->dropIndex('email');
+            $table->dropIndex('provider');
+            $table->dropIndex('provider_id');
+            $table->dropIndex('email_verified_at');
+            $table->dropIndex('is_staff');
+            $table->dropIndex('is_contributor');
+            $table->dropIndex('is_beta');
+            $table->dropIndex('is_patron');
+            $table->dropIndex('is_suspended');
+            $table->dropIndex('spammy');
         });
     }
 }
