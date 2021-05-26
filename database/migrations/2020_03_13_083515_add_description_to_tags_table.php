5283a8958eeaabb061aab39e33035ca23fc09be7
Yogi <yoginth@hey.com>
2021-05-26 12:04:32 +0530

    Fix CI

--- database/migrations/2020_03_13_083515_add_description_to_tags_table.php
+++ database/migrations/2020_03_13_083515_add_description_to_tags_table.php
@@ -1,18 +1,15 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 
-class AddDescriptionToTagsTable extends Migration {
-
+class AddDescriptionToTagsTable extends Migration
+{
     public function up()
     {
         Schema::table('tagging_tags', function ($table) {
             $table->text('description')->nullable();
         });
-
     }
-
 
     public function down()
     {
