57bc7a36f0d76494b2497f5d33fe3c9c8c4517b3
Yogi <yoginth@hey.com>
2021-05-27 11:19:51 +0530

    Fix gate

--- resources/views/components/shared/user-label-with-bio.blade.php
+++ resources/views/components/shared/user-label-with-bio.blade.php
@@ -34,11 +34,11 @@
                     <x-heroicon-s-star class="heroicon text-gold" />
                 </a>
             @endif
-            @can('is_staff')
+            @if ($user->is_staff)
                 <span class="staff" title="Staff">
                     <x-heroicon-o-shield-check class="heroicon text-primary" />
                 </span>
-            @endcan
+            @endif
             @if ($user->bio)
                 <div class="fw-normal">
                     {{ $user->bio }}
