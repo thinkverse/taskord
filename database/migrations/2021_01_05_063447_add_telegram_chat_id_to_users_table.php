f3e444b920d40feac846bf7e00839d4d0fa8383e
Yogi <yoginth@hey.com>
2021-05-28 19:32:52 +0530

    remove ✂️: Telegram integration

--- database/migrations/2021_01_05_063447_add_telegram_chat_id_to_users_table.php
+++ database/migrations/2021_01_05_063447_add_telegram_chat_id_to_users_table.php
@@ -26,7 +26,7 @@ class AddTelegramChatIdToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('telegram_chat_id');
         });
     }
 }
--- database/migrations/2021_05_28_140155_remove_telegram_chat_id_from_users_table.php
+++ database/migrations/2021_05_28_140155_remove_telegram_chat_id_from_users_table.php
@@ -14,7 +14,7 @@ class RemoveTelegramChatIdFromUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('telegram_chat_id');
         });
     }
 
@@ -26,7 +26,7 @@ class RemoveTelegramChatIdFromUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('telegram_chat_id')->nullable()->default(null);
         });
     }
 }
