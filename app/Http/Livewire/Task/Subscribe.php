68c397b3fca314737c89c663fddb513be6d09272
Yogi <yoginth@hey.com>
2021-05-28 19:13:11 +0530

    refactor: Gates

--- app/Http/Livewire/Task/Subscribe.php
+++ app/Http/Livewire/Task/Subscribe.php
@@ -7,6 +7,7 @@ use GrahamCampbell\Throttle\Facades\Throttle;
 use Helper;
 use Illuminate\Support\Facades\Request;
 use Livewire\Component;
+use Illuminate\Support\Facades\Gate;
 
 class Subscribe extends Component
 {
@@ -34,19 +35,10 @@ class Subscribe extends Component
             return toast($this, 'error', 'Your are rate limited, try again later!');
         }
 
-        if (! auth()->check()) {
+        if (Gate::denies('praise', $this->question)) {
             return toast($this, 'error', "Oops! You can't perform this action");
         }
 
-        if (! auth()->user()->hasVerifiedEmail()) {
-            return toast($this, 'error', 'Your email is not verified!');
-        }
-        if (auth()->user()->spammy) {
-            return toast($this, 'error', 'Your account is flagged!');
-        }
-        if (auth()->user()->id === $this->task->user->id) {
-            return toast($this, 'error', 'You can\'t subscribe your own task!');
-        }
         auth()->user()->toggleSubscribe($this->task);
         $this->task->refresh();
         auth()->user()->touch();
