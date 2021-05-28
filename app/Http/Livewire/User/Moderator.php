4b8d0810e5128da4877bc114a1e94a620f2daa52
Yogi <yoginth@hey.com>
2021-05-28 18:00:27 +0530

    refactor: Gates

--- app/Http/Livewire/User/Moderator.php
+++ app/Http/Livewire/User/Moderator.php
@@ -307,11 +307,7 @@ class Moderator extends Component
 
     public function masquerade()
     {
-        if (Gate::denies('staff_mode')) {
-            return toast($this, 'error', "Oops! You can't perform this action");
-        }
-
-        if ($this->user->id === 1) {
+        if (Gate::denies('staff_mode')  or $this->user->id === 1) {
             return toast($this, 'error', "Oops! You can't perform this action");
         }
 
