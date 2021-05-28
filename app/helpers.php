26a5423eb90d6f7351ece6a36662637b26806853
Yogi <yoginth@hey.com>
2021-05-28 09:49:12 +0530

    Remove version support #366

--- app/helpers.php
+++ app/helpers.php
@@ -10,23 +10,7 @@ use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 use League\CommonMark\GithubFlavoredMarkdownConverter;
 
-/**
- * This file contains helper functions for Taskord.
- * php version 7.4+.
- *
- * @category Helpers
- */
 if (! function_exists('git')) {
-
-    /**
-     * Execute git command
-     * Should be used safetly
-     * No direct end-user access.
-     *
-     * @param string $args
-     *
-     * @return string|null
-     */
     function git(string $args): ?string
     {
         $approved_args = ['rev-parse'];
@@ -47,11 +31,6 @@ if (! function_exists('git')) {
 }
 
 if (! function_exists('laravel_version')) {
-    /**
-     * Return current Laravel version.
-     *
-     * @return string
-     */
     function laravel_version(): string
     {
         return Application::VERSION;
@@ -59,14 +38,6 @@ if (! function_exists('laravel_version')) {
 }
 
 if (! function_exists('memory_usage')) {
-    /**
-     * Get formatted memory usage.
-     *
-     * @author Jeff Peck <jeff.peck@snet.net>
-     * @link   https://www.php.net/manual/en/function.memory-get-usage.php#93012
-     *
-     * @return string
-     */
     function memory_usage(): string
     {
         $mem_usage = memory_get_usage(true);
@@ -84,14 +55,6 @@ if (! function_exists('memory_usage')) {
 }
 
 if (! function_exists('pluralize')) {
-    /**
-     * Get the plural form of an English word.
-     *
-     * @param string $word  Word
-     * @param int    $count Count
-     *
-     * @return string
-     */
     function pluralize($word, $count): string
     {
         return Str::plural($word, $count);
@@ -99,17 +62,6 @@ if (! function_exists('pluralize')) {
 }
 
 if (! function_exists('carbon')) {
-
-    /**
-     * Returns new Carbon object.
-     *
-     * @param mixed ...$args arguments
-     *
-     * @author Caleb Porzio <calebporzio@gmail.com>
-     * @link   https://github.com/calebporzio/awesome-helpers
-     *
-     * @return \Illuminate\Support\Carbon|null
-     */
     function carbon(...$args): ?Carbon
     {
         try {
