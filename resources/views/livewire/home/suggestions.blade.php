4dfb2902815884603f91783ed3df1983e24a9982
Yogi <yoginth@hey.com>
2021-05-28 17:06:23 +0530

    Move to else

--- resources/views/livewire/home/suggestions.blade.php
+++ resources/views/livewire/home/suggestions.blade.php
@@ -7,6 +7,17 @@
             <div class="card-body text-center">
                 <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
             </div>
+        @else
+            <ul class="list-group list-group-flush">
+                @foreach ($users as $user)
+                    <li class="list-group-item d-flex align-items-center justify-content-between py-2" wire:key="{{ $user->id }}">
+                        <x:shared.user-label-big :user="$user" />
+                        <span>
+                            <livewire:home.follow :user="$user" :showText="$showText" :wire:key="$user->id" />
+                        </span>
+                    </li>
+                @endforeach
+            </ul>
         @endif
         @if ($readyToLoad and count($users) === 0)
             <div class="card-body text-center fw-bold text-secondary">
@@ -14,15 +25,5 @@
                 Nothing to suggest!
             </div>
         @endif
-        <ul class="list-group list-group-flush">
-            @foreach ($users as $user)
-                <li class="list-group-item d-flex align-items-center justify-content-between py-2" wire:key="{{ $user->id }}">
-                    <x:shared.user-label-big :user="$user" />
-                    <span>
-                        <livewire:home.follow :user="$user" :showText="$showText" :wire:key="$user->id" />
-                    </span>
-                </li>
-            @endforeach
-        </ul>
     </div>
 </div>
