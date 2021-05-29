ce387327fb4e4fdc5cfbcf75b91aa3b62feaf207
Yogi <yoginth@hey.com>
2021-05-29 11:50:42 +0530

    remove: Big rate limit engine

--- app/Http/Livewire/User/Settings/Api.php
+++ app/Http/Livewire/User/Settings/Api.php
@@ -13,6 +13,8 @@ use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 
 class Api extends Component
 {
+    use WithRateLimiting;
+    
     public User $user;
 
     public $listeners = [
