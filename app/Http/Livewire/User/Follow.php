db5c944fa4d39adb7177a4187e02a2cdf5f6d3d0
Yogi <yoginth@hey.com>
2021-05-29 11:43:48 +0530

    remove: Big rate limit engine

--- app/Http/Livewire/User/Follow.php
+++ app/Http/Livewire/User/Follow.php
@@ -8,6 +8,8 @@ use GrahamCampbell\Throttle\Facades\Throttle;
 use Helper;
 use Illuminate\Support\Facades\Request;
 use Livewire\Component;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 
 class Follow extends Component
 {
