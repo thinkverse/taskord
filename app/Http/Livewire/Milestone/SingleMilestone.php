7c58eb2ea4881121edb721b0ceca0ef91734253f
Yogi <yoginth@hey.com>
2021-05-29 11:45:52 +0530

    remove: Big rate limit engine

--- app/Http/Livewire/Milestone/SingleMilestone.php
+++ app/Http/Livewire/Milestone/SingleMilestone.php
@@ -13,6 +13,8 @@ use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
 
 class SingleMilestone extends Component
 {
+    use WithRateLimiting;
+    
     public Milestone $milestone;
     public $type;
 
