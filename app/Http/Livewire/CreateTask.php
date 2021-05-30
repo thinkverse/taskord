8cf0831f65ea8cbb5f7a4befaba3e027035b1020
Yogi <yoginth@hey.com>
2021-05-30 13:11:21 +0530

    fix: Issues reported by phpinsights

--- app/Http/Livewire/CreateTask.php
+++ app/Http/Livewire/CreateTask.php
@@ -113,7 +113,7 @@ class CreateTask extends Component
         $this->emit('refreshTasks');
         $this->reset(['task', 'images', 'dueAt']);
         if (auth()->user()->has_goal and $task->done) {
-            auth()->user()->daily_goal_reached++;
+            auth()->user()->daily_goal_reached += 1;
             auth()->user()->save();
             CheckGoal::dispatch(auth()->user(), $task);
         }
