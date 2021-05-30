3b33a1163486571b1b85d7f0fde21ef94617df62
Yogi <yoginth@hey.com>
2021-05-30 18:26:27 +0530

    fix: Larastan issues

--- app/Gamify/Badges/Enlightened.php
+++ app/Gamify/Badges/Enlightened.php
@@ -6,20 +6,8 @@ use QCod\Gamify\BadgeType;
 
 class Enlightened extends BadgeType
 {
-    /**
-     * Description for badge.
-     *
-     * @var string
-     */
     protected $description = '';
 
-    /**
-     * Check is user qualifies for badge.
-     *
-     * @param $user
-     *
-     * @return bool
-     */
     public function qualifier($user)
     {
         return $user->getPoints() >= 50000;
