a6d53aedce90d1c1ab0790a01a61b5836d00ba32
Yogi <yoginth@hey.com>
2021-05-30 18:33:57 +0530

    fix: Larastan issues

--- app/Gamify/Points/TaskCompleted.php
+++ app/Gamify/Points/TaskCompleted.php
@@ -8,28 +8,13 @@ class TaskCompleted extends PointType
 {
     public $allowDuplicates = false;
 
-    /**
-     * Number of points.
-     *
-     * @var int
-     */
     public $points = 1;
 
-    /**
-     * Point constructor.
-     *
-     * @param $subject
-     */
     public function __construct($subject)
     {
         $this->subject = $subject;
     }
 
-    /**
-     * User who will be receive points.
-     *
-     * @return mixed
-     */
     public function payee()
     {
         return $this->getSubject()->user;
