a728bc2b7bd7764e4e87bd01451fd453f100493a
Yogi <yoginth@hey.com>
2021-05-30 18:34:07 +0530

    fix: Larastan issues

--- app/Gamify/Points/TaskCreated.php
+++ app/Gamify/Points/TaskCreated.php
@@ -6,28 +6,13 @@ use QCod\Gamify\PointType;
 
 class TaskCreated extends PointType
 {
-    /**
-     * Number of points.
-     *
-     * @var int
-     */
     public $points = 1;
 
-    /**
-     * Point constructor.
-     *
-     * @param $subject
-     */
     public function __construct($subject)
     {
         $this->subject = $subject;
     }
 
-    /**
-     * User who will be receive points.
-     *
-     * @return mixed
-     */
     public function payee()
     {
         return $this->getSubject()->user;
