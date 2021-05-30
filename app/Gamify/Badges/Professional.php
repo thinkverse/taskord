91632a42d7fa2d8f828c3a0ffb4f9f9c7387bc13
Yogi <yoginth@hey.com>
2021-05-30 18:32:22 +0530

    fix: Larastan issues

--- app/Gamify/Badges/Professional.php
+++ app/Gamify/Badges/Professional.php
@@ -2,26 +2,13 @@
 
 namespace App\Gamify\Badges;
 
-use App\Models\User;
 use QCod\Gamify\BadgeType;
 
 class Professional extends BadgeType
 {
-    /**
-     * Description for badge.
-     *
-     * @var string
-     */
     protected $description = '';
 
-    /**
-     * Check is user qualifies for badge.
-     *
-     * @param $user
-     *
-     * @return bool
-     */
-    public function qualifier(User $user)
+    public function qualifier($user)
     {
         return $user->getPoints() >= 5000;
     }
