91faeb464413280c39f19c81271afe36055c0278
Yogi <yoginth@hey.com>
2021-05-30 12:32:08 +0530

    Fix insights issues

--- app/Rules/ReservedSlug.php
+++ app/Rules/ReservedSlug.php
@@ -6,33 +6,11 @@ use Illuminate\Contracts\Validation\Rule;
 
 class ReservedSlug implements Rule
 {
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
-     */
     public function passes($attribute, $value)
     {
         return ! in_array($value, config('taskord.reserved_slugs'));
     }
 
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
     public function message()
     {
         return 'This slug is reserved internally!';
