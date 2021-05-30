94b693d4163f62dc6bcc6c0620cb6875a288ae0e
Yogi <yoginth@hey.com>
2021-05-30 18:27:11 +0530

    fix: Larastan issues

--- app/Gamify/Badges/Novice.php
+++ app/Gamify/Badges/Novice.php
@@ -6,20 +6,8 @@ use QCod\Gamify\BadgeType;
 
 class Novice extends BadgeType
 {
-    /**
-     * Description for badge.
-     *
-     * @var string
-     */
     protected $description = '';
 
-    /**
-     * Check is user qualifies for badge.
-     *
-     * @param $user
-     *
-     * @return bool
-     */
     public function qualifier($user)
     {
         return $user->getPoints() >= 500;
