1fc9e6c0893164695fa5126bbcf0a1912cfdcf86
Yogi <yoginth@hey.com>
2021-05-30 18:51:00 +0530

    Remove loading spinner in follow button

--- resources/views/livewire/notification/follow.blade.php
+++ resources/views/livewire/notification/follow.blade.php
@@ -1,14 +1,12 @@
 <div>
     @if (auth()->user()->isFollowing($user))
         <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
-            <div wire:loading wire:target="toggleFollow" class="spinner-border spinner-border-sm me-1"></div>
-            <x-heroicon-o-user-remove wire:loading.remove wire:target="toggleFollow" class="heroicon" />
+            <x-heroicon-o-user-remove class="heroicon" />
             Unfollow <span class="fw-bold">{{ '@'.$user->username }}</span>
         </button>
     @else
         <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
-            <div wire:loading wire:target="toggleFollow" class="spinner-border spinner-border-sm me-1"></div>
-            <x-heroicon-o-user-add wire:loading.remove wire:target="toggleFollow" class="heroicon" />
+            <x-heroicon-o-user-add class="heroicon" />
             Follow back <span class="fw-bold">{{ '@'.$user->username }}</span>
         </button>
     @endif
