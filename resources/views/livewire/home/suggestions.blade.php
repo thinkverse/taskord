9b47b854e68a1b6ee48847ebee5c5258d26195da
Yogi <yoginth@hey.com>
2021-05-30 18:47:39 +0530

    Remove ul line on suggestions

--- resources/views/livewire/home/suggestions.blade.php
+++ resources/views/livewire/home/suggestions.blade.php
@@ -8,22 +8,23 @@
                 <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
             </div>
         @else
-            <ul class="list-group list-group-flush">
-                @foreach ($users as $user)
-                    <li class="list-group-item d-flex align-items-center justify-content-between py-2" wire:key="{{ $user->id }}">
-                        <x:shared.user-label-big :user="$user" />
-                        <span>
-                            <livewire:home.follow :user="$user" :showText="$showText" :wire:key="$user->id" />
-                        </span>
-                    </li>
-                @endforeach
-            </ul>
-        @endif
-        @if ($readyToLoad and count($users) === 0)
-            <div class="card-body text-center fw-bold text-secondary">
-                <x-heroicon-o-user class="heroicon heroicon-20px text-primary" />
-                Nothing to suggest!
-            </div>
+            @if (count($users) === 0)
+                <div class="card-body text-center fw-bold text-secondary">
+                    <x-heroicon-o-user class="heroicon heroicon-20px text-primary" />
+                    Nothing to suggest!
+                </div>
+            @else
+                <ul class="list-group list-group-flush">
+                    @foreach ($users as $user)
+                        <li class="list-group-item d-flex align-items-center justify-content-between py-2" wire:key="{{ $user->id }}">
+                            <x:shared.user-label-big :user="$user" />
+                            <span>
+                                <livewire:home.follow :user="$user" :showText="$showText" :wire:key="$user->id" />
+                            </span>
+                        </li>
+                    @endforeach
+                </ul>
+            @endif
         @endif
     </div>
 </div>
