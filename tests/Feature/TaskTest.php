150190553788a2c920f6cbae7e942028b8dda788
Yogi <yoginth@hey.com>
2021-05-30 11:30:14 +0530

    Remove tests

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Livewire\CreateTask;
-use function Pest\Livewire\livewire;
 use function Tests\actingAs;
 
 it('has task page', function ($url, $expected, $auth) {
@@ -13,33 +11,4 @@ it('has task page', function ($url, $expected, $auth) {
 })->with([
     ['/task/1', 200, false],
     ['/task/1', 200, true],
-]);
-
-it('cannot create task as un-authed user', function () {
-    livewire(CreateTask::class)
-        ->set('task', 'Hello world from test!')
-        ->call('submit')
-        ->assertNotEmitted('refreshTasks');
-});
-
-it('can create task as authed user', function ($task) {
-    actingAs(2)
-        ->livewire(CreateTask::class)
-        ->set('task', $task)
-        ->call('submit')
-        ->assertEmitted('refreshTasks');
-})->with([
-    ['Hello world from test!'],
-    ['😊🤗💜✨👍'],
-]);
-
-it('cannot create task - task validation', function ($task) {
-    actingAs(2)
-        ->livewire(CreateTask::class)
-        ->set('task', $task)
-        ->call('submit')
-        ->assertHasErrors('task');
-})->with([
-    [''],
-    ['1234'],
 ]);
