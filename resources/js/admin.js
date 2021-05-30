0cb3a984c6e9959e848583e42b07951e364e4971
Yogi <yoginth@hey.com>
2021-05-30 20:47:42 +0530

    Style analytics

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,5 +1,13 @@
 import { getLCP, getFID, getCLS } from 'web-vitals';
 
-getCLS(console.log);
-getFID(console.log);
-getLCP(console.log);
+function logInConsole(metric) {
+    const body = JSON.stringify({ [metric.name]: metric.value });
+    console.log(
+        `%c${metric.name}: ${metric.value}`,
+        "color: #6a63ec; font-family: monospace; font-size: 15px; font-weight: bold"
+    );
+}
+
+getCLS(logInConsole);
+getFID(logInConsole);
+getLCP(logInConsole);
