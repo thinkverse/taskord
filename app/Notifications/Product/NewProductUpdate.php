c9058037aaed1944c1dd98d9470d8eaa8803edaf
Yogi <yoginth@hey.com>
2021-05-30 13:34:46 +0530

    fix: Issues reported by phpinsights

--- app/Notifications/Product/NewProductUpdate.php
+++ app/Notifications/Product/NewProductUpdate.php
@@ -26,12 +26,12 @@ class NewProductUpdate extends Notification implements ShouldQueue
     public function toMail($notifiable)
     {
         return (new MailMessage())
-                    ->subject('New product update from "'.$this->update->product->name.'"')
-                    ->greeting('Hello @'.$notifiable->username.' 👋')
-                    ->line('New product update from "'.$this->update->product->name.'"')
-                    ->line($this->update->title)
-                    ->line($this->update->body)
-                    ->action('Go to '.$this->update->product->name, url('/product/'.$this->update->product->slug))
-                    ->line('Thank you for using Taskord!');
+            ->subject('New product update from "'.$this->update->product->name.'"')
+            ->greeting('Hello @'.$notifiable->username.' 👋')
+            ->line('New product update from "'.$this->update->product->name.'"')
+            ->line($this->update->title)
+            ->line($this->update->body)
+            ->action('Go to '.$this->update->product->name, url('/product/'.$this->update->product->slug))
+            ->line('Thank you for using Taskord!');
     }
 }
