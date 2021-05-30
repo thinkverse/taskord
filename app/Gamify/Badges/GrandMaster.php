5e00160036de9a46b973d1b84ae77fc7c100227d
Yogi <yoginth@hey.com>
2021-05-30 18:26:44 +0530

    fix: Larastan issues

--- app/Gamify/Badges/GrandMaster.php
+++ app/Gamify/Badges/GrandMaster.php
@@ -6,20 +6,8 @@ use QCod\Gamify\BadgeType;
 
 class GrandMaster extends BadgeType
 {
-    /**
-     * Description for badge.
-     *
-     * @var string
-     */
     protected $description = '';
 
-    /**
-     * Check is user qualifies for badge.
-     *
-     * @param $user
-     *
-     * @return bool
-     */
     public function qualifier($user)
     {
         return $user->getPoints() >= 20000;
