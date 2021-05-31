0e5390eccb4a087a64a76549dd5176cbef0eed10
Yogi <yoginth@hey.com>
2021-05-31 11:43:45 +0000

    Move to componenet

--- resources/views/components/markdown-toolbar.blade.php
+++ resources/views/components/markdown-toolbar.blade.php
@@ -1,3 +1,13 @@
 <div>
-    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+    <md-header class="me-2"><img src="/images/md/heading.svg" /></md-header>
+    <md-bold class="me-2"><img src="/images/md/bold.svg" /></md-bold>
+    <md-italic class="me-4"><img src="/images/md/italic.svg" /></md-italic>
+    <md-quote class="me-2"><img src="/images/md/quote.svg" /></md-quote>
+    <md-code class="me-2"><img src="/images/md/code.svg" /></md-code>
+    <md-link class="me-2"><img src="/images/md/link.svg" /></md-link>
+    <md-image class="me-4"><img src="/images/md/image.svg" /></md-image>
+    <md-unordered-list class="me-2"><img src="/images/md/list-unordered.svg" /></md-unordered-list>
+    <md-ordered-list class="me-2"><img src="/images/md/list-ordered.svg" /></md-ordered-list>
+    <md-task-list class="me-4"><img src="/images/md/tasklist.svg" /></md-task-list>
+    <md-mention><img src="/images/md/mention.svg" /></md-mention>
 </div>
