0b26e9ea1acdc8b282e76421e52ec643d56cb538
Yogi <yoginth@hey.com>
2021-05-31 11:27:45 +0000

    Add spacing

--- resources/views/livewire/comment/create-comment.blade.php
+++ resources/views/livewire/comment/create-comment.blade.php
@@ -9,16 +9,16 @@
         @enderror
         <div class="mb-2">
             <markdown-toolbar for="comment-box">
-                <md-bold><img src="/images/md/bold.svg" /></md-bold>
-                <md-header><img src="/images/md/heading.svg" /></md-header>
-                <md-italic><img src="/images/md/italic.svg" /></md-italic>
-                <md-quote><img src="/images/md/quote.svg" /></md-quote>
-                <md-code><img src="/images/md/code.svg" /></md-code>
-                <md-link><img src="/images/md/link.svg" /></md-link>
-                <md-image><img src="/images/md/image.svg" /></md-image>
-                <md-unordered-list><img src="/images/md/list-unordered.svg" /></md-unordered-list>
-                <md-ordered-list><img src="/images/md/list-ordered.svg" /></md-ordered-list>
-                <md-task-list><img src="/images/md/tasklist.svg" /></md-task-list>
+                <md-header class="me-2"><img src="/images/md/heading.svg" /></md-header>
+                <md-bold class="me-2"><img src="/images/md/bold.svg" /></md-bold>
+                <md-italic class="me-4"><img src="/images/md/italic.svg" /></md-italic>
+                <md-quote class="me-2"><img src="/images/md/quote.svg" /></md-quote>
+                <md-code class="me-2"><img src="/images/md/code.svg" /></md-code>
+                <md-link class="me-2"><img src="/images/md/link.svg" /></md-link>
+                <md-image class="me-4"><img src="/images/md/image.svg" /></md-image>
+                <md-unordered-list class="me-2"><img src="/images/md/list-unordered.svg" /></md-unordered-list>
+                <md-ordered-list class="me-2"><img src="/images/md/list-ordered.svg" /></md-ordered-list>
+                <md-task-list class="me-4"><img src="/images/md/tasklist.svg" /></md-task-list>
                 <md-mention><img src="/images/md/mention.svg" /></md-mention>
             </markdown-toolbar>
             <textarea placeholder="Add a comment" id="comment-box" class="mt-2 form-control mentionInput" rows="3" wire:model.defer="comment"></textarea>
