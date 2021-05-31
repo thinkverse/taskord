0fa3d56841333a9979c019a55c3799e9b94837ea
Yogi <yoginth@hey.com>
2021-05-31 10:04:32 +0530

    Remove getTTFB

--- resources/js/stafftools.js
+++ resources/js/stafftools.js
@@ -1,9 +1,15 @@
 import { getLCP, getFID, getCLS, getFCP, getTTFB } from 'web-vitals';
 
+console.log(
+  `%c🚀 Taskord stafftools turned on`,
+  "color: #38c172; font-family: monospace; font-size: 20px; font-weight: bold"
+);
+
 function logInConsole({ name, delta }) {
   console.log(
-    `%c${name}: ${parseFloat(name === 'CLS' ? delta * 1000 : delta).toFixed(2)}`,
-    "color: #6a63ec; font-family: monospace; font-size: 15px; font-weight: bold"
+    `%c[${name}] %c${parseFloat(name === 'CLS' ? delta * 1000 : delta).toFixed(2)}`,
+    "color: #6a63ec; font-family: monospace; font-size: 15px; font-weight: bold",
+    "font-family: monospace; font-size: 15px; font-weight: bold",
   );
 }
 
@@ -11,7 +17,6 @@ getCLS(logInConsole);
 getFID(logInConsole);
 getLCP(logInConsole);
 getFCP(logInConsole);
-getTTFB(logInConsole);
 
 // Toggle stats in adminbar
 var expandStats = document.getElementById("expand-stats");
