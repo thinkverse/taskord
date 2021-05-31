e726f2e26406d84f438b62f3a498823bee00a0a6
Yogi <yoginth@hey.com>
2021-05-31 10:39:34 +0530

    Add site sha

--- resources/js/stafftools.js
+++ resources/js/stafftools.js
@@ -1,27 +1,39 @@
-import { getLCP, getFID, getCLS, getFCP, getTTFB } from 'web-vitals';
+import { getLCP, getFID, getCLS, getFCP, getTTFB } from "web-vitals";
 
 console.log(
   `%c🚀 Taskord stafftools turned on`,
   "color: #38c172; font-family: monospace; font-size: 20px; font-weight: bold"
 );
 
-function logInConsole({ name, delta }) {
+function logVitalsInConsole({ name, delta }) {
   console.log(
-    `%c[${name}] %c${parseFloat(name === 'CLS' ? delta * 1000 : delta).toFixed(2)}`,
+    `%c[${name}] %c${parseFloat(name === "CLS" ? delta * 1000 : delta).toFixed(2)}`,
     "color: #6a63ec; font-family: monospace; font-size: 15px; font-weight: bold",
     "font-family: monospace; font-size: 15px; font-weight: bold",
   );
 }
 
-getCLS(logInConsole);
-getFID(logInConsole);
-getLCP(logInConsole);
-getFCP(logInConsole);
+function logInConsole({ key, value }) {
+  console.log(
+    `%c[${key}] %c${value}`,
+    "color: #6a63ec; font-family: monospace; font-size: 15px; font-weight: bold",
+    "font-family: monospace; font-size: 15px; font-weight: bold",
+  );
+}
+
+getCLS(logVitalsInConsole);
+getFID(logVitalsInConsole);
+getLCP(logVitalsInConsole);
+getFCP(logVitalsInConsole);
+logInConsole({
+  key: "Site SHA",
+  value: document.getElementById("site-sha").innerHTML
+});
 
 // Toggle stats in adminbar
 var expandStats = document.getElementById("expand-stats");
 if (expandStats) {
   expandStats.addEventListener("click", async () => {
-    document.getElementById("staffbar-stats").classList.toggle('d-none');
+    document.getElementById("staffbar-stats").classList.toggle("d-none");
   });
 }
