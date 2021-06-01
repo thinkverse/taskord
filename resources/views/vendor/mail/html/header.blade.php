0b662a713a5c2acb96a023eb02337f19f6d57616
Yogi <yoginth@hey.com>
2021-06-01 12:35:06 +0000

    Switch logo in email to png from svg

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Taskord')
-<img loading=lazy src="https://ik.imagekit.io/taskordimg/logo_8lLu9EPFa.svg" class="logo" alt="Taskord Logo">
+<img loading=lazy src="https://ik.imagekit.io/taskordimg/email-logo_zJ5cHE2wq.png" class="logo" alt="Taskord Logo">
 @else
 {{ $slot }}
 @endif
