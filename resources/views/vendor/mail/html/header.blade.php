05aab88ac94fbddf24c310fa535e74bd24f85fc8
Maanas Nair <bee.birdiecorporation@gmail.com>
2021-06-01 15:23:54 +0530

    Update image links as most email clients including Gmail does not support use of svg files as it requires a fallback.

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Taskord')
-<img loading=lazy src="https://ik.imagekit.io/taskordimg/logo_8lLu9EPFa.svg" class="logo" alt="Taskord Logo">
+<img loading=lazy src="https://i.ibb.co/9ypjVDG/logo-8l-Lu9-EPFa.png" class="logo" alt="Taskord Logo">
 @else
 {{ $slot }}
 @endif
