48d03736db7cc186397e080a0edc5dbd6acc2d8d
Yogi <yoginth@hey.com>
2021-06-02 18:25:15 +0530

    add: API page

--- resources/views/pages/api.blade.php
+++ resources/views/pages/api.blade.php
@@ -15,7 +15,10 @@
         </div>
         <div class="card-body">
             <div>
-                Taskord built the Taskord app as an Open Source app. This SERVICE is provided by Taskord at no cost and is intended for use as is.
+                The WIP API lets you build third-party apps for WIP and integrate with existing ones.
+            </div>
+            <div class="mt-3">
+                The API uses GraphQL. It's a relatively new technology that lets you request all the data you need in just one request. If you're unfamiliar with GraphQL we suggest you <a href="https://graphql.org/learn">read up on it first</a>.
             </div>
         </div>
     </div>
