5a1f622c1a75af82b95dce9a83f065b490b9c91b
Yogi <yoginth@hey.com>
2021-06-02 19:33:48 +0530

    Add CI script

--- script/ci.sh
+++ script/ci.sh
@@ -5,6 +5,7 @@ yarn install
 yarn production
 cp .env.example .env
 php artisan key:generate
+mysql -uroot --execute="DROP DATABASE taskord; CREATE DATABASE taskord;"
 php artisan migrate
 php artisan db:seed
 php artisan test
