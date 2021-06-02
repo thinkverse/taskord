eaa5135f13c7190f1916f84a3699fd0bd8a410d3
Yogi <yoginth@hey.com>
2021-06-02 10:30:22 +0000

    Add CanBeLiked trait

--- app/Models/CommentReply.php
+++ app/Models/CommentReply.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Multicaret\Acquaintances\Traits\CanBeLiked;
 use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class CommentReply extends Model
 {
     use HasFactory;
+    use CanBeLiked;
     use QueryCacheable;
 
     public $cacheFor = 3600;
