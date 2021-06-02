6e6d2da78323d08496a3167689287722ae88c1a1
Yogi <yoginth@hey.com>
2021-06-02 11:02:24 +0000

    Add reply praised

---
+++ app/Http/Livewire/Notification/Type/Comment/Reply/ReplyPraised.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Notification\Type\Comment\Reply;
+
+use Livewire\Component;
+
+class ReplyPraised extends Component
+{
+    public function render()
+    {
+        return view('livewire.notification.type.comment.reply.reply-praised');
+    }
+}
