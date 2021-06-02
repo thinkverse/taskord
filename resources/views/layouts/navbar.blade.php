8b4c6193ceffcabd55b8410516ab0fdb6424cff8
Yogi <yoginth@hey.com>
2021-06-02 04:06:00 +0000

    Add pride logo

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,13 +3,13 @@
         <a class="navbar-brand" href="{{ url('/') }}">
             @auth
                 @if (auth()->user()->is_beta)
-                    <img loading=lazy src="https://ik.imagekit.io/taskordimg/pride_vocaCTHn-.svg" height="35" alt="Taskord Beta" title="Taskord Beta">
+                    <img loading=lazy src="https://ik.imagekit.io/taskordimg/beta_J6zazpyIw.svg" height="35" alt="Taskord Beta" title="Taskord Beta">
                 @else
-                    <img loading=lazy src="https://ik.imagekit.io/taskordimg/pride_vocaCTHn-.svg" height="35" alt="Taskord">
+                    <img loading=lazy src="https://ik.imagekit.io/taskordimg/logo_8lLu9EPFa.svg" height="35" alt="Taskord">
                 @endif
             @endauth
             @guest
-                <img loading=lazy src="https://ik.imagekit.io/taskordimg/pride_vocaCTHn-.svg" height="35" alt="Taskord">
+                <img loading=lazy src="https://ik.imagekit.io/taskordimg/logo_8lLu9EPFa.svg" height="35" alt="Taskord">
             @endguest
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
