49b7925f529477e7e4964a773078203aedbe18f2
Yogi <yoginth@hey.com>
2021-06-02 18:31:59 +0530

    add: API page

--- resources/views/pages/api.blade.php
+++ resources/views/pages/api.blade.php
@@ -15,7 +15,7 @@
         </div>
         <div class="card-body">
             <div>
-                The WIP API lets you build third-party apps for WIP and integrate with existing ones.
+                The Taskord API lets you build third-party apps for WIP and integrate with existing ones.
             </div>
             <div class="mt-3">
                 The API uses GraphQL. It's a relatively new technology that lets you request all the data you need in just one request. If you're unfamiliar with GraphQL we suggest you <a href="https://graphql.org/learn">read up on it first</a>.
