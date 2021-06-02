1b68e1d164974f5cbf670cbb51048624485d539b
Yogi <yoginth@hey.com>
2021-06-02 17:46:08 +0530

    add: MilestonePraised notifications

---
+++ resources/views/livewire/notification/type/milestone/milestone-praised.blade.php
@@ -0,0 +1,15 @@
+<div>
+    @if ($comment)
+        <div class="mt-2 text-secondary">
+            praised your
+            <a class="fw-bold" href="{{ route('comment', ['id' => $comment->task->id, 'comment_id' => $comment->id]) }}">
+                comment
+            </a>
+        </div>
+        <div class="mt-3">
+            <livewire:comment.single-comment :comment="$comment" :wire:key="$comment->id" />
+        </div>
+    @else
+        <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
+    @endif
+</div>
