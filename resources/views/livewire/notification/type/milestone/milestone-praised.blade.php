eb67e5a5fa008cd190772189f7888a036dcc1765
Yogi <yoginth@hey.com>
2021-06-02 17:47:02 +0530

    add: MilestonePraised notifications

--- resources/views/livewire/notification/type/milestone/milestone-praised.blade.php
+++ resources/views/livewire/notification/type/milestone/milestone-praised.blade.php
@@ -1,9 +1,9 @@
 <div>
-    @if ($comment)
+    @if ($milestone)
         <div class="mt-2 text-secondary">
             praised your
-            <a class="fw-bold" href="{{ route('comment', ['id' => $comment->task->id, 'comment_id' => $comment->id]) }}">
-                comment
+            <a class="fw-bold" href="{{ route('milestones.milestone', ['milestone' => $milestone]) }}">
+                milestone
             </a>
         </div>
         <div class="mt-3">
