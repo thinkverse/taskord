4e3854bcdb3e28aa682916b7a21ac6533270c992
Yogi <yoginth@hey.com>
2021-06-02 17:43:40 +0530

    add: MilestonePraised notifications

---
+++ app/Http/Livewire/Notification/Type/Milestone/MilestonePraised.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Notification\Type\Milestone;
+
+use Livewire\Component;
+
+class MilestonePraised extends Component
+{
+    public function render()
+    {
+        return view('var.www.html.resources.views.livewire.notification.type.milestone.milestone-praised');
+    }
+}
