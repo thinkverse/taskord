faee0ce449bf245f73208ac0a2f902be8f323024
Yogi <yoginth@hey.com>
2021-06-03 03:56:05 +0000

    update: Rounded pill outlined buttons

--- resources/views/question/nav.blade.php
+++ resources/views/question/nav.blade.php
@@ -1,11 +1,11 @@
 <div class="mb-3">
-    <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.newest' ? '' : 'outline-' }}primary me-2" href="{{ route('questions.newest') }}">
+    <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.newest' ? '' : 'outline-' }}primary rounded-pill me-2" href="{{ route('questions.newest') }}">
         Newest
     </a>
-    <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.unanswered' ? '' : 'outline-' }}primary me-2" href="{{ route('questions.unanswered') }}">
+    <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.unanswered' ? '' : 'outline-' }}primary rounded-pill me-2" href="{{ route('questions.unanswered') }}">
         Unanswered
     </a>
-    <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.popular' ? '' : 'outline-' }}primary me-2" href="{{ route('questions.popular') }}">
+    <a class="mb-2 btn btn-{{ Route::currentRouteName() === 'questions.popular' ? '' : 'outline-' }}primary rounded-pill me-2" href="{{ route('questions.popular') }}">
         Popular
     </a>
     @auth
