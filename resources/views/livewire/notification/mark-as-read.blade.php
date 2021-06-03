e4543790e7cbd69bac67da13a69ee5b834fa1824
Yogi <yoginth@hey.com>
2021-06-03 03:39:20 +0000

    update: Rounded pill outlined buttons

--- resources/views/livewire/notification/mark-as-read.blade.php
+++ resources/views/livewire/notification/mark-as-read.blade.php
@@ -1,4 +1,4 @@
-<button class="btn btn-success text-white float-md-end mb-3" wire:click="markAsRead" wire:loading.attr="disabled">
+<button class="btn btn-outline-success rounded-pill float-md-end mb-3" wire:click="markAsRead" wire:loading.attr="disabled">
     <x-heroicon-o-check class="heroicon" />
     Mark all as Read
 </button>
