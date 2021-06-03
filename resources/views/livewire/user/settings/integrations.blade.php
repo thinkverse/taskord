a402088a169c1a5e4f6dac915b29c538bf84536e
Yogi <yoginth@hey.com>
2021-06-03 03:07:50 +0000

    update: Rounded pill outlined buttons

--- resources/views/livewire/user/settings/integrations.blade.php
+++ resources/views/livewire/user/settings/integrations.blade.php
@@ -202,7 +202,7 @@
                                 </td>
                                 <td>
                                     <button wire:loading.attr="disabled" wire:click="deleteWebhook({{ $webhook->id }})"
-                                        class="btn btn-sm w-100 btn-danger">
+                                        class="btn btn-sm w-100 btn-outline-danger rounded-pill">
                                         <x-heroicon-o-trash class="heroicon" />
                                         Delete
                                     </button>
