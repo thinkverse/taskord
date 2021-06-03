fb09d65866eef85fa56c04dca7a9bb60c9f34f0a
Yogi <yoginth@hey.com>
2021-06-03 02:58:40 +0000

    update: Rounded pill outlined buttons

--- resources/views/livewire/notification/follow.blade.php
+++ resources/views/livewire/notification/follow.blade.php
@@ -1,12 +1,12 @@
 <div>
     @if (auth()->user()->isFollowing($user))
         <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
-            <x-heroicon-o-user-remove class="heroicon" />
+            <x-heroicon-o-user-remove class="heroicon heroicon-15px" />
             Unfollow <span class="fw-bold">{{ '@'.$user->username }}</span>
         </button>
     @else
         <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
-            <x-heroicon-o-user-add class="heroicon" />
+            <x-heroicon-o-user-add class="heroicon heroicon-15px" />
             Follow back <span class="fw-bold">{{ '@'.$user->username }}</span>
         </button>
     @endif
