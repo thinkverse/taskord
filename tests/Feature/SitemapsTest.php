fcc64c662f4808ff0c24cb58accb98a441b82187
Yogi <yoginth@hey.com>
2021-06-03 12:38:55 +0000

    Add sitemaps test

---
+++ tests/Feature/SitemapsTest.php
@@ -0,0 +1,73 @@
+<?php
+
+use function Tests\actingAs;
+
+it('has sitemap_users', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/sitemap_users.txt', 200, true],
+]);
+
+it('has sitemap_products', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/sitemap_products.txt', 200, true],
+]);
+
+it('has sitemap_questions', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/sitemap_questions.txt', 200, true],
+]);
+
+it('has sitemap_tasks', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/sitemap_tasks.txt', 200, true],
+]);
+
+it('has sitemap_comments', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/sitemap_comments.txt', 200, true],
+]);
+
+it('has sitemap_milestones', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/sitemap_milestones.txt', 200, true],
+]);
+
+it('has sitemap_urls', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/sitemap_urls.txt', 200, true],
+]);
