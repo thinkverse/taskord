76867735caf6705976344431ba6667fd98bf5fb9
Yogi <yoginth@hey.com>
2021-06-03 03:42:13 +0000

    update: Rounded pill outlined buttons

--- resources/views/livewire/question/subscribe.blade.php
+++ resources/views/livewire/question/subscribe.blade.php
@@ -1,12 +1,12 @@
 <div>
     @auth
         @if (auth()->user()->hasSubscribed($question))
-            <button wire:click="subscribeQuestion" wire:loading.attr="disabled" class="btn btn-sm btn-danger">
+            <button wire:click="subscribeQuestion" wire:loading.attr="disabled" class="btn btn-sm btn-outline-danger rounded-pill">
                 <x-heroicon-o-status-offline class="heroicon" />
                 Unsubscribe
             </button>
         @else
-            <button wire:click="subscribeQuestion" wire:loading.attr="disabled" class="btn btn-sm btn-primary">
+            <button wire:click="subscribeQuestion" wire:loading.attr="disabled" class="btn btn-sm btn-outline-primary rounded-pill">
                 <x-heroicon-o-status-online class="heroicon" />
                 Subscribe
             </button>
