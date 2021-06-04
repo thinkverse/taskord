581a046464753bd0ad727c6affdf166ecba5a594
Yogi <yoginth@hey.com>
2021-06-04 19:04:32 +0530

    add: Domain verification

--- resources/views/product/verify.blade.php
+++ resources/views/product/verify.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('pageTitle', 'Edit Product ·')
+@section('pageTitle', 'Verify Product ·')
 
 @section('content')
 <div class="container-md">
@@ -8,11 +8,11 @@
         <div class="col-lg-7">
             @auth
                 @if (!auth()->user()->spammy)
-                    <livewire:product.edit-product :product="$product" />
+                    <livewire:product.verify :product="$product" />
                 @else
                     <div class="text-center">
                         <div class="alert alert-danger" role="alert">
-                            You can't edit this product, because your account has been flagged 😢
+                            You can't verify this product, because your account has been flagged 😢
                         </div>
                         <a class="btn btn-outline-primary rounded-pill" href="{{ route('home') }}">Go to home</a>
                     </div>
