8790c0fbc412cc055cf4d1450a25c45ccf905441
Yogi <yoginth@hey.com>
2021-06-04 19:14:12 +0530

    add: Domain verification

--- resources/views/livewire/product/verify.blade.php
+++ resources/views/livewire/product/verify.blade.php
@@ -11,13 +11,28 @@
                     <p>
                        1. Create a TXT record in your DNS configuration for the following hostname:
                     </p>
+                    <div class="input-group mb-3">
+                        <input type="text" id="personal-access-token" class="form-control @error('token') is-invalid @enderror" value="{{ $txt_code }}" readonly>
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy" data-bs-toggle="tooltip" data-for="#personal-access-token">
+                                <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px"></x-heroicon-o-clipboard-copy>
+                            </button>
+                        </div>
+                    </div>
                     <p>
                         2. Use this code as the value for the TXT record:
                     </p>
+                    <div class="input-group mb-3">
+                        <input type="text" id="personal-access-token" class="form-control @error('token') is-invalid @enderror" value="{{ $txt_code }}" readonly>
+                        <div class="input-group-append">
+                            <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy" data-bs-toggle="tooltip" data-for="#personal-access-token">
+                                <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px"></x-heroicon-o-clipboard-copy>
+                            </button>
+                        </div>
+                    </div>
                     <p>
                         3. Wait until your DNS configuration changes. This could take up to 72 hours.
                     </p>
-                    {{ $txt_code }}
                 </div>
                 <button type="submit" class="btn btn-outline-success rounded-pill">
                     Update
