d322aae834c4740aeb279ee7b0cc3dc51fe46069
Yogi <yoginth@hey.com>
2021-06-04 19:29:04 +0530

    check domain

--- resources/views/livewire/product/verify.blade.php
+++ resources/views/livewire/product/verify.blade.php
@@ -6,37 +6,46 @@
     <div class="card">
         <form wire:target="submit" wire:submit.prevent="submit">
             <div class="card-body">
-                <div>
-                    <h5>Add a DNS TXT record</h5>
-                    <p>
-                       1. Create a TXT record in your DNS configuration for the following hostname:
-                    </p>
-                    <div class="input-group mb-3">
-                        <input type="text" id="personal-access-token" class="form-control @error('token') is-invalid @enderror" value="{{ $txt_record }}" readonly>
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy" data-bs-toggle="tooltip" data-for="#personal-access-token">
-                                <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px"></x-heroicon-o-clipboard-copy>
-                            </button>
+                @if ($product->website)
+                    <div>
+                        <h5>Add a DNS TXT record</h5>
+                        <p>
+                        1. Create a TXT record in your DNS configuration for the following hostname:
+                        </p>
+                        <div class="input-group mb-3">
+                            <input type="text" id="personal-access-token" class="form-control @error('token') is-invalid @enderror" value="{{ $txt_record }}" readonly>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy" data-bs-toggle="tooltip" data-for="#personal-access-token">
+                                    <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px"></x-heroicon-o-clipboard-copy>
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                    <p>
-                        2. Use this code as the value for the TXT record:
-                    </p>
-                    <div class="input-group mb-3">
-                        <input type="text" id="personal-access-token" class="form-control @error('token') is-invalid @enderror" value="{{ $txt_code }}" readonly>
-                        <div class="input-group-append">
-                            <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy" data-bs-toggle="tooltip" data-for="#personal-access-token">
-                                <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px"></x-heroicon-o-clipboard-copy>
-                            </button>
+                        <p>
+                            2. Use this code as the value for the TXT record:
+                        </p>
+                        <div class="input-group mb-3">
+                            <input type="text" id="personal-access-token" class="form-control @error('token') is-invalid @enderror" value="{{ $txt_code }}" readonly>
+                            <div class="input-group-append">
+                                <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy" data-bs-toggle="tooltip" data-for="#personal-access-token">
+                                    <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px"></x-heroicon-o-clipboard-copy>
+                                </button>
+                            </div>
                         </div>
+                        <p>
+                            3. Wait until your DNS configuration changes. This could take up to 72 hours.
+                        </p>
                     </div>
-                    <p>
-                        3. Wait until your DNS configuration changes. This could take up to 72 hours.
-                    </p>
-                </div>
-                <button type="submit" class="btn btn-outline-success rounded-pill">
-                    Verify domain
-                </button>
+                    <button type="submit" class="btn btn-outline-success rounded-pill">
+                        Verify domain
+                    </button>
+                @else
+                    <div>
+                        <h5 class="mb-3">Add domain name in edit page</h5>
+                        <a class="btn btn-outline-success rounded-pill" href="{{ route('product.edit', ['slug' => $product->slug]) }}">
+                            Go to edit page
+                        </a>
+                    </div>
+                @endif
             </div>
         </form>
     </div>
