f6a23e8efb05c051972b8e11d56fc406b6657035
Yogi <yoginth@hey.com>
2021-06-04 20:08:19 +0530

    add: Domain verification

--- app/Http/Livewire/Product/Verify.php
+++ app/Http/Livewire/Product/Verify.php
@@ -36,7 +36,7 @@ class Verify extends Component
         if (in_array($this->product->txt_code, $txtList)) {
             auth()->user()->touch();
             loggy(request(), 'Product', auth()->user(), "Verified the product and domain | Product ID: {$this->product->id}");
-            return toast($this, 'success', "Domain and product has been successfully verified!");
+            return toast($this, 'success', "Domain and product has been successfully verified 🎉");
         } else {
             return toast($this, 'error', "We can't verify the domain at this time, please try again later!");
         }
