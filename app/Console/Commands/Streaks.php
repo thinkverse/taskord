c2a93205273690e23a3d653cca5ec0b55b31ff04
Yogi <yoginth@hey.com>
2021-06-04 14:47:51 +0530

    update: Simplify concatenation

--- app/Console/Commands/Streaks.php
+++ app/Console/Commands/Streaks.php
@@ -81,7 +81,7 @@ class Streaks extends Command
             }
 
             if ($user->vacation_mode) {
-                $this->info('Skipped streaks update for @'.$user->username.'! - '.$streaks.' Total Streaks');
+                $this->info("Skipped streaks update for @{$user->username}! - {$streaks} Total Streaks");
             } else {
                 $user->streaks = $streaks;
                 $this->info('Calculation Successful for @'.$user->username.'! - '.$streaks.' Total Streaks');
