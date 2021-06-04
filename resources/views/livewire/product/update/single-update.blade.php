4f718b2b1a098f3eade80a97515f097f4d71d830
Yogi <yoginth@hey.com>
2021-06-04 14:15:08 +0530

    remove: wire:offline.attr=disabled

--- resources/views/livewire/product/update/single-update.blade.php
+++ resources/views/livewire/product/update/single-update.blade.php
@@ -9,7 +9,7 @@
                 @if (!$update->user->is_private)
                     @if (auth()->user()->hasLiked($update))
                         <span>
-                            <button type="button" class="btn btn-action btn-like me-1" wire:click="toggleLike" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $update->id }}" aria-label="Like">
+                            <button type="button" class="btn btn-action btn-like me-1" wire:click="toggleLike" wire:loading.attr="disabled" wire:key="{{ $update->id }}" aria-label="Like">
                                 <span wire:loading wire:target="toggleLike" class="spinner-border spinner-border-task" role="status"></span>
                                 <x-heroicon-s-heart wire:loading.remove wire:target="toggleLike" class="heroicon heroicon-15px me-0" />
                                 <span class="small fw-bold">
@@ -19,7 +19,7 @@
                         </span>
                     @else
                         <span>
-                            <button type="button" class="btn btn-action btn-outline-like me-1" wire:click="toggleLike" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $update->id }}" aria-label="Likes">
+                            <button type="button" class="btn btn-action btn-outline-like me-1" wire:click="toggleLike" wire:loading.attr="disabled" wire:key="{{ $update->id }}" aria-label="Likes">
                                 <span wire:loading wire:target="toggleLike" class="spinner-border spinner-border-task" role="status"></span>
                                 <x-heroicon-o-heart wire:loading.remove wire:target="toggleLike" class="heroicon heroicon-15px me-0" />
                                 @if ($update->likerscount() !== 0)
@@ -50,7 +50,6 @@
                         onclick="confirm('Are you sure?') || event.stopImmediatePropagation()"
                         wire:click="deleteUpdate"
                         wire:loading.attr="disabled"
-                        wire:offline.attr="disabled"
                         aria-label="Delete"
                     >
                         <x-heroicon-o-trash class="heroicon heroicon-15px me-0" />
