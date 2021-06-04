a90e5eebba79eb13154d77c3422a3210be39a42c
Yogi <yoginth@hey.com>
2021-06-04 19:10:58 +0530

    add: Domain verification

--- resources/views/livewire/product/verify.blade.php
+++ resources/views/livewire/product/verify.blade.php
@@ -6,7 +6,19 @@
     <div class="card">
         <form wire:target="submit" wire:submit.prevent="submit">
             <div class="card-body">
-                {{ $txt_code }}
+                <div>
+                    <h5>Add a DNS TXT record</h5>
+                    <p>
+                       1. Create a TXT record in your DNS configuration for the following hostname:
+                    </p>
+                    <p>
+                        2. Use this code as the value for the TXT record:
+                    </p>
+                    <p>
+                        3. Wait until your DNS configuration changes. This could take up to 72 hours.
+                    </p>
+                    {{ $txt_code }}
+                </div>
                 <button type="submit" class="btn btn-outline-success rounded-pill">
                     Update
                 </button>
