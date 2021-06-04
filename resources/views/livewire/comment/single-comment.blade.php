9776f9c1c1222d5c61da8afa9c0121cdce2c4309
Yogi <yoginth@hey.com>
2021-06-04 14:14:25 +0530

    remove: wire:offline.attr=disabled

--- resources/views/livewire/comment/single-comment.blade.php
+++ resources/views/livewire/comment/single-comment.blade.php
@@ -19,7 +19,7 @@
         <div class="mt-2">
             @auth
                 @if (auth()->user()->hasLiked($comment))
-                    <button type="button" class="btn btn-action btn-like me-1" wire:click="toggleLike" wire:loading.attr="disabled" wire:offline.attr="disabled" aria-label="Like">
+                    <button type="button" class="btn btn-action btn-like me-1" wire:click="toggleLike" wire:loading.attr="disabled" aria-label="Like">
                         <span wire:loading wire:target="toggleLike" class="spinner-border spinner-border-task" role="status"></span>
                         <x-heroicon-s-heart wire:loading.remove wire:target="toggleLike" class="heroicon heroicon-15px me-0" />
                         <span class="small fw-bold">
@@ -27,7 +27,7 @@
                         </span>
                     </button>
                 @else
-                    <button type="button" class="btn btn-action btn-outline-like me-1" wire:click="toggleLike" wire:loading.attr="disabled" wire:offline.attr="disabled" aria-label="Likes">
+                    <button type="button" class="btn btn-action btn-outline-like me-1" wire:click="toggleLike" wire:loading.attr="disabled" aria-label="Likes">
                         <span wire:loading wire:target="toggleLike" class="spinner-border spinner-border-task" role="status"></span>
                         <x-heroicon-o-heart wire:loading.remove wire:target="toggleLike" class="heroicon heroicon-15px me-0" />
                         @if ($comment->likerscount() !== 0)
@@ -59,7 +59,7 @@
                     </button>
                 @endcan
                 @can('staff.ops')
-                    <button type="button" class="btn btn-action {{ $comment->hidden ? 'btn-info' : 'btn-outline-info' }} ms-1" wire:click="hide" wire:loading.attr="disabled" wire:offline.attr="disabled" wire:key="{{ $comment->id }}" aria-label="Hide">
+                    <button type="button" class="btn btn-action {{ $comment->hidden ? 'btn-info' : 'btn-outline-info' }} ms-1" wire:click="hide" wire:loading.attr="disabled" wire:key="{{ $comment->id }}" aria-label="Hide">
                         <x-heroicon-o-eye-off class="heroicon heroicon-15px me-0" />
                     </button>
                 @endcan
