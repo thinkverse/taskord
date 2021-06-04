f88d506b96e78cf53072523f81de24d907e3c5e7
Yogi <yoginth@hey.com>
2021-06-04 18:46:14 +0530

    Add txt_code column

--- database/migrations/2021_06_04_131504_add_txt_code_to_products_table.php
+++ database/migrations/2021_06_04_131504_add_txt_code_to_products_table.php
@@ -14,7 +14,7 @@ class AddTxtCodeToProductsTable extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('txt_code')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ class AddTxtCodeToProductsTable extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropColumn('txt_code');
         });
     }
 }
