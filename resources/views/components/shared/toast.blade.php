a4f9a18586e7b804db5dd482f544399861a4e215
Yogi <yoginth@hey.com>
2021-06-05 11:34:29 +0530

    fix: Toast break

--- resources/views/components/shared/toast.blade.php
+++ resources/views/components/shared/toast.blade.php
@@ -1,11 +1,13 @@
 <div class="position-fixed top-0 end-0" style="z-index: 5">
     <div id="liveToast" class="toast hide m-3" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-body d-flex justify-content-between align-items-center py-3">
-            <div class="d-flex align-items-center pe-2">
-                <div id="toast-icon" class="me-2"></div>
-                <div id="toast-body"></div>
+        <div class="d-flex">
+            <div id="toast-icon" class="rounded-start d-flex align-items-center px-3"></div>
+            <div class="toast-body d-flex w-100 justify-content-between align-items-center py-3">
+                <div class="d-flex align-items-center pe-2">
+                    <div id="toast-body"></div>
+                </div>
+                <button type="button" class="btn-close btn-sm" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
-            <button type="button" class="btn-close btn-sm" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
     </div>
 </div>
