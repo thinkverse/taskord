ff010aad022bf8061b9be8897cb1e4ad389db763
Yogi <yoginth@hey.com>
2021-06-05 11:28:19 +0530

    rollback: New Toast

--- resources/views/components/shared/toast.blade.php
+++ resources/views/components/shared/toast.blade.php
@@ -1,8 +1,8 @@
 <div class="position-fixed top-0 end-0" style="z-index: 5">
-    <div id="liveToast" class="d-flex toast hide m-3" role="alert" aria-live="assertive" aria-atomic="true">
-        <div id="toast-icon" class="rounded-start d-flex align-items-center px-3"></div>
-        <div class="toast-body d-flex w-100 justify-content-between align-items-center py-3">
+    <div id="liveToast" class="toast hide m-3" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-body d-flex justify-content-between align-items-center py-3">
             <div class="d-flex align-items-center pe-2">
+                <div id="toast-icon" class="me-2"></div>
                 <div id="toast-body"></div>
             </div>
             <button type="button" class="btn-close btn-sm" data-bs-dismiss="toast" aria-label="Close"></button>
