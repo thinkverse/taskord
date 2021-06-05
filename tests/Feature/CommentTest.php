c0d53cb019929b0edf4216c8312009cd2b214acb
Yogi <yoginth@hey.com>
2021-06-05 19:18:56 +0530

    update: Test

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -22,5 +22,28 @@ it('cannot create comment as un-authed user', function () {
     livewire(CreateComment::class, ['task' => $task])
         ->set('comment', 'Hello world from test!')
         ->call('submit')
-        ->assertNotEmitted('refreshTasks');
+        ->assertNotEmitted('refreshComments');
 });
+
+it('can create comment as authed user', function ($comment, $user, $status) {
+    $task = Task::factory()->create();
+
+    if ($status) {
+        return actingAs($user)
+            ->livewire(CreateComment::class, ['task' => $task])
+            ->set('comment', $comment)
+            ->call('submit')
+            ->assertEmitted('refreshComments');
+    }
+
+    return actingAs(2)
+        ->livewire(CreateComment::class, ['task' => $task])
+        ->set('task', $task)
+        ->call('submit')
+        ->assertNotEmitted('refreshComments');
+})->with([
+    ['Hello world from test!', 2, true],
+    ['😊🤗💜✨👍', 2, true],
+    ['', 2, false],
+    ['1234', 2, false],
+]);
