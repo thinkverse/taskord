ad7bedb512f67dc593e514b95dff67dc5c136b21
Yogi <yoginth@hey.com>
2021-06-05 11:16:13 +0530

    update: Toast style

--- resources/views/components/shared/toast.blade.php
+++ resources/views/components/shared/toast.blade.php
@@ -1,9 +1,9 @@
 <div class="position-fixed top-0 end-0" style="z-index: 5">
-    <div id="liveToast" class="toast hide m-3" role="alert" aria-live="assertive" aria-atomic="true">
-        <div class="toast-body d-flex justify-content-between align-items-center py-3">
-            <div class="d-flex align-items-center">
-                <div id="toast-icon" class="me-2"></div>
-                <div id="toast-body" class="me-1"></div>
+    <div id="liveToast" class="d-flex toast hide m-3" role="alert" aria-live="assertive" aria-atomic="true">
+        <div id="toast-icon" class="rounded-start d-flex align-items-center bg-success px-3 me-2"></div>
+        <div class="toast-body d-flex w-100 justify-content-between align-items-center py-3">
+            <div class="d-flex align-items-center pe-2">
+                <div id="toast-body"></div>
             </div>
             <button type="button" class="btn-close btn-sm" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
