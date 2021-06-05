65b2a8bda8bddbcc2ab5d21b15f8a9ff740feed3
Yogi <yoginth@hey.com>
2021-06-05 19:17:04 +0530

    update: Test

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use App\Models\Task;
 use App\Http\Livewire\Comment\CreateComment;
 use function Pest\Livewire\livewire;
 use function Tests\actingAs;
@@ -15,3 +15,12 @@ it('has comment page', function ($url, $expected, $auth) {
     ['/task/1/1', 200, false],
     ['/task/1/1', 200, true],
 ]);
+
+it('cannot create comment as un-authed user', function () {
+    $task = Task::factory()->create();
+
+    livewire(CreateComment::class, ['task' => $task])
+        ->set('comment', 'Hello world from test!')
+        ->call('submit')
+        ->assertNotEmitted('refreshTasks');
+});
