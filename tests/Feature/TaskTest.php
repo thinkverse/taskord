4f043217e19f8b2178677f928d028748709be09f
Yogi <yoginth@hey.com>
2021-06-06 11:37:32 +0530

    add: More tests

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -67,6 +67,17 @@ it('cannot toggle like on task', function ($user, $status) {
         ->assertNotEmitted('taskLiked');
 })->with('like-data');
 
+it('cannot toggle check on task', function ($user, $status) {
+    $task = Task::factory()->create([
+        'user_id' => 10,
+    ]);
+
+    actingAs($user)
+        ->livewire(SingleTask::class, ['task' => $task])
+        ->call('checkTask')
+        ->assertNotEmitted('refreshTasks');
+})->with('like-data');
+
 it('can toggle check on task', function ($user, $status) {
     $task = Task::factory()->create([
         'user_id' => $user,
