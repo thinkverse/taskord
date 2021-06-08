28be359f36eb2e00f40440c24af573e07b54875a
Yogi <yoginth@hey.com>
2021-06-08 09:38:01 +0530

    Update answer box

--- resources/views/livewire/answer/create-answer.blade.php
+++ resources/views/livewire/answer/create-answer.blade.php
@@ -1,25 +1,46 @@
-<div>
-    <div class="card mt-4">
+<div class="card mt-4">
+    <div x-data="{ tab: 'edit' }">
+        <div class="pt-2 rounded-top bg-body">
+            <ul class="nav nav-tabs d-flex justify-content-between px-3">
+                <div class="d-flex">
+                    <li class="nav-item">
+                        <div class="cursor-pointer nav-link active" :class="{ 'bg-white active': tab === 'edit' }" @click="tab = 'edit'">Edit</div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="cursor-pointer nav-link" :class="{ 'active bg-white': tab === 'preview' }" @click="tab = 'preview'">Preview</div>
+                    </li>
+                </div>
+                <div x-show="tab === 'edit'" class="pt-1">
+                    <x:markdown-toolbar htmlFor="answer-box" />
+                </div>
+            </ul>
+        </div>
         <div class="card-body">
             @error('answer')
-                <div class="alert alert-danger alert-dismissible fade show mt-2">
+                <div class="alert alert-danger alert-dismissible fade show">
                     <button type="button" class="btn-close small" data-bs-dismiss="alert"></button>
                     <x-heroicon-o-exclamation class="heroicon" />
                     {{ $message }}
                 </div>
             @enderror
             <div class="mb-2">
-                <x:markdown-toolbar htmlFor="answer-box" />
-                <textarea placeholder="Add a answer" id="answer-box" class="form-control mentionInput mt-3" rows="3" wire:model.lazy="answer"></textarea>
+                <textarea x-show="tab === 'edit'" placeholder="Leave a answer" id="answer-box" class="form-control mentionInput" rows="3" wire:model.lazy="answer"></textarea>
+                <div x-show="tab === 'preview'">
+                    @if ($answer)
+                        {!! markdown($answer) !!}
+                    @else
+                        Nothing to preview...
+                    @endif
+                </div>
             </div>
             <div class="d-flex justify-content-between align-items-center">
                 <a class="small fw-bold text-secondary" href="https://guides.github.com/features/mastering-markdown" target="_blank">
                     <x-heroicon-o-pencil-alt class="heroicon" />
                     Markdown is supported
-                     <x:labels.beta />
+                    <x:labels.beta />
                 </a>
-                <button wire:loading.attr="disabled" wire:target="submit" class="btn btn-sm btn-outline-primary rounded-pill" type="submit" wire:click="submit">
-                    <x-heroicon-o-plus class="heroicon" />
+                <button wire:loading.attr="disabled" wire:target="submit" class="btn btn-sm btn-outline-primary rounded-pill" type="submit" wire:click="submit" @click="tab = 'edit'">
+                    <x-heroicon-o-plus class="heroicon heroicon-15px" />
                     Answer
                 </button>
             </div>
