48c2ba54bf9f1630a35706eb2363a68a17cc64ab
Yogi <yoginth@hey.com>
2021-06-09 12:49:19 +0000

    move: To Route::is()

--- resources/views/explore/nav.blade.php
+++ resources/views/explore/nav.blade.php
@@ -1,10 +1,10 @@
 <ul class="nav nav-pills justify-content-center align-items-center explore-nav bg-white py-3">
     <li class="nav-item px-1">
-        <a class="nav-link @if (Route::currentRouteName() === 'explore.explore') active @endif" href="{{ route('explore.explore') }}">Popular Tasks</a>
+        <a class="nav-link {{ Route::is('explore.explore') ? 'active' : '' }}" href="{{ route('explore.explore') }}">Popular Tasks</a>
     </li>
     @if (feature('explore_makers'))
         <li class="nav-item px-1">
-            <a class="nav-link @if (Route::currentRouteName() === 'explore.makers') active @endif" href="{{ route('explore.makers') }}">Makers  <x:labels.staff-ship /></a>
+            <a class="nav-link {{ Route::is('explore.makers') ? 'active' : '' }}" href="{{ route('explore.makers') }}">Makers  <x:labels.staff-ship /></a>
         </li>
     @endif
     @if (feature('explore_products'))
