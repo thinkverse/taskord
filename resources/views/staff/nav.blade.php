f6f520cb4525edd6e287bf311e2d9c69d8b07820
Yogi <yoginth@hey.com>
2021-06-09 12:58:54 +0000

    move: To Route::is()

--- resources/views/staff/nav.blade.php
+++ resources/views/staff/nav.blade.php
@@ -1,54 +1,54 @@
 <ul class="nav nav-pills justify-content-center mb-3">
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.stats') active @endif" href="{{ route('staff.stats') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.stats') ? 'active' : '' }}" href="{{ route('staff.stats') }}">
             <x-heroicon-o-chart-bar class="heroicon heroicon heroicon-18px" />
             Stats
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.users') active @endif" href="{{ route('staff.users') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.stats') ? 'active' : '' }} @if (Route::currentRouteName() === 'staff.users') active @endif" href="{{ route('staff.users') }}">
             <x-heroicon-o-users class="heroicon heroicon heroicon-18px" />
             Users
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.tasks') active @endif" href="{{ route('staff.tasks') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.stats') ? 'active' : '' }} @if (Route::currentRouteName() === 'staff.tasks') active @endif" href="{{ route('staff.tasks') }}">
             <x-heroicon-o-check-circle class="heroicon heroicon-18px" />
             Tasks
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.activities') active @endif" href="{{ route('staff.activities') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.stats') ? 'active' : '' }} @if (Route::currentRouteName() === 'staff.activities') active @endif" href="{{ route('staff.activities') }}">
             <x-heroicon-o-fire class="heroicon heroicon-18px" />
             Activities
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.products') active @endif" href="{{ route('staff.products') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.stats') ? 'active' : '' }} @if (Route::currentRouteName() === 'staff.products') active @endif" href="{{ route('staff.products') }}">
             <x-heroicon-o-cube class="heroicon heroicon-18px" />
             Products
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.system') active @endif" href="{{ route('staff.system') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.stats') ? 'active' : '' }} @if (Route::currentRouteName() === 'staff.system') active @endif" href="{{ route('staff.system') }}">
             <x-heroicon-o-chip class="heroicon heroicon-18px" />
             System
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.features') active @endif" href="{{ route('staff.features') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.features') ? 'active' : '' }}" href="{{ route('staff.features') }}">
             <x-heroicon-o-adjustments class="heroicon heroicon-18px" />
             Features
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.jobs') active @endif" href="{{ route('staff.jobs') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.jobs') ? 'active' : '' }}" href="{{ route('staff.jobs') }}">
             <x-heroicon-o-collection class="heroicon heroicon-18px" />
             Jobs
         </a>
     </li>
     <li class="nav-item">
-        <a class="nav-link rounded-pill @if (Route::currentRouteName() === 'staff.deployments') active @endif" href="{{ route('staff.deployments') }}">
+        <a class="nav-link rounded-pill {{ Route::is('staff.deployments') ? 'active' : '' }}" href="{{ route('staff.deployments') }}">
             <x-heroicon-o-cloud class="heroicon heroicon-18px" />
             Deployments
         </a>
