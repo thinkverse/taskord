79a9a9ae558b7ed8ad79e64bdd888de8f415051a
Yogi <yoginth@hey.com>
2021-06-10 18:42:35 +0530

    add: Session page

--- app/Http/Livewire/User/Settings/Sessions.php
+++ app/Http/Livewire/User/Settings/Sessions.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Livewire\User\Settings;
 
+use App\Models\User;
 use Livewire\Component;
 
 class Sessions extends Component
 {
+    public User $user;
+
+    public function mount($user)
+    {
+        $this->user = $user;
+    }
+
     public function render()
     {
         return view('livewire.user.settings.sessions');
