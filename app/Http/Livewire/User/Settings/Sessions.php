670f8d3b49e05286ac76e9af7d15945eb0da0a61
Yogi <yoginth@hey.com>
2021-06-10 06:19:39 +0000

    add: Sessions settings

---
+++ app/Http/Livewire/User/Settings/Sessions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User\Settings;
+
+use Livewire\Component;
+
+class Sessions extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.settings.sessions');
+    }
+}
