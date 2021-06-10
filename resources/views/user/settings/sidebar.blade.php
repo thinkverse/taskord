309edb49483837c8d7b2be8c3cff07e6d0ea032d
Yogi <yoginth@hey.com>
2021-06-10 06:20:41 +0000

    add: Sessions settings

--- resources/views/user/settings/sidebar.blade.php
+++ resources/views/user/settings/sidebar.blade.php
@@ -69,8 +69,8 @@
                 API
             </a>
             <a
-                class="list-group-item text-dark pt-2 pb-2 {{ Route::is('user.settings.logs') ? 'active text-white' : '' }}"
-                href="{{ route('user.settings.logs') }}"
+                class="list-group-item text-dark pt-2 pb-2 {{ Route::is('user.settings.sessions') ? 'active text-white' : '' }}"
+                href="{{ route('user.settings.sessions') }}"
             >
                 <x-heroicon-o-identification class="heroicon" />
                 Sessions
