e73afae4821c92bf6584c4ebc5629330d7e77a76
Yogi <yoginth@hey.com>
2021-06-11 17:58:25 +0530

    add: Integrations test

--- tests/Feature/User/Settings/IntegrationsTest.php
+++ tests/Feature/User/Settings/IntegrationsTest.php
@@ -15,18 +15,18 @@ it('has settings/integrations page', function ($url, $expected, $auth) {
     ['/settings/integrations', 200, true],
 ]);
 
-it('can edit integrations (notificationsEmail) settings', function ($status) {
+it('can create new webhook', function ($status) {
     $newUser = User::factory()->create();
 
     if ($status) {
         return actingAs($newUser->id)
-            ->livewire(Notifications::class, ['user' => $newUser])
-            ->call('notificationsEmail')
+            ->livewire(Integrations::class, ['user' => $newUser])
+            ->call('submit')
             ->assertEmitted('toggledNotificationsEmail');
     }
 
     return actingAs(1)
-        ->livewire(Notifications::class, ['user' => $newUser])
+        ->livewire(Integrations::class, ['user' => $newUser])
         ->call('notificationsEmail')
         ->assertNotEmitted('toggledNotificationsEmail');
 })->with([
