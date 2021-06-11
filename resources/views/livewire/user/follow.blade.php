51707530d396619473dd6edd6ecf2d6a845c77f8
Yogi <yoginth@hey.com>
2021-06-11 04:51:07 +0000

    remove: @auth check for follow button

--- resources/views/livewire/user/follow.blade.php
+++ resources/views/livewire/user/follow.blade.php
@@ -1,19 +1,17 @@
 <div>
-    @auth
-        @can('user.follow', $user)
-            @if (auth()->user()->isFollowing($user))
-                <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-danger rounded-pill mb-2">
-                    <x-heroicon-o-user-remove class="heroicon heroicon-15px" />
-                    Unfollow
-                </button>
-            @else
-                <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-primary rounded-pill mb-2">
-                    <x-heroicon-o-user-add class="heroicon heroicon-15px" />
-                    Follow
-                </button>
-            @endif
-        @endcan
-    @endauth
+    @can('user.follow', $user)
+        @if (auth()->user()->isFollowing($user))
+            <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-danger rounded-pill mb-2">
+                <x-heroicon-o-user-remove class="heroicon heroicon-15px" />
+                Unfollow
+            </button>
+        @else
+            <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-primary rounded-pill mb-2">
+                <x-heroicon-o-user-add class="heroicon heroicon-15px" />
+                Follow
+            </button>
+        @endif
+    @endcan
     <div class="small">
         <a class="text-dark" href="{{ route('user.following', ['username' => $user->username]) }}">
             <span class="fw-bold">{{ $user->followings->count('id') }}</span>
