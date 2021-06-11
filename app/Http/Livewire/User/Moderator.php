8f770cf95ef02499874eb6ce1c157705d81e3776
Yogi <yoginth@hey.com>
2021-06-11 18:18:52 +0530

    add: Toast message in moderator

--- app/Http/Livewire/User/Moderator.php
+++ app/Http/Livewire/User/Moderator.php
@@ -407,6 +407,7 @@ class Moderator extends Component
         $user = User::find($this->user->id);
         $user->timestamps = false;
         $user->comments()->delete();
+        toast($this, 'success', config('taskord.toast.settings-updated'));
         loggy(
             request(),
             'Staff',
