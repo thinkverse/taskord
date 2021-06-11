7f2ced564b253e5653d14b315c8fa6acd85cb340
Yogi <yoginth@hey.com>
2021-06-11 05:33:59 +0000

    Add alpine for follow button

--- resources/views/livewire/user/follow.blade.php
+++ resources/views/livewire/user/follow.blade.php
@@ -1,16 +1,24 @@
 <div>
     @can('user.follow', $user)
-        @if (auth()->user()->isFollowing($user))
-            <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-danger rounded-pill mb-2">
+        <div
+            x-data="{isFollowing: '{{ auth()->user()->isFollowing($user) }}'}"
+        >
+            <button
+                x-cloak
+                x-on:click="isFollowing = ! isFollowing"
+                wire:click="toggleFollow"
+                class="btn btn-sm rounded-pill mb-2"
+                x-bind:class="{ 'btn-outline-danger': isFollowing, 'btn-outline-success':  ! isFollowing }"
+            >
                 <x-heroicon-o-user-remove class="heroicon heroicon-15px" />
-                Unfollow
+                <span x-show="isFollowing">
+                    Unfollow
+                </span>
+                <span x-show="!isFollowing">
+                    Follow
+                </span>
             </button>
-        @else
-            <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-primary rounded-pill mb-2">
-                <x-heroicon-o-user-add class="heroicon heroicon-15px" />
-                Follow
-            </button>
-        @endif
+        </div>
     @endcan
     <div class="small">
         <a class="text-dark" href="{{ route('user.following', ['username' => $user->username]) }}">
