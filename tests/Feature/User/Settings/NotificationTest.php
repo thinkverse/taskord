d1eaf16cdbf42f5c5ed287ec181034d5925eddee
Yogi <yoginth@hey.com>
2021-06-11 17:53:31 +0530

    add: Notification test

---
+++ tests/Feature/User/Settings/NotificationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+use App\Http\Livewire\User\Settings\Delete;
+use App\Models\User;
+use function Tests\actingAs;
+
+it('has settings/delete page', function ($url, $expected, $auth) {
+    if ($auth) {
+        actingAs(1)->get($url)->assertStatus($expected);
+    } else {
+        $this->get($url)->assertStatus($expected);
+    }
+})->with([
+    ['/settings/delete', 302, false],
+    ['/settings/delete', 200, true],
+]);
+
+it('can reset the account', function ($status) {
+    $newUser = User::factory()->create();
+
+    if ($status) {
+        return actingAs($newUser->id)
+            ->livewire(Delete::class, ['user' => $newUser])
+            ->call('resetAccount')
+            ->assertEmitted('accountResetted');
+    }
+
+    return actingAs(1)
+        ->livewire(Delete::class, ['user' => $newUser])
+        ->call('resetAccount')
+        ->assertNotEmitted('accountResetted');
+})->with([
+    [true],
+    [false],
+]);
+
+it('can delete the account', function ($status) {
+    $newUser = User::factory()->create();
+
+    if ($status) {
+        return actingAs($newUser->id)
+            ->livewire(Delete::class, ['user' => $newUser])
+            ->call('deleteAccount')
+            ->assertEmitted('accountDeleted');
+    }
+
+    return actingAs(1)
+        ->livewire(Delete::class, ['user' => $newUser])
+        ->call('deleteAccount')
+        ->assertNotEmitted('accountDeleted');
+})->with([
+    [true],
+    [false],
+]);
