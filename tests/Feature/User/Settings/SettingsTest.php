b584ce0d04bee79970e7c9f905f1e97102fe199e
Yogi <yoginth@hey.com>
2021-06-11 14:33:48 +0530

    add: Profile test

--- tests/Feature/User/Settings/SettingsTest.php
+++ tests/Feature/User/Settings/SettingsTest.php
@@ -6,17 +6,6 @@ use App\Models\User;
 use function Pest\Livewire\livewire;
 use function Tests\actingAs;
 
-it('has settings/profile page', function ($url, $expected, $auth) {
-    if ($auth) {
-        actingAs(1)->get($url)->assertStatus($expected);
-    } else {
-        $this->get($url)->assertStatus($expected);
-    }
-})->with([
-    ['/settings', 302, false],
-    ['/settings', 200, true],
-]);
-
 it('has settings/account page', function ($url, $expected, $auth) {
     if ($auth) {
         actingAs(1)->get($url)->assertStatus($expected);
@@ -167,84 +156,4 @@ test('can download user logs data', function ($url, $expected, $auth) {
     }
 })->with([
     ['/settings/export/logs', 302, false],
-]);
-
-it('can edit profile settings', function ($status) {
-    $newUser = User::factory()->create();
-
-    if ($status) {
-        return actingAs($newUser->id)
-            ->livewire(Profile::class, ['user' => $newUser])
-            ->set('firstname', 'New firstname')
-            ->set('lastname', 'New lastaname')
-            ->call('updateProfile')
-            ->assertEmitted('profileUpdated');
-    }
-
-    return actingAs(1)
-        ->livewire(Profile::class, ['user' => $newUser])
-        ->set('firstname', 'New firstname')
-        ->set('lastname', 'New lastaname')
-        ->call('updateProfile')
-        ->assertNotEmitted('profileUpdated');
-})->with([
-    [true],
-    [false],
-]);
-
-it('can edit profile (resetAvatar) settings', function ($status) {
-    $newUser = User::factory()->create();
-
-    if ($status) {
-        return actingAs($newUser->id)
-            ->livewire(Profile::class, ['user' => $newUser])
-            ->call('resetAvatar')
-            ->assertEmitted('avatarResetted');
-    }
-
-    return actingAs(1)
-        ->livewire(Profile::class, ['user' => $newUser])
-        ->call('resetAvatar')
-        ->assertNotEmitted('avatarResetted');
-})->with([
-    [true],
-    [false],
-]);
-
-it('can edit profile (useGravatar) settings', function ($status) {
-    $newUser = User::factory()->create();
-
-    if ($status) {
-        return actingAs($newUser->id)
-            ->livewire(Profile::class, ['user' => $newUser])
-            ->call('useGravatar')
-            ->assertEmitted('gravatarUsed');
-    }
-
-    return actingAs(1)
-        ->livewire(Profile::class, ['user' => $newUser])
-        ->call('useGravatar')
-        ->assertNotEmitted('gravatarUsed');
-})->with([
-    [true],
-    [false],
-]);
-
-it('can edit profile (enableGoal) settings', function ($status) {
-    $newUser = User::factory()->create();
-
-    if ($status) {
-        return actingAs($newUser->id)
-            ->livewire(Profile::class, ['user' => $newUser])
-            ->call('enableGoal')
-            ->assertEmitted('goalEnabled');
-    }
-
-    return actingAs(1)
-        ->livewire(Profile::class, ['user' => $newUser])
-        ->call('enableGoal')
-        ->assertNotEmitted('goalEnabled');
-})->with([
-    [true],
-    [false],
 ]);
