8af1dd2859b1793648f3ed5130715481d7345e0c
Yogi <yoginth@hey.com>
2021-06-11 07:37:44 +0000

    add: Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,6 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('app:clean')->everyMinute();
         $schedule->command('app:clean')->everyTenMinutes();
         $schedule->command('backup:clean')->everyThreeHours();
         $schedule->command('backup:run')->everyThreeHours();
