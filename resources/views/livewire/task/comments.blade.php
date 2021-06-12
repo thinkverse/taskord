e270d81fce55bd25951d7c5c34d04199cf7807b6
Yogi <yoginth@hey.com>
2021-06-12 12:26:11 +0530

    add: Replies count #419

--- resources/views/livewire/task/comments.blade.php
+++ resources/views/livewire/task/comments.blade.php
@@ -8,8 +8,8 @@
             @else
                 @foreach ($comments as $comment)
                     <x:shared.user-label-small :user="$comment->user" />
-                    <div class="d-flex align-items-center">
-                        <div class="mt-2 card d-inline-block bg-light {{ $loop->last ? '' : 'mb-3' }}">
+                    <div class="d-flex align-items-center mt-2 {{ $loop->last ? '' : 'mb-3' }}">
+                        <div class="card d-inline-block bg-light">
                             <div class="card-body px-2 py-1 body-font">
                                 {!! markdown($comment->comment) !!}
                             </div>
