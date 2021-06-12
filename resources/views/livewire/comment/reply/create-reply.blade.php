4af270d560cf678c175782ac59fa1527b199881f
Yogi <yoginth@hey.com>
2021-06-12 12:52:17 +0530

    fix: Markdown toolbar in dark mode #432

--- resources/views/livewire/comment/reply/create-reply.blade.php
+++ resources/views/livewire/comment/reply/create-reply.blade.php
@@ -1,6 +1,6 @@
 <div class="card {{ $comment->replies()->count('id') > 0 ? '' : 'mt-3' }}">
     <div x-data="{ tab: 'edit' }">
-        <div class="pt-2 rounded-top bg-body">
+        <div class="pt-2 rounded-top bg-light">
             <ul class="nav nav-tabs d-flex justify-content-between px-3">
                 <div class="d-flex">
                     <li class="nav-item">
