c06bc3c14300956d342a13f37bf9f37309a2ac1f
Yogi <yoginth@hey.com>
2021-06-12 12:25:00 +0530

    add: Replies count #419

--- resources/views/livewire/task/comments.blade.php
+++ resources/views/livewire/task/comments.blade.php
@@ -8,17 +8,19 @@
             @else
                 @foreach ($comments as $comment)
                     <x:shared.user-label-small :user="$comment->user" />
-                    <div class="mt-2 card d-inline-block bg-light {{ $loop->last ? '' : 'mb-3' }}">
-                        <div class="card-body px-2 py-1 body-font">
-                            {!! markdown($comment->comment) !!}
+                    <div class="d-flex align-items-center">
+                        <div class="mt-2 card d-inline-block bg-light {{ $loop->last ? '' : 'mb-3' }}">
+                            <div class="card-body px-2 py-1 body-font">
+                                {!! markdown($comment->comment) !!}
+                            </div>
                         </div>
+                        @if ($comment->replies_count > 0)
+                            <a class="text-secondary d-inline-flex align-items-center ms-2">
+                                <x-heroicon-o-chat-alt-2 class="heroicon heroicon-15px me-0" />
+                                <span class="ms-1">{{ $comment->replies_count }}</span>
+                            </a>
+                        @endif
                     </div>
-                    @if ($comment->replies_count > 0)
-                        <span class="text-secondary d-inline-flex align-items-center">
-                            <x-heroicon-o-chat-alt-2 class="heroicon heroicon-15px me-0" />
-                            <span class="ms-1">{{ $comment->replies_count }}</span>
-                        </span>
-                    @endif
                 @endforeach
             @endif
         </div>
