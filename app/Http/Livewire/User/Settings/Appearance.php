b5f675ae552df22192e5e8288ea5959ff5d93062
Yogi <yoginth@hey.com>
2021-06-12 14:10:35 +0530

    remove: Dark mode from DB

--- app/Http/Livewire/User/Settings/Appearance.php
+++ app/Http/Livewire/User/Settings/Appearance.php
@@ -19,16 +19,9 @@ class Appearance extends Component
     {
         if (auth()->user()->id === $this->user->id) {
             Cookie::queue('color_mode', $mode, config('session.lifetime'));
-            if ($mode === 'light') {
-                $this->user->dark_mode = false;
-                $this->user->save();
-                $this->emit('toggledMode');
-                return loggy(request(), 'User', auth()->user(), 'Disabled dark mode');
-            }
-            $this->user->dark_mode = true;
-            $this->user->save();
             $this->emit('toggledMode');
-            return loggy(request(), 'User', auth()->user(), 'Enabled dark mode');
+
+            return loggy(request(), 'User', auth()->user(), 'Toggled appearance');
         }
 
         return toast($this, 'error', config('taskord.toast.deny'));
