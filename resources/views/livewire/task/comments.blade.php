38cf2e26c847e1fb16984e082aa4e8ed455befe4
Yogi <yoginth@hey.com>
2021-06-14 16:43:29 +0530

    revert: Comment replies

--- resources/views/livewire/task/comments.blade.php
+++ resources/views/livewire/task/comments.blade.php
@@ -7,20 +7,12 @@
                 </div>
             @else
                 @foreach ($comments as $comment)
-                    <x:shared.user-label-small :user="$comment->user" />
-                    <a class="d-flex align-items-center mt-2 {{ $loop->last ? '' : 'mb-3' }}" href="{{ route('comment', ['id' => $comment->task->id, 'comment_id' => $comment->id]) }}">
-                        <div class="card d-inline-block bg-light">
-                            <div class="card-body px-2 py-1 body-font">
-                                {!! markdown($comment->comment) !!}
-                            </div>
-                        </div>
-                        @if ($comment->replies_count > 0)
-                            <span class="text-secondary d-inline-flex align-items-center ms-2">
-                                <x-heroicon-o-chat-alt-2 class="heroicon heroicon-15px me-0" />
-                                <span class="ms-1">{{ $comment->replies_count }}</span>
-                            </a>
-                        @endif
-                    </a>
+                <x:shared.user-label-small :user="$comment->user" />
+                <div class="mt-2 card d-inline-block bg-light {{ $loop->last ? '' : 'mb-3' }}">
+                    <div class="card-body px-2 py-1 body-font">
+                        {!! markdown($comment->comment) !!}
+                    </div>
+                </div>
                 @endforeach
             @endif
         </div>
