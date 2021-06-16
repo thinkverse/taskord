a943499dd9ead6b662378e09244714e75ef105ca
Yogi <yoginth@hey.com>
2021-06-16 12:01:12 +0530

    add: commit from local

--- resources/views/site/commit.blade.php
+++ resources/views/site/commit.blade.php
@@ -3,7 +3,7 @@
         <span class="fw-bold">Status: </span>
         @if (count($commits) === 0)
             <span class="badge bg-success">
-                Taskord is in latest version
+                Taskord is up-to-date
             </span>
         @else
             <span class="badge bg-danger">
@@ -14,7 +14,10 @@
             <ul class="bg-patron-question mt-3 py-2 rounded border border-warning">
                 @foreach ($commits as $commit)
                     <li>
-                        {{ $commit->title }}
+                        <span>{{ $commit->title }}</span>
+                        <a href="{{ $commit->web_url }}" class="fw-bold ms-1" target="_blank">
+                            <x-heroicon-o-external-link class="heroicon heroicon-15px" />
+                        </a>
                     </li>
                 @endforeach
             </ul>
