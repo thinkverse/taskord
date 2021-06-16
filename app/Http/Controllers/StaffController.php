04bd9dd965a38e764d10e5aecdc7c82995a3f707
Yogi <yoginth@hey.com>
2021-06-16 11:51:45 +0530

    add: ref_name

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -32,13 +32,13 @@ class StaffController extends Controller
         $client = new Client(['http_errors' => false]);
         $commit = $client->request('GET', 'https://gitlab.com/api/v4/projects/20359920/repository/commits', [
             'query' => [
-                'per_page' => 1,
+                'ref_name' => git('rev-parse HEAD')."...main",
             ],
         ]);
 
         if ($commit->getStatusCode() === 200) {
             return view('site.commit', [
-                'commit' => json_decode($commit->getBody()->getContents())[0],
+                'commit' => json_decode($commit->getBody()->getContents()),
             ]);
         }
 
--- resources/views/site/commit.blade.php
+++ resources/views/site/commit.blade.php
@@ -1,11 +1,8 @@
 <div>
-    @php
-        $deployed = git('rev-parse HEAD') ?: 'Something went wrong!';
-        $remote = $commit->id;
-    @endphp
     <div>
+        {{ count($commit) }}
         <span class="fw-bold">Status: </span>
-        @if ($deployed === $remote)
+        @if (count($commit) === 0)
             <span class="badge bg-success">
                 Latest
             </span>
@@ -14,35 +11,5 @@
                 Update available
             </span>
         @endif
-    </div>
-    <div>
-        <span class="fw-bold">Remote: </span>
-        <code class="fw-bold">
-            {{ $remote }}
-        </code>
-    </div>
-    <div>
-        <span class="fw-bold">Deployed: </span>
-        <code class="fw-bold">
-            {{ $deployed }}
-        </code>
-    </div>
-    <div>
-        <span class="fw-bold">Message: </span>
-        {{ $commit->title }}
-    </div>
-    <div>
-        <span class="fw-bold">Date: </span>
-        {{ carbon($commit->committed_date)->diffForHumans() }}
-    </div>
-    <div>
-        <span class="fw-bold">Author: </span>
-        {{ $commit->committer_name }} - {{ $commit->committer_email }}
-    </div>
-    <div class="mt-2">
-        <a href="{{ $commit->web_url }}" class="fw-bold" target="_blank">
-            Got to commit
-            <x-heroicon-o-external-link class="heroicon" />
-        </a>
     </div>
 </div>
