e1fbb6b6e69e4d930d47a4cd9d8f536d7633ae1f
Yogi <yoginth@hey.com>
2021-06-17 18:07:48 +0530

    publish: Update GQL vendor

--- resources/views/vendor/graphql-playground/index.blade.php
+++ resources/views/vendor/graphql-playground/index.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 
 <html>
-
 <head>
     <meta charset=utf-8 />
     <meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
@@ -16,20 +15,17 @@
     />
     <script src="{{ \MLL\GraphQLPlayground\DownloadAssetsCommand::jsPath() }}"></script>
 </head>
-
 <body>
+    <div id="root" />
+    <script type="text/javascript">
+        window.addEventListener('load', function () {
+            const root = document.getElementById('root');
 
-<div id="root"/>
-<script type="text/javascript">
-    window.addEventListener('load', function () {
-        const root = document.getElementById('root');
-
-        GraphQLPlayground.init(root, {
-            endpoint: "{{url(config('graphql-playground.endpoint'))}}",
-            subscriptionEndpoint: "{{config('graphql-playground.subscriptionEndpoint')}}",
+            GraphQLPlayground.init(root, {
+                endpoint: "{{url(config('graphql-playground.endpoint'))}}",
+                subscriptionEndpoint: "{{config('graphql-playground.subscriptionEndpoint')}}",
+            })
         })
-    })
-</script>
-
+    </script>
 </body>
 </html>
