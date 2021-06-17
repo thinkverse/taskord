5ab999b427361711dc4c98d989ccd84d8aa213e1
Yogi <yoginth@hey.com>
2021-06-17 18:07:23 +0530

    publish: Update GQL vendor

--- resources/views/vendor/graphql-playground/index.blade.php
+++ resources/views/vendor/graphql-playground/index.blade.php
@@ -6,16 +6,15 @@
     <meta charset=utf-8 />
     <meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
     <title>Taskord GraphQL Playground</title>
-
     <link
         rel="stylesheet"
-        href="{{\MLL\GraphQLPlayground\DownloadAssetsCommand::cssPath()}}"
+        href="{{ \MLL\GraphQLPlayground\DownloadAssetsCommand::cssPath() }}"
     />
     <link
         rel="shortcut icon"
-        href="{{\MLL\GraphQLPlayground\DownloadAssetsCommand::faviconPath()}}"
+        href="{{ \MLL\GraphQLPlayground\DownloadAssetsCommand::faviconPath() }}"
     />
-    <script src="{{\MLL\GraphQLPlayground\DownloadAssetsCommand::jsPath()}}"></script>
+    <script src="{{ \MLL\GraphQLPlayground\DownloadAssetsCommand::jsPath() }}"></script>
 </head>
 
 <body>
