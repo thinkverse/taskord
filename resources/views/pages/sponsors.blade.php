cf75237523612a1959038050c125d780b2412509
Yogi <yoginth@hey.com>
2021-06-18 18:48:41 +0530

    update: height of macstadium logo

--- resources/views/pages/sponsors.blade.php
+++ resources/views/pages/sponsors.blade.php
@@ -17,7 +17,7 @@
             <div class="col-lg-7">
                 <div class="text-center mb-5 pt-2">
                     <div>
-                        <img loading=lazy height="70" src="https://ik.imagekit.io/taskordimg/macstadium_rSBB5WsBB.png" />
+                        <img loading=lazy height="80" src="https://ik.imagekit.io/taskordimg/macstadium_rSBB5WsBB.png" />
                     </div>
                     <div class="h6 mt-4 lh-base">
                         MacStadium is the only provider of enterprise-class cloud solutions for Mac and iOS app development. Whether you need a Mac cloud for large-scale CI/CD or just need a single Mac mini to test your iOS app, MacStadium has a solution for all of your Mac development needs.
