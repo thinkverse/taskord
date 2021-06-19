52f7cda39f67dae3165e28c5b307de9ebc1058f4
Yogi <yoginth@hey.com>
2021-06-19 19:20:56 +0530

    add: Task mutator

--- app/GraphQL/Mutations/TaskMutator.php
+++ app/GraphQL/Mutations/TaskMutator.php
@@ -2,6 +2,10 @@
 
 namespace App\GraphQL\Mutations;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Task;
+use App\Gamify\Points\TaskCreated;
+use Illuminate\Support\Facades\Gate;
+use App\Actions\CreateNewTask;
 
 class TaskMutator
 {
@@ -11,6 +15,25 @@ class TaskMutator
      */
     public function __invoke($_, array $args)
     {
-        // TODO implement the resolver
+        if (Gate::denies('create')) {
+            return [
+                'message' => 'Permission denied!',
+            ];
+        }
+
+        $task = (new CreateNewTask(auth()->user(), [
+            'product_id' => null,
+            'task' => $args['task'],
+            'done' => $args['done'],
+            'done_at' => $args['done'] ? carbon()->now() : null,
+            'type' => 'user',
+            'source' => 'Taskord API',
+        ]))();
+
+        givePoint(new TaskCreated($task));
+
+        return [
+            'message' => 'Task Created',
+        ];
     }
 }
