5b7769df7d613c854768c94348eb2ee137a5f81d
Yogi <yoginth@hey.com>
2021-06-19 11:46:29 +0530

    remove: Honeybadger

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -32,10 +32,6 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $exception) {
-            if (app()->bound('honeybadger') && $this->shouldReport($exception)) {
-                app('honeybadger')->notify($exception, app('request'));
-            }
-        });
+        
     }
 }
