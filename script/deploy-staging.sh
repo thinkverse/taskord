3ce6dc8837753275eb5801bbbf15ca59dede8fb5
Yogi <yoginth@hey.com>
2021-06-19 12:23:34 +0530

    add: deploy staging

--- script/deploy-staging.sh
+++ script/deploy-staging.sh
@@ -3,14 +3,8 @@
 # Usage: ./script/deploy-staging.sh
 # Script to deploy staging environment
 
+git pull origin main
 composer install
 yarn install
 yarn production
-cp .env.example .env
-php artisan key:generate
-mysql -u root -e "DROP DATABASE taskord; CREATE DATABASE taskord;"
-php artisan migrate
-php artisan db:seed
-php artisan test
-cd ../
-rm -rf taskord
+php artisan migrate:fresh --seed --force
