3ceb5992b59e8ed22826539d4db4408004cff021
Yogi <yoginth@hey.com>
2021-06-19 20:03:57 +0530

    add: Delete task mutation

--- app/GraphQL/Mutations/TaskMutator.php
+++ app/GraphQL/Mutations/TaskMutator.php
@@ -38,4 +38,34 @@ class TaskMutator
             'task' => $task,
         ];
     }
+
+    public function deleteTask($_, array $args)
+    {
+        if (Gate::denies('edit/delete', $this->task)) {
+            return [
+                'status' => false,
+                'message' => config('taskord.toast.deny'),
+            ];
+        }
+
+        $product_id = Helper::getProductIDFromMention($args['task'], auth()->user());
+
+        $task = (new CreateNewTask(auth()->user(), [
+            'product_id' => $product_id,
+            'task' => $args['task'],
+            'done' => $args['done'],
+            'done_at' => $args['done'] ? carbon() : null,
+            'type' => 'user',
+            'type' => $product_id ? 'product' : 'user',
+            'source' => 'Taskord API',
+        ]))();
+
+        givePoint(new TaskCreated($task));
+
+        return [
+            'status' => true,
+            'message' => 'Task created successfully',
+            'task' => $task,
+        ];
+    }
 }
