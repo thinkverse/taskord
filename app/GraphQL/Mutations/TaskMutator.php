c936fc367bd71dd5e3969799b8874d4e0caef1ba
Yogi <yoginth@hey.com>
2021-06-19 20:07:14 +0530

    add: Delete task mutation

--- app/GraphQL/Mutations/TaskMutator.php
+++ app/GraphQL/Mutations/TaskMutator.php
@@ -3,9 +3,11 @@
 namespace App\GraphQL\Mutations;
 
 use App\Actions\CreateNewTask;
+use App\Models\Task;
 use App\Gamify\Points\TaskCreated;
 use Helper;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
 
 class TaskMutator
 {
@@ -41,6 +43,15 @@ class TaskMutator
 
     public function deleteTask($_, array $args)
     {
+        $task = Task::find($args['id']);
+
+        if (! $task) {
+            return [
+                'status' => false,
+                'message' => "No task found!",
+            ];
+        }
+
         if (Gate::denies('edit/delete', $this->task)) {
             return [
                 'status' => false,
@@ -48,19 +59,13 @@ class TaskMutator
             ];
         }
 
-        $product_id = Helper::getProductIDFromMention($args['task'], auth()->user());
-
-        $task = (new CreateNewTask(auth()->user(), [
-            'product_id' => $product_id,
-            'task' => $args['task'],
-            'done' => $args['done'],
-            'done_at' => $args['done'] ? carbon() : null,
-            'type' => 'user',
-            'type' => $product_id ? 'product' : 'user',
-            'source' => 'Taskord API',
-        ]))();
-
-        givePoint(new TaskCreated($task));
+        loggy(request(), 'Task', auth()->user(), "Deleted a task | Task ID: {$task->id}");
+        foreach ($task->images ?? [] as $image) {
+            Storage::delete($image);
+        }
+        $this->task->delete();
+        $this->emitUp('refreshTasks');
+        auth()->user()->touch();
 
         return [
             'status' => true,
