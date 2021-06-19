0f033d067229a97420b026ed3fff829a7f8fdf08
Yogi <yoginth@hey.com>
2021-06-19 18:50:17 +0530

    add: Taskmutator

---
+++ app/GraphQL/Mutations/TaskMutator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+class TaskMutator
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        // TODO implement the resolver
+    }
+}
