ed1cb144c4e9f592e012e6fb2b70fd44cf3f3e09
Yogi <yoginth@hey.com>
2021-06-20 14:34:16 +0530

    add: Comment mutation in GraphQL

--- app/GraphQL/Mutations/CommentMutator.php
+++ app/GraphQL/Mutations/CommentMutator.php
@@ -32,31 +32,19 @@ class CommentMutator
         Helper::mentionUsers($users, $comment, auth()->user(), 'comment');
         Helper::notifySubscribers($comment->task->subscribers, $comment, 'comment');
 
-        if (auth()->user()->id !== $this->task->user->id) {
+        if (auth()->user()->id !== $comment->task->user->id) {
             if (! auth()->user()->hasSubscribed($comment->task)) {
                 auth()->user()->subscribe($comment->task);
             }
-            $this->task->user->notify(new Commented($comment));
+            $comment->task->user->notify(new Commented($comment));
             givePoint(new CommentCreated($comment));
         }
         loggy(request(), 'Comment', auth()->user(), "Created a new comment | Comment ID: {$comment->id}");
 
-        $task = (new CreateNewTask(auth()->user(), [
-            'product_id' => $product_id,
-            'task' => $args['task'],
-            'done' => $args['done'],
-            'done_at' => $args['done'] ? carbon() : null,
-            'type' => 'user',
-            'type' => $product_id ? 'product' : 'user',
-            'source' => 'Taskord API',
-        ]))();
-
-        givePoint(new TaskCreated($task));
-
         return [
             'status' => true,
-            'message' => 'Task created successfully',
-            'task' => $task,
+            'message' => 'Comment created successfully',
+            'comment' => $comment,
         ];
     }
 }
