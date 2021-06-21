4b345192a62c23d248d888a6fb4d0fbeb0f41f0b
Yogi <yoginth@hey.com>
2021-06-21 11:02:50 +0530

    add: 🤝 Meetups feature

---
+++ app/Http/Livewire/Meetups/LoadMore.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Livewire\Meetups;
+
+use App\Models\Meetup;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Livewire\Component;
+
+class LoadMore extends Component
+{
+    public $type;
+    public $page;
+    public $perPage;
+    public $loadMore;
+    public $readyToLoad = true;
+
+    public function mount($page, $perPage, $type)
+    {
+        $this->type = $type;
+        $this->page = $page + 1;
+        $this->perPage = $perPage;
+        $this->loadMore = false;
+    }
+
+    public function paginate($items, $options = [])
+    {
+        $page = $this->page ?: (Paginator::resolveCurrentPage() ?: 1);
+        $items = $items instanceof Collection ? $items : Collection::make($items);
+
+        return new LengthAwarePaginator($items->forPage($page, $this->perPage), $items->count(), $this->perPage, $page, $options);
+    }
+
+    public function loadMore()
+    {
+        $this->loadMore = true;
+    }
+
+    public function render()
+    {
+        if ($this->loadMore) {
+            if ($this->type === 'milestones.opened') {
+                $milestones = Milestone::whereStatus(true)
+                    ->whereHas('user', function ($q) {
+                        $q->where([
+                            ['spammy', false],
+                            ['is_private', false],
+                        ]);
+                    })
+                    ->latest()
+                    ->get();
+            } elseif ($this->type === 'milestones.closed') {
+                $milestones = Milestone::whereStatus(false)
+                    ->whereHas('user', function ($q) {
+                        $q->where([
+                            ['spammy', false],
+                            ['is_private', false],
+                        ]);
+                    })
+                    ->latest()
+                    ->get();
+            }
+
+            return view('livewire.milestone.milestones', [
+                'milestones' => $this->paginate($milestones),
+            ]);
+        }
+
+        return view('livewire.load-more');
+    }
+}
---
+++ app/Http/Livewire/Meetups/Meetups.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Meetups;
+
+use Livewire\Component;
+
+class Meetups extends Component
+{
+    public function render()
+    {
+        return view('livewire.meetups.meetups');
+    }
+}
