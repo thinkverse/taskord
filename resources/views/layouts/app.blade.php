bfb70e07a0ab6dbfbd41fdffa496f42d3ba883bc
Yogi <yoginth@hey.com>
2021-06-22 16:05:28 +0530

    update: Theme color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="theme-color" content="#343a40">
+    <meta name="theme-color" content="#6a63ec">
     <meta name="description" content="@yield('description')">
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@taskord">
