8c6ada7418db8bdd7b362003b2d60646a33d41e8
Yogi <yoginth@hey.com>
2021-06-22 14:47:33 +0530

    update: Oembed

--- resources/views/livewire/task/oembed.blade.php
+++ resources/views/livewire/task/oembed.blade.php
@@ -1,27 +1,29 @@
 <div class="card mt-3 mb-2 w-75">
     <div class="{{ $oembed->type !== 'summary_large_image' ? 'd-flex align-items-center' : '' }}">
-        @if ($oembed->thumbnail_url)
-            @if ($oembed->type === 'summary_large_image')
-                <img class="card-img-top border-bottom" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
-            @else
-                <img class="rounded-start w-20" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
-            @endif
-        @endif
-        <div class="card-body">
-            <a href="{{ $oembed->url }}">
-                <div class="fw-bold text-dark">
-                    {{ $oembed->title }}
-                </div>
-                <div class="mt-1 text-dark">
-                    {{ $oembed->description }}
-                </div>
-            </a>
-            <div class="mt-1 d-flex align-items-center">
-                <img src="{{ Helper::getCDNImage($oembed->favicon, 20) }}" height="15" width="15" />
-                <a class="ms-2 text-secondary" href="{{ $oembed->provider_url }}" target="_blank">
-                    {{ $oembed->provider_name }}
+        <div class="card-body d-flex align-items-center">
+            <div class="me-4">
+                <a href="{{ $oembed->url }}">
+                    <div class="fw-bold text-dark">
+                        {{ $oembed->title }}
+                    </div>
+                    <div class="mt-1 text-dark">
+                        {{ $oembed->description }}
+                    </div>
                 </a>
+                <div class="mt-1 d-flex align-items-center">
+                    <img src="{{ Helper::getCDNImage($oembed->favicon, 20) }}" height="15" width="15" />
+                    <a class="ms-2 text-secondary" href="{{ $oembed->provider_url }}" target="_blank">
+                        {{ $oembed->provider_name }}
+                    </a>
+                </div>
             </div>
+            @if ($oembed->thumbnail_url)
+                @if ($oembed->type === 'summary_large_image')
+                    <img class="card-img-top border-bottom" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
+                @else
+                    <img class="rounded w-20" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
+                @endif
+            @endif
         </div>
     </div>
 </div>
