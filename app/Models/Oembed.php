d54eb0c694102a678d8b0b04d1c1df018e81839d
Yogi <yoginth@hey.com>
2021-06-22 10:52:26 +0530

    update: Model

--- app/Models/Oembed.php
+++ app/Models/Oembed.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Rennokki\QueryCache\Traits\QueryCacheable;
 
 class Oembed extends Model
 {
+    use QueryCacheable;
     use HasFactory;
+
+    public $cacheFor = 3600;
+    public $cacheTags = ['oembeds'];
+    public $cachePrefix = 'oembeds_';
+
+    protected static $flushCacheOnUpdate = true;
+
+    // protected $fillable = [
+    //     'name',
+    //     'offer',
+    //     'coupon',
+    //     'referral',
+    //     'description',
+    //     'website',
+    //     'logo',
+    // ];
 }
