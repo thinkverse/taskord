c9a07b07b085bf3a3a1c4bab3511c47752116caf
Yogi <yoginth@hey.com>
2021-06-22 19:34:07 +0530

    update: Question skeleton

--- resources/views/components/loaders/home/question-skeleton.blade.php
+++ resources/views/components/loaders/home/question-skeleton.blade.php
@@ -1,6 +1,6 @@
 <div class="ssc">
     @for ($i = 0; $i < $count; $i++)
-        <div class="d-flex align-items-center {{ ($i === 0 or $i === $count - 1) ? '' : 'my-4' }}">
+        <div class="d-flex align-items-center {{ ($i === 0 or $i === $count - 1) ? 'my-2' : 'my-4' }}">
             <div class="ssc-circle avatar-30"></div>
             <div class="w-70 ms-2">
                 <div class="ssc-line"></div>
