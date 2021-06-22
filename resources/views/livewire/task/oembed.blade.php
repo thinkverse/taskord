6e29588f5b3d95ec40dcd00f3a4851fa68fa1a05
Yogi <yoginth@hey.com>
2021-06-22 14:57:03 +0530

    add: bg-oembed

--- resources/views/livewire/task/oembed.blade.php
+++ resources/views/livewire/task/oembed.blade.php
@@ -1,7 +1,13 @@
-<div class="card mt-3 mb-2 w-75">
+<div class="card mt-3 mb-2 w-75 bg-oembed">
     <div class="{{ $oembed->type !== 'summary_large_image' ? 'd-flex align-items-center' : '' }}">
         <div class="card-body d-flex align-items-center">
-            <div class="me-4">
+            <div>
+                <div class="d-flex align-items-center mb-1">
+                    <img src="{{ Helper::getCDNImage($oembed->favicon, 20) }}" height="15" width="15" />
+                    <a class="ms-2 text-secondary" href="{{ $oembed->provider_url }}" target="_blank">
+                        {{ $oembed->provider_name }}
+                    </a>
+                </div>
                 <a href="{{ $oembed->url }}">
                     <div class="fw-bold text-dark">
                         {{ $oembed->title }}
@@ -10,18 +16,15 @@
                         {{ $oembed->description }}
                     </div>
                 </a>
-                <div class="mt-1 d-flex align-items-center">
-                    <img src="{{ Helper::getCDNImage($oembed->favicon, 20) }}" height="15" width="15" />
-                    <a class="ms-2 text-secondary" href="{{ $oembed->provider_url }}" target="_blank">
-                        {{ $oembed->provider_name }}
-                    </a>
-                </div>
+                @if ($oembed->thumbnail_url)
+                    @if ($oembed->type === 'summary_large_image')
+                        <img class="rounded w-100 mt-3" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
+                    @endif
+                @endif
             </div>
             @if ($oembed->thumbnail_url)
-                @if ($oembed->type === 'summary_large_image')
-                    <img class="card-img-top border-bottom" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
-                @else
-                    <img class="rounded w-20" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
+                @if ($oembed->type !== 'summary_large_image')
+                    <img class="rounded w-20 ms-4" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
                 @endif
             @endif
         </div>
