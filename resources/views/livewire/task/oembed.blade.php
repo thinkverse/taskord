1b10ff46df04c9f438b0eabac53ed34ac0eada08
Yogi <yoginth@hey.com>
2021-06-22 14:40:28 +0530

    remove: div

--- resources/views/livewire/task/oembed.blade.php
+++ resources/views/livewire/task/oembed.blade.php
@@ -1,28 +1,26 @@
-<div>
-    <div class="card mt-3 mb-2 w-75">
-        <div class="{{ $oembed->type !== 'summary_large_image' ? 'd-flex align-items-center' : '' }}">
-            @if ($oembed->thumbnail_url)
-                @if ($oembed->type === 'summary_large_image')
-                    <img class="card-img-top border-bottom" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
-                @else
-                    <img class="rounded-start w-20" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
-                @endif
+<div class="card mt-3 mb-2 w-75">
+    <div class="{{ $oembed->type !== 'summary_large_image' ? 'd-flex align-items-center' : '' }}">
+        @if ($oembed->thumbnail_url)
+            @if ($oembed->type === 'summary_large_image')
+                <img class="card-img-top border-bottom" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
+            @else
+                <img class="rounded-start w-20" src="{{ Helper::getCDNImage($oembed->thumbnail_url) }}" alt="{{ $oembed->title }}">
             @endif
-            <div class="card-body">
-                <a href="{{ $oembed->url }}">
-                    <div class="fw-bold text-dark">
-                        {{ $oembed->title }}
-                    </div>
-                    <div class="mt-1 text-dark">
-                        {{ $oembed->description }}
-                    </div>
-                </a>
-                <div class="mt-1 d-flex align-items-center">
-                    <img src="{{ Helper::getCDNImage($oembed->favicon, 20) }}" height="15" width="15" />
-                    <a class="ms-2 text-secondary" href="{{ $oembed->provider_url }}" target="_blank">
-                        {{ $oembed->provider_name }}
-                    </a>
+        @endif
+        <div class="card-body">
+            <a href="{{ $oembed->url }}">
+                <div class="fw-bold text-dark">
+                    {{ $oembed->title }}
                 </div>
+                <div class="mt-1 text-dark">
+                    {{ $oembed->description }}
+                </div>
+            </a>
+            <div class="mt-1 d-flex align-items-center">
+                <img src="{{ Helper::getCDNImage($oembed->favicon, 20) }}" height="15" width="15" />
+                <a class="ms-2 text-secondary" href="{{ $oembed->provider_url }}" target="_blank">
+                    {{ $oembed->provider_name }}
+                </a>
             </div>
         </div>
     </div>
