094561613546ed903913e625d6e11f3738035b43
Yogi <yoginth@hey.com>
2021-06-23 09:02:05 +0530

    lint: Blade file ✨

--- resources/views/explore/explore.blade.php
+++ resources/views/explore/explore.blade.php
@@ -7,43 +7,43 @@
 @section('url', url()->current())
 
 @section('content')
-@include('explore.nav')
-<div class="container-md">
-    <div class="row justify-content-center">
-        <div class="col-sm explore-user-card d-none d-lg-block">
-            @auth
-                <livewire:explore.user-card />
-            @else
-                <div class="card">
-                    <div class="card-body d-grid text-center">
-                        <div class="h5 text-secondary">
-                            Join Taskord today!
+    @include('explore.nav')
+    <div class="container-md">
+        <div class="row justify-content-center">
+            <div class="col-sm explore-user-card d-none d-lg-block">
+                @auth
+                    <livewire:explore.user-card />
+                @else
+                    <div class="card">
+                        <div class="card-body d-grid text-center">
+                            <div class="h5 text-secondary">
+                                Join Taskord today!
+                            </div>
+                            <a class="btn btn-outline-success rounded-pill mt-2" href="{{ route('login') }}">
+                                <x-heroicon-o-logout class="heroicon" />
+                                Login
+                            </a>
+                            <a class="btn btn-outline-primary rounded-pill mt-2" href="{{ route('register') }}">
+                                <x-heroicon-o-user-add class="heroicon" />
+                                Signup
+                            </a>
                         </div>
-                        <a class="btn btn-outline-success rounded-pill mt-2" href="{{ route('login') }}">
-                            <x-heroicon-o-logout class="heroicon" />
-                            Login
-                        </a>
-                        <a class="btn btn-outline-primary rounded-pill mt-2" href="{{ route('register') }}">
-                            <x-heroicon-o-user-add class="heroicon" />
-                            Signup
-                        </a>
                     </div>
-                </div>
-            @endauth
-        </div>
-        <div class="col-lg-6 mt-4">
-            <div class="pb-2 h5 text-secondary">
-                Recent popular tasks
+                @endauth
             </div>
-            <livewire:explore.popular-tasks />
+            <div class="col-lg-6 mt-4">
+                <div class="pb-2 h5 text-secondary">
+                    Recent popular tasks
+                </div>
+                <livewire:explore.popular-tasks />
+            </div>
+            <div class="col-sm mt-4">
+                @auth
+                    <livewire:home.suggestions :user="auth()->user()" :showText="false" :wire:key="$auth()->id()" />
+                @endauth
+                <livewire:explore.trending-makers />
+            </div>
+            <x-bottom-footer />
         </div>
-        <div class="col-sm mt-4">
-            @auth
-                <livewire:home.suggestions :user="auth()->user()" :showText="false" :wire:key="$auth()->id()" />
-            @endauth
-            <livewire:explore.trending-makers />
-        </div>
-        <x-bottom-footer />
     </div>
-</div>
 @endsection
