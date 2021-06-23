9ac9e71d6d3b59cf918daff3d15635efe523fc2d
Yogi <yoginth@hey.com>
2021-06-23 11:05:23 +0530

    add: User skeleton loader

--- resources/views/livewire/explore/trending-makers.blade.php
+++ resources/views/livewire/explore/trending-makers.blade.php
@@ -9,46 +9,47 @@
                 <div class="card-body">
                     <x:loaders.sidebar.user-skeleton count="5" />
                 </div>
-            @endif
-            @foreach ($users as $user)
-                <div class="d-flex align-items-center py-1 px-3">
-                    <a href="{{ route('user.done', ['username' => $user->username]) }}">
-                        <img loading=lazy class="rounded-circle avatar-40 mt-1 user-popover"
-                            src="{{ Helper::getCDNImage($user->avatar, 160) }}" data-id="{{ $user->id }}"
-                            height="40" width="40" alt="{{ $user->username }}'s avatar" />
-                    </a>
-                    <span class="ms-3">
-                        <a href="{{ route('user.done', ['username' => $user->username]) }}"
-                            class="align-text-top text-dark user-popover" data-id="{{ $user->id }}">
-                            <span class="fw-bold">
-                                @if ($user->firstname or $user->lastname)
-                                    {{ $user->firstname }}{{ ' ' . $user->lastname }}
-                                @else
-                                    {{ $user->username }}
-                                @endif
-                                @if ($user->status)
-                                    <span class="ms-1 small"
-                                        title="{{ $user->status }}">{{ $user->status_emoji }}</span>
-                                @endif
-                                @if ($user->is_verified)
-                                    <x-heroicon-s-badge-check class="heroicon ms-1 text-primary verified" />
-                                @endif
-                            </span>
-                            <div>
-                                @if ($user->bio)
-                                    <span class="small">
-                                        {{ $user->bio }}
-                                    </span>
-                                @else
-                                    <span class="small text-secondary">
-                                        {{ '@' . $user->username }}
-                                    </span>
-                                @endif
-                            </div>
+            @else
+                @foreach ($users as $user)
+                    <div class="d-flex align-items-center py-1 px-3">
+                        <a href="{{ route('user.done', ['username' => $user->username]) }}">
+                            <img loading=lazy class="rounded-circle avatar-40 mt-1 user-popover"
+                                src="{{ Helper::getCDNImage($user->avatar, 160) }}" data-id="{{ $user->id }}"
+                                height="40" width="40" alt="{{ $user->username }}'s avatar" />
                         </a>
-                    </span>
-                </div>
-            @endforeach
+                        <span class="ms-3">
+                            <a href="{{ route('user.done', ['username' => $user->username]) }}"
+                                class="align-text-top text-dark user-popover" data-id="{{ $user->id }}">
+                                <span class="fw-bold">
+                                    @if ($user->firstname or $user->lastname)
+                                        {{ $user->firstname }}{{ ' ' . $user->lastname }}
+                                    @else
+                                        {{ $user->username }}
+                                    @endif
+                                    @if ($user->status)
+                                        <span class="ms-1 small"
+                                            title="{{ $user->status }}">{{ $user->status_emoji }}</span>
+                                    @endif
+                                    @if ($user->is_verified)
+                                        <x-heroicon-s-badge-check class="heroicon ms-1 text-primary verified" />
+                                    @endif
+                                </span>
+                                <div>
+                                    @if ($user->bio)
+                                        <span class="small">
+                                            {{ $user->bio }}
+                                        </span>
+                                    @else
+                                        <span class="small text-secondary">
+                                            {{ '@' . $user->username }}
+                                        </span>
+                                    @endif
+                                </div>
+                            </a>
+                        </span>
+                    </div>
+                @endforeach
+            @endif
         </div>
     </div>
 </div>
