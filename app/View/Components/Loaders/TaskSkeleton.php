52d367e26cb18f9a908d94ae2f77b04e3aaac8ef
Yogi <yoginth@hey.com>
2021-06-23 10:46:14 +0530

    add: Task skeleton loader

--- app/View/Components/Loaders/TaskSkeleton.php
+++ app/View/Components/Loaders/TaskSkeleton.php
@@ -6,21 +6,13 @@ use Illuminate\View\Component;
 
 class TaskSkeleton extends Component
 {
-    /**
-     * Create a new component instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $count;
+
+    public function __construct($count)
     {
-        //
+        $this->count = $count;
     }
 
-    /**
-     * Get the view / contents that represent the component.
-     *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
-     */
     public function render()
     {
         return view('components.loaders.task-skeleton');
