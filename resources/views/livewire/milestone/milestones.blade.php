9634fa6fdb24128f298d962976ff76fc693ddd98
Yogi <yoginth@hey.com>
2021-06-23 12:33:15 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/milestone/milestones.blade.php
+++ resources/views/livewire/milestone/milestones.blade.php
@@ -9,19 +9,20 @@
         <div class="mt-2">
             <x:loaders.milestone-skeleton count="1" />
         </div>
-    @endif
-    @if ($readyToLoad and count($milestones) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-truck class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                No milestones found
+    @else
+        @if (count($milestones) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-truck class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    No milestones found
+                </div>
             </div>
-        </div>
-    @endif
-    @foreach ($milestones as $milestone)
-        <livewire:milestone.single-milestone :type="$type" :milestone="$milestone" :wire:key="$milestone->id" />
-    @endforeach
-    @if ($readyToLoad and $milestones->hasMorePages())
-        <livewire:milestone.load-more :type="$type" :page="$page" :perPage="$perPage" />
+        @endif
+        @foreach ($milestones as $milestone)
+            <livewire:milestone.single-milestone :type="$type" :milestone="$milestone" :wire:key="$milestone->id" />
+        @endforeach
+        @if ($milestones->hasMorePages())
+            <livewire:milestone.load-more :type="$type" :page="$page" :perPage="$perPage" />
+        @endif
     @endif
 </div>
