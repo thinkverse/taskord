7ef688fb4c85de149f8780bf3362d56c91cbd992
Yogi <yoginth@hey.com>
2021-06-23 09:14:28 +0530

    lint: Blade file ✨

--- resources/views/livewire/home/only-following.blade.php
+++ resources/views/livewire/home/only-following.blade.php
@@ -1,9 +1,11 @@
 <span class="float-end">
     <div class="form-check">
-        <input wire:click="onlyFollowingsTasks" class="form-check-input" type="checkbox" id="onlyFollowingsTasks" {{ auth()->user()->only_followings_tasks ? 'checked' : '' }}>
+        <input wire:click="onlyFollowingsTasks" class="form-check-input" type="checkbox" id="onlyFollowingsTasks"
+            {{ auth()->user()->only_followings_tasks ? 'checked' : '' }}>
         <label class="form-check-label" for="onlyFollowingsTasks">
             Only following
         </label>
-        <div wire:loading wire:target="onlyFollowingsTasks" class="spinner-border taskord-spinner spinner-border-sm ms-1"></div>
+        <div wire:loading wire:target="onlyFollowingsTasks"
+            class="spinner-border taskord-spinner spinner-border-sm ms-1"></div>
     </div>
 </span>
