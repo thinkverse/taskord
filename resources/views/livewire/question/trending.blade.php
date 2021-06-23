bdcfed3667a9d1e04e59e64b7dd7e745877e7823
Yogi <yoginth@hey.com>
2021-06-23 12:02:20 +0530

    add: Question trending skeleton

--- resources/views/livewire/question/trending.blade.php
+++ resources/views/livewire/question/trending.blade.php
@@ -6,39 +6,40 @@
     <div class="card mb-4">
         <div class="pt-2 pb-2">
             @if (!$readyToLoad)
-                <div class="card-body text-center">
-                    <div class="spinner-border spinner-border-sm taskord-spinner text-secondary" role="status"></div>
+                <div class="card-body">
+                    <x:loaders.question-skeleton count="5" />
                 </div>
-            @endif
-            @foreach ($trending as $question)
-                <div class="d-flex align-items-center justify-content-between py-2 px-3">
-                    <div>
-                        <a href="{{ route('question.question', ['slug' => $question->slug]) }}"
-                            class="align-text-top text-dark">
-                            <span class="fw-bold">
-                                {{ Str::words($question->title, '10') }}
-                            </span>
+            @else
+                @foreach ($trending as $question)
+                    <div class="d-flex align-items-center py-2 px-3">
+                        <a href="{{ route('user.done', ['username' => $question->user->username]) }}"
+                            class="user-popover" data-id="{{ $question->user->id }}">
+                            <img loading=lazy class="rounded-circle avatar-30 me-3 float-end"
+                                src="{{ Helper::getCDNImage($question->user->avatar, 80) }}" height="30" width="30"
+                                alt="{{ $question->user->username }}'s avatar" />
                         </a>
-                        <div class="text-secondary small mt-1">
-                            @php
-                                $views = views($question)
-                                    ->remember(now()->addHours(6))
-                                    ->unique()
-                                    ->count('id');
-                            @endphp
-                            <x-heroicon-o-eye class="heroicon" />
-                            <span class="fw-bold">{{ number_format($views) }}</span>
-                            {{ pluralize('View', $views) }}
+                        <div>
+                            <a href="{{ route('question.question', ['slug' => $question->slug]) }}"
+                                class="align-text-top text-dark">
+                                <span class="fw-bold">
+                                    {{ Str::words($question->title, '10') }}
+                                </span>
+                            </a>
+                            <div class="text-secondary small mt-1">
+                                @php
+                                    $views = views($question)
+                                        ->remember(now()->addHours(6))
+                                        ->unique()
+                                        ->count('id');
+                                @endphp
+                                <x-heroicon-o-eye class="heroicon" />
+                                <span class="fw-bold">{{ number_format($views) }}</span>
+                                {{ pluralize('View', $views) }}
+                            </div>
                         </div>
                     </div>
-                    <a href="{{ route('user.done', ['username' => $question->user->username]) }}" class="user-popover"
-                        data-id="{{ $question->user->id }}">
-                        <img loading=lazy class="rounded-circle avatar-30 ms-3 float-end"
-                            src="{{ Helper::getCDNImage($question->user->avatar, 80) }}" height="30" width="30"
-                            alt="{{ $question->user->username }}'s avatar" />
-                    </a>
-                </div>
-            @endforeach
+                @endforeach
+            @endif
         </div>
     </div>
 </div>
