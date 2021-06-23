86190875f3fd6b8fa349edd99bdc4e45afce2456
Yogi <yoginth@hey.com>
2021-06-23 09:02:09 +0530

    lint: Blade file ✨

--- resources/views/explore/makers.blade.php
+++ resources/views/explore/makers.blade.php
@@ -7,17 +7,17 @@
 @section('url', url()->current())
 
 @section('content')
-@include('explore.nav')
-<div class="container-md">
-    <div class="row justify-content-center">
-        <div class="col-lg-8 pt-4">
-            <div class="card">
-                <div class="card-body">
-                    WIP
+    @include('explore.nav')
+    <div class="container-md">
+        <div class="row justify-content-center">
+            <div class="col-lg-8 pt-4">
+                <div class="card">
+                    <div class="card-body">
+                        WIP
+                    </div>
                 </div>
             </div>
+            <x-bottom-footer />
         </div>
-        <x-bottom-footer />
     </div>
-</div>
 @endsection
