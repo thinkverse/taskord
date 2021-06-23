44c15ff3ef3fe91843cf2ead8d2b2cdcc68712c0
Yogi <yoginth@hey.com>
2021-06-23 09:08:34 +0530

    lint: Blade file ✨

--- resources/views/seo/sitemap_users.blade.php
+++ resources/views/seo/sitemap_users.blade.php
@@ -1,11 +1,11 @@
 @foreach ($users as $user)
-    {{ 'https://taskord.com/@'.$user->username }}
-    {{ 'https://taskord.com/@'.$user->username.'/pending' }}
-    {{ 'https://taskord.com/@'.$user->username.'/products' }}
-    {{ 'https://taskord.com/@'.$user->username.'/questions' }}
-    {{ 'https://taskord.com/@'.$user->username.'/answers' }}
-    {{ 'https://taskord.com/@'.$user->username.'/milestones' }}
-    {{ 'https://taskord.com/@'.$user->username.'/stats' }}
-    {{ 'https://taskord.com/@'.$user->username.'/following' }}
-    {{ 'https://taskord.com/@'.$user->username.'/followers' }}
+    {{ 'https://taskord.com/@' . $user->username }}
+    {{ 'https://taskord.com/@' . $user->username . '/pending' }}
+    {{ 'https://taskord.com/@' . $user->username . '/products' }}
+    {{ 'https://taskord.com/@' . $user->username . '/questions' }}
+    {{ 'https://taskord.com/@' . $user->username . '/answers' }}
+    {{ 'https://taskord.com/@' . $user->username . '/milestones' }}
+    {{ 'https://taskord.com/@' . $user->username . '/stats' }}
+    {{ 'https://taskord.com/@' . $user->username . '/following' }}
+    {{ 'https://taskord.com/@' . $user->username . '/followers' }}
 @endforeach
