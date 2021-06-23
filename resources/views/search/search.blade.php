1638eaecc0e0859424d50d80a0cd2aca4bf3175a
Yogi <yoginth@hey.com>
2021-06-23 09:08:08 +0530

    lint: Blade file ✨

--- resources/views/search/search.blade.php
+++ resources/views/search/search.blade.php
@@ -7,25 +7,25 @@
 @section('url', url()->current())
 
 @section('content')
-<div class="container pt-5">
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
-            <div class="h4 fw-normal d-flex align-items-center">
-                <x-heroicon-o-search class="heroicon heroicon-20px" />
-                <span class="ms-2">
-                    Search more than
-                    <span class="fw-bold">{{ $random }}</span>
-                </span>
-            </div>
-            <form action="/search/tasks" method="GET" role="search">
-                @csrf
-                <div class="input-group mt-3">
-                    <input type="text" class="form-control" name="q" placeholder="Search Taskord" autofocus>
-                    <button type="submit" class="btn btn-secondary">Search</button>
+    <div class="container pt-5">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="h4 fw-normal d-flex align-items-center">
+                    <x-heroicon-o-search class="heroicon heroicon-20px" />
+                    <span class="ms-2">
+                        Search more than
+                        <span class="fw-bold">{{ $random }}</span>
+                    </span>
                 </div>
-            </form>
+                <form action="/search/tasks" method="GET" role="search">
+                    @csrf
+                    <div class="input-group mt-3">
+                        <input type="text" class="form-control" name="q" placeholder="Search Taskord" autofocus>
+                        <button type="submit" class="btn btn-secondary">Search</button>
+                    </div>
+                </form>
+            </div>
         </div>
+        <x-bottom-footer />
     </div>
-    <x-bottom-footer />
-</div>
 @endsection
