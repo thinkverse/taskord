48e0ff1e6d502b2a13f5f79ec9a4afca50a908d8
Yogi <yoginth@hey.com>
2021-06-23 09:17:55 +0530

    lint: Blade file ✨

--- resources/views/livewire/notification/type/comment/reply/reply-liked.blade.php
+++ resources/views/livewire/notification/type/comment/reply/reply-liked.blade.php
@@ -2,7 +2,8 @@
     @if ($reply)
         <div class="mt-2 text-secondary">
             liked your
-            <a class="fw-bold" href="{{ route('comment', ['id' => $reply->comment->task->id, 'comment_id' => $reply->comment->id]) }}">
+            <a class="fw-bold"
+                href="{{ route('comment', ['id' => $reply->comment->task->id, 'comment_id' => $reply->comment->id]) }}">
                 comment
             </a>
         </div>
