bd634b5e2996a3e57b4609ff0558c05a60e0b6d6
Yogi <yoginth@hey.com>
2021-06-23 09:26:15 +0530

    lint: Blade file ✨

--- resources/views/livewire/load-more.blade.php
+++ resources/views/livewire/load-more.blade.php
@@ -1,3 +1,4 @@
 <div class="mt-3">
-    <button wire:loading.attr="disabled" wire:click="loadMore" wire:target="loadMore" class="btn w-100 btn-outline-primary rounded-pill mb-4" id="load-more">Load More</button>
+    <button wire:loading.attr="disabled" wire:click="loadMore" wire:target="loadMore"
+        class="btn w-100 btn-outline-primary rounded-pill mb-4" id="load-more">Load More</button>
 </div>
