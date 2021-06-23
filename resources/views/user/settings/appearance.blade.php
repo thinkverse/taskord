5bafd450b41b41bae70e8379cde444b49e34979c
Yogi <yoginth@hey.com>
2021-06-23 09:10:20 +0530

    lint: Blade file ✨

--- resources/views/user/settings/appearance.blade.php
+++ resources/views/user/settings/appearance.blade.php
@@ -3,12 +3,12 @@
 @section('pageTitle', 'Settings / Appearance ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center mt-4">
-        @include('user.settings.sidebar')
-        @livewire('user.settings.appearance', [
+    <div class="container-md">
+        <div class="row justify-content-center mt-4">
+            @include('user.settings.sidebar')
+            @livewire('user.settings.appearance', [
             'user' => $user
-        ])
+            ])
+        </div>
     </div>
-</div>
 @endsection
