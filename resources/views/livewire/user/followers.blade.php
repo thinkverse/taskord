71dd800e0024358621f5750c0a93a66b97e11e49
Yogi <yoginth@hey.com>
2021-06-23 09:25:15 +0530

    lint: Blade file ✨

--- resources/views/livewire/user/followers.blade.php
+++ resources/views/livewire/user/followers.blade.php
@@ -18,12 +18,15 @@
     @foreach ($followers as $user)
         <div class="card mb-3">
             <div class="card-body d-flex align-items-center">
-            <img loading=lazy class="rounded-circle avatar-40 mt-1" src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="40" width="40" alt="{{ $user->username }}'s avatar" />
+                <img loading=lazy class="rounded-circle avatar-40 mt-1"
+                    src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="40" width="40"
+                    alt="{{ $user->username }}'s avatar" />
                 <span class="ms-3">
-                    <a href="{{ route('user.done', ['username' => $user->username]) }}" class="align-text-top text-dark">
+                    <a href="{{ route('user.done', ['username' => $user->username]) }}"
+                        class="align-text-top text-dark">
                         <span class="fw-bold">
                             @if ($user->firstname or $user->lastname)
-                                {{ $user->firstname }}{{ ' '.$user->lastname }}
+                                {{ $user->firstname }}{{ ' ' . $user->lastname }}
                             @else
                                 {{ $user->username }}
                             @endif
@@ -33,7 +36,7 @@
                     @auth
                         @if (auth()->user()->id !== $user->id && !$user->spammy)
                             @livewire('user.follow', [
-                                'user' => $user
+                            'user' => $user
                             ], key($user->id))
                         @endif
                     @endauth
