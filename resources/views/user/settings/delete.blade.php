64a37d631cad84a51b2616065882555886579b29
Yogi <yoginth@hey.com>
2021-06-23 09:10:26 +0530

    lint: Blade file ✨

--- resources/views/user/settings/delete.blade.php
+++ resources/views/user/settings/delete.blade.php
@@ -3,12 +3,12 @@
 @section('pageTitle', 'Settings / Delete ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center mt-4">
-        @include('user.settings.sidebar')
-        @livewire('user.settings.delete', [
+    <div class="container-md">
+        <div class="row justify-content-center mt-4">
+            @include('user.settings.sidebar')
+            @livewire('user.settings.delete', [
             'user' => $user
-        ])
+            ])
+        </div>
     </div>
-</div>
 @endsection
