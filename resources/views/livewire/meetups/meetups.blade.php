bc50f933e100bd542e9002a268b86097242443ba
Yogi <yoginth@hey.com>
2021-06-23 12:32:53 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/meetups/meetups.blade.php
+++ resources/views/livewire/meetups/meetups.blade.php
@@ -6,19 +6,20 @@
                 Loading meetups...
             </div>
         </div>
-    @endif
-    @if ($readyToLoad and count($meetups) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-truck class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                No meetups found
+    @else
+        @if (count($meetups) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-truck class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    No meetups found
+                </div>
             </div>
-        </div>
-    @endif
-    @foreach ($meetups as $meetup)
-        <livewire:meetups.single-meetup :type="$type" :meetup="$meetup" :wire:key="$meetup->id" />
-    @endforeach
-    @if ($readyToLoad and $meetups->hasMorePages())
-        <livewire:meetups.load-more :type="$type" :page="$page" :perPage="$perPage" />
+        @endif
+        @foreach ($meetups as $meetup)
+            <livewire:meetups.single-meetup :type="$type" :meetup="$meetup" :wire:key="$meetup->id" />
+        @endforeach
+        @if ($meetups->hasMorePages())
+            <livewire:meetups.load-more :type="$type" :page="$page" :perPage="$perPage" />
+        @endif
     @endif
 </div>
