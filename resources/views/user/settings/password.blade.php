fa90da46adbda90135ced37347b0b92184bcf280
Yogi <yoginth@hey.com>
2021-06-23 09:10:42 +0530

    lint: Blade file ✨

--- resources/views/user/settings/password.blade.php
+++ resources/views/user/settings/password.blade.php
@@ -3,12 +3,12 @@
 @section('pageTitle', 'Settings / Password ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center mt-4">
-        @include('user.settings.sidebar')
-        @livewire('user.settings.password', [
+    <div class="container-md">
+        <div class="row justify-content-center mt-4">
+            @include('user.settings.sidebar')
+            @livewire('user.settings.password', [
             'user' => $user
-        ])
+            ])
+        </div>
     </div>
-</div>
 @endsection
