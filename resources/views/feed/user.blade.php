702843f78a1d5acac4f3fb9c67cc965f9202be00
Yogi <yoginth@hey.com>
2021-06-23 09:02:23 +0530

    lint: Blade file ✨

--- resources/views/feed/user.blade.php
+++ resources/views/feed/user.blade.php
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <feed xmlns="http://www.w3.org/2005/Atom" xmlns:media="http://search.yahoo.com/mrss/">
-    <title>{{ '@'.$user->username }}'s Feed - {{ config('app.name') }}</title>
+    <title>{{ '@' . $user->username }}'s Feed - {{ config('app.name') }}</title>
     <link href="https://taskord.com/feed/user/{{ $user->username }}" rel="self" type="application/atom+xml"/>
-    <link href="https://taskord.com/{{ '@'.$user->username }}" rel="alternate" type="text/html"/>
+    <link href="https://taskord.com/{{ '@' . $user->username }}" rel="alternate" type="text/html"/>
     <id>https://taskord.com/feed/user/{{ $user->username }}</id>
     <updated>{{ $user->updated_at->format('D, d M Y H:i:s') }}</updated>
     @foreach ($tasks as $task)
@@ -12,7 +12,7 @@
         <id>https://taskord.com/task/{{ $task->id }}</id>
         <author>
             <username>{{ $task->user->username }}</username>
-            <name>{{ $task->user->firstname .' '. $user->lastname }}</name>
+            <name>{{ $task->user->firstname . ' ' . $user->lastname }}</name>
         </author>
         <created>{{ $task->created_at->format('D, d M Y H:i:s') }}</created>
         <updated>{{ $task->updated_at->format('D, d M Y H:i:s') }}</updated>
