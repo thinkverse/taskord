3bb6ef511538a464ca37f67b5eab8bc0994e999f
Yogi <yoginth@hey.com>
2021-06-23 09:19:57 +0530

    lint: Blade file ✨

--- resources/views/livewire/product/single-product.blade.php
+++ resources/views/livewire/product/single-product.blade.php
@@ -1,12 +1,15 @@
 <div class="d-flex align-items-center">
     <a href="{{ route('product.done', ['slug' => $product->slug]) }}">
-        <img loading=lazy class="rounded avatar-50" src="{{ Helper::getCDNImage($product->avatar, 160) }}" height="50" width="50" alt="{{ $product->slug }}'s avatar" height="50" width="50" />
+        <img loading=lazy class="rounded avatar-50" src="{{ Helper::getCDNImage($product->avatar, 160) }}" height="50"
+            width="50" alt="{{ $product->slug }}'s avatar" height="50" width="50" />
     </a>
     <span class="ms-3">
-        <a href="{{ route('product.done', ['slug' => $product->slug]) }}" class="me-2 h5 align-text-top fw-bold text-dark">
+        <a href="{{ route('product.done', ['slug' => $product->slug]) }}"
+            class="me-2 h5 align-text-top fw-bold text-dark">
             {{ $product->name }}
             @if ($product->launched)
-                <a href="{{ route('products.launched') }}" class="small" data-bs-toggle="tooltip" data-placement="right" title="Launched">
+                <a href="{{ route('products.launched') }}" class="small" data-bs-toggle="tooltip"
+                    data-placement="right" title="Launched">
                     🚀
                 </a>
             @endif
@@ -17,12 +20,11 @@
         @if ($product->members()->count() > 1)
             <span class="me-2 text-secondary fw-bold">+{{ $product->members()->count() - 1 }} more</span>
         @endif
-        <a
-            href="{{ route('user.done', ['username' => $product->user->username]) }}"
-            class="user-popover"
-            data-id="{{ $product->user->id }}"
-        >
-            <img loading=lazy class="rounded-circle avatar-30" src="{{ Helper::getCDNImage($product->user->avatar, 80) }}" height="30" width="30" alt="{{ $product->user->username }}'s avatar" />
+        <a href="{{ route('user.done', ['username' => $product->user->username]) }}" class="user-popover"
+            data-id="{{ $product->user->id }}">
+            <img loading=lazy class="rounded-circle avatar-30"
+                src="{{ Helper::getCDNImage($product->user->avatar, 80) }}" height="30" width="30"
+                alt="{{ $product->user->username }}'s avatar" />
         </a>
     </span>
 </div>
