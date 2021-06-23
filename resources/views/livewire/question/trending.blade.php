6f5211bcd43a8897e2899c004b1ebd7bde6d216b
Yogi <yoginth@hey.com>
2021-06-23 09:21:15 +0530

    lint: Blade file ✨

--- resources/views/livewire/question/trending.blade.php
+++ resources/views/livewire/question/trending.blade.php
@@ -13,26 +13,29 @@
             @foreach ($trending as $question)
                 <div class="d-flex align-items-center justify-content-between py-2 px-3">
                     <div>
-                        <a href="{{ route('question.question', ['slug' => $question->slug]) }}" class="align-text-top text-dark">
+                        <a href="{{ route('question.question', ['slug' => $question->slug]) }}"
+                            class="align-text-top text-dark">
                             <span class="fw-bold">
                                 {{ Str::words($question->title, '10') }}
                             </span>
                         </a>
                         <div class="text-secondary small mt-1">
                             @php
-                                $views = views($question)->remember(now()->addHours(6))->unique()->count('id')
+                                $views = views($question)
+                                    ->remember(now()->addHours(6))
+                                    ->unique()
+                                    ->count('id');
                             @endphp
                             <x-heroicon-o-eye class="heroicon" />
                             <span class="fw-bold">{{ number_format($views) }}</span>
                             {{ pluralize('View', $views) }}
                         </div>
                     </div>
-                    <a
-                        href="{{ route('user.done', ['username' => $question->user->username]) }}"
-                        class="user-popover"
-                        data-id="{{ $question->user->id }}"
-                    >
-                        <img loading=lazy class="rounded-circle avatar-30 ms-3 float-end" src="{{ Helper::getCDNImage($question->user->avatar, 80) }}" height="30" width="30" alt="{{ $question->user->username }}'s avatar" />
+                    <a href="{{ route('user.done', ['username' => $question->user->username]) }}" class="user-popover"
+                        data-id="{{ $question->user->id }}">
+                        <img loading=lazy class="rounded-circle avatar-30 ms-3 float-end"
+                            src="{{ Helper::getCDNImage($question->user->avatar, 80) }}" height="30" width="30"
+                            alt="{{ $question->user->username }}'s avatar" />
                     </a>
                 </div>
             @endforeach
