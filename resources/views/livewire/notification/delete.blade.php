3a36832ac948fff0378937c41bcaa5c467d20ec2
Yogi <yoginth@hey.com>
2021-06-23 09:18:48 +0530

    lint: Blade file ✨

--- resources/views/livewire/notification/delete.blade.php
+++ resources/views/livewire/notification/delete.blade.php
@@ -1,4 +1,5 @@
-<button class="btn btn-outline-danger rounded-pill float-md-end mb-3" wire:click="deleteAll" wire:loading.attr="disabled">
+<button class="btn btn-outline-danger rounded-pill float-md-end mb-3" wire:click="deleteAll"
+    wire:loading.attr="disabled">
     <x-heroicon-o-trash class="heroicon" />
     Delete all Notification
 </button>
