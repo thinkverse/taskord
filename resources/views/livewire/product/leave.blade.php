f564672923065e8db3191d2cabb8aabd9b53005d
Yogi <yoginth@hey.com>
2021-06-23 09:19:52 +0530

    lint: Blade file ✨

--- resources/views/livewire/product/leave.blade.php
+++ resources/views/livewire/product/leave.blade.php
@@ -1,6 +1,7 @@
 <div class="card mb-4">
     <div class="card-body">
-        <button type="button" class="btn w-100 btn-outline-danger rounded-pill text-white fw-bold" wire:click="leaveProduct" wire:loading.attr="disabled">
+        <button type="button" class="btn w-100 btn-outline-danger rounded-pill text-white fw-bold"
+            wire:click="leaveProduct" wire:loading.attr="disabled">
             <x-heroicon-o-logout class="heroicon" />
             Leave the Team
         </button>
