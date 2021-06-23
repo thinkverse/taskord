4bc82693e9161b4b23ecb63d6b06fba0d9f9b4d0
Yogi <yoginth@hey.com>
2021-06-23 12:07:58 +0530

    add: Reply skeleton

--- resources/views/components/loaders/reply-skeleton.blade.php
+++ resources/views/components/loaders/reply-skeleton.blade.php
@@ -1,15 +1,24 @@
 <div class="ssc">
-    @for ($i = 0; $i < $count; $i++)
-        <div class="d-flex align-items-center {{ ($i === 0 or $i === $count - 1) ? 'my-2' : 'my-4' }}">
-            <div class="ssc-circle avatar-20"></div>
-            <div class="w-70 ms-3">
-                <div class="ssc-line"></div>
-                <div class="d-flex align-items-center">
-                    <div class="ssc-line w-20 m-0"></div>
-                    <div class="ssc-line w-5 m-0 ms-1"></div>
-                    <div class="ssc-line w-5 m-0 ms-1"></div>
-                </div>
-            </div>
-        </div>
-    @endfor
+    <div class="card">
+        <ul class="list-group list-group-flush">
+            @for ($i = 0; $i < $count; $i++)
+                <li class="list-group-item py-3">
+                    <div class="d-flex align-items-center">
+                        <div class="ssc-circle avatar-25"></div>
+                        <div class="w-70 ms-2">
+                            <div class="ssc-line w-20"></div>
+                        </div>
+                        <div class="ssc-line w-10 m-0 ms-auto"></div>
+                    </div>
+                    <div class="mt-3">
+                        <div class="ssc-line w-70"></div>
+                    </div>
+                    <div class="d-flex align-items-center mt-3">
+                        <div class="ssc-line w-5 m-0"></div>
+                        <div class="ssc-line w-5 m-0 ms-2"></div>
+                    </div>
+                </li>
+            @endfor
+        </ul>
+    </div>
 </div>
