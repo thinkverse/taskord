3323306c4c4d36e259a1f3e262b4e366568764b3
Yogi <yoginth@hey.com>
2021-06-23 11:51:32 +0530

    add: Question big skeleton

--- resources/views/components/loaders/question-big-skeleton.blade.php
+++ resources/views/components/loaders/question-big-skeleton.blade.php
@@ -1,3 +1,26 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
+<div class="ssc">
+    <div class="card">
+        <ul class="list-group list-group-flush">
+            @for ($i = 0; $i < $count; $i++)
+                <li class="list-group-item py-3">
+                    <div class="d-flex align-items-center">
+                        <div class="ssc-circle avatar-40"></div>
+                        <div class="w-70 ms-2">
+                            <div class="ssc-line w-20"></div>
+                            <div class="ssc-line w-10"></div>
+                        </div>
+                        <div class="ssc-line w-10 ms-auto"></div>
+                    </div>
+                    <div class="d-flex align-items-center mt-3">
+                        <div class="ssc-circle avatar-20"></div>
+                        <div class="ssc-line w-50 ms-2"></div>
+                    </div>
+                    <div class="d-flex align-items-center mt-3">
+                        <div class="ssc-line w-5 m-0"></div>
+                        <div class="ssc-line w-5 m-0 ms-2"></div>
+                    </div>
+                </li>
+            @endfor
+        </ul>
+    </div>
 </div>
