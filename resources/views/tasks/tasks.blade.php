c061d70351d3ca0ca6496e186f7949f73782c2f6
Yogi <yoginth@hey.com>
2021-06-23 09:10:08 +0530

    lint: Blade file ✨

--- resources/views/tasks/tasks.blade.php
+++ resources/views/tasks/tasks.blade.php
@@ -3,16 +3,16 @@
 @section('pageTitle', 'Tasks ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center">
-        <div class="col-lg-8">
-            @auth
-                @if (!auth()->user()->spammy)
-                    <livewire:tasks.create-task />
-                @endif
-            @endauth
-            <livewire:tasks.all-tasks />
+    <div class="container-md">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                @auth
+                    @if (!auth()->user()->spammy)
+                        <livewire:tasks.create-task />
+                    @endif
+                @endauth
+                <livewire:tasks.all-tasks />
+            </div>
         </div>
     </div>
-</div>
 @endsection
