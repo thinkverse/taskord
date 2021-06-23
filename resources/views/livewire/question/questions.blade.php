5cd4c97970d67da52a7cf0a94e8e0014de979474
Yogi <yoginth@hey.com>
2021-06-23 12:38:26 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/question/questions.blade.php
+++ resources/views/livewire/question/questions.blade.php
@@ -9,22 +9,23 @@
         <div class="mt-2">
             <x:loaders.question-big-skeleton count="1" />
         </div>
-    @endif
-    @if ($readyToLoad and count($questions) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-question-mark-circle class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                No questions asked
+    @else
+        @if (count($questions) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-question-mark-circle class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    No questions asked
+                </div>
             </div>
-        </div>
-    @endif
-    @foreach ($questions as $question)
-        @livewire('question.single-question', [
-        'type' => $type,
-        'question' => $question,
-        ], key($question->id))
-    @endforeach
-    @if ($readyToLoad and $questions->hasMorePages())
-        <livewire:question.load-more :type="$type" :page="$page" :perPage="$perPage" />
+        @endif
+        @foreach ($questions as $question)
+            @livewire('question.single-question', [
+            'type' => $type,
+            'question' => $question,
+            ], key($question->id))
+        @endforeach
+        @if ($questions->hasMorePages())
+            <livewire:question.load-more :type="$type" :page="$page" :perPage="$perPage" />
+        @endif
     @endif
 </div>
