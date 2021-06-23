002966285e8c8d411a0d0e84dfccf6d3645997b3
Yogi <yoginth@hey.com>
2021-06-23 09:21:11 +0530

    lint: Blade file ✨

--- resources/views/livewire/question/subscribe.blade.php
+++ resources/views/livewire/question/subscribe.blade.php
@@ -1,12 +1,14 @@
 <div>
     @auth
         @if (auth()->user()->hasSubscribed($question))
-            <button wire:click="subscribeQuestion" wire:loading.attr="disabled" class="btn btn-sm btn-outline-danger rounded-pill">
+            <button wire:click="subscribeQuestion" wire:loading.attr="disabled"
+                class="btn btn-sm btn-outline-danger rounded-pill">
                 <x-heroicon-o-status-offline class="heroicon heroicon-15px" />
                 Unsubscribe
             </button>
         @else
-            <button wire:click="subscribeQuestion" wire:loading.attr="disabled" class="btn btn-sm btn-outline-primary rounded-pill">
+            <button wire:click="subscribeQuestion" wire:loading.attr="disabled"
+                class="btn btn-sm btn-outline-primary rounded-pill">
                 <x-heroicon-o-status-online class="heroicon heroicon-15px" />
                 Subscribe
             </button>
