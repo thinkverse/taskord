813a2e9e5dc4e4368a86543b7c96450dd69d3eae
Yogi <yoginth@hey.com>
2021-06-23 09:10:16 +0530

    lint: Blade file ✨

--- resources/views/user/settings/api.blade.php
+++ resources/views/user/settings/api.blade.php
@@ -3,12 +3,12 @@
 @section('pageTitle', 'Settings / API ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center mt-4">
-        @include('user.settings.sidebar')
-        @livewire('user.settings.api', [
+    <div class="container-md">
+        <div class="row justify-content-center mt-4">
+            @include('user.settings.sidebar')
+            @livewire('user.settings.api', [
             'user' => $user
-        ])
+            ])
+        </div>
     </div>
-</div>
 @endsection
