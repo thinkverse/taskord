cd212de19e43257171b5d935bf6773ae4b4e9968
Yogi <yoginth@hey.com>
2021-06-23 09:23:53 +0530

    lint: Blade file ✨

--- resources/views/livewire/user/settings/data.blade.php
+++ resources/views/livewire/user/settings/data.blade.php
@@ -8,26 +8,33 @@
             <div class="mb-4">
                 <h5 class="mb-3">Export your account</h5>
                 <div class="mb-3">
-                    Most of the personal data that Taskord has about you is accessible through the Taskord app (e.g. tasks, comments, questions, answers, patron details, and user account). If you would like to get a consolidated copy of this data, you can download it by clicking the "Export Now" button.
+                    Most of the personal data that Taskord has about you is accessible through the Taskord app (e.g.
+                    tasks, comments, questions, answers, patron details, and user account). If you would like to get a
+                    consolidated copy of this data, you can download it by clicking the "Export Now" button.
                 </div>
                 <div class="mb-3">
-                    As the downloadable file you will receive will contain your profile information, you should keep it secure and be careful when storing, sending, or uploading it to any other services.
+                    As the downloadable file you will receive will contain your profile information, you should keep it
+                    secure and be careful when storing, sending, or uploading it to any other services.
                 </div>
                 <div class="mb-3">
-                    If you have any questions or concerns about the personal data contained in your downloadable file, please <a href="https://taskord.com/contact" target="_blank">contact us</a>.
+                    If you have any questions or concerns about the personal data contained in your downloadable file,
+                    please <a href="https://taskord.com/contact" target="_blank">contact us</a>.
                 </div>
-                <a class="btn btn-outline-success rounded-pill" href="{{ route('user.settings.export.account') }}" target="_blank">
+                <a class="btn btn-outline-success rounded-pill" href="{{ route('user.settings.export.account') }}"
+                    target="_blank">
                     <x-heroicon-o-download class="heroicon" />
                     Export account now
                 </a>
             </div>
-            <hr/>
+            <hr />
             <div class="mt-4">
                 <h5 class="mb-3">Export your logs</h5>
                 <div class="mb-3">
-                    You can download and review the security log for your user account to better understand actions you've performed and actions others have performed that involve you.
+                    You can download and review the security log for your user account to better understand actions
+                    you've performed and actions others have performed that involve you.
                 </div>
-                <a class="btn btn-outline-success rounded-pill" href="{{ route('user.settings.export.logs') }}" target="_blank">
+                <a class="btn btn-outline-success rounded-pill" href="{{ route('user.settings.export.logs') }}"
+                    target="_blank">
                     <x-heroicon-o-download class="heroicon" />
                     Export logs now
                 </a>
