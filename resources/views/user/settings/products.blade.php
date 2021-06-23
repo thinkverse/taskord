9681eae1251ea162dc805b13384fd30bfb1f08e9
Yogi <yoginth@hey.com>
2021-06-23 09:10:49 +0530

    lint: Blade file ✨

--- resources/views/user/settings/products.blade.php
+++ resources/views/user/settings/products.blade.php
@@ -3,12 +3,12 @@
 @section('pageTitle', 'Settings / Products ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center mt-4">
-        @include('user.settings.sidebar')
-        @livewire('user.settings.products', [
+    <div class="container-md">
+        <div class="row justify-content-center mt-4">
+            @include('user.settings.sidebar')
+            @livewire('user.settings.products', [
             'user' => $user
-        ])
+            ])
+        </div>
     </div>
-</div>
 @endsection
