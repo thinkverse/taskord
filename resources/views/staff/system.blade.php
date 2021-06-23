619dd975ca7289dbb9c8dcb39b946ae9811b6e7c
Yogi <yoginth@hey.com>
2021-06-23 09:09:33 +0530

    lint: Blade file ✨

--- resources/views/staff/system.blade.php
+++ resources/views/staff/system.blade.php
@@ -7,14 +7,16 @@
 @section('url', url()->current())
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="container-md">
-            @include('staff.nav')
-            <div class="card">
-                <iframe src="https://p.datadoghq.eu/sb/b0c5b97c-d25c-11eb-85ec-da7ad0900005-4e34db2b858119245ce51dec85fb9bde?live=true" style="height:72vh">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="container-md">
+                @include('staff.nav')
+                <div class="card">
+                    <iframe
+                        src="https://p.datadoghq.eu/sb/b0c5b97c-d25c-11eb-85ec-da7ad0900005-4e34db2b858119245ce51dec85fb9bde?live=true"
+                        style="height:72vh">
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
