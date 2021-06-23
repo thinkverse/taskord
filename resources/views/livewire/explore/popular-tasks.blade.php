289cd66ae3b4c37c628a85ae4b6bab17d95d6ec8
Yogi <yoginth@hey.com>
2021-06-23 11:04:11 +0530

    add: Task skeleton loader

--- resources/views/livewire/explore/popular-tasks.blade.php
+++ resources/views/livewire/explore/popular-tasks.blade.php
@@ -1,10 +1,13 @@
 <div wire:init="loadPopularTasks">
     @if (!$readyToLoad)
-        <div class="card-body text-center mt-3 mb-3">
-            <div class="spinner-border taskord-spinner text-secondary mb-3" role="status"></div>
-            <div class="h6">
-                Loading popular tasks...
-            </div>
+        <div>
+            <x:loaders.task-skeleton count="1" />
+        </div>
+        <div class="mt-3">
+            <x:loaders.task-skeleton count="1" />
+        </div>
+        <div class="mt-3">
+            <x:loaders.task-skeleton count="1" />
         </div>
     @endif
     @foreach ($tasks as $task)
