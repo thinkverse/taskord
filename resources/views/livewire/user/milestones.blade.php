5b485d601aa523905dd282c36690c78eae540dc5
Yogi <yoginth@hey.com>
2021-06-23 12:42:20 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/user/milestones.blade.php
+++ resources/views/livewire/user/milestones.blade.php
@@ -9,19 +9,20 @@
         <div class="mt-2">
             <x:loaders.milestone-skeleton count="1" />
         </div>
-    @endif
-    @if ($readyToLoad and count($milestones) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-chat-alt-2 class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                No milestones made
+    @else
+        @if (count($milestones) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-chat-alt-2 class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    No milestones made
+                </div>
             </div>
-        </div>
-    @endif
-    @foreach ($milestones as $milestone)
-        <livewire:milestone.single-milestone type="milestones.opened" :milestone="$milestone"
-            :wire:key="$milestone->id" />
-    @endforeach
+        @endif
+        @foreach ($milestones as $milestone)
+            <livewire:milestone.single-milestone type="milestones.opened" :milestone="$milestone"
+                :wire:key="$milestone->id" />
+        @endforeach
 
-    {{ $readyToLoad ? $milestones->links() : '' }}
+        {{ $milestones->links() }}
+    @endif
 </div>
