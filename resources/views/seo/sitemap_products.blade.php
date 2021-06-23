690162517bb9606d51e3851690f902bbeeaa2059
Yogi <yoginth@hey.com>
2021-06-23 09:08:21 +0530

    lint: Blade file ✨

--- resources/views/seo/sitemap_products.blade.php
+++ resources/views/seo/sitemap_products.blade.php
@@ -1,6 +1,6 @@
 @foreach ($products as $product)
-    {{ 'https://taskord.com/product/'.$product->slug }}
-    {{ 'https://taskord.com/product/'.$product->slug.'/pending' }}
-    {{ 'https://taskord.com/product/'.$product->slug.'/updates' }}
-    {{ 'https://taskord.com/product/'.$product->slug.'/subscribers' }}
+    {{ 'https://taskord.com/product/' . $product->slug }}
+    {{ 'https://taskord.com/product/' . $product->slug . '/pending' }}
+    {{ 'https://taskord.com/product/' . $product->slug . '/updates' }}
+    {{ 'https://taskord.com/product/' . $product->slug . '/subscribers' }}
 @endforeach
