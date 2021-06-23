cc69be46823db274107a0fa6c776dcc6f44824ab
Yogi <yoginth@hey.com>
2021-06-23 09:09:18 +0530

    lint: Blade file ✨

--- resources/views/staff/features.blade.php
+++ resources/views/staff/features.blade.php
@@ -7,12 +7,12 @@
 @section('url', url()->current())
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="container-md">
-            @include('staff.nav')
-            <livewire:staff.features.features />
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="container-md">
+                @include('staff.nav')
+                <livewire:staff.features.features />
+            </div>
         </div>
     </div>
-</div>
 @endsection
