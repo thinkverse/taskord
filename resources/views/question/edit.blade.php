f77947ad2afbd11c5f2a8e69918107f476c98ebb
Yogi <yoginth@hey.com>
2021-06-23 09:07:23 +0530

    lint: Blade file ✨

--- resources/views/question/edit.blade.php
+++ resources/views/question/edit.blade.php
@@ -3,23 +3,23 @@
 @section('pageTitle', 'Edit Question ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center">
-        <div class="col-lg-7">
-            @auth
-                @if (!auth()->user()->spammy)
-                    <livewire:question.edit-question :question="$question" />
-                @else
-                    <div class="text-center">
-                        <div class="alert alert-danger" role="alert">
-                            You can't edit this question, because your account has been flagged 😢
+    <div class="container-md">
+        <div class="row justify-content-center">
+            <div class="col-lg-7">
+                @auth
+                    @if (!auth()->user()->spammy)
+                        <livewire:question.edit-question :question="$question" />
+                    @else
+                        <div class="text-center">
+                            <div class="alert alert-danger" role="alert">
+                                You can't edit this question, because your account has been flagged 😢
+                            </div>
+                            <a class="btn btn-outline-primary rounded-pill" href="{{ route('home') }}">Go to home</a>
                         </div>
-                        <a class="btn btn-outline-primary rounded-pill" href="{{ route('home') }}">Go to home</a>
-                    </div>
-                @endif
-            @endauth
+                    @endif
+                @endauth
+            </div>
         </div>
     </div>
-</div>
-<x-bottom-footer />
+    <x-bottom-footer />
 @endsection
