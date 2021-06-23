ab1fd04c3f3d034aeed2612312329cef7303c6f7
Yogi <yoginth@hey.com>
2021-06-23 12:14:42 +0530

    add: Milestone skeleton

--- resources/views/components/loaders/milestone-skeleton.blade.php
+++ resources/views/components/loaders/milestone-skeleton.blade.php
@@ -16,9 +16,17 @@
                         <div class="ssc-line w-70 mt-3"></div>
                         <div class="ssc-line w-80"></div>
                     </div>
+                    <div class="mt-4">
+                        <div class="ssc-line w-20"></div>
+                        <div class="ssc-line w-20"></div>
+                    </div>
+                    <div class="mt-4">
+                        <div class="ssc-line w-40"></div>
+                    </div>
                     <div class="d-flex align-items-center mt-4">
                         <div class="ssc-line w-5 m-0"></div>
                         <div class="ssc-line w-5 m-0 ms-2"></div>
+                        <div class="ssc-line w-10 m-0 ms-auto"></div>
                     </div>
                 </li>
             @endfor
