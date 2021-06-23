3b8656626b7f29f2431b4f591d766c3f0f89d8c7
Yogi <yoginth@hey.com>
2021-06-23 09:20:35 +0530

    lint: Blade file ✨

--- resources/views/livewire/product/verify.blade.php
+++ resources/views/livewire/product/verify.blade.php
@@ -9,13 +9,16 @@
                 <div>
                     <h5>Add a DNS TXT record</h5>
                     <p>
-                    1. Create a TXT record in your DNS configuration for the following hostname:
+                        1. Create a TXT record in your DNS configuration for the following hostname:
                     </p>
                     <div class="input-group mb-3">
-                        <input type="text" id="txt-record" class="form-control @error('token') is-invalid @enderror" value="{{ $txt_record }}" readonly>
+                        <input type="text" id="txt-record" class="form-control @error('token') is-invalid @enderror"
+                            value="{{ $txt_record }}" readonly>
                         <div class="input-group-append">
-                            <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy" data-bs-toggle="tooltip" data-for="#txt-record">
-                                <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px"></x-heroicon-o-clipboard-copy>
+                            <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy"
+                                data-bs-toggle="tooltip" data-for="#txt-record">
+                                <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px">
+                                </x-heroicon-o-clipboard-copy>
                             </button>
                         </div>
                     </div>
@@ -23,10 +26,13 @@
                         2. Use this code as the value for the TXT record:
                     </p>
                     <div class="input-group mb-3">
-                        <input type="text" id="txt-code" class="form-control @error('token') is-invalid @enderror" value="{{ $txt_code }}" readonly>
+                        <input type="text" id="txt-code" class="form-control @error('token') is-invalid @enderror"
+                            value="{{ $txt_code }}" readonly>
                         <div class="input-group-append">
-                            <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy" data-bs-toggle="tooltip" data-for="#txt-code">
-                                <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px"></x-heroicon-o-clipboard-copy>
+                            <button class="btn btn-outline-secondary js-clipboard" type="button" title="Copy"
+                                data-bs-toggle="tooltip" data-for="#txt-code">
+                                <x-heroicon-o-clipboard-copy class="heroicon heroicon-18px">
+                                </x-heroicon-o-clipboard-copy>
                             </button>
                         </div>
                     </div>
@@ -38,18 +44,21 @@
                     <div class="fw-bold text-success mb-3">
                         Your domain has been verified at {{ carbon($product->verified_at)->format('d M Y g:i A') }}
                     </div>
-                    <button class="btn btn-outline-success rounded-pill" wire:click="verifyDomain" wire:loading.attr="disabled">
+                    <button class="btn btn-outline-success rounded-pill" wire:click="verifyDomain"
+                        wire:loading.attr="disabled">
                         Reverify domain
                     </button>
                 @else
-                    <button class="btn btn-outline-success rounded-pill" wire:click="verifyDomain" wire:loading.attr="disabled">
+                    <button class="btn btn-outline-success rounded-pill" wire:click="verifyDomain"
+                        wire:loading.attr="disabled">
                         Verify domain
                     </button>
                 @endif
             @else
                 <div>
                     <h5 class="mb-3">Add domain name in edit page</h5>
-                    <a class="btn btn-outline-success rounded-pill" href="{{ route('product.edit', ['slug' => $product->slug]) }}">
+                    <a class="btn btn-outline-success rounded-pill"
+                        href="{{ route('product.edit', ['slug' => $product->slug]) }}">
                         Go to edit page
                     </a>
                 </div>
