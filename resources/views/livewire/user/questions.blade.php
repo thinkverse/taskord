e701b08b4164198fa80c5121c69afc4981779fb6
Yogi <yoginth@hey.com>
2021-06-23 12:43:41 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/user/questions.blade.php
+++ resources/views/livewire/user/questions.blade.php
@@ -9,21 +9,22 @@
         <div class="mt-2">
             <x:loaders.question-big-skeleton count="1" />
         </div>
-    @endif
-    @if ($readyToLoad and count($questions) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-question-mark-circle class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                No questions asked
+    @else
+        @if (count($questions) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-question-mark-circle class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    No questions asked
+                </div>
             </div>
-        </div>
-    @endif
-    @foreach ($questions as $question)
-        @livewire('question.single-question', [
-        'type' => 'question.newest',
-        'question' => $question,
-        ], key($question->id))
-    @endforeach
+        @endif
+        @foreach ($questions as $question)
+            @livewire('question.single-question', [
+            'type' => 'question.newest',
+            'question' => $question,
+            ], key($question->id))
+        @endforeach
 
-    {{ $readyToLoad ? $questions->links() : '' }}
+        {{ $questions->links() }}
+    @endif
 </div>
