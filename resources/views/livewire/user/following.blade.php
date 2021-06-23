8d99e3ba4f07b41c32bc2096514a8ac02e82fa13
Yogi <yoginth@hey.com>
2021-06-23 12:40:41 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/user/following.blade.php
+++ resources/views/livewire/user/following.blade.php
@@ -6,43 +6,44 @@
                 Loading followings...
             </div>
         </div>
-    @endif
-    @if ($readyToLoad and count($followings) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-users class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                {{ $user->username }} isn’t following anybody.
+    @else
+        @if (count($followings) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-users class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    {{ $user->username }} isn’t following anybody.
+                </div>
             </div>
-        </div>
-    @endif
-    @foreach ($followings as $user)
-        <div class="card mb-3">
-            <div class="card-body d-flex align-items-center">
-                <img loading=lazy class="rounded-circle avatar-40 mt-1"
-                    src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="40" width="40"
-                    alt="{{ $user->username }}'s avatar" />
-                <span class="ms-3">
-                    <a href="{{ route('user.done', ['username' => $user->username]) }}"
-                        class="align-text-top text-dark">
-                        <span class="fw-bold">
-                            @if ($user->firstname or $user->lastname)
-                                {{ $user->firstname }}{{ ' ' . $user->lastname }}
-                            @else
-                                {{ $user->username }}
+        @endif
+        @foreach ($followings as $user)
+            <div class="card mb-3">
+                <div class="card-body d-flex align-items-center">
+                    <img loading=lazy class="rounded-circle avatar-40 mt-1"
+                        src="{{ Helper::getCDNImage($user->avatar, 80) }}" height="40" width="40"
+                        alt="{{ $user->username }}'s avatar" />
+                    <span class="ms-3">
+                        <a href="{{ route('user.done', ['username' => $user->username]) }}"
+                            class="align-text-top text-dark">
+                            <span class="fw-bold">
+                                @if ($user->firstname or $user->lastname)
+                                    {{ $user->firstname }}{{ ' ' . $user->lastname }}
+                                @else
+                                    {{ $user->username }}
+                                @endif
+                            </span>
+                            <div class="mb-2">{{ $user->bio }}</div>
+                        </a>
+                        @auth
+                            @if (auth()->user()->id !== $user->id && !$user->spammy)
+                                @livewire('user.follow', [
+                                'user' => $user
+                                ], key($user->id))
                             @endif
-                        </span>
-                        <div class="mb-2">{{ $user->bio }}</div>
-                    </a>
-                    @auth
-                        @if (auth()->user()->id !== $user->id && !$user->spammy)
-                            @livewire('user.follow', [
-                            'user' => $user
-                            ], key($user->id))
-                        @endif
-                    @endauth
-                </span>
+                        @endauth
+                    </span>
+                </div>
             </div>
-        </div>
-    @endforeach
-    {{ $readyToLoad ? $followings->links() : '' }}
+        @endforeach
+        {{ $followings->links() }}
+    @endif
 </div>
