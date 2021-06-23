be6a797d8b4dd618afbaedf6852b86ce0a8959c4
Yogi <yoginth@hey.com>
2021-06-23 09:18:51 +0530

    lint: Blade file ✨

--- resources/views/livewire/notification/follow.blade.php
+++ resources/views/livewire/notification/follow.blade.php
@@ -1,13 +1,15 @@
 <div>
     @if (auth()->user()->isFollowing($user))
-        <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-danger rounded-pill">
+        <button wire:click="toggleFollow" wire:loading.attr="disabled"
+            class="btn btn-sm btn-outline-danger rounded-pill">
             <x-heroicon-o-user-remove class="heroicon heroicon-15px" />
-            Unfollow <span class="fw-bold">{{ '@'.$user->username }}</span>
+            Unfollow <span class="fw-bold">{{ '@' . $user->username }}</span>
         </button>
     @else
-        <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-primary rounded-pill">
+        <button wire:click="toggleFollow" wire:loading.attr="disabled"
+            class="btn btn-sm btn-outline-primary rounded-pill">
             <x-heroicon-o-user-add class="heroicon heroicon-15px" />
-            Follow back <span class="fw-bold">{{ '@'.$user->username }}</span>
+            Follow back <span class="fw-bold">{{ '@' . $user->username }}</span>
         </button>
     @endif
 </div>
