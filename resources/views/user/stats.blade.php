a23a85c7179cd96a64bdabb95dcd8f1b99509c65
Yogi <yoginth@hey.com>
2021-06-23 09:11:56 +0530

    lint: Blade file ✨

--- resources/views/user/stats.blade.php
+++ resources/views/user/stats.blade.php
@@ -1,46 +1,46 @@
 @extends('layouts.app')
 
 @php
-    if ($user->lastname and $user->lastname) {
-        $name = '('.$user->firstname.' '.$user->lastname.')';
-    } else if ($user->firstname) {
-        $name = '('.$user->firstname.')';
-    } else {
-        $name = '';
-    }
+if ($user->lastname and $user->lastname) {
+    $name = '(' . $user->firstname . ' ' . $user->lastname . ')';
+} elseif ($user->firstname) {
+    $name = '(' . $user->firstname . ')';
+} else {
+    $name = '';
+}
 @endphp
 
-@section('pageTitle', $user->username.' '.$name.' / Stats ·')
-@section('title', $user->username.' '.$name.' / Stats ·')
-@section('title', $user->username.' ('.$name.') ·')
+@section('pageTitle', $user->username . ' ' . $name . ' / Stats ·')
+@section('title', $user->username . ' ' . $name . ' / Stats ·')
+@section('title', $user->username . ' (' . $name . ') ·')
 @section('description', $user->bio)
 @section('image', $user->avatar)
 @section('url', url()->current())
 
 @section('content')
-<script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-<div class="container-md">
-    @include('user.profile')
-    <div class="row justify-content-center mt-4">
-        <div class="col-lg-8 mb-4">
-            <div class="card">
-                <div class="card-body">
-                    @livewire('user.stats.all-tasks', [
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+    <div class="container-md">
+        @include('user.profile')
+        <div class="row justify-content-center mt-4">
+            <div class="col-lg-8 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        @livewire('user.stats.all-tasks', [
                         'user' => $user
-                    ])
-                    @livewire('user.stats.completed-tasks', [
+                        ])
+                        @livewire('user.stats.completed-tasks', [
                         'user' => $user
-                    ])
-                    @livewire('user.stats.comments', [
+                        ])
+                        @livewire('user.stats.comments', [
                         'user' => $user
-                    ])
-                    @livewire('user.stats.answers', [
+                        ])
+                        @livewire('user.stats.answers', [
                         'user' => $user
-                    ])
+                        ])
+                    </div>
                 </div>
             </div>
+            @include('user.sidebar')
         </div>
-        @include('user.sidebar')
     </div>
-</div>
 @endsection
