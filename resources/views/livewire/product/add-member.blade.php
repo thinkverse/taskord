e33af7fbaf4d5289a808bef66235110ec56cf47f
Yogi <yoginth@hey.com>
2021-06-23 09:19:39 +0530

    lint: Blade file ✨

--- resources/views/livewire/product/add-member.blade.php
+++ resources/views/livewire/product/add-member.blade.php
@@ -1,4 +1,5 @@
-<div wire:ignore.self class="modal fade" id="addMemberModal" tabindex="-1" role="dialog" aria-labelledby="addMemberModalTitle" aria-hidden="true">
+<div wire:ignore.self class="modal fade" id="addMemberModal" tabindex="-1" role="dialog"
+    aria-labelledby="addMemberModalTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -9,11 +10,13 @@
                 <div class="modal-body">
                     <div class="mb-3">
                         <label class="form-label fw-bold">Username</label>
-                        <input type="text" class="form-control" placeholder="Enter the username" wire:model.defer="username">
+                        <input type="text" class="form-control" placeholder="Enter the username"
+                            wire:model.defer="username">
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary rounded-pill" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-outline-secondary rounded-pill"
+                        data-bs-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-outline-primary rounded-pill">
                         Add Member
                     </button>
