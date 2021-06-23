4b32fbc914619e94a8afee2d76d2eeacaba7da6b
Yogi <yoginth@hey.com>
2021-06-23 12:39:55 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/user/answers.blade.php
+++ resources/views/livewire/user/answers.blade.php
@@ -9,34 +9,35 @@
         <div class="mt-3 mb-3">
             <x:loaders.reply-skeleton count="1" />
         </div>
-    @endif
-    @if ($readyToLoad and count($answers) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-chat-alt-2 class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                No answers made
+    @else
+        @if (count($answers) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-chat-alt-2 class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    No answers made
+                </div>
             </div>
-        </div>
-    @endif
-    @foreach ($answers as $answer)
-        <div class="card mb-4">
-            <div class="card-header h6 py-3">
-                <a href="{{ route('user.done', ['username' => $answer->question->user->username]) }}"
-                    class="user-popover" data-id="{{ $answer->question->user->id }}">
-                    <img loading=lazy class="rounded-circle avatar-30"
-                        src="{{ Helper::getCDNImage($answer->question->user->avatar, 80) }}" height="30" width="30"
-                        alt="{{ $answer->question->user->username }}'s avatar" />
-                </a>
-                <a class="align-middle text-dark ms-2"
-                    href="{{ route('question.question', ['slug' => $answer->question->slug]) }}">
-                    {{ $answer->question->title }}
-                </a>
+        @endif
+        @foreach ($answers as $answer)
+            <div class="card mb-4">
+                <div class="card-header h6 py-3">
+                    <a href="{{ route('user.done', ['username' => $answer->question->user->username]) }}"
+                        class="user-popover" data-id="{{ $answer->question->user->id }}">
+                        <img loading=lazy class="rounded-circle avatar-30"
+                            src="{{ Helper::getCDNImage($answer->question->user->avatar, 80) }}" height="30"
+                            width="30" alt="{{ $answer->question->user->username }}'s avatar" />
+                    </a>
+                    <a class="align-middle text-dark ms-2"
+                        href="{{ route('question.question', ['slug' => $answer->question->slug]) }}">
+                        {{ $answer->question->title }}
+                    </a>
+                </div>
+                @livewire('answer.single-answer', [
+                'answer' => $answer
+                ], key($answer->id))
             </div>
-            @livewire('answer.single-answer', [
-            'answer' => $answer
-            ], key($answer->id))
-        </div>
-    @endforeach
+        @endforeach
 
-    {{ $readyToLoad ? $answers->links() : '' }}
+        {{ $answers->links() }}
+    @endif
 </div>
