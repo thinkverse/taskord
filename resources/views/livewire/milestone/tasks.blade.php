c5567dc7e5d0c946d162e6563f1657f1fd3b07f1
Yogi <yoginth@hey.com>
2021-06-23 12:33:58 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/milestone/tasks.blade.php
+++ resources/views/livewire/milestone/tasks.blade.php
@@ -1,27 +1,28 @@
 <div wire:init="loadTasks">
     @if (!$readyToLoad)
         <x:loaders.task-skeleton count="3" />
-    @endif
-    @if ($readyToLoad and count($tasks) === 0)
-        <div class="card-body text-center mt-3 mb-3">
-            <x-heroicon-o-check-circle class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                No tasks found for this milestone
+    @else
+        @if (count($tasks) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-check-circle class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    No tasks found for this milestone
+                </div>
             </div>
-        </div>
-    @endif
-    @if ($page === 1)
-        <ul class="list-group">
-    @endif
-    @foreach ($tasks as $task)
-        <li class="list-group-item p-3 {{ ($loop->index == 0 and $page > 1) ? 'border-top-0 rounded-0' : '' }}">
-            <livewire:task.single-task :task="$task" :wire:key="$task->id" />
-        </li>
-    @endforeach
-    @if ($readyToLoad and $tasks->hasMorePages())
-        <livewire:milestone.load-more-tasks :milestone="$task->milestone" :page="$page" />
-    @endif
-    @if ($page === 1)
-        </ul>
+        @endif
+        @if ($page === 1)
+            <ul class="list-group">
+        @endif
+        @foreach ($tasks as $task)
+            <li class="list-group-item p-3 {{ ($loop->index == 0 and $page > 1) ? 'border-top-0 rounded-0' : '' }}">
+                <livewire:task.single-task :task="$task" :wire:key="$task->id" />
+            </li>
+        @endforeach
+        @if ($tasks->hasMorePages())
+            <livewire:milestone.load-more-tasks :milestone="$task->milestone" :page="$page" />
+        @endif
+        @if ($page === 1)
+            </ul>
+        @endif
     @endif
 </div>
