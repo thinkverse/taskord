6eba9b4005c1034ae9f3a3262b339ce873344c50
Yogi <yoginth@hey.com>
2021-06-23 09:10:36 +0530

    lint: Blade file ✨

--- resources/views/user/settings/logs.blade.php
+++ resources/views/user/settings/logs.blade.php
@@ -3,12 +3,12 @@
 @section('pageTitle', 'Settings / Logs ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center mt-4">
-        @include('user.settings.sidebar')
-        @livewire('user.settings.logs', [
+    <div class="container-md">
+        <div class="row justify-content-center mt-4">
+            @include('user.settings.sidebar')
+            @livewire('user.settings.logs', [
             'user' => $user
-        ])
+            ])
+        </div>
     </div>
-</div>
 @endsection
