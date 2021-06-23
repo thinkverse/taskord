68546c614acef9d3339d3d2a8501d06132022796
Yogi <yoginth@hey.com>
2021-06-23 09:23:58 +0530

    lint: Blade file ✨

--- resources/views/livewire/user/settings/delete.blade.php
+++ resources/views/livewire/user/settings/delete.blade.php
@@ -9,23 +9,28 @@
         <div class="card-body">
             <div class="h5 text-danger mb-3">Reset your Account</div>
             <div class="mb-3">
-                Resetting your account is will be wiped out all your data immediately and you won't be able to get it back.
+                Resetting your account is will be wiped out all your data immediately and you won't be able to get it
+                back.
             </div>
-            <button class="btn btn-outline-danger rounded-pill" data-bs-toggle="modal" data-bs-target="#resetAccountModal">
+            <button class="btn btn-outline-danger rounded-pill" data-bs-toggle="modal"
+                data-bs-target="#resetAccountModal">
                 <x-heroicon-o-trash class="heroicon" />
                 Reset now
             </button>
             <div class="h5 text-danger mb-3 mt-4">Delete your Account</div>
             <div class="mb-3">
-                Deleting your account is permanent. All your data will be wiped out immediately and you won't be able to get it back.
+                Deleting your account is permanent. All your data will be wiped out immediately and you won't be able to
+                get it back.
             </div>
-            <button class="btn btn-outline-danger rounded-pill" data-bs-toggle="modal" data-bs-target="#deleteAccountModal">
+            <button class="btn btn-outline-danger rounded-pill" data-bs-toggle="modal"
+                data-bs-target="#deleteAccountModal">
                 <x-heroicon-o-trash class="heroicon" />
                 Delete now
             </button>
         </div>
     </div>
-    <div class="modal fade" id="resetAccountModal" tabindex="-1" aria-labelledby="resetAccountModalLabel" aria-hidden="true">
+    <div class="modal fade" id="resetAccountModal" tabindex="-1" aria-labelledby="resetAccountModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -36,13 +41,16 @@
                     Are you sure you want to reset your account?
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary rounded-pill" data-bs-dismiss="modal">Cancel</button>
-                    <button wire:loading.attr="disabled" wire:click="resetAccount" type="button" class="btn btn-outline-danger rounded-pill">Reset Account</button>
+                    <button type="button" class="btn btn-outline-secondary rounded-pill"
+                        data-bs-dismiss="modal">Cancel</button>
+                    <button wire:loading.attr="disabled" wire:click="resetAccount" type="button"
+                        class="btn btn-outline-danger rounded-pill">Reset Account</button>
                 </div>
             </div>
         </div>
     </div>
-    <div class="modal fade" id="deleteAccountModal" tabindex="-1" aria-labelledby="deleteAccountModalLabel" aria-hidden="true">
+    <div class="modal fade" id="deleteAccountModal" tabindex="-1" aria-labelledby="deleteAccountModalLabel"
+        aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -53,8 +61,10 @@
                     Are you sure you want to delete your account?
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary rounded-pill" data-bs-dismiss="modal">Cancel</button>
-                    <button wire:loading.attr="disabled" wire:click="deleteAccount" type="button" class="btn btn-outline-danger rounded-pill">Delete Account</button>
+                    <button type="button" class="btn btn-outline-secondary rounded-pill"
+                        data-bs-dismiss="modal">Cancel</button>
+                    <button wire:loading.attr="disabled" wire:click="deleteAccount" type="button"
+                        class="btn btn-outline-danger rounded-pill">Delete Account</button>
                 </div>
             </div>
         </div>
