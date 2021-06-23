86164032d21f3af754b3da7a79815a535687d2cd
Yogi <yoginth@hey.com>
2021-06-23 09:10:57 +0530

    lint: Blade file ✨

--- resources/views/user/settings/sessions.blade.php
+++ resources/views/user/settings/sessions.blade.php
@@ -3,12 +3,12 @@
 @section('pageTitle', 'Settings / Sessions ·')
 
 @section('content')
-<div class="container-md">
-    <div class="row justify-content-center mt-4">
-        @include('user.settings.sidebar')
-        @livewire('user.settings.sessions', [
+    <div class="container-md">
+        <div class="row justify-content-center mt-4">
+            @include('user.settings.sidebar')
+            @livewire('user.settings.sessions', [
             'user' => $user
-        ])
+            ])
+        </div>
     </div>
-</div>
 @endsection
