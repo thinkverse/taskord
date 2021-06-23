258606658ab952916430b576f5e15b8fc8437c64
Yogi <yoginth@hey.com>
2021-06-23 09:14:14 +0530

    lint: Blade file ✨

--- resources/views/livewire/home/follow.blade.php
+++ resources/views/livewire/home/follow.blade.php
@@ -1,11 +1,13 @@
 <div>
     @if (auth()->user()->isFollowing($user))
-        <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-danger rounded-pill">
+        <button wire:click="toggleFollow" wire:loading.attr="disabled"
+            class="btn btn-sm btn-outline-danger rounded-pill">
             <x-heroicon-o-user-remove class="heroicon heroicon-15px" />
             {{ $showText ? 'Unfollow' : '' }}
         </button>
     @else
-        <button wire:click="toggleFollow" wire:loading.attr="disabled" class="btn btn-sm btn-outline-primary rounded-pill">
+        <button wire:click="toggleFollow" wire:loading.attr="disabled"
+            class="btn btn-sm btn-outline-primary rounded-pill">
             <x-heroicon-o-user-add class="heroicon heroicon-15px" />
             {{ $showText ? 'Follow' : '' }}
         </button>
