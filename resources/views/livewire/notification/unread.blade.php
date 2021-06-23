cce2895e4c9dd4cdcecd3bcb300ba2f5a23a21e5
Yogi <yoginth@hey.com>
2021-06-23 12:35:01 +0530

    refactor: readyToLoad condition in blade ✨

--- resources/views/livewire/notification/unread.blade.php
+++ resources/views/livewire/notification/unread.blade.php
@@ -6,30 +6,31 @@
                 Loading notifications...
             </div>
         </div>
-    @endif
-    @if ($readyToLoad and count($notifications) === 0)
-        <div class="card-body text-center mt-5">
-            <x-heroicon-o-bell class="heroicon heroicon-60px text-primary mb-2" />
-            <div class="h4">
-                Inbox zero
+    @else
+        @if (count($notifications) === 0)
+            <div class="card-body text-center mt-5">
+                <x-heroicon-o-bell class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    Inbox zero
+                </div>
+                <p class="text-secondary">
+                    Congratulations, you have read all your notifications.
+                </p>
             </div>
-            <p class="text-secondary">
-                Congratulations, you have read all your notifications.
-            </p>
-        </div>
-    @endif
-    @foreach ($notifications as $notification)
-        <div>
-            @livewire('notification.single-notification', [
-            'notificationId' => $notification->id,
-            'type' => $notification->type,
-            'data' => $notification->data,
-            'createdAt' => $notification->created_at,
-            'pageType' => 'unread',
-            ], key($notification->id))
-        </div>
-    @endforeach
-    @if ($readyToLoad and $notifications->hasMorePages())
-        <livewire:notification.load-more type="unread" :page="$page" :perPage="$perPage" />
+        @endif
+        @foreach ($notifications as $notification)
+            <div>
+                @livewire('notification.single-notification', [
+                'notificationId' => $notification->id,
+                'type' => $notification->type,
+                'data' => $notification->data,
+                'createdAt' => $notification->created_at,
+                'pageType' => 'unread',
+                ], key($notification->id))
+            </div>
+        @endforeach
+        @if ($notifications->hasMorePages())
+            <livewire:notification.load-more type="unread" :page="$page" :perPage="$perPage" />
+        @endif
     @endif
 </div>
