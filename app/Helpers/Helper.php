194d64a927e4eeda2635a4cac810328ac449ea53
Yogi <yoginth@hey.com>
2021-06-24 17:55:15 +0530

    update: URL regex for tasks

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -168,7 +168,7 @@ class Helper
 
     public static function renderTask($task)
     {
-        $urlRegex = "/(http|https)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,100}(\/\S*)?/";
+        $urlRegex = '#\bhttps?://[^,\s()<>]+(?:\([\w\d]+\)|([^,[:punct:]\s]|/))#';
 
         $task = preg_replace('/#([\w-]+)/', '<a href="/product/$1">#$1</a>', $task);
         $task = preg_replace('/@([\w-]+)/', '<a href="/@$1">@$1</a>', $task);
