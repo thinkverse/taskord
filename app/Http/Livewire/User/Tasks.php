288d0b02d717c173e1efee1806a374cf82b78d96
Yogi <yoginth@hey.com>
2021-06-24 14:16:53 +0530

    lazy load oembeds

--- app/Http/Livewire/User/Tasks.php
+++ app/Http/Livewire/User/Tasks.php
@@ -31,7 +31,7 @@ class Tasks extends Component
     public function getTasks()
     {
         return $this->user->tasks()
-            ->with('comments.user')
+            ->with(['comments.user', 'oembed'])
             ->whereDone($this->type === 'user.done' ? true : false)
             ->latest('updated_at')
             ->paginate(10, '*', null, $this->page);
