55336e25ff9c6c02419e14bf10c6cf747ca0a563
Yogi <yoginth@hey.com>
2021-06-24 15:35:05 +0530

    refactor: Add eager loading

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -18,21 +18,24 @@ class TaskController extends Controller
             auth()->check() && auth()->user()->id === $task->user->id or
             auth()->check() && auth()->user()->staff_mode
         ) {
-            return view('task/task', $response);
+            return view('task.task', $response);
         }
 
         if ($task->user->spammy or $task->user->is_private) {
             return abort(404);
         }
 
-        return view('task/task', $response);
+        return view('task.task', $response);
     }
 
     public function comment($id, $comment_id)
     {
         $task = Task::where('id', $id)
+            ->with(['user', 'comments.user'])
             ->firstOrFail();
-        $comment = $task->comments->where('id', $comment_id)->first();
+        $comment = $task->comments()
+            ->with(['user'])
+            ->where('id', $comment_id)->first();
         if (! $comment) {
             return abort(404);
         }
@@ -46,13 +49,13 @@ class TaskController extends Controller
             auth()->check() && auth()->user()->id === $task->user->id or
             auth()->check() && auth()->user()->staff_mode
         ) {
-            return view('comment/comment', $response);
+            return view('comment.comment', $response);
         }
 
         if ($task->user->spammy or $task->user->is_private) {
             return abort(404);
         }
 
-        return view('comment/comment', $response);
+        return view('comment.comment', $response);
     }
 }
