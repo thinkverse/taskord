5b36792c63e3cdd8059c800d6beca0a28bbfb08b
Yogi <yoginth@hey.com>
2021-06-24 13:38:59 +0530

    modal lg

--- resources/views/layouts/modals/lightbox.blade.php
+++ resources/views/layouts/modals/lightbox.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="lightboxModal" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-body">
                 <div class="mb-3 d-flex justify-content-end">
