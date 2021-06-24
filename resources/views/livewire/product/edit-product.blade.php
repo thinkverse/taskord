d003d23bf9651e1a042eeec29cdaf9100e0c5e44
Yogi <yoginth@hey.com>
2021-06-24 18:35:16 +0530

    fix: hide products if there is no domain associated

--- resources/views/livewire/product/edit-product.blade.php
+++ resources/views/livewire/product/edit-product.blade.php
@@ -117,21 +117,23 @@
                         </span>
                     @enderror
                 </div>
-                <div class="mb-3">
-                    <div class="fw-bold mb-2">Verification Status</div>
-                    @if ($product->verified_at)
-                        <div class="fw-bold text-success mb-3">
-                            Your domain has been verified at
-                            {{ carbon($product->verified_at)->format('d M Y g:i A') }}
-                        </div>
-                    @else
-                        <div>This product is not yet verified</div>
-                        <div class="mt-2">
-                            <a href="{{ route('product.verify', ['slug' => $product->slug]) }}"
-                                class="btn btn-sm btn-outline-success rounded-pill">Verify now</a>
-                        </div>
-                    @endif
-                </div>
+                @if ($product->website)
+                    <div class="mb-3">
+                        <div class="fw-bold mb-2">Verification Status</div>
+                        @if ($product->verified_at)
+                            <div class="fw-bold text-success mb-3">
+                                Your domain has been verified at
+                                {{ carbon($product->verified_at)->format('d M Y g:i A') }}
+                            </div>
+                        @else
+                            <div>This product is not yet verified</div>
+                            <div class="mt-2">
+                                <a href="{{ route('product.verify', ['slug' => $product->slug]) }}"
+                                    class="btn btn-sm btn-outline-success rounded-pill">Verify now</a>
+                            </div>
+                        @endif
+                    </div>
+                @endif
                 <div class="mb-3">
                     <div class="fw-bold mb-2">Launch Status</div>
                     <div class="form-check">
