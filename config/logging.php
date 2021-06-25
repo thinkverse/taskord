28b31b4e729a44cdb97f10f28a9a31d77fe5004b
Yogi <yoginth@hey.com>
2021-06-25 16:07:42 +0530

    add: Sentry logging

--- config/logging.php
+++ config/logging.php
@@ -100,6 +100,10 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'sentry' => [
+            'driver' => 'sentry',
+        ],
     ],
 
 ];
