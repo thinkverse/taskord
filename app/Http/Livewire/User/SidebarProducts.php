da6d4ada78f2e48578e0cefd5cdbf89783b0996f
Yogi <yoginth@hey.com>
2021-06-25 10:16:05 +0530

    add: Defer loading for products in user sidebar

--- app/Http/Livewire/User/SidebarProducts.php
+++ app/Http/Livewire/User/SidebarProducts.php
@@ -3,11 +3,33 @@
 namespace App\Http\Livewire\User;
 
 use Livewire\Component;
+use App\Models\User;
 
 class SidebarProducts extends Component
 {
+    public User $user;
+
+    public function mount($user)
+    {
+        $this->user = $user;
+    }
+
+    public function loadProducts()
+    {
+        $this->readyToLoad = true;
+    }
+
+    public function getProducts()
+    {
+        return $this->user->ownedProducts()
+            ->with(['user', 'members'])
+            ->paginate(10);
+    }
+
     public function render()
     {
-        return view('livewire.user.sidebar-products');
+        return view('livewire.user.sidebar-products', [
+            'products' => $this->readyToLoad ? $this->getProducts() : [],
+        ]);
     }
 }
