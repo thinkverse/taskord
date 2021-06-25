d7509703dc348f232af5b3a3484b0f68babd7156
Yogi <yoginth@hey.com>
2021-06-25 19:58:55 +0530

    add: EditComment file

---
+++ app/Http/Livewire/Comment/EditComment.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Livewire\Comment;
+
+use App\Models\Comment;
+use Illuminate\Support\Facades\Gate;
+use Livewire\Component;
+
+class EditComment extends Component
+{
+    public $comment;
+    public $commentId;
+
+    protected $rules = [
+        'comment' => ['required', 'min:3', 'max:20000'],
+    ];
+
+    public function mount($comment)
+    {
+        $this->comment = $comment->comment;
+        $this->commentId = $comment->id;
+    }
+
+    public function submit()
+    {
+        if (Gate::denies('create')) {
+            return toast($this, 'error', config('taskord.toast.deny'));
+        }
+
+        $this->validate();
+        $comment = Comment::find($this->commentId);
+        $comment->comment = $this->comment;
+        $comment->save();
+        $this->emit('commentEdited');
+
+        loggy(request(), 'Comment', auth()->user(), "Edited a comment | Comment ID: {$this->commentId}");
+
+        return toast($this, 'success', 'Comment has been edited!');
+    }
+
+    public function render()
+    {
+        return view('livewire.comment.create-comment');
+    }
+}
