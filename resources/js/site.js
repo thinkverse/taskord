15de5b18cf9b71917c4384bb37b5965713f36d18
Yogi <yoginth@hey.com>
2021-06-25 10:06:15 +0530

    fix: Strings must use doublequote

--- resources/js/site.js
+++ resources/js/site.js
@@ -2,7 +2,7 @@
 const shortcutsModal = document.getElementById("shortcutsModal");
 shortcutsModal.addEventListener("shown.bs.modal", async () => {
   const shortcutsModalBody = document.getElementById("shortcutsModalBody");
-  const res = await window.fetch(`/site/shortcuts`);
+  const res = await window.fetch("/site/shortcuts");
   shortcutsModalBody.innerHTML = await res.text();
 });
 
@@ -12,21 +12,21 @@ if (deployModal) {
   // Load Commit Data
   deployModal.addEventListener("shown.bs.modal", async () => {
     const deployModalCommitBody = document.getElementById("deployModalCommitBody");
-    const res = await window.fetch(`/site/commits-data`);
+    const res = await window.fetch("/site/commits-data");
     deployModalCommitBody.innerHTML = await res.text();
   });
 
   // Load CI Data
   deployModal.addEventListener("shown.bs.modal", async () => {
     const deployModalCIBody = document.getElementById("deployModalCIBody");
-    const res = await window.fetch(`/site/ci-data`);
+    const res = await window.fetch("/site/ci-data");
     deployModalCIBody.innerHTML = await res.text();
   });
 
   // Load Deployment Data
   deployModal.addEventListener("shown.bs.modal", async () => {
     const deployModalDeploymentBody = document.getElementById("deployModalDeploymentBody");
-    const res = await window.fetch(`/site/deployment-data`);
+    const res = await window.fetch("/site/deployment-data");
     deployModalDeploymentBody.innerHTML = await res.text();
   });
 }
