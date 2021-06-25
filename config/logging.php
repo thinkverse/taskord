2845473543684b7b88db380f11b45be36ce44f72
Yogi <yoginth@hey.com>
2021-06-25 16:07:58 +0530

    add: Sentry logging

--- config/logging.php
+++ config/logging.php
@@ -37,7 +37,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'sentry'],
             'ignore_exceptions' => false,
         ],
 
