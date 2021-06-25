8611baa1df08b816dfd59bc3449af73e5a7b5eef
Yogi <yoginth@hey.com>
2021-06-25 09:57:34 +0530

    refactor: Move to const

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,31 +1,31 @@
 // Load shortcuts
-var shortcutsModal = document.getElementById("shortcutsModal");
+const shortcutsModal = document.getElementById("shortcutsModal");
 shortcutsModal.addEventListener("shown.bs.modal", async () => {
-  var shortcutsModalBody = document.getElementById("shortcutsModalBody");
+  const shortcutsModalBody = document.getElementById("shortcutsModalBody");
   const res = await window.fetch(`/site/shortcuts`);
   shortcutsModalBody.innerHTML = await res.text();
 });
 
 // Load Data from GitLab
-var deployModal = document.getElementById("deployModal");
+const deployModal = document.getElementById("deployModal");
 if (deployModal) {
   // Load Commit Data
   deployModal.addEventListener("shown.bs.modal", async () => {
-    var deployModalCommitBody = document.getElementById("deployModalCommitBody");
+    const deployModalCommitBody = document.getElementById("deployModalCommitBody");
     const res = await window.fetch(`/site/commits-data`);
     deployModalCommitBody.innerHTML = await res.text();
   });
 
   // Load CI Data
   deployModal.addEventListener("shown.bs.modal", async () => {
-    var deployModalCIBody = document.getElementById("deployModalCIBody");
+    const deployModalCIBody = document.getElementById("deployModalCIBody");
     const res = await window.fetch(`/site/ci-data`);
     deployModalCIBody.innerHTML = await res.text();
   });
 
   // Load Deployment Data
   deployModal.addEventListener("shown.bs.modal", async () => {
-    var deployModalDeploymentBody = document.getElementById("deployModalDeploymentBody");
+    const deployModalDeploymentBody = document.getElementById("deployModalDeploymentBody");
     const res = await window.fetch(`/site/deployment-data`);
     deployModalDeploymentBody.innerHTML = await res.text();
   });
