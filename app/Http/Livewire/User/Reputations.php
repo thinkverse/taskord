23b7eb4a789907d447020999a5345eb2d9276af8
Yogi <yoginth@hey.com>
2021-06-26 12:39:28 +0530

    add: View return type in controllers

--- app/Http/Livewire/User/Reputations.php
+++ app/Http/Livewire/User/Reputations.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\User;
 use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Contracts\View\View;
 
 class Reputations extends Component
 {
