471a6c405e0f7afa8e4dd3f037419909d9a09e70
Yogi <yoginth@hey.com>
2021-06-26 11:50:34 +0530

    add: Relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use App\Jobs\VerifyEmailQueue;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -84,7 +85,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(Product::class);
     }
 
-    public function products(): BelongsTo
+    public function products(): BelongsToMany
     {
         return $this->belongsToMany(Product::class);
     }
