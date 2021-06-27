98b03692a899acc6f7a8f332f4472646e8cab691
Yogi <yoginth@hey.com>
2021-06-27 17:44:23 +0530

    add: Report modal for staffs

--- app/Jobs/Report.php
+++ app/Jobs/Report.php
@@ -8,8 +8,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Artisan;
 
 class Report implements ShouldQueue
 {
