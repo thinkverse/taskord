403a54c6956525d85b220edad62568f8ff6a73c2
Yogi <yoginth@hey.com>
2021-06-27 11:40:06 +0530

    add: Answer replies

---
+++ resources/views/livewire/notification/type/answer/reply/reply-liked.blade.php
@@ -0,0 +1,17 @@
+<div>
+    @if ($reply)
+        <div class="mt-2 text-secondary">
+            liked your
+            <a class="fw-bold" href="">
+                answer
+            </a>
+        </div>
+        <div class="card mt-3">
+            <div class="card-body body-font">
+                {!! markdown($reply->reply) !!}
+            </div>
+        </div>
+    @else
+        <div class="body-font fst-italic text-secondary mt-2">Notification source was deleted</div>
+    @endif
+</div>
