7d2773a3e6f643ac7ba713fddd13f3cd2b666ed8
Yogi <yoginth@hey.com>
2021-06-27 19:09:36 +0530

    add: Social component

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -63,54 +63,7 @@
                             Sign up
                         </span>
                     </button>
-                    @if (feature('social_auth'))
-                        <div class="mt-3 row">
-                            <div class="col-5 pe-1">
-                                <a href="/login/github" class="btn btn-social btn-github rounded-pill w-100">
-                                    <span class="small">
-                                        <img class="brand-icon github-logo"
-                                            src="https://ik.imagekit.io/taskordimg/icons/github_9E8bhMFJtH.svg"
-                                            loading=lazy />
-                                        GitHub
-                                    </span>
-                                </a>
-                            </div>
-                            <div class="col-5 pe-1">
-                                <a href="/login/twitter" class="btn btn-social btn-twitter rounded-pill w-100">
-                                    <span class="small">
-                                        <img class="brand-icon"
-                                            src="https://ik.imagekit.io/taskordimg/icons/twitter_4cXueyhRfH.svg"
-                                            loading=lazy />
-                                        Twitter
-                                    </span>
-                                </a>
-                            </div>
-                            <div class="col-2">
-                                <button class="btn btn-outline-secondary rounded-circle" id="moreSocialMenuItem"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-                                    <x-heroicon-o-dots-vertical class="heroicon heroicon-15px text-secondary m-0" />
-                                </button>
-                                <ul class="dropdown-menu mt-2 mb-4" aria-labelledby="moreSocialMenuItem">
-                                    <li>
-                                        <a class="dropdown-item cursor-pointer">
-                                            <img class="brand-icon" href="/login/google"
-                                                src="https://ik.imagekit.io/taskordimg/icons/google_LPvasOP5AT.svg"
-                                                loading=lazy />
-                                            <span class="ms-1">Google</span>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a class="dropdown-item cursor-pointer">
-                                            <img class="brand-icon" href="/login/discord"
-                                                src="https://ik.imagekit.io/taskordimg/icons/discord_MCCBaztWr.webp"
-                                                loading=lazy />
-                                            <span class="ms-1">Discord</span>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    @endif
+                    @include('auth.social')
                 </form>
             </div>
         </div>
