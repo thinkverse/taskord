9f3fa680f789ff990a9fde536ed15c4cf75cbf47
Yogi <yoginth@hey.com>
2021-06-27 11:04:35 +0530

    add: Answer replies

--- resources/views/livewire/answer/reply/create-reply.blade.php
+++ resources/views/livewire/answer/reply/create-reply.blade.php
@@ -1,3 +1,60 @@
-<div>
-    {{-- The Master doesn't talk, he acts. --}}
+<div class="card {{ $answer->replies()->count('id') > 0 ? '' : 'mt-3' }}">
+    <div x-data="{ tab: 'edit' }">
+        <div class="pt-2 rounded-top bg-light">
+            <ul class="nav nav-tabs d-flex justify-content-between px-3">
+                <div class="d-flex">
+                    <li class="nav-item">
+                        <div class="cursor-pointer nav-link active" :class="{ 'bg-white active': tab === 'edit' }"
+                            x-on:click="tab = 'edit'">Edit</div>
+                    </li>
+                    <li class="nav-item">
+                        <div class="cursor-pointer nav-link" :class="{ 'active bg-white': tab === 'preview' }"
+                            x-on:click="tab = 'preview'">Preview</div>
+                    </li>
+                </div>
+                <div x-show="tab === 'edit'" class="pt-1">
+                    <x:markdown-toolbar htmlFor="reply-box-{{ $answer->id }}" />
+                </div>
+            </ul>
+        </div>
+        <div class="card-body">
+            @error('reply')
+                <div class="alert alert-danger alert-dismissible fade show">
+                    <button type="button" class="btn-close small" data-bs-dismiss="alert"></button>
+                    <x-heroicon-o-exclamation class="heroicon" />
+                    {{ $message }}
+                </div>
+            @enderror
+            <div class="mb-2">
+                <textarea x-show="tab === 'edit'" placeholder="Leave a reply" id="reply-box-{{ $answer->id }}"
+                    class="form-control mentionInput" rows="3" wire:model.lazy="reply"></textarea>
+                <div x-show="tab === 'preview'">
+                    <div wire:loading>
+                        Loading preview...
+                    </div>
+                    <div wire:loading.remove>
+                        @if ($reply)
+                            {!! markdown($reply) !!}
+                        @else
+                            Nothing to preview...
+                        @endif
+                    </div>
+                </div>
+            </div>
+            <div class="d-flex justify-content-between align-items-center">
+                <a class="small fw-bold text-secondary" href="https://guides.github.com/features/mastering-markdown"
+                    target="_blank">
+                    <x-heroicon-o-pencil-alt class="heroicon" />
+                    Markdown is supported
+                    <x:labels.beta />
+                </a>
+                <button wire:loading.attr="disabled" wire:target="submit"
+                    class="btn btn-sm btn-outline-primary rounded-pill" type="submit" wire:click="submit"
+                    x-on:click="tab = 'edit'">
+                    <x-heroicon-o-plus class="heroicon heroicon-15px" />
+                    Reply
+                </button>
+            </div>
+        </div>
+    </div>
 </div>
--- resources/views/livewire/answer/reply/single-reply.blade.php
+++ resources/views/livewire/answer/reply/single-reply.blade.php
@@ -1,3 +1,35 @@
-<div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+<div id="reply_{{ $reply->id }}">
+    <div class="align-items-center d-flex">
+        <x:shared.user-label-small :user="$reply->user" />
+        <span class="align-text-top small float-end ms-auto text-secondary">
+            {{ carbon($reply->created_at)->diffForHumans() }}
+        </span>
+    </div>
+    <div class="border-2 border-start ps-3 reply-line pt-2 pb-3">
+        @if ($reply->hidden)
+            <span class="body-font fst-italic text-secondary">Reply was hidden by moderator</span>
+        @else
+            <span class="body-font">
+                {!! markdown($reply->reply) !!}
+            </span>
+        @endif
+        <div class="mt-2">
+            @auth
+                <x:like-button :entity="$reply" />
+            @endauth
+            @can('edit/delete', $reply)
+                <button type="button" class="btn btn-action btn-outline-danger"
+                    onclick="confirm('Are you sure?') || event.stopImmediatePropagation()" wire:click="deleteReply"
+                    wire:loading.attr="disabled" wire:target="deleteReply" aria-label="Delete">
+                    <x-heroicon-o-trash class="heroicon heroicon-15px me-0 text-secondary" />
+                </button>
+            @endcan
+            @can('staff.ops')
+                <button type="button" class="btn btn-action {{ $reply->hidden ? 'btn-info' : 'btn-outline-info' }} ms-1"
+                    wire:click="hide" wire:loading.attr="disabled" wire:key="{{ $reply->id }}" aria-label="Hide">
+                    <x-heroicon-o-eye-off class="heroicon heroicon-15px me-0" />
+                </button>
+            @endcan
+        </div>
+    </div>
 </div>
