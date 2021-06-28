febaab5d2251b65ac5653b08649b8795fabf2e3f
Yogi <yoginth@hey.com>
2021-06-28 10:48:41 +0530

    add: Return types

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -147,7 +147,7 @@ class SearchController extends Controller
         return redirect()->route('search.home');
     }
 
-    public function products(Request $request)
+    public function products(Request $request): View|RedirectResponse
     {
         $searchTerm = $request->input('q');
         if ($searchTerm) {
