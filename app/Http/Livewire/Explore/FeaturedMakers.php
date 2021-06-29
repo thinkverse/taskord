ebcc2f88a8b0f7a732c948620a6df4c086be61f9
Yogi <yoginth@hey.com>
2021-06-29 12:16:43 +0530

    add: Featured makers

---
+++ app/Http/Livewire/Explore/FeaturedMakers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Explore;
+
+use Livewire\Component;
+
+class FeaturedMakers extends Component
+{
+    public function render()
+    {
+        return view('livewire.explore.featured-makers');
+    }
+}
