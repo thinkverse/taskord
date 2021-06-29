83fc89b2ae9656fb4346b287da6df4968a2350b7
Yogi <yoginth@hey.com>
2021-06-29 12:55:07 +0530

    add: Featured makers skeleton

--- resources/views/components/loaders/featured-makers-skeleton.blade.php
+++ resources/views/components/loaders/featured-makers-skeleton.blade.php
@@ -1,26 +1,16 @@
 <div class="ssc">
-    <div class="card">
-        <ul class="list-group list-group-flush">
-            @for ($i = 0; $i < $count; $i++)
-                <li class="list-group-item py-3">
-                    <div class="d-flex align-items-center">
-                        <div class="ssc-circle avatar-40"></div>
-                        <div class="w-70 ms-2">
-                            <div class="ssc-line w-20"></div>
-                            <div class="ssc-line w-10"></div>
-                        </div>
-                        <div class="ssc-line w-10 ms-auto"></div>
+    <ul class="list-group list-group-flush">
+        @for ($i = 0; $i < $count; $i++)
+            <li class="list-group-item py-3">
+                <div class="d-flex align-items-center">
+                    <div class="ssc-circle avatar-40"></div>
+                    <div class="w-70 ms-2">
+                        <div class="ssc-line w-20"></div>
+                        <div class="ssc-line w-10"></div>
                     </div>
-                    <div class="d-flex align-items-center mt-3">
-                        <div class="ssc-circle avatar-20"></div>
-                        <div class="ssc-line w-50 ms-2"></div>
-                    </div>
-                    <div class="d-flex align-items-center mt-3">
-                        <div class="ssc-line w-5 m-0"></div>
-                        <div class="ssc-line w-5 m-0 ms-2"></div>
-                    </div>
-                </li>
-            @endfor
-        </ul>
-    </div>
+                    <div class="ssc-head-line w-10 ms-auto"></div>
+                </div>
+            </li>
+        @endfor
+    </ul>
 </div>
