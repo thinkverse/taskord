68085f4e2457a91e1438605a6573115949573006
Yogi <yoginth@hey.com>
2021-06-29 12:14:20 +0530

    add: Feature toggle

--- resources/views/livewire/user/moderator.blade.php
+++ resources/views/livewire/user/moderator.blade.php
@@ -135,6 +135,7 @@
                     </button>
                     @if ($user->featured_at)
                         <div class="small mt-2 fw-bold text-secondary">
+                            <x-heroicon-o-clock class="heroicon heroicon-15px text-secondary" />
                             {{ carbon($user->featured_at)->format('M d, Y g:i A') }}
                         </div>
                     @endif
