88766a664ad58a14537a40e2b302fce45c0257a7
Yogi <yoginth@hey.com>
2021-06-30 11:14:39 +0530

    add: badges and single-badge

---
+++ app/Http/Livewire/Badges/SingleBadge.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Badges;
+
+use Livewire\Component;
+
+class SingleBadge extends Component
+{
+    public function render()
+    {
+        return view('livewire.badges.single-badge');
+    }
+}
