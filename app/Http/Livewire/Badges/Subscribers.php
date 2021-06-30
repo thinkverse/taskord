19a395fe830ab6f68c3dfe41b28c0a9086f41ffd
Yogi <yoginth@hey.com>
2021-06-30 13:20:09 +0530

    add: Single badge subscribers

---
+++ app/Http/Livewire/Badges/Subscribers.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Badges;
+
+use Livewire\Component;
+
+class Subscribers extends Component
+{
+    public function render()
+    {
+        return view('livewire.badges.subscribers');
+    }
+}
