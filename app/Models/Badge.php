5e56558b5c4112f3e747c42c13e2d2e7ef22cae2
Yogi <yoginth@hey.com>
2021-06-30 10:04:49 +0530

    add: BelongsTo and HasMany

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Nicolaslopezj\Searchable\SearchableTrait;
 use Multicaret\Acquaintances\Traits\CanBeSubscribed;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Badge extends Model
 {
