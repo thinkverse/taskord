3d6a48e8f86252151773cab3cd5512c28c714206
Yogi <yoginth@hey.com>
2021-06-30 17:37:14 +0530

    remove: explore_makers flag

--- resources/views/explore/nav.blade.php
+++ resources/views/explore/nav.blade.php
@@ -3,14 +3,12 @@
         <a class="nav-link {{ Route::is('explore.explore') ? 'active' : '' }}"
             href="{{ route('explore.explore') }}">Popular Tasks</a>
     </li>
-    @if (feature('explore_makers'))
-        <li class="nav-item px-1">
-            <a class="nav-link {{ Route::is('explore.makers') ? 'active' : '' }}"
-                href="{{ route('explore.makers') }}">Featured Makers
-                <x:labels.beta />
-            </a>
-        </li>
-    @endif
+    <li class="nav-item px-1">
+        <a class="nav-link {{ Route::is('explore.makers') ? 'active' : '' }}"
+            href="{{ route('explore.makers') }}">Featured Makers
+            <x:labels.beta />
+        </a>
+    </li>
     @if (feature('explore_products'))
         <li class="nav-item px-1">
             <a class="nav-link" href="#">Products
