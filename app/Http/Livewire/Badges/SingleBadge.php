4f827ca9308a70f74ef63efc5157d05109d1f35a
Yogi <yoginth@hey.com>
2021-06-30 13:07:36 +0530

    add: MVP

--- app/Http/Livewire/Badges/SingleBadge.php
+++ app/Http/Livewire/Badges/SingleBadge.php
@@ -5,14 +5,44 @@ namespace App\Http\Livewire\Badges;
 use App\Models\ProfileBadge;
 use Illuminate\View\View;
 use Livewire\Component;
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
+use Illuminate\Support\Facades\Gate;
 
 class SingleBadge extends Component
 {
+    use WithRateLimiting;
+
     public ProfileBadge $badge;
 
     public function mount($badge)
     {
         $this->badge = $badge;
+    }
+
+    public function toggleAddBadge()
+    {
+        try {
+            $this->rateLimit(50);
+        } catch (TooManyRequestsException $exception) {
+            return toast($this, 'error', config('taskord.error.rate-limit'));
+        }
+
+        if (Gate::denies('like/subscribe', $this->badge)) {
+            return toast($this, 'error', config('taskord.toast.deny'));
+        }
+
+        if ($this->badge->isSubscribedBy(auth()->user())) {
+            auth()->user()->unsubscribe($this->badge);
+            toast($this, 'success', "Badge has been removed from your profile successfully");
+        } else {
+            auth()->user()->subscribe($this->badge);
+            toast($this, 'success', "Badge has been added to your profile successfully");
+        }
+
+        $this->emit('badgeAdded');
+
+        return loggy(request(), 'Badge', auth()->user(), "Toggled badge add | Badge ID: {$this->badge->id}");
     }
 
     public function render(): View
--- resources/views/livewire/badges/single-badge.blade.php
+++ resources/views/livewire/badges/single-badge.blade.php
@@ -1,16 +1,38 @@
 <div class="card mb-2">
-    <div class="card-body d-flex align-items-center">
-        <span class="card d-inline-block" style="background: {{ $badge->color }}">
-            <div class="p-4">
-                <img class="avatar-40" src="{{ $badge->icon }}" />
-            </div>
-        </span>
-        <div class="ms-3">
-            <div class="h5">{{ $badge->title }}</div>
-            <div class="text-secondary small mb-2">Created by {{ '@' . $badge->user->username }}</div>
-            <div class="text-secondary">
-                <span class="fw-bold">{{ $badge->subscribersCount() }}</span> people have this badge
+    <div class="card-body d-flex align-items-center justify-content-between">
+        <div class="d-flex align-items-center">
+            <span class="card d-inline-block" style="background: {{ $badge->color }}">
+                <div class="p-4">
+                    <img class="avatar-40" src="{{ $badge->icon }}" />
+                </div>
+            </span>
+            <div class="ms-3">
+                <div class="h5">{{ $badge->title }}</div>
+                <div class="text-secondary small mb-2">Created by {{ '@' . $badge->user->username }}</div>
+                <div class="text-secondary">
+                    <span class="fw-bold">{{ $badge->subscribersCount() }}</span> people have this badge
+                </div>
             </div>
         </div>
+        @auth
+            @if ($badge->isSubscribedBy(auth()->user()))
+                <button wire:click="toggleAddBadge" wire:loading.attr="disabled"
+                    class="btn btn-sm btn-outline-danger rounded-pill">
+                    <x-heroicon-o-minus class="heroicon heroicon-15px" />
+                    Remove Badge
+                </button>
+            @else
+                <button wire:click="toggleAddBadge" wire:loading.attr="disabled"
+                    class="btn btn-sm btn-outline-primary rounded-pill">
+                    <x-heroicon-o-plus class="heroicon heroicon-15px" />
+                    Add Badge
+                </button>
+            @endif
+        @else
+            <a href="/login" class="btn btn-sm btn-outline-primary rounded-pill">
+                <x-heroicon-o-plus class="heroicon heroicon-15px" />
+                Add Badge
+            </a>
+        @endauth
     </div>
 </div>
