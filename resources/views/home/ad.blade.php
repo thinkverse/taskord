f630b4976cbebc4e024fcfed00a256b895a58088
Yogi <yoginth@hey.com>
2021-06-30 17:54:06 +0530

    add: Image

--- resources/views/home/ad.blade.php
+++ resources/views/home/ad.blade.php
@@ -10,6 +10,8 @@
             <p class="mb-0">
                 <a href="{{ route('badges.badges') }}">Badges</a> are now available public beta 🎉
             </p>
+            <img class="border container mt-3 p-0 rounded shadow w-75"
+                src="https://ik.imagekit.io/taskordimg/ad/Screenshot_2021-06-30_at_5.49.21_PM.png">
         </div>
     </div>
 @endif
