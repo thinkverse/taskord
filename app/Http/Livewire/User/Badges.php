586e32b49863252dd193074e3f21733068381695
Yogi <yoginth@hey.com>
2021-06-30 16:31:30 +0530

    add: Badges in sidebar

---
+++ app/Http/Livewire/User/Badges.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\User;
+
+use Livewire\Component;
+
+class Badges extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.badges');
+    }
+}
