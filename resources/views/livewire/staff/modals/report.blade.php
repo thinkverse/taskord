ca0ac361bc339f12aeaa6c75c6f50be22067d2a2
Yogi <yoginth@hey.com>
2021-06-30 09:44:16 +0530

    rename: Report -> Create Issue

--- resources/views/livewire/staff/modals/report.blade.php
+++ resources/views/livewire/staff/modals/report.blade.php
@@ -3,7 +3,7 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title text-dark" id="reportModalLabel">Report an Issue</h5>
+                <h5 class="modal-title text-dark" id="reportModalLabel">Creata a new Issue</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
