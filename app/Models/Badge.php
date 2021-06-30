a94b50a66ddd19357e761148dae0614b71054c20
Yogi <yoginth@hey.com>
2021-06-30 10:03:21 +0530

    add: QueryCacheable

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Rennokki\QueryCache\Traits\QueryCacheable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Badge extends Model
 {
     use HasFactory;
+    use QueryCacheable;
 }
