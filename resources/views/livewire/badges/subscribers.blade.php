f32b828f2f3ab916d6138154242be268710550de
Yogi <yoginth@hey.com>
2021-06-30 13:51:23 +0530

    add: Empty placeholder

--- resources/views/livewire/badges/subscribers.blade.php
+++ resources/views/livewire/badges/subscribers.blade.php
@@ -2,6 +2,14 @@
     @if (!$readyToLoad)
         <x:loaders.featured-makers-skeleton count="3" />
     @else
+        @if (count($subscribers) === 0)
+            <div class="card-body text-center mt-3 mb-3">
+                <x-heroicon-o-tag class="heroicon heroicon-60px text-primary mb-2" />
+                <div class="h4">
+                    No people added this badge
+                </div>
+            </div>
+        @endif
         <ul class="list-group list-group-flush">
             @foreach ($subscribers as $user)
                 <li class="list-group-item d-flex align-items-center justify-content-between py-3">
