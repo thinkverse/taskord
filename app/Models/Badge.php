42e704c2f9c80e087083c63450369797b7eb6324
Yogi <yoginth@hey.com>
2021-06-30 10:03:35 +0530

    add: SearchableTrait

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Rennokki\QueryCache\Traits\QueryCacheable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Nicolaslopezj\Searchable\SearchableTrait;
 
 class Badge extends Model
 {
     use HasFactory;
     use QueryCacheable;
+    use SearchableTrait;
 }
