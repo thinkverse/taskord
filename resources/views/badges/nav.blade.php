dc4e52bda32c955ae10b8bd780b82f64a3ef1466
Yogi <yoginth@hey.com>
2021-06-30 10:30:55 +0530

    add: Views for badges

--- resources/views/badges/nav.blade.php
+++ resources/views/badges/nav.blade.php
@@ -1,17 +1,12 @@
 <div class="mb-3">
-    <a class="mb-2 btn btn-{{ Route::is('products.newest') ? '' : 'outline-' }}primary rounded-pill me-2"
-        href="{{ route('products.newest') }}">
-        Newest
-    </a>
-    <a class="mb-2 btn btn-{{ Route::is('products.launched') ? '' : 'outline-' }}primary rounded-pill me-2"
-        href="{{ route('products.launched') }}">
-        Launched
-    </a>
+    <h5>
+        Badges
+    </h5>
     @auth
         <a type="button" class="mb-2 btn btn-outline-success rounded-pill float-md-end"
             href="{{ route('products.new') }}">
             <x-heroicon-o-plus class="heroicon" />
-            Add your Product
+            Add new badge
         </a>
     @endauth
 </div>
