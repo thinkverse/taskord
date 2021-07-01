98d43873af993ed6aa1220742227168e93e70893
Yogi <yoginth@hey.com>
2021-07-01 14:00:44 +0530

    refactor: Settings params

--- resources/views/user/settings/integrations.blade.php
+++ resources/views/user/settings/integrations.blade.php
@@ -6,9 +6,7 @@
     <div class="container-md">
         <div class="row justify-content-center mt-4">
             @include('user.settings.sidebar')
-            @livewire('user.settings.integrations', [
-            'user' => $user
-            ])
+            <livewire:user.settings.integrations />
         </div>
     </div>
 @endsection
