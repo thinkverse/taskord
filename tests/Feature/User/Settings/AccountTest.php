d4c726c69a7955d6cb88138ab1bd90c24fb500d3
Yogi <yoginth@hey.com>
2021-07-01 14:32:12 +0530

    update: Test

--- tests/Feature/User/Settings/AccountTest.php
+++ tests/Feature/User/Settings/AccountTest.php
@@ -16,63 +16,31 @@ it('has settings/account page', function ($url, $expected, $auth) {
     ['/settings/account', 200, true],
 ]);
 
-it('can edit account (enrollBeta) settings', function ($status) {
+it('can edit account (enrollBeta) settings', function () {
     $newUser = User::factory()->create();
 
-    if ($status) {
-        return actingAs($newUser->id)
-            ->livewire(Account::class, ['user' => $newUser])
-            ->call('enrollBeta')
-            ->assertEmitted('enrolledBeta');
-    }
-
-    return actingAs(1)
+    return actingAs($newUser->id)
         ->livewire(Account::class, ['user' => $newUser])
         ->call('enrollBeta')
-        ->assertNotEmitted('enrolledBeta');
-})->with([
-    [true],
-    [false],
-]);
+        ->assertEmitted('enrolledBeta');
+});
 
-it('can edit account (enrollPrivate) settings', function ($status) {
+it('can edit account (enrollPrivate) settings', function () {
     $newUser = User::factory()->create();
 
-    if ($status) {
         return actingAs($newUser->id)
             ->livewire(Account::class, ['user' => $newUser])
             ->call('enrollPrivate')
             ->assertEmitted('enrolledPrivate');
-    }
+});
 
-    return actingAs(1)
-        ->livewire(Account::class, ['user' => $newUser])
-        ->call('enrollPrivate')
-        ->assertNotEmitted('enrolledPrivate');
-})->with([
-    [true],
-    [false],
-]);
-
-it('can edit account (updateAccount) settings', function ($status) {
+it('can edit account (updateAccount) settings', function () {
     $newUser = User::factory()->create();
 
-    if ($status) {
         return actingAs($newUser->id)
             ->livewire(Account::class, ['user' => $newUser])
             ->set('email', Str::random(8).'@gmail.com')
             ->set('username', Str::random(8))
             ->call('updateAccount')
             ->assertEmitted('accountUpdated');
-    }
-
-    return actingAs(1)
-        ->livewire(Account::class, ['user' => $newUser])
-        ->set('email', Str::random(8).'@gmail.com')
-        ->set('username', Str::random(8))
-        ->call('updateAccount')
-        ->assertNotEmitted('accountUpdated');
-})->with([
-    [true],
-    [false],
-]);
+});
