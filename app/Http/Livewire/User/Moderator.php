3968cc44358b3f19a42b6e7f17e58586559aa467
Yogi <yoginth@hey.com>
2021-07-02 09:53:47 +0530

    move: To collection query in mod 🛡

--- app/Http/Livewire/User/Moderator.php
+++ app/Http/Livewire/User/Moderator.php
@@ -288,9 +288,8 @@ class Moderator extends Component
             return toast($this, 'error', config('taskord.toast.deny'));
         }
 
-        $user = User::find($this->user->id);
-        $user->timestamps = false;
-        $user->answers()->delete();
+        $this->user->answers()->delete();
+        $this->user->timestamps = false;
         toast($this, 'success', config('taskord.toast.settings-updated'));
 
         return redirect()->route('user.done', ['username' => $this->user->username]);
